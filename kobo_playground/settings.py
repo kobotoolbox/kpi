cf0363a2b7fee9116a6432769d183b04b168e4ef
Alex Dorey <dorey415@gmail.com>
2015-02-13 01:25:38 -0500

    using dj_database_url

--- kobo_playground/settings.py
+++ kobo_playground/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -59,10 +61,7 @@ WSGI_APPLICATION = 'kobo_playground.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default="sqlite:///%s/db.sqlite3" % BASE_DIR),
 }
 
 # Internationalization
