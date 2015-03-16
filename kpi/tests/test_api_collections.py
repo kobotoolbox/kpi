841eb2f06559df63f6e9f5f7fdb59544c89c50c2
Esmail <esmail.fadae@kobotoolbox.org>
2015-03-16 16:19:44 -0400

    Additional collections tests.

--- kpi/tests/test_api_collections.py
+++ kpi/tests/test_api_collections.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework.test import APITestCase
 from django.core.urlresolvers import reverse
 from rest_framework import status
@@ -17,3 +19,15 @@ class CollectionsTests(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['name'], 'my collection')
+
+    def test_collection_detail(self):
+        url= reverse('collection-detail', kwargs={'uid': 'cifPmxrj2AAjWAWBJzTYPU'})
+        response= self.client.get(url, format='json')
+        self.assertEqual(response.data['name'], 'fixture collection')
+
+    def test_collection_delete(self):
+        url= reverse('collection-detail', kwargs={'uid': 'cifPmxrj2AAjWAWBJzTYPU'})
+        response= self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response= self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
