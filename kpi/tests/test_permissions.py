78597b2e7954b6a13e277c7fd8b3fd4e0c2fd3ac
Esmail <esmail.fadae@kobotoolbox.org>
2015-03-20 11:11:49 -0400

    Working permissions tests.

--- kpi/tests/test_permissions.py
+++ kpi/tests/test_permissions.py
@@ -6,6 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
 from ..models.collection import Collection
+from ..models.object_permission import get_all_objects_for_user
 from ..models.survey_asset import SurveyAsset
 
 class PermissionsTestCase(TestCase):
@@ -15,69 +16,94 @@ class PermissionsTestCase(TestCase):
         self.admin= User.objects.get(username='admin')
         self.someuser= User.objects.get(username='someuser')
         self.admin_collection= Collection.objects.create(owner=self.admin)
-        self.admin_asset= SurveyAsset.objects.get(name='fixture admin asset')
+        self.admin_asset= SurveyAsset.objects.create(content=[
+            {'type': 'text', 'label': 'Question 1', 'name': 'q1', 'kuid': 'abc'},
+            {'type': 'text', 'label': 'Question 2', 'name': 'q2', 'kuid': 'def'},
+        ], owner=self.admin)
+
+    def _get_perm_name_from_prefix(self, perm_name_prefix, obj):
+        perm_name= Permission.objects.get(
+            content_type= ContentType.objects.get_for_model(obj),
+            codename__startswith=perm_name_prefix
+        ).natural_key()[0]
+        return perm_name
 
     def _test_add_permission(self, assign_perm_object, perm_name_prefix, user, has_perm_object=None):
-        return # FIXME
+        '''
+        Test that a permission can be added and that the permission successfully
+        takes effect.
+
+        :param assign_perm_object: Object to assign the permission to.
+        :param str perm_name_prefix: The prefix of the permission to be added
+            (e.g. "view_" or "change_")
+        :param user: The user who will be receiving the permission.
+        :param has_perm_object: Optional parameter. The object to check for the
+            new permission (e.g. an asset contained in the altered collection).
+            Defaults to `assign_perm_object` if not provided.
+        '''
+
         if not has_perm_object:
             has_perm_object= assign_perm_object
-        assign_perm_name= Permission.objects.get(
-            content_type= ContentType.objects.get_for_model(assign_perm_object),
-            codename__startswith=perm_name_prefix
-        )
-        has_perm_name= Permission.objects.get(
-            content_type= ContentType.objects.get_for_model(has_perm_object),
-            codename__startswith=perm_name_prefix
-        )
-        self.assertFalse(has_perm_object.has_perm(assign_perm_name, user)) # TODO: 'obj.has_perm' will likely change.
-        assign_perm_object.assign_perm(perm_name_prefix, user)
-        self.assertTrue(has_perm_object.has_perm(has_perm_name, user)) # TODO: 'obj.has_perm' will likely change.
+        assign_perm_name= self._get_perm_name_from_prefix(perm_name_prefix, assign_perm_object)
+        has_perm_name= self._get_perm_name_from_prefix(perm_name_prefix, has_perm_object)
+        self.assertFalse(user.has_perm(assign_perm_name, has_perm_object))
+        assign_perm_object.assign_perm(user, assign_perm_name)
+        self.assertTrue(user.has_perm(has_perm_name, has_perm_object))
 
-    def _test_remove_permission(self, remove_perm_object, perm, user, has_perm_object=None):
-        return # FIXME
+    def _test_remove_permission(self, remove_perm_object, perm_name_prefix, user, has_perm_object=None):
+        '''
+        Test that a permission can be added and removed, and that the removal
+        successfully takes effect.
+
+        :param assign_perm_object: Object to remove the permission from.
+        :param str perm_name_prefix: The prefix of the permission to be remove
+            (e.g. "view_" or "change_")
+        :param user: The user whose permission will be revoked.
+        :param has_perm_object: Optional parameter. The object to check for the
+            removed permission (e.g. an asset contained in the altered
+            collection). Defaults to `assign_perm_object` if not provided.
+        '''
+
         if not has_perm_object:
             has_perm_object= remove_perm_object
-        self._test_add_permission(remove_perm_object, perm, user, has_perm_object)
-        remove_perm_object.remove_perm(perm, user)
-        self.assertFalse(has_perm_object.has_perm(perm, user))
+        remove_perm_name= self._get_perm_name_from_prefix(perm_name_prefix, remove_perm_object)
+        has_perm_name= self._get_perm_name_from_prefix(perm_name_prefix, has_perm_object)
+        self._test_add_permission(remove_perm_object, perm_name_prefix, user, has_perm_object)
+        remove_perm_object.remove_perm(user, remove_perm_name)
+        self.assertFalse(user.has_perm(has_perm_name, has_perm_object))
 
     def test_add_asset_permission(self):
-        return # FIXME
         self._test_add_permission(self.admin_asset, 'view_', self.someuser)
         self._test_add_permission(self.admin_asset, 'change_', self.someuser)
 
     def test_remove_asset_permission(self):
-        return # FIXME
         self._test_remove_permission(self.admin_asset, 'view_', self.someuser)
         self._test_remove_permission(self.admin_asset, 'change_', self.someuser)
 
     def test_add_collection_permission(self):
-        return # FIXME
         self._test_add_permission(self.admin_collection, 'view_', self.someuser)
         self._test_add_permission(self.admin_collection, 'change_', self.someuser)
 
     def test_remove_collection_permission(self):
-        return # FIXME
         self._test_remove_permission(self.admin_collection, 'view_', self.someuser)
         self._test_remove_permission(self.admin_collection, 'change_', self.someuser)
 
     def test_add_asset_inherited_permission(self):
-        return # FIXME
-        self.admin_collection.add(self.admin_asset)
+        self.admin_collection.survey_assets.add(self.admin_asset)
         self._test_add_permission(self.admin_collection, 'view_', self.someuser, has_perm_object=self.admin_asset)
         self._test_add_permission(self.admin_collection, 'change_', self.someuser, has_perm_object=self.admin_asset)
 
     def test_remove_collection_inherited_permission(self):
-        return
-        self.admin_collection.add(self.admin_asset)
+        self.admin_collection.survey_assets.add(self.admin_asset)
         self._test_remove_permission(self.admin_collection, 'view_', self.someuser, has_perm_object=self.admin_asset)
         self._test_remove_permission(self.admin_collection, 'change_', self.someuser, has_perm_object=self.admin_asset)
 
     def test_get_objects_for_user(self):
-        return # FIXME
-        admin_objects= get_objects_for_user('view_', self.admin) # FIXME
-        someuser_objects= get_objects_for_user('view_', self.someuser) # FIXME
-        self.assertIn(self.admin_asset, admin_objects)
-        self.assertIn(self.admin_collection, admin_objects)
-        self.assertNotIn(self.admin_asset, someuser_objects)
-        self.assertNotIn(self.admin_collection, someuser_objects)
+        admin_assets= get_all_objects_for_user(self.admin, SurveyAsset)
+        admin_collections= get_all_objects_for_user(self.admin, Collection)
+        someuser_assets= get_all_objects_for_user(self.someuser, SurveyAsset)
+        someuser_collections= get_all_objects_for_user(self.someuser, Collection)
+        self.assertIn(self.admin_asset, admin_assets)
+        self.assertIn(self.admin_collection, admin_collections)
+        self.assertNotIn(self.admin_asset, someuser_assets)
+        self.assertNotIn(self.admin_collection, someuser_collections)
