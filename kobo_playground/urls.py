4562e267f2a4731f62aedb7599321f0190bded6e
Alex Dorey <dorey415@gmail.com>
2015-05-15 16:22:54 -0400

    querying '/me/' returns info about current user

--- kobo_playground/urls.py
+++ kobo_playground/urls.py
@@ -9,6 +9,7 @@ from kpi.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework import renderers
 from django.conf.urls import url, include
+from kpi.views import current_user
 
 router = DefaultRouter()
 router.register(r'survey_assets', SurveyAssetViewSet)
@@ -17,70 +18,10 @@ router.register(r'users', UserViewSet)
 router.register(r'tags', TagViewSet)
 router.register(r'permissions', ObjectPermissionViewSet)
 
+
 urlpatterns = [
+    url(r'^me/$', current_user),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 ]
-
-# from django.http import HttpResponse
-
-# survey_asset_list = SurveyAssetViewSet.as_view({
-#     'get': 'list',
-#     'post': 'create'
-# })
-# survey_asset_detail = SurveyAssetViewSet.as_view({
-#     'get': 'retrieve',
-#     'put': 'update',
-#     'patch': 'partial_update',
-#     'delete': 'destroy'
-# })
-
-# survey_asset_table_view = SurveyAssetViewSet.as_view({
-#     'get': 'table_view'
-# }, renderer_classes=[renderers.StaticHTMLRenderer])
-
-# user_list = UserViewSet.as_view({
-#     'get': 'list',
-# })
-# user_detail = UserViewSet.as_view({
-#     'get': 'retrieve',
-# })
-
-# collection_list = CollectionViewSet.as_view({
-#     'get': 'list',
-#     'post': 'create',
-# })
-# collection_detail = CollectionViewSet.as_view({
-#     'get': 'retrieve',
-#     'put': 'update',
-#     'patch': 'partial_update',
-#     'delete': 'destroy',
-# })
-
-
-
-#  url(r'^xyz/(?P<uid>[A-Za-z0-9]{8})@(?P<vid>\d+)/$', abc, name='abc-version'),
-
-# urlpatterns += format_suffix_patterns([
-#     # url(r'^survey_assets/$', survey_asset_list, name='surveyasset-list'),
-#     # url(r'^survey_assets/(?P<vuid>[A-Za-z0-9]{8})@(?P<vnum>\d+)/$', survey_asset_detail_uid, name='surveyassetuid-detail'),
-#     # url(r'^survey_assets/(?P<pk>[A-Za-z0-9]+)/$', survey_asset_detail, name='surveyasset-detail'),
-# ], allowed=[
-#     'json',
-#     'ssjson',
-#     'mdtable',
-#     'xml',
-#     # 'xls',
-#     # 'enketopreviewlink',
-# ])
-
-# urlpatterns += [url(r'^survey_assets/(?P<pk>[0-9]+)/table_view/$', survey_asset_table_view, name='surveyasset-tableview'),]
-
-# urlpatterns += format_suffix_patterns([
-#     url(r'^collections/$', collection_list, name='collection-list'),
-#     url(r'^collections/(?P<pk>[0-9]+)/$', collection_detail, name='collection-detail'),
-
-#     url(r'^users/$', user_list, name='user-list'),
-#     url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
-# ])
