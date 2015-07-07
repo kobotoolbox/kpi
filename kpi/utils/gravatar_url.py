d039062469102f53dd08df8ca003788917712735
Alex Dorey <dorey415@gmail.com>
2015-07-07 17:39:36 -0400

    https gravatars

--- kpi/utils/gravatar_url.py
+++ kpi/utils/gravatar_url.py
@@ -1,8 +1,9 @@
 import hashlib
 import urllib
 
-def gravatar_url(email):
-    return "http://www.gravatar.com/avatar/%s?%s" % (
+def gravatar_url(email, https=True):
+    return "%s://www.gravatar.com/avatar/%s?%s" % (
+        'https' if https else 'http',
         hashlib.md5(email.lower()).hexdigest(),
         urllib.urlencode({'s': '40'}),
         )
