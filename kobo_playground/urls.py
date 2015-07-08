9fd6fa54775fd4ca14dc944470c42bb0850ceabe
Esmail <esmail.fadae@kobotoolbox.org>
2015-07-08 16:21:46 -0400

    Reorganize URL configuration.
    Conflicts:
    	kobo_playground/urls.py

--- kobo_playground/urls.py
+++ kobo_playground/urls.py
@@ -1,32 +1,6 @@
-from kpi.views import (
-    AssetViewSet,
-    AssetSnapshotViewSet,
-    UserViewSet,
-    CollectionViewSet,
-    TagViewSet,
-    ImportTaskViewSet,
-    ObjectPermissionViewSet,
-    AssetDeploymentViewSet,
+from django.conf.urls import (
+    url,
+    include
 )
-from rest_framework.routers import DefaultRouter
-from django.conf.urls import url, include
-from kpi.views import home, current_user
 
-router = DefaultRouter()
-router.register(r'assets', AssetViewSet)
-router.register(r'asset_snapshots', AssetSnapshotViewSet)
-router.register(r'collections', CollectionViewSet)
-router.register(r'users', UserViewSet)
-router.register(r'tags', TagViewSet)
-router.register(r'permissions', ObjectPermissionViewSet)
-router.register(r'imports', ImportTaskViewSet)
-router.register(r'deployments', AssetDeploymentViewSet)
-
-
-urlpatterns = [
-    url(r'^$', home),
-    url(r'^me/$', current_user, name='current-user'),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework')),
-]
+urlpatterns= [url(r'^', include('kpi.urls'))]
