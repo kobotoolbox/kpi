09c9e08e7cb56661c4d41ea533a61dc47cadcb2d
John N. Milner <john@tmoj.net>
2015-07-27 21:07:08 -0400

    Restrict usernames to `^[a-z][a-z0-9_]+$`

--- kpi/urls.py
+++ kpi/urls.py
@@ -1,4 +1,9 @@
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from rest_framework import renderers
+from registration.backends.default.views import RegistrationView
+
 from kpi.views import (
     AssetViewSet,
     AssetSnapshotViewSet,
@@ -9,10 +14,8 @@ from kpi.views import (
     ObjectPermissionViewSet,
     AssetDeploymentViewSet,
 )
-from rest_framework.routers import DefaultRouter
-from rest_framework import renderers
-from django.conf.urls import url, include
 from kpi.views import current_user, home
+from kpi.forms import RegistrationForm
 
 router = DefaultRouter()
 router.register(r'assets', AssetViewSet)
@@ -31,6 +34,8 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url(r'^accounts/register/$', RegistrationView.as_view(
+        form_class=RegistrationForm), name='registration_register'),
     url(r'^accounts/logout/', 'django.contrib.auth.views.logout',
         {'next_page': '/'}),
     url(r'^accounts/', include('registration.backends.default.urls')),
