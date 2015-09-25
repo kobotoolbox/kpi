bfe148334299408b26630fbf1db198b139ded1c8
John N. Milner <john@tmoj.net>
2015-09-24 23:21:45 -0400

    Compatibility with dkobo's South

--- hub/migrations/0002_formbuilderpreference.py
+++ hub/migrations/0002_formbuilderpreference.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import models, migrations, ProgrammingError
 from django.conf import settings
+
+
+def south_forwards(apps, schema_editor):
+    statement = "INSERT INTO south_migrationhistory (app_name, migration, " \
+                "applied) VALUES ('hub', '0002_formbuilderpreference', NOW());"
+    try:
+        schema_editor.execute(statement, params=None)
+    except ProgrammingError:
+        # South may have never touched this database
+        pass
+
+def south_backwards(apps, schema_editor):
+    statement = "DELETE FROM south_migrationhistory WHERE app_name='hub' " \
+                "AND migration='0002_formbuilderpreference';"
+    try:
+        schema_editor.execute(statement, params=None)
+    except ProgrammingError:
+        pass
 
 
 class Migration(migrations.Migration):
@@ -24,4 +42,5 @@ class Migration(migrations.Migration):
             },
             bases=(models.Model,),
         ),
+        migrations.RunPython(south_forwards, south_backwards),
     ]
