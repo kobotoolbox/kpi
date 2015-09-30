95faaa37c7ae03d469c39816c2c6e95515de82a5
John N. Milner <john@tmoj.net>
2015-09-30 12:22:38 -0400

    Use introspection to determine if South table exists

--- hub/migrations/0002_formbuilderpreference.py
+++ hub/migrations/0002_formbuilderpreference.py
@@ -1,28 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations, ProgrammingError, OperationalError
+from django.db import models, migrations, connection
 from django.conf import settings
 
 
 def south_forwards(apps, schema_editor):
     statement = "INSERT INTO south_migrationhistory (app_name, migration, " \
                 "applied) VALUES ('hub', '0002_formbuilderpreference', NOW());"
-    try:
+    # South may have never touched this database
+    if u'south_migrationhistory' in connection.introspection.table_names():
         schema_editor.execute(statement, params=None)
-    # Different backends raise different exceptions when a table is missing
-    except (ProgrammingError, OperationalError):
-        # South may have never touched this database
-        pass
 
 def south_backwards(apps, schema_editor):
     statement = "DELETE FROM south_migrationhistory WHERE app_name='hub' " \
                 "AND migration='0002_formbuilderpreference';"
-    try:
+    if u'south_migrationhistory' in connection.introspection.table_names():
         schema_editor.execute(statement, params=None)
-    except (ProgrammingError, OperationalError):
-        pass
-
 
 class Migration(migrations.Migration):
 
