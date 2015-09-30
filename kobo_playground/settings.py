0662bfea270a8fa1c83a1de99a2acfb28d4d3536
John N. Milner <john@tmoj.net>
2015-09-30 14:45:22 -0400

    Disable backwards-incompatible SessionAuthenticationMiddleware

--- kobo_playground/settings.py
+++ kobo_playground/settings.py
@@ -72,7 +72,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    # TODO: Uncomment this when interoperability with dkobo is no longer
+    # needed. See https://code.djangoproject.com/ticket/21649
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'hub.middleware.OtherFormBuilderRedirectMiddleware',
