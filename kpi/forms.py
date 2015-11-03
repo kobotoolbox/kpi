969bb3d3ace9e1107122c5bfa15ecfd8e6d03311
John N. Milner <john@tmoj.net>
2015-11-03 10:40:41 -0500

    Proper validation when external apps create users

--- kpi/forms.py
+++ kpi/forms.py
@@ -2,13 +2,17 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 from registration import forms as registration_forms
 
+USERNAME_REGEX = r'^[a-z][a-z0-9_]+$'
+USERNAME_MAX_LENGTH = 30
+USERNAME_INVALID_MESSAGE = _(
+    'A username may only contain lowercase letters, numbers, and '
+    'underscores (_).'
+)
+
 class RegistrationForm(registration_forms.RegistrationForm):
     username = forms.RegexField(
-        regex=r'^[a-z][a-z0-9_]+$',
-        max_length=30,
+        regex=USERNAME_REGEX,
+        max_length=USERNAME_MAX_LENGTH,
         label=_("Username"),
-        error_messages={'invalid': _(
-            'A username may only contain lowercase letters, numbers, and '
-            'underscores (_).'
-        )}
+        error_messages={'invalid': USERNAME_INVALID_MESSAGE}
     )
