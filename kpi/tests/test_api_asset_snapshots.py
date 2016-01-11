306cc16adbb366d47b17dcd6534a7d048c54347e
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-01-11 16:24:45 -0500

    Additional ORM-only test.

--- kpi/tests/test_api_asset_snapshots.py
+++ kpi/tests/test_api_asset_snapshots.py
@@ -8,23 +8,23 @@ from .kpi_test_case import KpiTestCase
 class TestAssetSnapshotList(KpiTestCase):
     fixtures = ['test_data']
 
+    form_source= '''
+                    {
+                        "survey": [
+                            {"type":"text","label":"Text+Question.","required":"true"},
+                            {"name":"start","type":"start"},
+                            {"name":"end","type":"end"}],
+                        "settings": [
+                            {"form_title":"New+form",
+                            "form_id":"new_form"}]
+                    }
+                 '''
+
     def test_create_asset_snapshot(self):
         self.client.login(username='someuser', password='someuser')
         url = reverse('assetsnapshot-list')
 
-        source= '''
-                {
-                    "survey": [
-                        {"type":"text","label":"Text+Question.","required":"true"},
-                        {"name":"start","type":"start"},
-                        {"name":"end","type":"end"}],
-                    "settings": [
-                        {"form_title":"New+form",
-                        "form_id":"new_form"}]
-                }
-                '''
-        data = {'source': source}
+        data = {'source': self.form_source}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED,
                          msg=response.data)
-        
