b11e3e2a1ec6bd1bca579c12936dc88e3d0e8e35
John N. Milner <john@tmoj.net>
2016-02-02 17:58:56 -0500

    Fabric uses `pip-sync`

--- fabfile.py
+++ fabfile.py
@@ -71,7 +71,8 @@ def deploy_ref(deployment_name, ref):
         run('find . -type d -empty -delete')
 
     with kobo_workon(env.kpi_virtualenv_name):
-        run("pip install -r %s" % env.pip_requirements_file)
+        run("pip install --upgrade 'pip>=7.0' pip-tools")
+        run("pip-sync '%s'" % env.pip_requirements_file)
 
     with cd(env.kpi_path):
         with kobo_workon(env.kpi_virtualenv_name):
