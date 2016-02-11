8549c98f2093cb506bfa740f2210445c657aa0f2
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-02-11 17:19:06 -0500

    Wait for Postgres.

--- docker/setup_kpi.bash
+++ docker/setup_kpi.bash
@@ -2,6 +2,24 @@
 set -e
 
 cd "${KPI_SRC_DIR}"
+
+echo 'Waiting for Postgres.'
+KOBO_PSQL_DB_NAME=${KOBO_PSQL_DB_NAME:-"kobotoolbox"}
+KOBO_PSQL_DB_USER=${KOBO_PSQL_DB_USER:-"kobo"}
+KOBO_PSQL_DB_PASS=${KOBO_PSQL_DB_PASS:-"kobo"}
+dockerize -timeout=20s -wait ${PSQL_PORT}
+until $(PGPASSWORD="${KOBO_PSQL_DB_PASS}" psql -d ${KOBO_PSQL_DB_NAME} -h psql -U ${KOBO_PSQL_DB_USER} -c '' 2> /dev/null); do
+    sleep 1
+done
+echo 'Postgres ready.'
+
+echo 'Synchronizing database.'
+python manage.py syncdb --noinput
+
+echo 'Running migrations.'
 python manage.py migrate --noinput
+
 echo "Copying static files to nginx volume..."
 rsync -aq --chown=www-data "${KPI_SRC_DIR}/staticfiles/" "${NGINX_STATIC_DIR}/"
+
+echo '\`kpi\` initialization completed.'
