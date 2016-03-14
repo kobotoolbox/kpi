444a561f626872e70a2a41fc8fd2e26bd9981533
John N. Milner <john@tmoj.net>
2016-03-13 20:06:49 -0400

    Make `dryrun` work; context manager to disable `auto_now` and friends

--- kpi/management/commands/remove_deprecated_settings.py
+++ kpi/management/commands/remove_deprecated_settings.py
@@ -1,56 +1,70 @@
 import copy
 import json
+from contextlib import contextmanager
+from optparse import make_option
 from django.core.management.base import BaseCommand
 
 from kpi.models import Asset
 
 
-def _set_auto_field_update(kls, field_name, val):
-    field = filter(lambda f: f.name == field_name, kls._meta.fields)[0]
-    field.auto_now = val
-    field.auto_now_add = val
+@contextmanager
+def _disable_auto_field_update(kls, field_names):
+    AUTO_ATTRS = ('auto_now', 'auto_now_add')
+    previous_values = {}
+    for field_name in field_names:
+        field = filter(lambda f: f.name == field_name, kls._meta.fields)[0]
+        for attr in AUTO_ATTRS:
+            previous_values[field_name] = previous_values.get(field_name, {})
+            previous_values[field_name][attr] = getattr(field, attr)
+            # Turn it off!
+            setattr(field, attr, False)
+    yield
+    # Restore the previous values
+    for field_name in field_names:
+        for attr in AUTO_ATTRS:
+            setattr(field, attr, previous_values[field_name][attr])
 
 
 class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--dryrun',
+            action='store_true',
+            dest='dryrun',
+            default=False,
+            help='Print what would be done, but do not make any changes'),
+        )
+
     def handle(self, *args, **options):
-        if len(args) > 0 and args[0] == 'dryrun':
-            commit_changes = False
-        else:
-            commit_changes = True
-
+        commit_changes = not options.get('dryrun')
         # prevent timestamps from updating
-        _set_auto_field_update(Asset, "date_created", True)
-        _set_auto_field_update(Asset, "date_modified", True)
-
-        for asset in Asset.objects.all():
-            # settingslist
-            if len(asset.content.get('settings', [])) > 0:
-                changed = False
-                settings = asset.content['settings'][0]
-                old_settings = copy.copy(settings)
-                if 'form_id' in settings:
-                    if settings['form_id'] != 'new_form':
-                        settings['id_string'] = settings['form_id']
-                    del settings['form_id']
-                    changed = True
-                if 'form_title' in settings and \
-                        settings['form_title'] == 'New form':
-                    del settings['form_title']
-                    changed = True
-                if changed:
-                    if commit_changes:
-                        try:
-                            asset.content['settings'] = [settings]
-                            asset.save()
-                        except Exception as err:
-                            print('Error running migration:')
-                            print(err.message)
-                            import pdb
-                            pdb.set_trace()
-                    else:
-                        print('settings changed on asset %s: ' % asset.uid)
-                        print('  ' + json.dumps(old_settings))
-                        print('  ' + json.dumps(settings))
-
-        _set_auto_field_update(Asset, "date_created", False)
-        _set_auto_field_update(Asset, "date_modified", False)
+        with _disable_auto_field_update(
+                Asset, ('date_created', 'date_modified')):
+            for asset in Asset.objects.all():
+                # settingslist
+                if len(asset.content.get('settings', [])) > 0:
+                    changed = False
+                    settings = asset.content['settings'][0]
+                    old_settings = copy.copy(settings)
+                    if 'form_id' in settings:
+                        if settings['form_id'] != 'new_form':
+                            settings['id_string'] = settings['form_id']
+                        del settings['form_id']
+                        changed = True
+                    if 'form_title' in settings and \
+                            settings['form_title'] == 'New form':
+                        del settings['form_title']
+                        changed = True
+                    if changed:
+                        if commit_changes:
+                            try:
+                                asset.content['settings'] = [settings]
+                                asset.save()
+                            except Exception as err:
+                                print('Error running migration:')
+                                print(err.message)
+                                import pdb
+                                pdb.set_trace()
+                        else:
+                            print('settings changed on asset %s: ' % asset.uid)
+                            print('  ' + json.dumps(old_settings))
+                            print('  ' + json.dumps(settings))
