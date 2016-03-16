acd67412eb12fe2e3df35331a93e32ecbec68e0a
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-03-16 18:46:51 -0400

    Transifex script fixes.

--- scripts/download_translations.bash
+++ scripts/download_translations.bash
@@ -5,10 +5,10 @@ set -e
 export KOBOFORM_SRC_DIR=${KOBOFORM_SRC_DIR:-"$(cd $(dirname $0)/.. && pwd)/"}
 
 echo 'Downloading translations from Transifex.'
-bash -c 'cd ${KOBOFORM_SRC_DIR} && tx pull --all'
+(cd ${KOBOFORM_SRC_DIR} && tx pull --all)
 # FIXME: Don't pull "pseudo-translations" once we have real translations.
-bash -c 'cd ${KOBOFORM_SRC_DIR} && tx pull --all --pseudo'
+(cd ${KOBOFORM_SRC_DIR} && tx pull --all --pseudo)
 
 echo 'Compiling translations.'
-bash -c 'cd ${KOBOFORM_SRC_DIR} && python manage.py compilemessages'
+(cd ${KOBOFORM_SRC_DIR} && python manage.py compilemessages)
 echo 'Compiliation complete!'
--- scripts/upload_translatable_strings.bash
+++ scripts/upload_translatable_strings.bash
@@ -5,11 +5,11 @@ set -e
 export KOBOFORM_SRC_DIR=${KOBOFORM_SRC_DIR:-"$(cd $(dirname $0)/.. && pwd)/"}
 
 echo 'Extracting translatable strings from Django code.'
-bash -c 'cd ${KOBOFORM_SRC_DIR} && python manage.py makemessages --locale en'
+(cd ${KOBOFORM_SRC_DIR} && python manage.py makemessages --locale en)
 
 echo 'Extracting translatable strings from client code.'
-bash -c 'cd ${KOBOFORM_SRC_DIR} && python manage.py makemessages --locale en --domain djangojs --extension es6'
+(cd ${KOBOFORM_SRC_DIR} && python manage.py makemessages --locale en --domain djangojs --extension es6)
 
 echo 'Uploading translatable strings to Transifex.'
-bash -c 'cd ${KOBOFORM_SRC_DIR} && tx push -s'
+(cd ${KOBOFORM_SRC_DIR} && tx push -s)
 echo 'Upload complete!'
