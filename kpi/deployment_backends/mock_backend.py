12b6814e558402032e0c12170c678657f1455d08
John N. Milner <john@tmoj.net>
2016-05-19 11:09:43 -0400

    Add `get_enketo_survey_links` to mock backend

--- kpi/deployment_backends/mock_backend.py
+++ kpi/deployment_backends/mock_backend.py
@@ -23,3 +23,14 @@ class MockDeploymentBackend(BaseDeploymentBackend):
         self.store_data({
                 'active': bool(active),
             })
+
+    def get_enketo_survey_links(self):
+        # `self` is a demo Enketo form, but there's no guarantee it'll be
+        # around forever.
+        return {
+            'url': 'https://enke.to/::self',
+            'iframe_url': 'https://enke.to/i/::self',
+            'offline_url': 'https://enke.to/_/#self',
+            'preview_url': 'https://enke.to/preview/::self',
+            'preview_iframe_url': 'https://enke.to/preview/i/::self',
+        }
