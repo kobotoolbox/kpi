d9eb96c61daed1809ae68624ef460388480cc27d
pmusaraj <pmusaraj@gmail.com>
2016-08-02 15:58:14 -0400

    add temporary static data to test reports on staging

--- kobo/apps/reports/report_data.py
+++ kobo/apps/reports/report_data.py
@@ -8,6 +8,6 @@ import json
 
 
 def data(asset, kuids, lang=None, fields=None, split_by=None):
-    with open(os.path.join(settings.BASE_DIR, 'sensitive_data.json'), 'r') as ff:
+    with open(os.path.join(settings.BASE_DIR, 'temporary_data.json'), 'r') as ff:
         content = json.loads(ff.read())
     return content
