3d932be6e0667e68621fe7d8aad6839da01a17ab
John N. Milner <john@tmoj.net>
2016-08-09 10:35:57 -0400

    Access KC's `UserProfile` data per #821
    Copies, but does not ovewrite, all fields (once) from KC's `UserProfile` to
    KPI's `ExtraUserDetail`. Writes to KC's `UserProfile.require_auth` when
    necessary.

--- kpi/deployment_backends/kc_reader/utils.py
+++ kpi/deployment_backends/kc_reader/utils.py
@@ -1,4 +1,6 @@
+import json
 from .shadow_models import _models, safe_kc_read
+from django.db import ProgrammingError
 
 
 @safe_kc_read
@@ -7,3 +9,44 @@ def instance_count(xform_id_string, user_id):
                                            xform__user_id=user_id,
                                            xform__id_string=xform_id_string,
                                            ).count()
+
+@safe_kc_read
+def get_kc_profile_data(user_id):
+    ''' Retrieve all fields from the user's KC profile (if it exists) and
+    return them in a dictionary '''
+    try:
+        profile = _models.UserProfile.objects.get(user_id=user_id)
+    except _models.UserProfile.DoesNotExist:
+        return {}
+    fields = [
+        'name',
+        'city',
+        'country',
+        'organization',
+        'home_page',
+        'twitter',
+        'description',
+        'require_auth',
+        'address',
+        'phonenumber',
+        'metadata',
+    ]
+    result = {}
+    for field in fields:
+        value = getattr(profile, field)
+        if not isinstance(value, basestring):
+            value = json.dumps(value)
+        result[field] = value
+    return result
+
+def set_kc_require_auth(user_id, require_auth):
+    ''' WRITES to KC's UserProfile.require_auth '''
+    try:
+        profile, created = _models.UserProfile.objects.get_or_create(
+            user_id=user_id)
+        if profile.require_auth != require_auth:
+            profile.require_auth = require_auth
+            profile.save()
+    except ProgrammingError as e:
+        raise ProgrammingError(u'set_kc_require_auth error accessing kobocat '
+                               u'tables: {}'.format(e.message))
