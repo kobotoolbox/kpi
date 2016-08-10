4a9a18f92e1c76fa971e640f99f09ca0430e0905
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-08-10 15:06:07 -0400

    `NODE_PATH` is being manipulated (presumably by Node), so switch to `KPI_NODE_PATH`.

--- docker/run_uwsgi.bash
+++ docker/run_uwsgi.bash
@@ -3,4 +3,8 @@ set -e
 
 source /etc/profile
 
-exec /usr/local/bin/uwsgi --ini "${KPI_SRC_DIR}/uwsgi.ini"
+if [[ "${kpi_WEB_SERVER}" == 'uWSGI' ]]; then
+    exec /usr/local/bin/uwsgi --ini "${KPI_SRC_DIR}/uwsgi.ini"
+else
+    cd "${KPI_SRC_DIR}" && exec python manage.py runserver 0:8000
+fi
