35c2842cb04ba93d07c5523a3ba400a962eb53ee
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-08-10 15:25:37 -0400

    Default value for `kpi_WEB_SERVER`. Debug with `ipython` and `runserver_plus`.

--- docker/run_uwsgi.bash
+++ docker/run_uwsgi.bash
@@ -3,8 +3,11 @@ set -e
 
 source /etc/profile
 
+kpi_WEB_SERVER="${kpi_WEB_SERVER:-uWSGI}"
 if [[ "${kpi_WEB_SERVER}" == 'uWSGI' ]]; then
     exec /usr/local/bin/uwsgi --ini "${KPI_SRC_DIR}/uwsgi.ini"
 else
-    cd "${KPI_SRC_DIR}" && exec python manage.py runserver 0:8000
+    cd "${KPI_SRC_DIR}"
+    pip install werkzeug ipython
+    exec python manage.py runserver_plus 0:8000
 fi
