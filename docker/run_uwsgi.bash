5d5ec27bc097415a5643a63fcd4cc8cf07093489
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-08-12 11:09:46 -0400

    Also run Celery and clean up processes when using `runserver_plus`.

--- docker/run_uwsgi.bash
+++ docker/run_uwsgi.bash
@@ -9,5 +9,10 @@ if [[ "${kpi_WEB_SERVER}" == 'uWSGI' ]]; then
 else
     cd "${KPI_SRC_DIR}"
     pip install werkzeug ipython
-    exec python manage.py runserver_plus 0:8000
+    celery -A kobo worker --beat --loglevel=info --logfile $(KPI_LOGS_DIR)/celery.log --pidfile=/tmp/celery.pid &
+    CELERY_PID="$!"
+    python manage.py runserver_plus 0:8000 &
+    RUNSERVER_PID="$!"
+    trap "pkill -P ${RUNSERVER_PID}; kill ${CELERY_PID} ${RUNSERVER_PID}" SIGINT SIGTERM SIGKILL
+    wait "${CELERY}" "${RUNSERVER_PID}"
 fi
