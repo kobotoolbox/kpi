932d0095fd600d69cd6bb05597e734deb9469ece
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-09-19 14:44:45 -0400

    Codacy Python test coverage metrics.

--- docker/run_tests.bash
+++ docker/run_tests.bash
@@ -3,5 +3,11 @@ set -e
 
 source /etc/profile
 
-python manage.py test
+pip install coverage codacy-coverage
+(cd "${KPI_SRC_DIR}" && \
+    coverage run --source '.' manage.py test && \
+    coverage xml && \
+    python-codacy-coverage -r coverage.xml \
+) 
+
 npm run test
