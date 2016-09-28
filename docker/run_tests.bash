8f1339a27f539f26f5b8f5e652a934306efeb6aa
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-09-28 16:01:52 -0400

    More selective coverage measurement.

--- docker/run_tests.bash
+++ docker/run_tests.bash
@@ -5,7 +5,7 @@ source /etc/profile
 
 pip install coverage
 (cd "${KPI_SRC_DIR}" && \
-    coverage run --source '.' manage.py test && \
+    coverage run --source='hub,kpi,kobo' --omit='*/tests/*,*/migrations/*,*/management/commands/*' manage.py test && \
     coverage xml \
 ) 
 
