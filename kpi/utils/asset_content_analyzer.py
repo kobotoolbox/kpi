a69c42fd4ed591f655edede0a2418fd80b851e9e
Alex Dorey <dorey415@gmail.com>
2016-10-10 20:45:22 -0400

    asset.summary no longer displays lists of labels on translated forms.

--- kpi/utils/asset_content_analyzer.py
+++ kpi/utils/asset_content_analyzer.py
@@ -1,28 +1,18 @@
 import re
-import json
+from collections import OrderedDict
 
-import pyxform
-
-# possibly pull these aliases from pyxform
-GEO_TYPES = ['gps', 'geopoint', 'geoshape', 'geotrace',]
-_unlisted_meta_types = ['username']
-META_QUESTION_TYPES = pyxform.constants.XLSFORM_METADATA_TYPES | set(_unlisted_meta_types)
+from formpack.utils.replace_aliases import META_TYPES, GEO_TYPES
 
 class AssetContentAnalyzer(object):
     def __init__(self, *args, **kwargs):
         self.survey = kwargs.get('survey')
         self.settings = kwargs.get('settings', False)
         self.choices = kwargs.get('choices', [])
+        self.translations = kwargs.get('translations', [])
+        self.default_translation = False
+        if len(self.translations) > 0:
+            self.default_translation = self.translations[0]
         self.summary = self.get_summary()
-
-    def _get_languages_from_column_names(self, cols):
-        langs = set()
-        for col in cols:
-            media_mtch = re.match('^media\:', col)
-            mtch = re.match('.*\:\:?(.+)', col)
-            if mtch and not media_mtch:
-                langs.add(mtch.groups()[0])
-        return list(langs)
 
     def get_summary(self):
         row_count = 0
@@ -32,7 +22,7 @@ class AssetContentAnalyzer(object):
         metas = set()
         types = set()
         summary_errors = []
-        keys = set()
+        keys = OrderedDict()
         if not self.survey:
             return {}
 
@@ -44,29 +34,27 @@ class AssetContentAnalyzer(object):
                 _label = row.get('label')
                 if _type in GEO_TYPES:
                     geo = True
-                if isinstance(_type, dict):
-                    summary_errors.append(['invalidtype', str(_type)])
-                    _type = _type.keys()[0]
 
-                if not _type:
+                if not _type or _type.startswith('end'):
                     summary_errors.append(row)
                     continue
-                if re.match('^end', _type):
-                    continue
-                if _type in META_QUESTION_TYPES:
+                if _type in META_TYPES:
                     metas.add(_type)
                     continue
                 row_count += 1
                 types.add(_type)
-                if _label != None and len(_label) > 0:
+                if isinstance(_label, list) and len(_label) > 0:
+                    labels.append(_label[0])
+                elif isinstance(_label, basestring) and len(_label) > 0:
                     labels.append(_label)
-                keys = keys | set(row.keys())
+                keys.update(OrderedDict.fromkeys(row.keys()))
 
         summary = {
             'row_count': row_count,
-            'languages': self._get_languages_from_column_names(keys),
+            'languages': self.translations,
+            'default_translation': self.default_translation,
             'geo': geo,
             'labels': labels[0:5],
-            'columns': list(keys),
+            'columns': keys.keys(),
         }
         return summary
