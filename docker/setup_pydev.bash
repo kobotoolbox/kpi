4b5ca695c178212b336283e9cd89fa2abc52ace0
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2017-01-26 16:58:31 -0500

    Clarifications regarding remote debugging.

--- docker/setup_pydev.bash
+++ docker/setup_pydev.bash
@@ -13,6 +13,10 @@ if [[ -z "${KPI_PATH_FROM_ECLIPSE_TO_PYTHON_PAIRS}" ]]; then
     exit 1
 fi
 
+echo 'Setting up PyDev remote debugger path mappings.'
+
+# Set up the `PATHS_FROM_ECLIPSE_TO_PYTHON` variable from the environment per 
+#   https://github.com/fabioz/PyDev.Debugger/blob/master/pydevd_file_utils.py.
 find_string='PATHS_FROM_ECLIPSE_TO_PYTHON = []'
 replace_string="\
 import os\n\
@@ -25,4 +29,5 @@ escaped_replace_string=$(echo "${replace_string}" | sed -e '/\\n/b; s/[]\/$*.^|[
 
 sed -i "s/${escaped_find_sting}/${escaped_replace_string}/" /srv/pydev/pydevd_file_utils.py
 
+echo 'Adding `PYTHONPATH` modifications to profile.'
 echo 'export PYTHONPATH=${PYTHONPATH}:/srv/pydev' > /etc/profile.d/pydev_debugger.bash.sh
