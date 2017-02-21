8ea8c4bb3138424f4aa7bccd1dd8aea3dcd72971
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2017-02-21 12:20:07 -0500

    Facilitate use of New Relic analytics. Closes #1126.

--- docker/run_uwsgi.bash
+++ docker/run_uwsgi.bash
@@ -4,11 +4,20 @@ set -e
 source /etc/profile
 
 KPI_WEB_SERVER="${KPI_WEB_SERVER:-uWSGI}"
-if [[ "${KPI_WEB_SERVER^^}" == 'UWSGI' ]]; then
-    exec /usr/local/bin/uwsgi --ini "${KPI_SRC_DIR}/uwsgi.ini"
+uwsgi_command="/usr/local/bin/uwsgi --ini ${KPI_SRC_DIR}/uwsgi.ini"
+if [[ "${KPI_WEB_SERVER,,}" == 'uwsgi' && -z "${NEW_RELIC_LICENSE_KEY}" ]]; then
+    # Use the uWSGI application server.
+    echo 'Running `kpi` container with uWSGI application server.'
+    exec ${uwsgi_command}
+elif [[ "${KPI_WEB_SERVER,,}" == 'uwsgi' && ! -z "${NEW_RELIC_LICENSE_KEY}" ]]; then
+    echo 'Running `kpi` container with uWSGI application server and New Relic analytics.'
+    export NEW_RELIC_APP_NAME="${NEW_RELIC_APP_NAME:-KoBoForm}"
+    export NEW_RELIC_LOG="${NEW_RELIC_LOG:-stdout}"
+    export NEW_RELIC_LOG_LEVEL="${NEW_RELIC_LOG_LEVEL:-info}"
+    exec newrelic-admin run-program ${uwsgi_command}
 else
+    echo 'Running `kpi` container with `runserver_plus` debugging application server.'
     cd "${KPI_SRC_DIR}"
-
     pip install werkzeug ipython
     exec python manage.py runserver_plus 0:8000
 fi
