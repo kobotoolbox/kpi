22ad399d5fd21568c51f2aed4741dbeddcd393cb
Phil Reisman <preisman@theadkgroup.com>
2017-05-04 12:41:44 -0400

    Fixed url serialization bug

--- kpi/serializers.py
+++ kpi/serializers.py
@@ -819,27 +819,27 @@ class AttachmentSerializer(serializers.ModelSerializer):
             result = image_url(obj, 'original')
         else:
             result = obj.media_file.url if obj.media_file.url else None
-        return result if not request else request.build_absolute_uri(result)
+        return result if not request or not result else request.build_absolute_uri(result)
 
     def get_small_download_url(self, obj):
         if obj.mimetype.startswith('image'):
             request = self.context.get('request')
             result = image_url(obj, 'small')
-            return result if not request else request.build_absolute_uri(result)
+            return result if not request or not result else request.build_absolute_uri(result)
         return None
 
     def get_medium_download_url(self, obj):
         if obj.mimetype.startswith('image'):
             request = self.context.get('request')
             result = image_url(obj, 'medium')
-            return result if not request else request.build_absolute_uri(result)
+            return result if not request or not result else request.build_absolute_uri(result)
         return None
 
     def get_large_download_url(self, obj):
         if obj.mimetype.startswith('image'):
             request = self.context.get('request')
             result = image_url(obj, 'large')
-            return result if not request else request.build_absolute_uri(result)
+            return result if not request or not result else request.build_absolute_uri(result)
         return None
 
 
