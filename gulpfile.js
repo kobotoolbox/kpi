76976fd6025a620e3639274b191f13b9e7f57151
Alex Dorey <dorey415@gmail.com>
2017-05-12 01:14:31 -0500

    * addresses #1276 * has not yet been tested with docker

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,15 @@ gulp.task("browserify", function() {
             .pipe(gulp.dest(__dirname + "/tmp"));
 });
 
+gulp.task("copy", function () {
+    return gulp.src([
+          './node_modules/font-awesome/fonts/*',
+          './node_modules/roboto-fontface/fonts/*.wof*',
+          './node_modules/material-design-icons/iconfont/*',
+          './jsapp/scss/fonts/k-icons/*',
+        ]).pipe(gulp.dest('./jsapp/fants/'));
+});
+
 gulp.task("clean", function () {
     return gulp.src("tmp/tests-index-compiled.js", {read: false})
             .pipe(clean());
