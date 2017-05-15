f18e83ee8aa87f76ff2b04567d3db45e7741c72c
pmusaraj <pmusaraj@gmail.com>
2017-05-15 16:56:23 -0400

    fix "gulp copy" location typo, remove reference to "material-icons" font

--- gulpfile.js
+++ gulpfile.js
@@ -34,9 +34,8 @@ gulp.task("copy", function () {
     return gulp.src([
           './node_modules/font-awesome/fonts/*',
           './node_modules/roboto-fontface/fonts/*.wof*',
-          './node_modules/material-design-icons/iconfont/*',
           './jsapp/scss/fonts/k-icons/*',
-        ]).pipe(gulp.dest('./jsapp/fants/'));
+        ]).pipe(gulp.dest('./jsapp/fonts/'));
 });
 
 gulp.task("clean", function () {
