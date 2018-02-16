3bfab5cff501c1a08a9cfe51865ce33ecafc50f9
Sebastian Iturra <sebastian@asimov.cl>
2018-02-16 19:19:21 -0300

    avances

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/private/var/www/kpi/env/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- env/lib/python2.7/site-packages/whitenoise/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import WhiteNoise
-
-__version__ = '3.3.1'
-
-__all__ = ['WhiteNoise']
