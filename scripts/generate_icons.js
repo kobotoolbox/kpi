f08b1c2de14a5bb3b77ca6ee274f087016e1df03
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-06-28 10:05:30 +0200

    use new script for generating icons

--- scripts/generate_icons.js
+++ scripts/generate_icons.js
@@ -3,13 +3,23 @@ const fs = require('fs');
 const sourceDir = 'jsapp/svg-icons/';
 const destDir = 'jsapp/fonts/';
 
+console.warn(
+  '\x1b[31m***\n',
+  'Please make sure SVGs are at least 1200px in size! Otherwise glyphs will look terrible during svg2ttf conversion.',
+  '\n***',
+  '\x1b[0m'
+);
+
+console.info('Reading files…');
 const files = [];
 fs.readdirSync(sourceDir).forEach(file => {
   if (file.endsWith('.svg')) {
     files.push(`${sourceDir}${file}`);
   }
 })
+console.info(`${files.length} SVGs found.`)
 
+console.info('Generating fonts…');
 webfontsGenerator(
   {
     files: files,
@@ -21,22 +31,36 @@ webfontsGenerator(
     html: true,
     htmlTemplate: './jsapp/k-icons-html-template.hbs',
     types: ['eot', 'svg', 'ttf', 'woff2', 'woff'],
+    order: ['woff2', 'woff', 'ttf', 'eot', 'svg'],
     templateOptions: {
       classPrefix: 'k-icon-',
       baseSelector: '.k-icon',
       baseClassName: 'k-icon'
     },
-    fixedWidth: false,
-    centerHorizontally: false,
-    normalize: false,
-    round: 0
+    formatOptions: {
+      svg: {
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fixedWidth: true,
+        centerHorizontally: false,
+        normalize: false,
+        height: 10000,
+        round: 0,
+        descent: 0,
+        ascent: 0
+      },
+      ttf: {},
+      woff2: {},
+      woff: {},
+      eot: {}
+    }
   },
   function(error) {
     if (error) {
-      console.log('Fail!', error);
+      throw new Error('Fail!', error);
     } else {
       fs.copyFileSync(`${destDir}k-icons.css`, `${destDir}_k-icons.scss`);
-      console.log('Done!');
+      console.log('\x1b[32m', 'Fonts generated :-)', '\x1b[0m');
     }
   }
 );
