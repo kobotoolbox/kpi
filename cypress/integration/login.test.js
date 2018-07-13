0fda25fc021c8f1f6dc7e9cf396ce91c752a8ef6
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-07-13 15:30:32 +0200

    debug travis more

--- cypress/integration/login.test.js
+++ cypress/integration/login.test.js
@@ -1,11 +1,9 @@
 describe('Login', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  })
-
   it('should log in to a proper user account', () => {
     const username = 'kobo';
     const pass = 'kobo';
+
+    cy.visit('/accounts/login/?next=/');
 
     cy.get('#id_username').type('kobo').should('have.value', username);
     cy.get('#id_password').type('kobo').should('have.value', pass);
@@ -14,5 +12,11 @@ describe('Login', () => {
     // check username in UI
     cy.get('.account-box .popover-menu__toggle').click();
     cy.get('.account-box .popover-menu__content.popover-menu__content--visible .account-username').should('contain', username);
+  });
+
+  it('travis-test-1', () => {
+    cy.visit('http://127.0.0.1:8001/');
+    cy.visit('http://127.0.0.1:8002/');
+    cy.visit('http://localhost:8000/');
   });
 });
