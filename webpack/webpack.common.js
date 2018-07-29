d59aa9bbf9214aba84f24e2953c68fbff300278a
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-07-29 14:24:29 +0200

    add browserslist file and configure postcss to use it through autoprefixer

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -25,7 +25,7 @@ var defaultOptions = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader', 'postcss-loader']
       },
       {
         test: /\.scss$/,
