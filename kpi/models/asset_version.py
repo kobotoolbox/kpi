9665972469ff06baaeb73ffb17d606df9d7643d5
Olivier Léger <olivierleger@gmail.com>
2018-08-17 09:51:11 -0400

    Tiny change for PEP8 compliance

--- kpi/models/asset_version.py
+++ kpi/models/asset_version.py
@@ -58,7 +58,7 @@ class AssetVersion(models.Model):
     def content_hash(self):
         # used to determine changes in the content from version to version
         # not saved, only compared with other asset_versions
-        _json_string = json.dumps(self.version_content,sort_keys=True)
+        _json_string = json.dumps(self.version_content, sort_keys=True)
         return hashlib.sha1(_json_string).hexdigest()
 
     def __unicode__(self):
