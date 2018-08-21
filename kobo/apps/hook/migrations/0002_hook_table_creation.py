605c5dee484d214ec9d31d5306cc7c0ccbc4489e
Olivier Léger <olivierleger@gmail.com>
2018-08-21 18:53:08 -0400

    Merged migrations

--- kobo/apps/hook/migrations/0002_hook_table_creation.py
+++ kobo/apps/hook/migrations/0002_hook_table_creation.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import kpi.fields
 
 
 class Migration(migrations.Migration):
@@ -15,21 +16,31 @@ class Migration(migrations.Migration):
             name='HookLog',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('uid', models.CharField(unique=True, max_length=36)),
-                ('tries', models.IntegerField(default=0)),
+                ('uid', kpi.fields.KpiUidField(uid_prefix=b'hl')),
+                ('instance_uuid', models.CharField(default=b'', max_length=36, db_index=True)),
+                ('tries', models.PositiveSmallIntegerField(default=0)),
+                ('status', models.PositiveSmallIntegerField(default=1)),
                 ('status_code', models.IntegerField(default=200)),
-                ('message', models.CharField(default=b'', max_length=500)),
+                ('message', models.TextField(default=b'')),
                 ('date_created', models.DateTimeField(auto_now_add=True)),
                 ('date_modified', models.DateTimeField(auto_now_add=True)),
             ],
             options={
-                'ordering': ['date_modified'],
+                'ordering': ['-date_created'],
             },
         ),
         migrations.RenameField(
             model_name='hook',
             old_name='destination_url',
             new_name='endpoint',
+        ),
+        migrations.RemoveField(
+            model_name='hook',
+            name='failed_count',
+        ),
+        migrations.RemoveField(
+            model_name='hook',
+            name='success_count',
         ),
         migrations.AddField(
             model_name='hook',
