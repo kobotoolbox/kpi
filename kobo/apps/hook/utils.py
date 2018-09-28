72c04d37ca59b3ab3780c903a2e117acbb0074be
Olivier Léger <olivierleger@gmail.com>
2018-09-28 12:29:36 -0400

    Use instance_id instead of instance_uuid to retrieve data from kc

--- kobo/apps/hook/utils.py
+++ kobo/apps/hook/utils.py
@@ -8,21 +8,21 @@ from .tasks import service_definition_task
 class HookUtils(object):
 
     @staticmethod
-    def call_services(asset, instance_uuid):
+    def call_services(asset, instance_id):
         """
         Delegates to Celery data submission to remote servers
 
         :param asset: Asset.
-        :param instance_uuid: str. `Instance.uuid`
+        :param instance_id: int. Instance primary key
         """
         # Retrieve `Hook` ids, to send data to their respective endpoint.
         hooks_ids = asset.hooks.filter(active=True).values_list("id", flat=True).distinct()
-        # At least, one of the hooks must not have a log that corresponds to `instance_uuid`
+        # At least, one of the hooks must not have a log that corresponds to `instance_id`
         # to make success equal True
         success = False
         for hook_id in hooks_ids:
-            if not HookLog.objects.filter(instance_uuid=instance_uuid, hook_id=hook_id).exists():
+            if not HookLog.objects.filter(instance_id=instance_id, hook_id=hook_id).exists():
                 success = True
-                service_definition_task.delay(hook_id, instance_uuid)
+                service_definition_task.delay(hook_id, instance_id)
 
         return success
