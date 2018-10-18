fd6dd509a86dcc3433e0870c23ba62a766e27bab
Olivier Léger <olivierleger@gmail.com>
2018-10-18 15:15:10 -0400

    Expose 'instance_id' in HookLogViewSet

--- kobo/apps/hook/serializers/hook_log.py
+++ kobo/apps/hook/serializers/hook_log.py
@@ -11,10 +11,10 @@ class HookLogSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = HookLog
-        fields = ("url", "uid", "tries", "status", "status_str", "status_code",
+        fields = ("url", "uid", "instance_id", "tries", "status", "status_str", "status_code",
                   "message", "date_modified")
 
-        read_only_fields = ("uid", "tries", "status", "status_str", "status_code", "message", "date_modified")
+        read_only_fields = ("uid", "instance_id", "tries", "status", "status_str", "status_code", "message", "date_modified")
 
     url = serializers.SerializerMethodField()
 
