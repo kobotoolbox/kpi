1d66e55ae4d88c63660826a9b604cd25e3881ad6
Olivier Léger <olivierleger@gmail.com>
2018-10-30 13:52:07 -0400

    Fixed bad commit - Refactored delete_assets_snapshot command to extend 'BaseCommand'

--- kpi/management/commands/delete_assets_snapshots.py
+++ kpi/management/commands/delete_assets_snapshots.py
@@ -1,58 +1,22 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import
 
 from datetime import timedelta
-import sys
 
-from django.core.management.base import BaseCommand
-from django.db import transaction, connection
 from django.db.models import Max
 from django.utils import timezone
 
+from .delete_base_command import BaseCommand
 from kpi.models import AssetSnapshot
-import time
+
 
 class Command(BaseCommand):
 
     help = "Deletes assets snapshots"
 
-    def add_arguments(self, parser):
-        super(Command, self).add_arguments(parser)
-        parser.add_argument(
-            "--days",
-            default=90,
-            type=int,
-            help="Delete only import tasks older than the specified number of days. Default: 90",
-        )
-
-        parser.add_argument(
-            "--chunks",
-            default=1000,
-            type=int,
-            help="Delete only import tasks by batch of `chunks` records. Default: 1000",
-        )
-
-        parser.add_argument(
-            "--vacuum",
-            action='store_true',
-            default=False,
-            help="Run `VACUUM` on tables after deletion",
-        )
-
-        parser.add_argument(
-            "--vacuum-full",
-            action='store_true',
-            default=False,
-            help="Run `VACUUM FULL` instead of `VACUUM`.",
-        )
-
-    def handle(self, *args, **options):
+    def _prepare_delete_queryset(self, **options):
         days = options["days"]
-        chunks = options["chunks"]
-        verbosity = options["verbosity"]
-        vacuum_full = options["vacuum_full"]
-        vacuum = options["vacuum"]
-
+        self._model = AssetSnapshot
 
         # Retrieve Snapshots linked to assets' latest versions.
         # Use iterator to by-pass Django QuerySet caching.
@@ -63,46 +27,7 @@ class Command(BaseCommand):
         if None in latest_versions_ids:
             latest_versions_ids.remove(None)
 
-        snapshots_to_delete = AssetSnapshot.objects.filter(
+        # Retrieve all records older than days that are not linked to latest versions
+        return AssetSnapshot.objects.filter(
             date_created__lt=timezone.now() - timedelta(days=days),
         ).exclude(asset_version_id__in=latest_versions_ids)
-
-        chunked_delete_ids = []
-        chunks_cpt = 1
-        total = snapshots_to_delete.count()
-
-        for snapshot_id in snapshots_to_delete.values_list("id", flat=True).iterator():
-
-            chunked_delete_ids.append(snapshot_id)
-
-            if (chunks_cpt % chunks) == 0 or chunks_cpt == total:
-                with transaction.atomic():  # Wrap into a transaction because of CASCADE, post_delete signals
-                    chuncked_objects_to_delete = AssetSnapshot.objects.filter(id__in=chunked_delete_ids)
-                    if verbosity >= 1:
-                        progress = "\rDeleting {chunk}/{total} assets snapshots...".format(
-                            chunk=chunks_cpt,
-                            total=total
-                        )
-                        sys.stdout.write(progress)
-                        sys.stdout.flush()
-                    chuncked_objects_to_delete.delete()
-                chunked_delete_ids = []
-
-            chunks_cpt += 1
-
-        print("")
-
-        if vacuum is True or vacuum_full is True:
-            self.do_vacuum(vacuum_full)
-
-        print("Done!")
-
-    def do_vacuum(self, full=False):
-        cursor = connection.cursor()
-        if full:
-            print("Vacuuming (full) table {}...".format(AssetSnapshot._meta.db_table))
-            cursor.execute("VACUUM FULL {}".format(AssetSnapshot._meta.db_table))
-        else:
-            print("Vacuuming table {}...".format(AssetSnapshot._meta.db_table))
-            cursor.execute("VACUUM {}".format(AssetSnapshot._meta.db_table))
-        connection.commit()
--- kpi/management/commands/delete_import_tasks.py
+++ kpi/management/commands/delete_import_tasks.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import
 
 from datetime import timedelta
-import sys
 
-from django.core.management.base import BaseCommand
-from django.db import transaction, connection
 from django.utils import timezone
 
+from .delete_base_command import BaseCommand
 from kpi.models import ImportTask
 
 
@@ -15,82 +13,10 @@ class Command(BaseCommand):
 
     help = "Deletes import tasks"
 
-    def add_arguments(self, parser):
-        super(Command, self).add_arguments(parser)
-        parser.add_argument(
-            "--days",
-            default=90,
-            type=int,
-            help="Delete only import tasks older than the specified number of days.",
-        )
-
-        parser.add_argument(
-            "--chunks",
-            default=1000,
-            type=int,
-            help="Delete only import tasks by batch of `chunks` records.",
-        )
-
-        parser.add_argument(
-            "--vacuum",
-            action='store_true',
-            default=False,
-            help="Run `VACUUM` on tables after deletion.",
-        )
-
-        parser.add_argument(
-            "--vacuum-full",
-            action='store_true',
-            default=False,
-            help="Run `VACUUM FULL` instead of `VACUUM`.",
-        )
-
-    def handle(self, *args, **options):
+    def _prepare_delete_queryset(self, **options):
         days = options["days"]
-        chunks = options["chunks"]
-        verbosity = options["verbosity"]
-        vacuum_full = options["vacuum_full"]
-        vacuum = options["vacuum"]
-
-        tasks_to_delete = ImportTask.objects.filter(
+        self._model = ImportTask
+        return self._model.objects.filter(
             date_created__lt=timezone.now() - timedelta(days=days),
         )
-        chunked_delete_ids = []
-        chunks_cpt = 1
-        total = tasks_to_delete.count()
 
-        for import_task_id in tasks_to_delete.values_list("id", flat=True).iterator():
-
-            chunked_delete_ids.append(import_task_id)
-
-            if (chunks_cpt % chunks) == 0 or chunks_cpt == total:
-                with transaction.atomic():  # Wrap into a transaction because of CASCADE, post_delete signals
-                    chuncked_objects_to_delete = ImportTask.objects.filter(id__in=chunked_delete_ids)
-                    if verbosity >= 1:
-                        progress = "\rDeleting {chunk}/{total} import tasks...".format(
-                            chunk=chunks_cpt,
-                            total=total
-                        )
-                        sys.stdout.write(progress)
-                        sys.stdout.flush()
-                    chuncked_objects_to_delete.delete()
-                chunked_delete_ids = []
-
-            chunks_cpt += 1
-
-        print("")
-
-        if vacuum is True or vacuum_full is True:
-            self.do_vacuum(vacuum_full)
-
-        print("Done!")
-
-    def do_vacuum(self, full=False):
-        cursor = connection.cursor()
-        if full:
-            print("Vacuuming (full) table {}...".format(ImportTask._meta.db_table))
-            cursor.execute("VACUUM FULL {}".format(ImportTask._meta.db_table))
-        else:
-            print("Vacuuming table {}...".format(ImportTask._meta.db_table))
-            cursor.execute("VACUUM {}".format(ImportTask._meta.db_table))
-        connection.commit()
