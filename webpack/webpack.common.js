fec7433c4d1d325adc9b8bc5c42d95ec42e2da15
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-12-11 18:25:30 +0100

    try giving postcss-loader our postcss config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,6 +3,16 @@ const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
 var merge = require('lodash.merge');
 
+const postCssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    sourceMap: true,
+    config: {
+       path: path.resolve(__dirname, '../postcss.config.js')
+    }
+  }
+};
+
 var defaultOptions = {
   module: {
     rules: [
@@ -25,11 +35,11 @@ var defaultOptions = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
+        use: ['style-loader', 'css-loader', postCssLoader]
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', postCssLoader, 'sass-loader']
       },
       {
         test: /\.coffee$/,
