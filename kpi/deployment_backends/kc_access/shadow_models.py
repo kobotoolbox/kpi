1000a9e0f4b5e68c4894585361788969bd3b9e9c
John N. Milner <john@tmoj.net>
2019-01-02 22:49:17 -0500

    Define shadow UserObjectPermission; fixes #2148
    Also removes django-guardian dependency

--- kpi/deployment_backends/kc_access/shadow_models.py
+++ kpi/deployment_backends/kc_access/shadow_models.py
@@ -1,9 +1,16 @@
-from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.db import models
-from django.db import ProgrammingError
-from django.utils.translation import ugettext_lazy
 from hashlib import md5
+
+from django.db import models
+from django.conf import settings
+from django.db import ProgrammingError
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User, Permission
+from django.contrib.contenttypes.models import ContentType
+
+try:
+    from django.contrib.contenttypes.fields import GenericForeignKey
+except ImportError:
+    from django.contrib.contenttypes.generic import GenericForeignKey
 
 from jsonfield import JSONField
 
@@ -42,12 +49,19 @@ class LazyModelGroup:
             self._define()
         return self._UserProfile
 
+    @property
+    def UserObjectPermission(self):
+        if not hasattr(self, '_UserObjectPermission'):
+            self._define()
+        return self._UserObjectPermission
+
     @staticmethod
     def get_content_type_for_model(model):
         MODEL_NAME_MAPPING = {
             '_readonlyxform': ('logger', 'xform'),
             '_readonlyinstance': ('logger', 'instance'),
-            '_userprofile': ('main', 'userprofile')
+            '_userprofile': ('main', 'userprofile'),
+            '_userobjectpermission': ('guardian', 'userobjectpermission'),
         }
         try:
             app_label, model_name = MODEL_NAME_MAPPING[model._meta.model_name]
@@ -88,6 +102,7 @@ class LazyModelGroup:
                 ''' Matches what's returned by the KC API '''
                 return u"md5:%s" % self.hash
 
+
         class _ReadOnlyInstance(_ReadOnlyModel):
             class Meta:
                 managed = False
@@ -104,6 +119,7 @@ class LazyModelGroup:
             status = models.CharField(max_length=20,
                                       default=u'submitted_via_web')
             uuid = models.CharField(max_length=249, default=u'')
+
 
         class _UserProfile(models.Model):
             '''
@@ -129,7 +145,7 @@ class LazyModelGroup:
             description = models.CharField(max_length=255, blank=True)
             require_auth = models.BooleanField(
                 default=False,
-                verbose_name=ugettext_lazy(
+                verbose_name=_(
                     "Require authentication to see forms and submit data"
                 )
             )
@@ -139,9 +155,55 @@ class LazyModelGroup:
             num_of_submissions = models.IntegerField(default=0)
             metadata = JSONField(default={}, blank=True)
 
+
+        class _UserObjectPermission(models.Model):
+            '''
+            For the _sole purpose_ of letting us manipulate KoBoCAT
+            permissions, this comprises the following django-guardian classes
+            all condensed into one:
+
+              * UserObjectPermission
+              * UserObjectPermissionBase
+              * BaseGenericObjectPermission
+              * BaseObjectPermission
+
+            CAVEAT LECTOR: The django-guardian custom manager,
+            UserObjectPermissionManager, is NOT included!
+            '''
+            permission = models.ForeignKey(Permission)
+            content_type = models.ForeignKey(ContentType)
+            object_pk = models.CharField(_('object ID'), max_length=255)
+            content_object = GenericForeignKey(fk_field='object_pk')
+            user = models.ForeignKey(
+                getattr(settings, 'AUTH_USER_MODEL', 'auth.User'))
+
+            class Meta:
+                db_table = 'guardian_userobjectpermission'
+                unique_together = ['user', 'permission', 'object_pk']
+
+            def __unicode__(self):
+                return '%s | %s | %s' % (
+                    unicode(self.content_object),
+                    unicode(getattr(self, 'user', False) or self.group),
+                    unicode(self.permission.codename))
+
+            def save(self, *args, **kwargs):
+                content_type = ContentType.objects.get_for_model(
+                    self.content_object)
+                if content_type != self.permission.content_type:
+                    raise ValidationError(
+                        "Cannot persist permission not designed for this "
+                        "class (permission's type is %r and object's type is "
+                        "%r)"
+                        % (self.permission.content_type, content_type)
+                    )
+                return super(UserObjectPermission, self).save(*args, **kwargs)
+
+
         self._XForm = _ReadOnlyXform
         self._Instance = _ReadOnlyInstance
         self._UserProfile = _UserProfile
+        self._UserObjectPermission = _UserObjectPermission
 
 _models = LazyModelGroup()
 
