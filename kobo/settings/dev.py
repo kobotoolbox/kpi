6dcdab2c3f4249312fb542da0705407744b80225
Olivier Léger <olivierleger@gmail.com>
2019-02-04 17:05:45 -0500

    simplified monkey.patch in dev settings

--- kobo/settings/dev.py
+++ kobo/settings/dev.py
@@ -3,34 +3,14 @@ from __future__ import absolute_import
 import sys
 
 from .base import *
-from kpi.utils.log import logging
+
+LOGGING['handlers']['console'] = {
+    'level': 'DEBUG',
+    'class': 'logging.StreamHandler',
+    'formatter': 'verbose'
+}
 
 # When using `./manage.py runserver_plus`, print output is not
-# displayed in the console. This monkey-patch makes it work.
-# Pretty buggy, only for dev environment
-
-old_stdout = sys.stdout
-is_ipython = False
-
-try:
-    get_ipython()
-    is_ipython = True
-except Exception as e:
-    pass
-
-
-class F:
-    def __init__(self):
-        pass
-
-    def write(self, message):
-        logging.debug(message.rstrip())
-
-
-# Patch only if not in iPython console
-if not is_ipython:
-    F.flush = old_stdout.flush
-    try:
-        sys.stdout = F()
-    except Exception as e:
-        sys.stdout = old_stdout
+# displayed in the console and later when buffer is flushed.
+# This monkey-patch makes stout.write to flush buffer right away
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
