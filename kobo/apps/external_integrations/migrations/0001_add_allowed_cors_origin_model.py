5b932f98559dc0940b960e56534b957326d4e396
John N. Milner <john@tmoj.net>
2019-02-26 23:46:38 -0500

    Allow trusted origins (CORS) to be configured…
    in the Django admin interface. Closes #2210

---
+++ kobo/apps/external_integrations/migrations/0001_add_allowed_cors_origin_model.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CorsModel',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('cors', models.CharField(help_text='do not include http:// or https://', max_length=255)),
+            ],
+            options={
+                'verbose_name': 'allowed CORS origin',
+            },
+        ),
+    ]
