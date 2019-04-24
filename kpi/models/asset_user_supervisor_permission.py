9c1c0902bf1a70b99730eef1a5636e3d128e6949
Olivier Léger <olivierleger@gmail.com>
2019-04-24 15:37:59 -0400

    Refactored permissions variables, use variables from 'constant.py' instead of hardcoded string

---
+++ kpi/models/asset_user_supervisor_permission.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from django.utils import timezone
+from django.db import models
+from jsonbfield.fields import JSONField as JSONBField
+
+
+class AssetUserSupervisorPermission(models.Model):
+    """
+    Many-to-Many table which provides users' permissions
+    on other users's submissions
+
+    For example,
+        - Asset:
+            - uid: aAAAAAA
+            - id: 1
+        - User:
+            - username: someuser
+            - id: 1
+    We want someuser to be able to view otheruser's submissions
+    Records should be
+    `permissions` is dict formatted as is:
+    asset_id | user_id | permissions
+        1    |    1    | {"someuser": ["view_submissions"]}
+
+    Using a list per user for permissions, gives the opportunity to add other permissions
+    such as `change_submissions`, `delete_submissions` for later purpose
+    """
+    asset = models.ForeignKey('Asset', related_name='asset_supervisor_permissions', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', related_name='user_supervisor_permissions', on_delete=models.CASCADE)
+    permissions = JSONBField(default=dict)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+
+        if self.pk is not None:
+            self.date_modified = timezone.now()
+
+        super(AssetUserSupervisorPermissions, self).save(*args, **kwargs)
