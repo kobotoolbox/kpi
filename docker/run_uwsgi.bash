095f2ce6755041878c1b90b8a12f407d55f86e2c
Olivier Léger <olivierleger@gmail.com>
2019-07-08 09:24:19 -0400

    Install dev dependencies when DJANGO web server is activated

--- docker/run_uwsgi.bash
+++ docker/run_uwsgi.bash
@@ -5,12 +5,13 @@ source /etc/profile
 
 KPI_WEB_SERVER="${KPI_WEB_SERVER:-uWSGI}"
 uwsgi_command="/usr/local/bin/uwsgi --ini ${KPI_SRC_DIR}/uwsgi.ini"
+
 if [[ "${KPI_WEB_SERVER,,}" == 'uwsgi' ]]; then
     echo 'Running `kpi` container with uWSGI application server.'
     exec ${uwsgi_command}
 else
     echo 'Running `kpi` container with `runserver_plus` debugging application server.'
     cd "${KPI_SRC_DIR}"
-    pip install werkzeug ipython
+    pip-sync dependencies/pip/dev_requirements.txt
     exec python manage.py runserver_plus 0:8000
 fi
