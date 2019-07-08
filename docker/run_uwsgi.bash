2240dce00e2b8f6a432f40cedd312e59e4b40284
Olivier Léger <olivierleger@gmail.com>
2019-07-08 09:22:46 -0400

    Removed new_relic

--- docker/run_uwsgi.bash
+++ docker/run_uwsgi.bash
@@ -5,15 +5,9 @@ source /etc/profile
 
 KPI_WEB_SERVER="${KPI_WEB_SERVER:-uWSGI}"
 uwsgi_command="/usr/local/bin/uwsgi --ini ${KPI_SRC_DIR}/uwsgi.ini"
-if [[ "${KPI_WEB_SERVER,,}" == 'uwsgi' && -z "${NEW_RELIC_LICENSE_KEY}" ]]; then
+if [[ "${KPI_WEB_SERVER,,}" == 'uwsgi' ]]; then
     echo 'Running `kpi` container with uWSGI application server.'
     exec ${uwsgi_command}
-elif [[ "${KPI_WEB_SERVER,,}" == 'uwsgi' && ! -z "${NEW_RELIC_LICENSE_KEY}" ]]; then
-    echo 'Running `kpi` container with uWSGI application server and New Relic analytics.'
-    export NEW_RELIC_APP_NAME="${NEW_RELIC_APP_NAME:-KoBoForm}"
-    export NEW_RELIC_LOG="${NEW_RELIC_LOG:-stdout}"
-    export NEW_RELIC_LOG_LEVEL="${NEW_RELIC_LOG_LEVEL:-info}"
-    exec newrelic-admin run-program ${uwsgi_command}
 else
     echo 'Running `kpi` container with `runserver_plus` debugging application server.'
     cd "${KPI_SRC_DIR}"
