5aeda36fd8d76539897b3ce25e3c28133c307d08
John N. Milner <john@tmoj.net>
2019-08-08 13:40:36 -0400

    Use `GenericRelation` to check content type

--- kpi/utils/object_permission_helper.py
+++ kpi/utils/object_permission_helper.py
@@ -34,7 +34,9 @@ class ObjectPermissionHelper(object):
     @classmethod
     def get_assignments_queryset(cls, affected_object, user):
 
-        queryset = ObjectPermission.objects.filter(object_id=affected_object.pk)
+        # `affected_object.permissions` is a `GenericRelation(ObjectPermission)`
+        queryset = affected_object.permissions.all()
+
         # Filtering is done in `get_queryset` instead of FilteredBackend class
         # because it's specific to `ObjectPermission`.
 
