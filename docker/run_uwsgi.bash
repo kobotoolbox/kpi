9aab5448ed4859a2222ae3a72b3a84312b606fcb
Olivier Leger <olivierleger@gmail.com>
2019-10-24 01:11:09 -0300

    Changed startup script to avoid resyncing pip dependencies if they are the same

--- docker/run_uwsgi.bash
+++ docker/run_uwsgi.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 source /etc/profile
 
@@ -7,11 +6,23 @@ KPI_WEB_SERVER="${KPI_WEB_SERVER:-uWSGI}"
 UWSGI_COMMAND="$(which uwsgi) --ini ${KPI_SRC_DIR}/uwsgi.ini"
 
 if [[ "${KPI_WEB_SERVER,,}" == 'uwsgi' ]]; then
+    cd "${KPI_SRC_DIR}"
+    DIFF=$(diff "${KPI_SRC_DIR}/dependencies/pip/external_services.txt" "/srv/tmp/pip_dependencies.txt")
+    if [[ "$DIFF" != "" ]]; then
+        echo "Syncing pip dependencies..."
+        pip-sync dependencies/pip/external_services.txt 1>/dev/null
+        cp "dependencies/pip/external_services.txt" "/srv/tmp/pip_dependencies.txt"
+    fi
     echo 'Running `kpi` container with uWSGI application server.'
     exec ${UWSGI_COMMAND}
 else
-    echo 'Running `kpi` container with `runserver_plus` debugging application server.'
     cd "${KPI_SRC_DIR}"
-    pip-sync dependencies/pip/dev_requirements.txt
+    DIFF=$(diff "${KPI_SRC_DIR}/dependencies/pip/dev_requirements.txt" "/srv/tmp/pip_dependencies.txt")
+    if [[ "$DIFF" != "" ]]; then
+        echo "Syncing pip dependencies..."
+        pip-sync dependencies/pip/dev_requirements.txt 1>/dev/null
+        cp "dependencies/pip/dev_requirements.txt" "/srv/tmp/pip_dependencies.txt"
+    fi
+    echo 'Running `kpi` container with `runserver_plus` debugging application server.'
     exec python manage.py runserver_plus 0:8000
 fi
