930a8265a658dec5c027da877918c84884a5ad38
Olivier Leger <olivierleger@gmail.com>
2019-11-15 13:11:20 -0400

    Replaced `django.conf.urls.url()` with `django.urls.re_path()`

--- kpi/urls/__init__.py
+++ kpi/urls/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import private_storage.urls
-from django.conf.urls import url, include
 from django.contrib.auth import logout
+from django.urls import include, re_path
 from django.views.i18n import JavaScriptCatalog
 
 from hub.models import ConfigurationFile
@@ -28,37 +28,37 @@ js_info_dict = {
 }
 
 urlpatterns = [
-    url(r'^$', home, name='kpi-root'),
-    url(r'^me/$', CurrentUserViewSet.as_view({
+    re_path(r'^$', home, name='kpi-root'),
+    re_path(r'^me/$', CurrentUserViewSet.as_view({
         'get': 'retrieve',
         'patch': 'partial_update',
     }), name='currentuser-detail'),
-    url(r'^', include(router_api_v1.urls)),
-    url(r'^api/v2/', include((router_api_v2.urls, URL_NAMESPACE))),
-    url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework')),
-    url(r'^accounts/register/$', ExtraDetailRegistrationView.as_view(
+    re_path(r'^', include(router_api_v1.urls)),
+    re_path(r'^api/v2/', include((router_api_v2.urls, URL_NAMESPACE))),
+    re_path(r'^api-auth/', include('rest_framework.urls',
+                                   namespace='rest_framework')),
+    re_path(r'^accounts/register/$', ExtraDetailRegistrationView.as_view(
         form_class=RegistrationForm), name='registration_register'),
-    url(r'^accounts/logout/', logout, {'next_page': '/'}),
-    url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    url(
+    re_path(r'^accounts/logout/', logout, {'next_page': '/'}),
+    re_path(r'^accounts/', include('registration.backends.default.urls')),
+    re_path(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    re_path(
         r'^authorized_application/authenticate_user/$',
         authorized_application_authenticate_user
     ),
-    url(r'^browser_tests/$', browser_tests),
-    url(r'^authorized_application/one_time_login/$', one_time_login),
-    url(r'^i18n/', include('django.conf.urls.i18n')),
+    re_path(r'^browser_tests/$', browser_tests),
+    re_path(r'^authorized_application/one_time_login/$', one_time_login),
+    re_path(r'^i18n/', include('django.conf.urls.i18n')),
     # Translation catalog for client code.
-    url(r'^jsi18n/$', JavaScriptCatalog, js_info_dict, name='javascript-catalog'),
+    re_path(r'^jsi18n/$', JavaScriptCatalog, js_info_dict, name='javascript-catalog'),
 
-    url(r'^token/$', TokenView.as_view(), name='token'),
-    url(r'^environment/$', EnvironmentView.as_view(), name='environment'),
-    url(r'^configurationfile/(?P<slug>[^/]+)/?',
-        ConfigurationFile.redirect_view, name='configurationfile'),
-    url(r'^private-media/', include(private_storage.urls)),
+    re_path(r'^token/$', TokenView.as_view(), name='token'),
+    re_path(r'^environment/$', EnvironmentView.as_view(), name='environment'),
+    re_path(r'^configurationfile/(?P<slug>[^/]+)/?',
+            ConfigurationFile.redirect_view, name='configurationfile'),
+    re_path(r'^private-media/', include(private_storage.urls)),
     # Statistics for superusers
-    url(r'^superuser_stats/user_report/$', user_report),
-    url(r'^superuser_stats/user_report/(?P<base_filename>[^/]+)$',
-        retrieve_user_report),
+    re_path(r'^superuser_stats/user_report/$', user_report),
+    re_path(r'^superuser_stats/user_report/(?P<base_filename>[^/]+)$',
+            retrieve_user_report),
 ]
