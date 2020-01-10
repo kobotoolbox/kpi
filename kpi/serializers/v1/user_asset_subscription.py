bd2ac46feaa13b08968e8cae7e1b0baa3c7fd24a
Olivier Leger <olivierleger@gmail.com>
2020-01-10 16:45:17 -0400

    Added UserAssetSubscription to v2

--- kpi/serializers/v1/user_asset_subscription.py
+++ kpi/serializers/v1/user_asset_subscription.py
@@ -1,34 +1,9 @@
 # coding: utf-8
-from rest_framework import serializers
-
-from kpi.constants import PERM_DISCOVER_ASSET, PERM_VIEW_ASSET
-from kpi.fields import RelativePrefixHyperlinkedRelatedField
-from kpi.models import Asset
-from kpi.models import UserAssetSubscription
-from kpi.models.object_permission import get_anonymous_user, get_objects_for_user
+from kpi.serializers.v2.user_asset_subscription import (
+    UserAssetSubscriptionSerializer as UserAssetSubscriptionSerializerV2
+)
 
 
-class UserAssetSubscriptionSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        lookup_field='uid',
-        view_name='userassetsubscription-detail'
-    )
-    asset = RelativePrefixHyperlinkedRelatedField(
-        lookup_field='uid',
-        view_name='asset-detail',
-        queryset=Asset.objects.none() # will be set in __init__()
-    )
-    uid = serializers.ReadOnlyField()
+class UserAssetSubscriptionSerializer(UserAssetSubscriptionSerializerV2):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['asset'].queryset = get_objects_for_user(
-            get_anonymous_user(),
-            [PERM_VIEW_ASSET, PERM_DISCOVER_ASSET],
-            Asset
-        )
-
-    class Meta:
-        model = UserAssetSubscription
-        lookup_field = 'uid'
-        fields = ('url', 'asset', 'uid')
+    pass
