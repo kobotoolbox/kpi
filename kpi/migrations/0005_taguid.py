e6518c2e0f1469a1cc127bfc13a8e60f8cb80d80
John N. Milner <john@tmoj.net>
2020-02-17 12:11:48 -0500

    Remove bytes prefix from migration strings
    See https://code.djangoproject.com/ticket/23226

--- kpi/migrations/0005_taguid.py
+++ kpi/migrations/0005_taguid.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
             name='TagUid',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('uid', models.CharField(default=b'', max_length=22, blank=True)),
+                ('uid', models.CharField(default='', max_length=22, blank=True)),
                 ('tag', models.OneToOneField(to='taggit.Tag', on_delete=models.CASCADE)),
             ],
             options={
