d7eb4c523c75e5fb8bf7e12db54a7017e046fa13
Michał Łazowik <m1@qed.ai>
2020-05-02 04:10:13 +0200

    Improve performance of the reports query
    * annotations insted of distinct(): distinct() had to compare big JSON
      fields (you can pass field names to distinct(), but annotations are a
      more canonical solution here IMO)
    * defer loading of big JSON fields, as the query is also a base for
      the detail view

--- kobo/apps/reports/views.py
+++ kobo/apps/reports/views.py
@@ -1,8 +1,14 @@
 # coding: utf-8
+from django.db.models import Count, Case, When
 from rest_framework import viewsets, mixins
 
+<<<<<<< Updated upstream
 from kpi.constants import PERM_VIEW_SUBMISSIONS, PERM_PARTIAL_SUBMISSIONS
 from kpi.models import Asset
+=======
+
+from kpi.models import AssetVersion, Asset
+>>>>>>> Stashed changes
 from kpi.models.object_permission import get_objects_for_user, get_anonymous_user
 from .serializers import ReportsListSerializer, ReportsDetailSerializer
 
@@ -19,9 +25,18 @@ class ReportsViewSet(mixins.ListModelMixin,
             return ReportsDetailSerializer
 
     def get_queryset(self):
-        
-        # Retrieve all deployed assets first.
-        deployed_assets = Asset.objects.filter(asset_versions__deployed=True).distinct()
+        # Retrieve all deployed assets first. Defer large JSON fields, because this queryset
+        # is a base for the detail view that selects a specific Asset. Without defer()
+        # we might be downloading those fields for Assets that will not be looked at.
+        deployed_assets = Asset.objects.annotate(
+            count_deployed=Count(Case(When(asset_versions__deployed=True, then=1)))
+        ).filter(
+            count_deployed__gt=0
+        ).defer(
+            'content',
+            'report_styles'
+        )
+
         # Then retrieve all assets user is allowed to view
         # (user must have 'view_submissions' on Asset objects)
         required_permissions = [
