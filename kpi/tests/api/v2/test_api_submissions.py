9fa5b43ff6d0f70b7e95d2f2bca26e5b255ef29d
John N. Milner <john@tmoj.net>
2020-06-04 01:05:06 -0400

    Use permission constants instead of strings…
    in some tests

--- kpi/tests/api/v2/test_api_submissions.py
+++ kpi/tests/api/v2/test_api_submissions.py
@@ -210,10 +210,10 @@ class SubmissionApiTests(BaseSubmissionTestCase):
     def test_list_submissions_anonymous_asset_publicly_shared(self):
         self.client.logout()
         anonymous_user = get_anonymous_user()
-        self.asset.assign_perm(anonymous_user, 'view_submissions')
+        self.asset.assign_perm(anonymous_user, PERM_VIEW_SUBMISSIONS)
         response = self.client.get(self.submission_url, {"format": "json"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.asset.remove_perm(anonymous_user, 'view_submissions')
+        self.asset.remove_perm(anonymous_user, PERM_VIEW_SUBMISSIONS)
 
     def test_list_submissions_authenticated_asset_publicly_shared(self):
         # https://github.com/kobotoolbox/kpi/issues/2698
@@ -224,10 +224,10 @@ class SubmissionApiTests(BaseSubmissionTestCase):
         Asset.objects.create(name='i own it', owner=self.anotheruser)
 
         anonymous_user = get_anonymous_user()
-        self.asset.assign_perm(anonymous_user, 'view_submissions')
+        self.asset.assign_perm(anonymous_user, PERM_VIEW_SUBMISSIONS)
         response = self.client.get(self.submission_url, {"format": "json"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.asset.remove_perm(anonymous_user, 'view_submissions')
+        self.asset.remove_perm(anonymous_user, PERM_VIEW_SUBMISSIONS)
 
     def test_retrieve_submission_owner(self):
         submission = self.submissions[0]
@@ -325,7 +325,7 @@ class SubmissionApiTests(BaseSubmissionTestCase):
         # Only owner can delete submissions on `kpi`. `delete_submissions` is
         # a calculated permission and thus, can not be assigned.
         # TODO Review this test when kpi#2282 is released.
-        self.asset.assign_perm(self.anotheruser, 'change_submissions')
+        self.asset.assign_perm(self.anotheruser, PERM_CHANGE_SUBMISSIONS)
         response = self.client.delete(url,
                                       content_type="application/json",
                                       HTTP_ACCEPT="application/json")
