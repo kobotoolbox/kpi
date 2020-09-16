9eba5a261bd1516c48fa12964a6d9cec7b32ed14
Anji Tong <ollejna@gmail.com>
2020-09-16 17:09:07 -0400

    Changed str formatting and quotes

--- kpi/utils/sheet_converter.py
+++ kpi/utils/sheet_converter.py
@@ -12,12 +12,12 @@ def _iswhitespace(string):
     return isinstance(string, str) and len(string.strip()) == 0
 
 def xls_value_to_unicode(value, value_type):
-    """
+    '''
     Take a xls formatted value and try to make a unicode string
     representation.
-    """
+    '''
     if value_type == xlrd.XL_CELL_BOOLEAN:
-        return "TRUE" if value else "FALSE"
+        return 'TRUE' if value else 'FALSE'
     elif value_type == xlrd.XL_CELL_NUMBER:
         # Try to display as an int if possible.
         int_value = int(value)
@@ -29,7 +29,7 @@ def xls_value_to_unicode(value, value_type):
         # Warn that it is better to single quote as a string.
         # error_location = cellFormatString % (ss_row_idx, ss_col_idx)
         # raise Exception(
-        #   "Cannot handle excel formatted date at " + error_location)
+        #   'Cannot handle excel formatted date at ' + error_location)
         datetime_or_time_only = xlrd.xldate_as_tuple(
             value, workbook.datemode)
         if datetime_or_time_only[:3] == (0, 0, 0):
@@ -43,13 +43,13 @@ def xls_value_to_unicode(value, value_type):
         return str(value).replace(chr(160), ' ')
 
 def convert_xls_to_ss_structure(xls_file_object, strip_empty_rows=True):
-    """
+    '''
     The goal: Convert an XLS file object to a CSV string.
 
     This draws on code from `pyxform.xls2json_backends` and `convert_file_to_csv_string`, however
     this works as it is expected (does not add extra sheets or perform misc conversions which are
     a part of `pyxform.xls2json_backends.xls_to_dict`.)
-    """
+    '''
 
     def _escape_newline_chars(cell):
         return re.sub(r'\r', '\\\\r', re.sub(r'\n', '\\\\n', cell))
@@ -65,9 +65,9 @@ def convert_xls_to_ss_structure(xls_file_object, strip_empty_rows=True):
                     value = _escape_newline_chars(value.strip())
                 if (value is not None) and (not _iswhitespace(value)):
                     value = xls_value_to_unicode(value, sheet.cell_type(row, col))
-                if value != "":
+                if value != '':
                     row_empty = False
-                if value == "":
+                if value == '':
                     value = None
                 row_results.append(value)
             if not strip_empty_rows or not row_empty:
@@ -83,14 +83,14 @@ def convert_xls_to_ss_structure(xls_file_object, strip_empty_rows=True):
     return ss_structure
 
 def convert_xls_to_dict(xls_file_object, strip_empty_rows=True):
-    """
+    '''
 
     Change sheets to dict for `library` uploads to bypass pyxform's parsing
     since xls_to_dict does not like forms with `library` sheets.
     https://www.flowdock.com/app/kobotoolbox/kobo/threads/x-uUMo8M2PfHGoC04PEBcDlmKz4
     https://github.com/XLSForm/pyxform/blob/50921a8e48b853565abd48b543558b5e24d6c4f4/pyxform/constants.py#L77-L86
 
-    """
+    '''
     def _sheet_to_dict(sheet):
         result = []
         for row in range(1, sheet.nrows):
@@ -98,7 +98,7 @@ def convert_xls_to_dict(xls_file_object, strip_empty_rows=True):
             for column in range(0, sheet.ncols):
                 # Changing to cell_value function
                 # convert to string, in case it is not string
-                key = "%s" % sheet.cell_value(0, column)
+                key = '%s' % sheet.cell_value(0, column)
                 key = key.strip()
                 value = sheet.cell_value(row, column)
                 # remove whitespace at the beginning and end of value
@@ -116,7 +116,7 @@ def convert_xls_to_dict(xls_file_object, strip_empty_rows=True):
     try:
         workbook = xlrd.open_workbook(file_contents=xls_file_object.read())
     except XLRDError as error:
-        raise PyXFormError("Error reading .xls file: %s" % error)
+        raise PyXFormError('Error reading .xls file: %s' % error)
 
     ss_structure = {}
     for sheet in workbook.sheets():
