596d5cf9d9b83dc66a1cdc4a365501b8f61dc08c
joshuaberetta <joshuaberetta@gmail.com>
2020-11-24 20:33:10 +0000

    adding reference for LazyFile class

--- kpi/utils/lazy_file.py
+++ kpi/utils/lazy_file.py
@@ -2,6 +2,10 @@
 
 
 class LazyFile:
+    """
+    Copied over from https://github.com/kobotoolbox/xls-import/blob/master/post.py#L22-L30
+    to handle xlm file uploads to kobocat from `kobocat_backend.py`
+    """
     def __init__(self, filename, mode):
         self.filename = filename
         self.mode = mode
