d3f6808ec2bdfb979fe36706a12ae894f843e00a
joshuaberetta <joshuaberetta@gmail.com>
2020-12-15 01:45:02 +0000

    Updating `PROTECTED_XML_FIELDS` to remove `start` and `end` fields

--- kpi/deployment_backends/kobocat_backend.py
+++ kpi/deployment_backends/kobocat_backend.py
@@ -51,12 +51,10 @@ class KobocatDeploymentBackend(BaseDeploymentBackend):
     """
 
     PROTECTED_XML_FIELDS = [
-            '__version__',
-            'end',
-            'formhub',
-            'meta',
-            'start',
-            ]
+        '__version__',
+        'formhub',
+        'meta',
+    ]
 
     def bulk_assign_mapped_perms(self):
         """
