68386f90bb0e78bfda2667132c607608eb8f5513
Olivier Leger <olivierleger@gmail.com>
2021-01-27 07:22:51 -0500

    Reorder alphabatically methods in MockBackend class

--- kpi/deployment_backends/mock_backend.py
+++ kpi/deployment_backends/mock_backend.py
@@ -19,11 +19,41 @@ class MockDeploymentBackend(BaseDeploymentBackend):
 
     defines the interface for a deployment backend.
 
-    # TODO. Stop using protected property `_deployment_data`.
     """
 
     def bulk_assign_mapped_perms(self):
         pass
+
+    def bulk_update_submissions(
+        self, request_data: dict, requesting_user_id: int
+    ) -> dict:
+        payload = self.__prepare_bulk_update_payload(request_data)
+        all_submissions = copy.copy(self.asset._deployment_data['submissions'])
+        instance_ids = payload.pop('submission_ids')
+
+        responses = []
+        for submission in all_submissions:
+            if submission['_id'] in instance_ids:
+                _uuid = uuid.uuid4()
+                submission['deprecatedID'] = submission['instanceID']
+                submission['instanceID'] = f'uuid:{_uuid}'
+                for k, v in payload['data'].items():
+                    submission[k] = v
+                responses.append(
+                    {
+                        'uuid': _uuid,
+                        'response': {},
+                    }
+                )
+
+        return self.__prepare_bulk_update_response(responses)
+
+    def calculated_submission_count(self, requesting_user_id, **kwargs):
+        params = self.validate_submission_list_params(requesting_user_id,
+                                                      validate_count=True,
+                                                      **kwargs)
+        instances = self.get_submissions(requesting_user_id, **params)
+        return len(instances)
 
     def connect(self, active=False):
         self.store_data({
@@ -32,24 +62,39 @@ class MockDeploymentBackend(BaseDeploymentBackend):
                 'active': active,
             })
 
-    def redeploy(self, active=None):
+    def delete_submission(self, pk, user):
         """
-        Replace (overwrite) the deployment, keeping the same identifier, and
-        optionally changing whether the deployment is active
+        Deletes submission
+        :param pk: int
+        :param user: User
+        :return: JSON
         """
-        if active is None:
-            active = self.active
-        self.set_active(active)
+        # No need to delete data, just fake it
+        return {
+            "content_type": "application/json",
+            "status": status.HTTP_204_NO_CONTENT,
+        }
 
-    def set_active(self, active):
-        self.store_data({
-            'active': bool(active),
-        })
+    def duplicate_submission(
+        self, requesting_user_id: int, instance_id: int, **kwargs: dict
+    ) -> dict:
+        all_submissions = self.asset._deployment_data['submissions']
+        submission = next(
+            filter(lambda sub: sub['_id'] == instance_id, all_submissions)
+        )
+        next_id = max((sub['_id'] for sub in all_submissions)) + 1
+        updated_time = datetime.now(tz=pytz.UTC).isoformat('T', 'milliseconds')
+        updated_fields = {
+                '_id': next_id,
+                'start': updated_time,
+                'end': updated_time,
+                'instanceID': f'uuid:{uuid.uuid4()}'
+                }
 
-    def set_namespace(self, namespace):
-        self.store_data({
-            'namespace': namespace,
-        })
+        return {**submission, **updated_fields}
+
+    def get_data_download_links(self):
+        return {}
 
     def get_enketo_survey_links(self):
         # `self` is a demo Enketo form, but there's no guarantee it'll be
@@ -62,15 +107,70 @@ class MockDeploymentBackend(BaseDeploymentBackend):
             # 'preview_iframe_url': 'https://enke.to/preview/i/::self',
         }
 
-    @property
-    def submission_list_url(self):
-        # This doesn't really need to be implemented.
-        # We keep it to stay close to `KobocatDeploymentBackend`
-        view_name = 'submission-list'
-        namespace = self.asset.deployment_data.get('namespace', None)
-        if namespace is not None:
-            view_name = '{}:{}'.format(namespace, view_name)
-        return reverse(view_name, kwargs={"parent_lookup_asset": self.asset.uid})
+    def get_submissions(self, requesting_user_id,
+                        format_type=INSTANCE_FORMAT_TYPE_JSON,
+                        instance_ids=[], **kwargs):
+        """
+        Retrieves submissions on `format_type`.
+        It can be filtered on instances ids.
+
+        Args:
+            requesting_user_id (int)
+            format_type (str): INSTANCE_FORMAT_TYPE_JSON|INSTANCE_FORMAT_TYPE_XML
+            instance_ids (list): Instance ids to retrieve
+            kwargs (dict): Filters to pass to MongoDB. See
+                https://docs.mongodb.com/manual/reference/operator/query/
+
+        Returns:
+            (dict|str|`None`): Depending of `format_type`, it can return:
+                - Mongo JSON representation as a dict
+                - Instances' XML as string
+                - `None` if no results
+        """
+
+        submissions = self.asset.deployment_data.get("submissions", [])
+        kwargs['instance_ids'] = instance_ids
+        params = self.validate_submission_list_params(requesting_user_id,
+                                                      format_type=format_type,
+                                                      **kwargs)
+        permission_filters = params['permission_filters']
+
+        if len(instance_ids) > 0:
+            if format_type == INSTANCE_FORMAT_TYPE_XML:
+                instance_ids = [str(instance_id) for instance_id in
+                                instance_ids]
+                # ugly way to find matches, but it avoids to load each xml in memory.
+                pattern = r'<{id_field}>({instance_ids})<\/{id_field}>'.format(
+                    instance_ids='|'.join(instance_ids),
+                    id_field=self.INSTANCE_ID_FIELDNAME
+                )
+                submissions = [submission for submission in submissions
+                               if re.search(pattern, submission)]
+            else:
+                instance_ids = [int(instance_id) for instance_id in
+                                instance_ids]
+                submissions = [submission for submission in submissions
+                               if submission.get(self.INSTANCE_ID_FIELDNAME)
+                               in instance_ids]
+
+        if permission_filters:
+            submitted_by = [k.get('_submitted_by') for k in permission_filters]
+            if format_type == INSTANCE_FORMAT_TYPE_XML:
+                # TODO handle `submitted_by` too.
+                raise NotImplementedError
+            else:
+                submissions = [submission for submission in submissions
+                               if
+                               submission.get('_submitted_by') in submitted_by]
+
+        # Python-only attribute used by `kpi.views.v2.data.DataViewSet.list()`
+        self.current_submissions_count = len(submissions)
+
+        # TODO: support other query parameters?
+        if 'limit' in params:
+            submissions = submissions[:params['limit']]
+
+        return submissions
 
     def get_submission_detail_url(self, submission_pk):
         # This doesn't really need to be implemented.
@@ -105,25 +205,62 @@ class MockDeploymentBackend(BaseDeploymentBackend):
         )
         return url
 
-    def delete_submission(self, pk, user):
-        """
-        Deletes submission
-        :param pk: int
-        :param user: User
-        :return: JSON
-        """
-        # No need to delete data, just fake it
+    def get_validation_status(self, submission_pk, params, user):
+        submission = self.get_submission(submission_pk, user.id,
+                                         INSTANCE_FORMAT_TYPE_JSON)
         return {
-            "content_type": "application/json",
-            "status": status.HTTP_204_NO_CONTENT,
+            "data": submission.get("_validation_status")
         }
 
-    def get_data_download_links(self):
-        return {}
+    def mock_submissions(self, submissions):
+        """
+        Insert dummy submissions into `asset._deployment_data`
+        :param submissions: list
+        """
+        self.store_data({"submissions": submissions})
+        self.asset.save(create_version=False)
 
-    def _submission_count(self):
-        submissions = self.asset.deployment_data.get('submissions', [])
-        return len(submissions)
+    def redeploy(self, active=None):
+        """
+        Replace (overwrite) the deployment, keeping the same identifier, and
+        optionally changing whether the deployment is active
+        """
+        if active is None:
+            active = self.active
+        self.set_active(active)
+
+    def set_active(self, active):
+        self.store_data({
+            'active': bool(active),
+        })
+
+    def set_namespace(self, namespace):
+        self.store_data({
+            'namespace': namespace,
+        })
+
+    def set_has_kpi_hooks(self):
+        """
+        Store results in self.asset.deployment_data
+        """
+        has_active_hooks = self.asset.has_active_hooks
+        self.store_data({
+            "has_kpi_hooks": has_active_hooks,
+        })
+
+    def set_validation_statuses(self, data, user, method):
+        pass
+
+    @property
+    def submission_list_url(self):
+        # This doesn't really need to be implemented.
+        # We keep it to stay close to `KobocatDeploymentBackend`
+        view_name = 'submission-list'
+        namespace = self.asset.deployment_data.get('namespace', None)
+        if namespace is not None:
+            view_name = '{}:{}'.format(namespace, view_name)
+        return reverse(view_name,
+                       kwargs={"parent_lookup_asset": self.asset.uid})
 
     def _mock_submission(self, submission):
         """
@@ -136,105 +273,11 @@ class MockDeploymentBackend(BaseDeploymentBackend):
             'submissions': submissions,
             })
 
-    def mock_submissions(self, submissions):
-        """
-        Insert dummy submissions into `asset._deployment_data`
-        :param submissions: list
-        """
-        self.store_data({"submissions": submissions})
-        self.asset.save(create_version=False)
-
-    def get_submissions(self, requesting_user_id,
-                        format_type=INSTANCE_FORMAT_TYPE_JSON,
-                        instance_ids=[], **kwargs):
-        """
-        Retrieves submissions on `format_type`.
-        It can be filtered on instances ids.
-
-        Args:
-            requesting_user_id (int)
-            format_type (str): INSTANCE_FORMAT_TYPE_JSON|INSTANCE_FORMAT_TYPE_XML
-            instance_ids (list): Instance ids to retrieve
-            kwargs (dict): Filters to pass to MongoDB. See
-                https://docs.mongodb.com/manual/reference/operator/query/
-
-        Returns:
-            (dict|str|`None`): Depending of `format_type`, it can return:
-                - Mongo JSON representation as a dict
-                - Instances' XML as string
-                - `None` if no results
-        """
-
-        submissions = self.asset.deployment_data.get("submissions", [])
-        kwargs['instance_ids'] = instance_ids
-        params = self.validate_submission_list_params(requesting_user_id,
-                                                      format_type=format_type,
-                                                      **kwargs)
-        permission_filters = params['permission_filters']
-
-        if len(instance_ids) > 0:
-            if format_type == INSTANCE_FORMAT_TYPE_XML:
-                instance_ids = [str(instance_id) for instance_id in instance_ids]
-                # ugly way to find matches, but it avoids to load each xml in memory.
-                pattern = r'<{id_field}>({instance_ids})<\/{id_field}>'.format(
-                    instance_ids='|'.join(instance_ids),
-                    id_field=self.INSTANCE_ID_FIELDNAME
-                )
-                submissions = [submission for submission in submissions
-                               if re.search(pattern, submission)]
-            else:
-                instance_ids = [int(instance_id) for instance_id in instance_ids]
-                submissions = [submission for submission in submissions
-                               if submission.get(self.INSTANCE_ID_FIELDNAME)
-                               in instance_ids]
-
-        if permission_filters:
-            submitted_by = [k.get('_submitted_by') for k in permission_filters]
-            if format_type == INSTANCE_FORMAT_TYPE_XML:
-                # TODO handle `submitted_by` too.
-                raise NotImplementedError
-            else:
-                submissions = [submission for submission in submissions
-                               if submission.get('_submitted_by') in submitted_by]
-
-        # Python-only attribute used by `kpi.views.v2.data.DataViewSet.list()`
-        self.current_submissions_count = len(submissions)
-
-        # TODO: support other query parameters?
-        if 'limit' in params:
-            submissions = submissions[:params['limit']]
-
-        return submissions
-
-    def duplicate_submission(
-        self, requesting_user_id: int, instance_id: int, **kwargs: dict
-    ) -> dict:
-        all_submissions = self.asset._deployment_data['submissions']
-        submission = next(
-            filter(lambda sub: sub['_id'] == instance_id, all_submissions)
-        )
-        next_id = max((sub['_id'] for sub in all_submissions)) + 1
-        updated_time = datetime.now(tz=pytz.UTC).isoformat('T', 'milliseconds')
-        updated_fields = {
-                '_id': next_id,
-                'start': updated_time,
-                'end': updated_time,
-                'instanceID': f'uuid:{uuid.uuid4()}'
-                }
-
-        return {**submission, **updated_fields}
-
-    def get_validation_status(self, submission_pk, params, user):
-        submission = self.get_submission(submission_pk, user.id,
-                                         INSTANCE_FORMAT_TYPE_JSON)
-        return {
-            "data": submission.get("_validation_status")
-        }
+    def _submission_count(self):
+        submissions = self.asset.deployment_data.get('submissions', [])
+        return len(submissions)
 
     def set_validation_status(self, submission_pk, data, user, method):
-        pass
-
-    def set_validation_statuses(self, data, user, method):
         pass
 
     @staticmethod
@@ -264,43 +307,3 @@ class MockDeploymentBackend(BaseDeploymentBackend):
                 'results': kc_responses,
             },
         }
-
-    def bulk_update_submissions(
-        self, request_data: dict, requesting_user_id: int
-    ) -> dict:
-        payload = self.__prepare_bulk_update_payload(request_data)
-        all_submissions = copy.copy(self.asset._deployment_data['submissions'])
-        instance_ids = payload.pop('submission_ids')
-
-        responses = []
-        for submission in all_submissions:
-            if submission['_id'] in instance_ids:
-                _uuid = uuid.uuid4()
-                submission['deprecatedID'] = submission['instanceID']
-                submission['instanceID'] = f'uuid:{_uuid}'
-                for k, v in payload['data'].items():
-                    submission[k] = v
-                responses.append(
-                    {
-                        'uuid': _uuid,
-                        'response': {},
-                    }
-                )
-
-        return self.__prepare_bulk_update_response(responses)
-
-    def set_has_kpi_hooks(self):
-        """
-        Store results in self.asset.deployment_data
-        """
-        has_active_hooks = self.asset.has_active_hooks
-        self.store_data({
-            "has_kpi_hooks": has_active_hooks,
-        })
-
-    def calculated_submission_count(self, requesting_user_id, **kwargs):
-        params = self.validate_submission_list_params(requesting_user_id,
-                                                      validate_count=True,
-                                                      **kwargs)
-        instances = self.get_submissions(requesting_user_id, **params)
-        return len(instances)
