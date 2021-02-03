d0668a61f455c76f7f53aa5f81864f3b6d0103d4
joshuaberetta <joshuaberetta@gmail.com>
2021-02-03 22:22:14 +0000

    Remove AssetExportSettingsPermission doc string

--- kpi/permissions.py
+++ kpi/permissions.py
@@ -274,10 +274,6 @@ class SubmissionValidationStatusPermission(SubmissionPermission):
 
 
 class AssetExportSettingsPermission(AssetNestedObjectPermission):
-    """
-    Maps POST requests to the change_model permission instead of DRF's default
-    of add_model
-    """
     perms_map = {
         'GET': ['%(app_label)s.view_submissions'],
         'POST': ['%(app_label)s.manage_asset'],
