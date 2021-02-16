5043a5fa847c45e0c86ad1da1f7c9bc1b8c35699
joshuaberetta <joshuaberetta@gmail.com>
2021-02-16 22:05:59 +0000

    Revert v1 export task changes

--- kpi/views/v1/export_task.py
+++ kpi/views/v1/export_task.py
@@ -1,29 +1,56 @@
 # coding: utf-8
+from django.db.models import TextField
+from django.db.models.functions import Cast
 from rest_framework import exceptions, serializers, status
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 from kpi.models import ExportTask
 from kpi.models.import_export_task import _resolve_url_to_asset
+from kpi.model_utils import remove_string_prefix
 from kpi.serializers import ExportTaskSerializer
 from kpi.tasks import export_in_background
 from kpi.views.no_update_model import NoUpdateModelViewSet
-from kpi.filters import (
-    ExportObjectPermissionsFilter,
-    ExportObjectFilter,
-    ExportObjectOrderingFilter,
-)
 
 
 class ExportTaskViewSet(NoUpdateModelViewSet):
     queryset = ExportTask.objects.all()
     serializer_class = ExportTaskSerializer
     lookup_field = 'uid'
-    filter_backends = (
-        ExportObjectPermissionsFilter,
-        ExportObjectFilter,
-        ExportObjectOrderingFilter,
-    )
+
+    def get_queryset(self, *args, **kwargs):
+        if self.request.user.is_anonymous:
+            return ExportTask.objects.none()
+
+        queryset = ExportTask.objects.filter(
+            user=self.request.user).order_by('date_created')
+
+        # Ultra-basic filtering by:
+        # * source URL or UID if `q=source:[URL|UID]` was provided;
+        # * comma-separated list of `ExportTask` UIDs if
+        #   `q=uid__in:[UID],[UID],...` was provided
+        q = self.request.query_params.get('q', False)
+        if not q:
+            # No filter requested
+            return queryset
+        if q.startswith('source:'):
+            q = remove_string_prefix(q, 'source:')
+            # Crude, but `data__source` is a URL. Cast `data__source` to a
+            # `TextField` to avoid the special behavior of `__contains` for
+            # `JSONField`s. See
+            # https://docs.djangoproject.com/en/2.2/ref/contrib/postgres/fields/#std:fieldlookup-hstorefield.contains
+            queryset = queryset.annotate(
+                source_str=Cast('data__source', output_field=TextField())
+            ).filter(source_str__contains=q)
+        elif q.startswith('uid__in:'):
+            q = remove_string_prefix(q, 'uid__in:')
+            uids = [uid.strip() for uid in q.split(',')]
+            queryset = queryset.filter(uid__in=uids)
+        else:
+            # Filter requested that we don't understand; make it obvious by
+            # returning nothing
+            return ExportTask.objects.none()
+        return queryset
 
     def create(self, request, *args, **kwargs):
         if self.request.user.is_anonymous:
