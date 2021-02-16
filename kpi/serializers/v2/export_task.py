0312e8fda03dd6126fef8bbb76830ba3ea77701f
joshuaberetta <joshuaberetta@gmail.com>
2021-02-16 04:13:18 +0000

    Migrate `/exports` to `/api/v2/exports`

---
+++ kpi/serializers/v2/export_task.py
@@ -0,0 +1,101 @@
+# coding: utf-8
+from django.utils.translation import ugettext as _
+from rest_framework import serializers
+from rest_framework.reverse import reverse
+
+from kpi.constants import (
+    EXPORT_SETTING_SOURCE,
+    REQUIRED_EXPORT_SETTINGS,
+    VALID_EXPORT_SETTINGS,
+)
+from kpi.fields import ReadOnlyJSONField, WritableJSONField
+from kpi.models.import_export_task import _resolve_url_to_asset
+from kpi.models import ExportTask
+
+
+class ExportTaskSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.SerializerMethodField()
+    messages = ReadOnlyJSONField(required=False)
+    data = WritableJSONField()
+
+    class Meta:
+        model = ExportTask
+        fields = (
+            'url',
+            'status',
+            'messages',
+            'uid',
+            'date_created',
+            'last_submission_time',
+            'result',
+            'data',
+        )
+        read_only_fields = (
+            'status',
+            'uid',
+            'last_submission_time',
+            'result',
+        )
+
+    def validate_data(self, data: dict) -> dict:
+        valid_export_settings = VALID_EXPORT_SETTINGS + [EXPORT_SETTING_SOURCE]
+
+        # Complain if no source was specified
+        if EXPORT_SETTING_SOURCE not in data:
+            raise serializers.ValidationError(
+                {EXPORT_SETTING_SOURCE: _('This field is required.')}
+            )
+
+        # Get the source object
+        source = _resolve_url_to_asset(data[EXPORT_SETTING_SOURCE])
+
+        # Complain if it's not deployed
+        if not source.has_deployment:
+            raise serializers.ValidationError(
+                _('The specified asset must be deployed.')
+            )
+
+        for required in REQUIRED_EXPORT_SETTINGS:
+            if required not in data:
+                raise serializers.ValidationError(
+                    _(
+                        '`data` must contain all the following '
+                        'required keys: {}'
+                    ).format(
+                        self.__format_exception_values(
+                            REQUIRED_EXPORT_SETTINGS, 'and'
+                        )
+                    )
+                )
+
+        for key in data:
+            if key not in valid_export_settings:
+                raise serializers.ValidationError(
+                    _(
+                        '`data` can contain only the following '
+                        'valid keys: {}'
+                    ).format(
+                        self.__format_exception_values(
+                            valid_export_settings, 'and'
+                        )
+                    )
+                )
+
+        return data
+
+    def get_url(self, obj) -> str:
+        # TODO: Not sure why this fails if this check is not made... sometimes
+        # `obj` is an OrderedDict which causes `obj.uid` to fail
+        if isinstance(obj, ExportTask):
+            return reverse(
+                'api_v2:exporttask-detail',
+                kwargs={'uid': obj.uid},
+                request=self.context.get('request', None),
+            )
+
+    @staticmethod
+    def __format_exception_values(values: list, sep: str = 'or') -> str:
+        return "{} {} '{}'".format(
+            ', '.join([f"'{v}'" for v in values[:-1]]), sep, values[-1]
+        )
+
---
+++ kpi/views/v2/export_task.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+from rest_framework import exceptions, serializers, status, renderers, viewsets
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from kpi.models import ExportTask
+from kpi.serializers.v2.export_task import ExportTaskSerializer
+from kpi.tasks import export_in_background
+from kpi.views.no_update_model import NoUpdateModelViewSet
+from kpi.filters import (
+    ExportObjectPermissionsFilter,
+    ExportObjectFilter,
+    ExportObjectOrderingFilter,
+)
+from kpi.utils.viewset_mixins import AssetNestedObjectViewsetMixin
+from rest_framework_extensions.mixins import NestedViewSetMixin
+
+
+# TODO: make sure to see what NoUpdateModelViewSet is all about
+class ExportTaskViewSet(NoUpdateModelViewSet):
+    model = ExportTask
+    queryset = ExportTask.objects.all()
+    serializer_class = ExportTaskSerializer
+    lookup_field = 'uid'
+    renderer_classes = (
+        renderers.BrowsableAPIRenderer,
+        renderers.JSONRenderer,
+    )
+    filter_backends = (
+        ExportObjectPermissionsFilter,
+        ExportObjectFilter,
+        ExportObjectOrderingFilter,
+    )
+
+    def create(self, request, *args, **kwargs):
+        if self.request.user.is_anonymous:
+            raise exceptions.NotAuthenticated()
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Create a new export task
+        export_task = ExportTask.objects.create(user=request.user,
+                                                data=serializer.data['data'])
+        # Have Celery run the export in the background
+        export_in_background.delay(export_task_uid=export_task.uid)
+        return Response({
+            'uid': export_task.uid,
+            'url': reverse(
+                'api_v2:exporttask-detail',
+                kwargs={'uid': export_task.uid},
+                request=request),
+            'status': ExportTask.PROCESSING
+        }, status.HTTP_201_CREATED)
+
