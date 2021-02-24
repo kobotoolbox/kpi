0f1621494967895c7d8acd246dc22e3ca4455c01
joshuaberetta <joshuaberetta@gmail.com>
2021-02-24 21:26:09 +0000

    Clean up filters imports

--- kpi/filters.py
+++ kpi/filters.py
@@ -5,16 +5,7 @@ from distutils.util import strtobool
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.core.exceptions import FieldError
-from django.db.models import (
-    Case,
-    Count,
-    F,
-    IntegerField,
-    Q,
-    Value,
-    When,
-    TextField,
-)
+from django.db.models import Case, Count, F, IntegerField, Q, Value, When
 from django.db.models.functions import Cast
 from django.db.models.query import QuerySet
 from rest_framework import filters
