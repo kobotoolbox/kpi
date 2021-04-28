3e494e72162f2e72ce77185ed32be1677092aa4b
Joshua Beretta <joshuaberetta@gmail.com>
2021-04-28 21:00:04 +0000

    Relace `locked` with `lock_all` and `lock_any` in asset summary, change logic

--- kpi/utils/asset_content_analyzer.py
+++ kpi/utils/asset_content_analyzer.py
@@ -19,7 +19,8 @@ class AssetContentAnalyzer:
     def get_summary(self):
         row_count = 0
         geo = False
-        locked = False
+        lock_all = False
+        lock_any = False
         labels = []
         metas = set()
         types = set()
@@ -29,6 +30,7 @@ class AssetContentAnalyzer:
         if not self.survey:
             return {}
 
+        locks = []
         for row in self.survey:
             if isinstance(row, dict):
                 if '$given_name' in row:
@@ -37,6 +39,8 @@ class AssetContentAnalyzer:
                 _label = row.get('label')
                 if _type in GEO_TYPES:
                     geo = True
+
+                locks.append(row.get(KOBO_LOCK_COLUMN, False))
 
                 if not _type or isinstance(_type, dict) or _type.startswith('end'):
                     summary_errors.append(row)
@@ -54,23 +58,29 @@ class AssetContentAnalyzer:
 
         columns = [k for k in keys.keys() if not k.startswith('$')]
 
-        # Display whether the survey or template is locked: if `kobo--lock_all`
-        # is `True` in the "settings" or the column name of
-        # `kobo--locking-profile` is present in the "survey" or the "settings",
-        # `locked` is set to `True`
+        # Display whether the survey or template is full locked or has any
+        # locks: if `kobo--lock_all` is `True` in the "settings" then the asset
+        # is both fully locked (both `lock_all` and `lock_any` are `True`). if
+        # a value for `kobo--locking-profile` is present in the "settings" or
+        # the column name of `kobo--locking-profile` is present in the "survey"
+        # and there is at least one locking profile assigned, then `lock_any`
+        # is set to `True`
+        if self.settings.get(KOBO_LOCK_ALL, False):
+            lock_all = True
         if (
-            self.settings.get(KOBO_LOCK_ALL, False)
-            or KOBO_LOCK_COLUMN in columns
-            or KOBO_LOCK_COLUMN in self.settings
+            lock_all
+            or (KOBO_LOCK_COLUMN in columns and any(locks))
+            or self.settings.get(KOBO_LOCK_COLUMN, False)
         ):
-            locked = True
+            lock_any = True
 
         summary = {
             'row_count': row_count,
             'languages': self.translations,
             'default_translation': self.default_translation,
             'geo': geo,
-            'locked': locked,
+            'lock_all': lock_all,
+            'lock_any': lock_any,
             'labels': labels[0:5],
             'columns': columns,
         }
