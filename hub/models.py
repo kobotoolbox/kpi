4aef6bfcd3ebbeaef7b6452e52adbaa836fd7204
JacquelineMorrissette <jacquelinemariemorrissette@gmail.com>
2021-05-10 22:28:12 -0400

    readded translations to validation

--- hub/models.py
+++ hub/models.py
@@ -8,7 +8,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from django.utils.translation import ugettext_lazy as _
 from markitup.fields import MarkupField
 
 from kpi.models.object_permission import get_anonymous_user
