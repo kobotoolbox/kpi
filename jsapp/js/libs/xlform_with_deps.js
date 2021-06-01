e7ac201718c495c67103cb9130284bdc667eedb1
Anji Tong <ollejna@gmail.com>
2021-06-01 09:02:54 -0400

    move js/ to src/

--- jsapp/js/libs/xlform_with_deps.js
+++
@@ -1,9 +0,0 @@
-var $ = require('jquery');
-window.jQuery = $;
-window.$ = $;
-window._ = require('underscore');
-window.Backbone = require('backbone');
-window.Backbone.$ = $;
-window.BackboneValidation = require('backbone-validation');
-
-module.exports = require('./xlform');
