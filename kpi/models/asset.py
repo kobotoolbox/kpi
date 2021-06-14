7847dd953028d8a8f9e3a5b2bfbcca3aaa158fcb
Joshua Beretta <joshuaberetta@gmail.com>
2021-06-14 18:59:45 +0000

    make requested changes and update formpack hash

--- kpi/models/asset.py
+++ kpi/models/asset.py
@@ -24,7 +24,7 @@ from formpack import FormPack
 from formpack.utils.flatten_content import flatten_content
 from formpack.utils.json_hash import json_hash
 from formpack.utils.kobo_locking import (
-    revert_kobo_lock_structre,
+    revert_kobo_lock_structure,
     strip_kobo_locking_profile,
 )
 from formpack.utils.spreadsheet_content import flatten_to_spreadsheet_content
@@ -243,12 +243,6 @@ class FormpackXLSFormUtils:
             if '$prev' in row:
                 del row['$prev']
 
-    def _revert_kobo_lock_structre(self, content):
-        revert_kobo_lock_structre(content)
-
-    def _strip_kobo_locking_profile(self, content):
-        strip_kobo_locking_profile(content)
-
     def _remove_empty_expressions(self, content):
         remove_empty_expressions_in_place(content)
 
@@ -408,7 +402,7 @@ class XlsExportable:
             self._autoname(content)
             self._populate_fields_with_autofields(content)
             self._strip_kuids(content)
-            self._revert_kobo_lock_structre(content)
+            revert_kobo_lock_structure(content)
         content = OrderedDict(content)
         self._xlsform_structure(content, ordered=True, kobo_specific=kobo_specific_types)
         return content
@@ -714,7 +708,7 @@ class Asset(ObjectPermissionMixin,
         if self.asset_type not in [ASSET_TYPE_SURVEY, ASSET_TYPE_TEMPLATE]:
             # instead of deleting the settings, simply clear them out
             self.content['settings'] = {}
-            self._strip_kobo_locking_profile(self.content)
+            strip_kobo_locking_profile(self.content)
 
         if _title is not None:
             self.name = _title
