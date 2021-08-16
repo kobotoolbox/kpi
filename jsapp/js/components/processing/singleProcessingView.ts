4d081e6b4e185f2f57bce616e40733cbd189b7cb
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-08-16 18:42:36 +0100

    start using actions in single processing view

--- jsapp/js/components/processing/singleProcessingView.ts
+++ jsapp/js/components/processing/singleProcessingView.ts
@@ -1,20 +1,31 @@
 import React from 'react';
 import {RouteComponentProps} from 'react-router';
+import {actions} from 'js/actions';
 
-type ComposedProps = RouteComponentProps<{
+type Props = RouteComponentProps<{
   uid: string,
   questionName: string,
   submissionId: string,
 }, {}>;
 
-export default class SingleProcessingView extends React.Component<ComposedProps, {}> {
+export default class SingleProcessingView extends React.Component<Props, {}> {
+  constructor(props: Props) {
+    super(props);
+  }
+  
   componentDidMount() {
+    actions.submissions.getSubmission.completed.listen(this.onGetSubmissionCompleted.bind(this))
     console.log(
       this.props.params.uid,
       this.props.params.questionName,
       this.props.params.submissionId
     );
   }
+  
+  onGetSubmissionCompleted(response): void {
+    console.log('onGetSubmissionCompleted response', response);
+  }
+  
   render() {
     return 'single processing view';
   }
