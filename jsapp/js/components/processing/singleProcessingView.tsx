d6472991a0da7593bf07cb552a1645dc5dc0f962
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-08-19 01:58:27 +0100

    add bem and loading spinner to singleprocessingview

--- jsapp/js/components/processing/singleProcessingView.tsx
+++ jsapp/js/components/processing/singleProcessingView.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import {RouteComponentProps} from 'react-router';
 import {actions} from 'js/actions';
+import bem from 'js/bem';
+import LoadingSpinner from 'js/components/common/loadingSpinner';
+
+bem.SingleProcessingView = bem.create('single-processing-view', 'section');
+bem.SingleProcessingView__header = bem.SingleProcessingView.__('header', 'header');
 
 /**
  * this.props.params properties
  */
-type Props = RouteComponentProps<{
+type SingleProcessingViewProps = RouteComponentProps<{
   uid: string,
   questionName: string,
   submissionId: string,
 }, {}>;
 
-export default class SingleProcessingView extends React.Component<Props, {}> {
-  constructor(props: Props) {
+type SingleProcessingViewState = {
+  isReady: boolean
+  submissionData: SubmissionResponse | null
+  error: string | null
+}
+
+export default class SingleProcessingView extends React.Component<SingleProcessingViewProps, SingleProcessingViewState> {
+  constructor(props: SingleProcessingViewProps) {
     super(props);
     this.state = {
       isReady: false,
@@ -42,6 +53,9 @@ export default class SingleProcessingView extends React.Component<Props, {}> {
   }
   
   render() {
+    if (!this.state.isReady) {
+      return <LoadingSpinner/>;
+    }
     return 'single processing view';
   }
 }
