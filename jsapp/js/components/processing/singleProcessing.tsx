1852e9b26ba54db20a0c2bafe1354613c144d800
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-08-19 15:24:57 +0100

    initial styles

--- jsapp/js/components/processing/singleProcessing.tsx
+++ jsapp/js/components/processing/singleProcessing.tsx
@@ -3,8 +3,9 @@ import {RouteComponentProps} from 'react-router';
 import {actions} from 'js/actions';
 import bem from 'js/bem';
 import LoadingSpinner from 'js/components/common/loadingSpinner';
+import './singleProcessing.scss';
 
-bem.SingleProcessing = bem.create('single-processing-view', 'section');
+bem.SingleProcessing = bem.create('single-processing', 'section');
 bem.SingleProcessing__top = bem.SingleProcessing.__('top', 'section');
 bem.SingleProcessing__left = bem.SingleProcessing.__('left', 'section');
 bem.SingleProcessing__right = bem.SingleProcessing.__('right', 'section');
@@ -33,42 +34,42 @@ export default class SingleProcessing extends React.Component<SingleProcessingPr
       error: null,
     }
   }
-  
+
   componentDidMount() {
     actions.submissions.getSubmission.completed.listen(this.onGetSubmissionCompleted.bind(this))
     actions.submissions.getSubmission.failed.listen(this.onGetSubmissionFailed.bind(this))
     actions.submissions.getSubmission(this.props.params.uid, this.props.params.submissionId);
   }
-  
+
   onGetSubmissionCompleted(response: SubmissionResponse): void {
     this.setState({
       isReady: true,
       submissionData: response,
     });
   }
-  
+
   onGetSubmissionFailed(response: FailResponse): void {
     this.setState({
       isReady: true,
       error: response.responseJSON?.detail || t('Failed to get submission.'),
     });
   }
-  
+
   render() {
     if (!this.state.isReady) {
       return <LoadingSpinner/>;
     }
-    
+
     return (
       <bem.SingleProcessing>
         <bem.SingleProcessing__top>
           header
         </bem.SingleProcessing__top>
-        
+
         <bem.SingleProcessing__left>
           left
         </bem.SingleProcessing__left>
-        
+
         <bem.SingleProcessing__right>
           right
         </bem.SingleProcessing__right>
