e8297fbfc0131032e4ef87542a53164b9a64a120
JacquelineMorrissette <jackie@kobotoolbox.org>
2021-08-23 12:02:59 -0400

    Usage Endpoint initial commit

---
+++ kpi/serializers/v2/user_usage.py
@@ -0,0 +1,8 @@
+import datetime
+
+from rest_framework import exceptions, mixins, renderers, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
