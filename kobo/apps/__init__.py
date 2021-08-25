742a6b9ea85fae2b6d7c2c0ed138d3b6b336e895
John N. Milner <john@tmoj.net>
2021-08-25 00:18:20 -0400

    Allow setting Celery worker count in Django admin
    When lots of I/O-bound tasks are enqueued, like REST Services pushes to
    slow external servers, it can be helpful to increase the number of
    asynchronous worker processes well beyond the number of CPU cores. This
    allows that without modifying environment variables or having to restart
    server processes for changes to take effect.

--- kobo/apps/__init__.py
+++ kobo/apps/__init__.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import kombu.exceptions
 from django.apps import AppConfig
 from django.core.checks import register, Tags
 
@@ -8,6 +9,20 @@ from kpi.utils.two_database_configuration_checker import \
 
 class KpiConfig(AppConfig):
     name = 'kpi'
+
+    def ready(self, *args, **kwargs):
+        # Once it's okay to read from the database, apply the user-desired
+        # autoscaling configuration for Celery workers
+        from kobo.celery import update_concurrency_from_constance
+        # oh boy oberto
+        try:
+            update_concurrency_from_constance.delay()
+        except kombu.exceptions.OperationalError as e:
+            # It's normal for Django to start without access to a message
+            # broker, e.g. while running `./manage.py collectstatic`
+            # during a Docker image build
+            pass
+        return super().ready(*args, **kwargs)
 
 
 register(TwoDatabaseConfigurationChecker().as_check(), Tags.database)
