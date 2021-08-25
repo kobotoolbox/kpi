985a03d8777c76970c6b5c046a7006ee9d4fa3d9
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-08-25 21:50:15 +0100

    typescriptize permConfig

--- jsapp/js/components/permissions/permConfig.ts
+++ jsapp/js/components/permissions/permConfig.ts
@@ -1,42 +1,21 @@
 import Reflux from 'reflux';
 
-interface PermConfig extends Reflux.Store {
-  setPermissions: (permissions: PermissionDefinition[]) => void
-  getPermissionByCodename: () => PermissionDefinition
-  getPermission: () => PermissionDefinition
-  verifyReady: () => void | never
-  isReady: () => boolean
-}
+class PermConfigStore extends Reflux.Store {
+  permissions: PermissionDefinition[] = []
 
-/**
- * NOTE: this store relies on the app being initialized by calling
- * `actions.permissions.getConfig()` and then manually setting results here,
- * otherwise expect `verifyReady` to throw
- */
-const permConfig = <PermConfig>Reflux.createStore({
-  permissions: [],
-
-  setPermissions(permissions) {
+  public setPermissions(permissions: PermissionDefinition[]) {
     this.permissions = permissions;
-  },
+  }
 
-  /**
-   * @param {string} wantedCodename
-   * @returns {PermDefinition|undefined}
-   */
-  getPermissionByCodename(wantedCodename) {
+  public getPermissionByCodename(wantedCodename: string) {
     this.verifyReady();
     return this.permissions.find((perm) => perm.codename === wantedCodename);
-  },
+  }
 
-  /**
-   * @param {string} wantedUrl
-   * @returns {PermDefinition|undefined}
-   */
-  getPermission(wantedUrl) {
+  public getPermission(wantedUrl: string) {
     this.verifyReady();
     return this.permissions.find((perm) => perm.url === wantedUrl);
-  },
+  }
 
   /**
    * Throws if trying to use permConfig before it fetches data from BE or if
@@ -46,11 +25,18 @@ const permConfig = <PermConfig>Reflux.createStore({
     if (!this.isReady()) {
       throw new Error(t('Permission config is not ready or failed to initialize!'));
     }
-  },
+  }
 
-  isReady() {
+  public isReady() {
     return Array.isArray(this.permissions) && this.permissions.length !== 0;
-  },
-});
+  }
+}
+
+/**
+ * NOTE: this store relies on the app being initialized by calling
+ * `actions.permissions.getConfig()` and then manually setting results here,
+ * otherwise expect `verifyReady` to throw
+ */
+const permConfig = new PermConfigStore();
 
 export default permConfig;
