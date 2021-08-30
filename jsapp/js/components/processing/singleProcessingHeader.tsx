ec08d4659984269d14f850044e0ccc3ea2431b28
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-08-30 17:19:32 +0100

    start building header layout

--- jsapp/js/components/processing/singleProcessingHeader.tsx
+++ jsapp/js/components/processing/singleProcessingHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import bem from 'js/bem';
 import {QuestionTypeName} from 'js/constants';
+import TextBox from 'js/components/common/textBox';
 
 bem.SingleProcessingHeader = bem.create('single-processing-header', 'header');
 
@@ -9,7 +10,7 @@ bem.SingleProcessingHeader = bem.create('single-processing-header', 'header');
  */
 type SingleProcessingHeaderProps = {
   questionType: QuestionTypeName | undefined
-  questionName: string | null
+  questionName: string
   submissionId: string
   submissionsIds: string[]
 }
@@ -18,18 +19,50 @@ type SingleProcessingHeaderProps = {
  * This route component is being loaded with PermProtectedRoute so we know that
  * the call to backend to get asset was already made :happy_face:
  */
-export default class SingleProcessing extends React.Component<SingleProcessingHeaderProps, {}> {
+export default class SingleProcessingHeader extends React.Component<SingleProcessingHeaderProps, {}> {
   constructor(props: SingleProcessingHeaderProps) {
     super(props);
+  }
+
+  onDone(evt: React.MouseEvent<HTMLButtonElement>) {
+    console.log(evt)
+  }
+
+  onSubmissionIndexInputChange(newValue: string) {
+    console.log(newValue);
   }
 
   render() {
     return (
       <bem.SingleProcessingHeader>
-        <div>type: {this.props.questionType}</div>
-        <div>name: {this.props.questionName}</div>
-        <div>id: {this.props.submissionId}</div>
-        <div>{this.props.submissionsIds.indexOf(this.props.submissionId) + 1} of {this.props.submissionsIds.length}</div>
+        <div>icon in a colorful square: {this.props.questionType}</div>
+
+        <div>
+          {this.props.submissionsIds.indexOf(this.props.submissionId) + 1} of {this.props.submissionsIds.length}
+          {t('Q: ##question_name##').replace('##question_name', this.props.questionName)}
+        </div>
+
+        <div>
+          <bem.KoboLightButton>
+            {t('< prev')}
+          </bem.KoboLightButton>
+
+          <TextBox
+            type={'number'}
+            value={this.props.submissionsIds.indexOf(this.props.submissionId) + 1}
+            onChange={this.onSubmissionIndexInputChange.bind(this)}
+          />
+
+          <bem.KoboLightButton>
+            {t('next >')}
+          </bem.KoboLightButton>
+        </div>
+
+        <div>
+          <bem.KoboLightButton m='blue' onClick={this.onDone.bind(this)}>
+            {t('Done')}
+          </bem.KoboLightButton>
+        </div>
       </bem.SingleProcessingHeader>
     )
   }
