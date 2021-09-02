35fcb7c4baa8e921b263a3c4b26e65b600a07748
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-09-02 02:40:17 +0100

    improve makeBem to work with m objects
    plus fix few issues

--- jsapp/js/bem.ts
+++ jsapp/js/bem.ts
@@ -1,16 +1,15 @@
 import React from 'react';
-import assign from 'object-assign';
 
 /**
  * USAGE:
  *
- * There are three exports here:
- * - bem - an object that holds all defined components
- * - makeBem - for creating the block component
+ * There are two exports here:
+ * - bem - an object that holds all defined components (default export)
+ * - makeBem - for creating the block or element component
  */
 
 interface bemInstances {
-  [bemName: string]: BemComponentType
+  [bemName: string]: BemInstance
 }
 
 /**
@@ -20,22 +19,29 @@ const bem: bemInstances = {}
 
 export default bem;
 
-interface BemComponentProps extends React.ComponentProps<any> {
-  m?: string | string[]
+type BemModifiersObject = {
+  [modifierName: string]: boolean
 }
 
-interface BemComponentType extends React.ComponentClass<BemComponentProps, {}> {
+interface BemComponentProps extends React.ComponentProps<any> {
+  m?: string | string[] | BemModifiersObject
+}
+
+interface BemInstance extends React.ComponentClass<BemComponentProps, {}> {
   blockName: string
 }
 
 /**
- * Creates a BEM class for block or element. Please no angle brackets for `htmlTagName`.
+ * Creates a BEM class for block or element.
+ * For first parameter pass `null` to create a Block component,
+ * or pass existing Block component to create Element component (a child).
+ * Please no angle brackets for `htmlTagName`.
  */
 export function makeBem(
-  parent: BemComponentType | null,
+  parent: BemInstance | null,
   name: string,
   htmlTagName: string = 'div'
-): BemComponentType {
+): BemInstance {
   class BemComponent extends React.Component<BemComponentProps, {}> {
     static blockName: string = parent ? parent.blockName : name
 
@@ -44,19 +50,52 @@ export function makeBem(
     }
 
     render() {
-      const classNames = [parent ? `${parent.blockName}__${name}` : name];
+      let classNames: string[] = [];
 
+      // Keep existing className attribute if given (either string or array)
+      if (typeof this.props.className === 'string') {
+        classNames.push(this.props.className);
+      } else if (Array.isArray(this.props.className)) {
+        classNames.push(this.props.className.join(' '));
+      }
+
+      // wholeName includes parent, e.g. `parent-block__child-element`
+      const wholeName = parent ? `${parent.blockName}__${name}` : name
+      classNames.push(wholeName);
+
+      // modifiers could be a single string or array of strings or object with booleans
       if (typeof this.props.m === 'string' && this.props.m.length >= 1) {
-        classNames.push(`${name}--${this.props.m}`);
+        // Case 1: string
+        classNames.push(`${wholeName}--${this.props.m}`);
       } else if (Array.isArray(this.props.m)) {
+        // Case 2: array
         this.props.m.forEach((modifier) => {
-          classNames.push(`${name}--${modifier}`);
+          if (typeof modifier === 'string' && modifier.length >= 1) {
+            classNames.push(`${wholeName}--${modifier}`);
+          }
+        });
+      } else if (typeof this.props.m === 'object') {
+        // Case 3: object
+        Object.entries(this.props.m).forEach((entry) => {
+          if (entry[1] === true) {
+            classNames.push(`${wholeName}--${entry[0]}`);
+          }
         });
       }
 
-      const props = assign({className: classNames.join(' ')}, this.props);
+      const newProps: {[propName: string]: any} = {
+        className: classNames.join(' ')
+      }
 
-      return React.createElement(htmlTagName, props);
+      // Keep all the original props expect for modifiers (don't need it) and
+      // className (we use our own).
+      Object.entries(this.props).forEach((propEntry) => {
+        if (propEntry[0] !== 'm' && propEntry[0] !== 'className') {
+          newProps[propEntry[0]] = propEntry[1];
+        }
+      });
+
+      return React.createElement(htmlTagName, newProps);
     }
   }
 
