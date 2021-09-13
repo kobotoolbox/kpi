c0ac7c2e7cc7db1e06631ca829fdb4a37b66f911
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-09-13 15:28:44 +0100

    render list of questions

--- jsapp/js/components/submissions/submissionDataList.tsx
+++ jsapp/js/components/submissions/submissionDataList.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import bem, {makeBem} from 'js/bem';
+import {
+  getFlatQuestionsList,
+  FlatQuestion
+} from 'js/assetUtils'
+import {getRowData} from 'js/components/submissions/submissionUtils'
 import './submissionDataList.scss';
 
 bem.SubmissionDataList = makeBem(null, 'submission-data-list', 'ul')
+bem.SubmissionDataListQuestion = makeBem(null, 'submission-data-list-question', 'li')
+bem.SubmissionDataListQuestion__path = makeBem(bem.SubmissionDataListQuestion, 'path')
+bem.SubmissionDataListQuestion__label = makeBem(bem.SubmissionDataListQuestion, 'label', 'h3')
+bem.SubmissionDataListQuestion__response = makeBem(bem.SubmissionDataListQuestion, 'response')
 
 type SubmissionDataListProps = {
   assetContent: AssetContent
   submissionData: SubmissionResponse
-  /** Whether to hide meta type question from the rendered list. */
-  hideMeta?: boolean
+  /** Whether to include meta type questions in the rendered list. */
+  includeMeta?: boolean
 }
 
 type SubmissionDataListState = {}
@@ -22,10 +31,46 @@ export default class SubmissionDataList extends React.Component<
     this.state = {}
   }
 
+  renderQuestion(question: FlatQuestion) {
+    const response = getRowData(
+      question.name,
+      this.props.assetContent.survey || [],
+      this.props.submissionData
+    )
+
+    return (
+      <bem.SubmissionDataListQuestion key={question.name}>
+        {question.parents.length >= 1 &&
+          <bem.SubmissionDataListQuestion__path>
+            {question.parents.join(' / ')}
+          </bem.SubmissionDataListQuestion__path>
+        }
+
+        <bem.SubmissionDataListQuestion__label>
+          {question.label}
+        </bem.SubmissionDataListQuestion__label>
+
+        <bem.SubmissionDataListQuestion__response>
+          {response ? response : t('N/A')}
+        </bem.SubmissionDataListQuestion__response>
+      </bem.SubmissionDataListQuestion>
+    )
+  }
+
   render() {
+    if (!this.props.assetContent.survey) {
+      return null
+    }
+
+    const items = getFlatQuestionsList(
+      this.props.assetContent.survey,
+      0,
+      Boolean(this.props.includeMeta)
+    );
+
     return (
       <bem.SubmissionDataList>
-        submission data list hi!
+        {items.map(this.renderQuestion.bind(this))}
       </bem.SubmissionDataList>
     )
   }
