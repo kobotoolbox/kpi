3afee353a4661bdc2073b2aa299a2fd4149f578a
JacquelineMorrissette <jackie@kobotoolbox.org>
2021-09-26 13:28:27 -0400

    Created testcases

--- kpi/tests/api/v2/test_api_draft_nlp.py
+++ kpi/tests/api/v2/test_api_draft_nlp.py
@@ -1,3 +1,5 @@
+import json
+
 from django.urls import reverse
 from rest_framework import status
 
@@ -15,8 +17,10 @@ class DraftNLPTestCase(BaseAssetTestCase):
 
     def test_api_create_draft_nlp(self):
         """
-        Tests GET, POST, PATCH, and DELETE
+        Tests POST, PATCH, and DELETE
         """
+
+        # Create asset
         data = {
             'content': '{}',
             'asset_type': 'survey',
@@ -24,12 +28,85 @@ class DraftNLPTestCase(BaseAssetTestCase):
         list_url = reverse(self._get_endpoint('asset-list'))
         response = self.client.post(list_url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Create draft_nlp
         uid = response.data.get('uid')
         data_draft_nlp = {
             'content': '{}',
             'draft_nlp_type': 'transcript',
             'question_path': 'path/to/question',
-            'submission_id': '201',
+            'submission_id': 201,
+        }
+        draft_nlp_args = {
+            'parent_lookup_asset': uid,
+            'parent_lookup_data': 201,
+        }
+        draft_nlp_url_list = reverse(
+            self._get_endpoint('data-nlp-list'),
+            kwargs=draft_nlp_args
+        )
+        draft_nlp_response = self.client.post(
+            draft_nlp_url_list,
+            data_draft_nlp
+        )
+        draft_uid = draft_nlp_response.data.get('uid')
+        draft_nlp_detail_args = {
+            'parent_lookup_asset': uid,
+            'parent_lookup_data': 201,
+            'uid': draft_uid,
+        }
+        self.assertEqual(draft_nlp_response.status_code, status.HTTP_201_CREATED)
+
+        # Update draft_nlp
+        draft_nlp_url_detail = reverse(
+            self._get_endpoint('data-nlp-detail'),
+            kwargs=draft_nlp_detail_args
+        )
+        data_draft_nlp_update = {
+            'content': '{"content": "test content"}',
+            'draft_nlp_type': 'transcript',
+            'question_path': 'path/to/question',
+            'submission_id': 201,
+        }
+        draft_nlp_update_response = self.client.put(
+            draft_nlp_url_detail,
+            data_draft_nlp_update
+        )
+        self.assertEqual(
+            draft_nlp_update_response.status_code,
+            status.HTTP_200_OK
+        )
+        draft_nlp_get_response = self.client.get(draft_nlp_url_detail)
+        self.assertEqual(
+            json.dumps(data_draft_nlp_update.get('content')),
+            draft_nlp_get_response.data.get('content')
+        )
+
+        # delete draft_nlp
+        draft_nlp_delete = self.client.delete(draft_nlp_url_detail)
+        self.assertEqual(draft_nlp_delete.status_code, status.HTTP_200_OK)
+
+        # Confirm draft_nlp is deleted
+        draft_nlp_delete = self.client.delete(draft_nlp_url_detail)
+        self.assertEqual(draft_nlp_delete.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_api_db_contents(self):
+        # Create asset
+        data = {
+            'content': '{}',
+            'asset_type': 'survey',
+        }
+        list_url = reverse(self._get_endpoint('asset-list'))
+        response = self.client.post(list_url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # create draft_nlp
+        uid = response.data.get('uid')
+        data_draft_nlp = {
+            'content': '{"language_code": "xh", "language": "Xhosa", "content": "Le yimixholo eguqulweyo kaGoogle"}',
+            'draft_nlp_type': 'transcript',
+            'question_path': 'path/to/question',
+            'submission_id': 201,
         }
         draft_nlp_args = {
             'parent_lookup_asset': uid,
@@ -43,30 +120,15 @@ class DraftNLPTestCase(BaseAssetTestCase):
             'parent_lookup_data': 201,
             'uid': draft_uid,
         }
-        draft_nlp_url_detail = reverse(self._get_endpoint('data-nlp-detail'), kwargs=draft_nlp_detail_args)
         self.assertEqual(draft_nlp_response.status_code, status.HTTP_201_CREATED)
-        data_draft_nlp_update = {
-            'content': '{"content": "test content"}',
-            'draft_nlp_type': 'transcript',
-            'question_path': 'path/to/question',
-            'submission_id': '201',
-        }
-        draft_nlp_update_response = self.client.put(draft_nlp_url_detail, data_draft_nlp_update)
-        self.assertEqual(draft_nlp_update_response.status_code, status.HTTP_200_OK)
-        draft_nlp_delete = self.client.delete(draft_nlp_url_detail)
-        self.assertEqual(draft_nlp_delete.status_code, status.HTTP_200_OK)
-        draft_nlp_delete = self.client.delete(draft_nlp_url_detail)
-        self.assertEqual(draft_nlp_delete.status_code, status.HTTP_404_NOT_FOUND)
+        draft_nlp_url_detail = reverse(self._get_endpoint('data-nlp-detail'), kwargs=draft_nlp_detail_args)
+        draft_nlp_response_detail = self.client.get(draft_nlp_url_detail)
+        self.assertEqual(draft_nlp_response_detail.status_code, status.HTTP_200_OK)
 
-
-    def test_api_patch_draft_nlp(self):
-        pass
-
-    def test_api_delete_draft_nlp(self):
-        pass
-
-    def test_api_list_view_draft_nlp(self):
-        pass
-
-    def test_api_detail_view_draft_nlp(self):
-        pass
+        # Check contents match database
+        draft_nlp_object = DraftNLPModel.objects.get(uid=draft_uid)
+        self.assertTrue(draft_nlp_object)
+        self.assertJSONEqual(json.dumps(draft_nlp_object.content), data_draft_nlp.get('content'))
+        self.assertEqual(draft_nlp_object.draft_nlp_type, data_draft_nlp.get('draft_nlp_type'))
+        self.assertEqual(draft_nlp_object.question_path, data_draft_nlp.get('question_path'))
+        self.assertEqual(draft_nlp_object.submission_id, data_draft_nlp.get('submission_id'))
