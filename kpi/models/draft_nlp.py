3efb1172b97c69cd2ac705c2eb7d3bb9052e7cdb
John N. Milner <john@tmoj.net>
2021-09-27 16:40:09 -0400

    Round 2 of draft NLP model(s!)

--- kpi/models/draft_nlp.py
+++ kpi/models/draft_nlp.py
@@ -6,62 +6,58 @@ from django.utils import timezone
 from kpi.fields import KpiUidField
 
 
-# FIXME: Rename!
-class DraftNLPModel(models.Model):
-    TRANSCRIPT = 'transcript'
-    TRANSLATION = 'translation'
+class AnalysisQuestions(models.Model):
+    # Is it best to have all analysis questions together, like a mini-form
+    # that's attached to a particular question in the main form (asset)?
 
-    ### Future ###
-
-    # All coding questions together, like a mini-form that's attached to a
-    # particular question in the main form (asset)
-    # CODING_QUESTIONS = 'coding_questions'
-
-    # Do we want all the responses together in one object, like a
-    # mini-submission to the coding mini-form?
-    # CODING_RESPONSES = 'coding_responses'
-
-    TYPE_CHOICES = (
-        (TRANSCRIPT, TRANSCRIPT),
-        (TRANSLATION, TRANSLATION),
-        # Future
-        # (CODING_QUESTIONS, CODING_QUESTIONS),
-        # (CODING_RESPONSES, CODING_RESPONSES),
-    )
-
+    # TODO: specify JSON structure. Could be something like:
+    """
+    [
+        {
+            "uid": "aq12345", /* kuid? */
+            "question_path": "weather_group/What_is_the_weather_like",
+            "label": "Transcription", /* maybe ignored for standard types like transcription and translation? */
+            "type": "transcription", /* only a few of these */
+            "language": "en"
+        },
+        {
+            "uid": "aq12346",
+            "question_path": "weather_group/What_is_the_weather_like",
+            "label": "Spanish Translation",
+            "type": "translation",
+            "language": "es"
+        }
+    ]
+    """
     content = JSONBField(default=dict)
     date_created = models.DateTimeField(default=timezone.now)
     date_modified = models.DateTimeField(default=timezone.now)
-    draft_nlp_type = models.CharField(choices=TYPE_CHOICES, max_length=32)
-    uid = KpiUidField(uid_prefix='draft_nlp')
+    uid = KpiUidField(uid_prefix='aq')
+    asset = models.ForeignKey('Asset', on_delete=models.CASCADE)
 
-    asset = models.ForeignKey(
-        'Asset', related_name='draft_nlp_instances', on_delete=models.CASCADE
-    )
-    parent = models.ForeignKey(
-        'DraftNLPModel',
-        related_name='children',
-        null=True,
-        blank=True,
-        on_delete=models.CASCADE,
-    )
-    question_path = models.CharField(max_length=2048, blank=True, default='')
+    def save(self, *args, **kwargs):
+        if self.pk is not None:
+            self.date_modified = timezone.now()
+        super().save(*args, **kwargs)
+
+
+class AnalysisResponses(models.Model):
+    # Do we want all the responses together in one object, like a
+    # mini-submission to the analysis mini-form?
+
+    # TODO: specify JSON structure. Could be something like:
+    """
+    {
+        "aq12345": "It's cool today and starting to feel like fall. The sky is clear with few clouds.",
+        "aq12346": "Empieza a parecer otoño…"
+    }
+    """
+    content = JSONBField(default=dict)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(default=timezone.now)
+    uid = KpiUidField(uid_prefix='ar')
+    asset = models.ForeignKey('Asset', on_delete=models.CASCADE)
     submission_id = models.BigIntegerField(null=True, blank=True)
-
-    """
-    Relation examples:
-    * If I'm a transcript, I'm related to an asset, a question_path, and a
-        submission_id.
-    * If I'm a translation, I'm related to an asset and a transcript (where
-        transcript is an instance of the same model, stored in `parent`). If
-        denormalization is helpful, I could also store my question_path and
-        submission_id.
-    * If I'm a set of coding questions, I'm related to an asset and a
-        question_path.
-    * If I'm a set of coding responses, I'm related to an asset, a set of
-        coding questions (`parent`), and a submission_id, plus further
-        denormalization if desired.
-    """
 
     def save(self, *args, **kwargs):
         if self.pk is not None:
