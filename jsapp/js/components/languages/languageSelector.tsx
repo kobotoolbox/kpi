f8647b55bb2b7c32a39a910c8dc2fa5915ced7d1
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-10-20 15:18:56 +0100

    prepare bem elements

--- jsapp/js/components/languages/languageSelector.tsx
+++ jsapp/js/components/languages/languageSelector.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import bem from 'js/bem'
+import bem, {makeBem} from 'js/bem'
 import TextBox from 'js/components/common/textBox'
 import './languageSelector.scss'
 
+bem.LanguageSelector = makeBem(null, 'language-selector', 'section')
+bem.LanguageSelector__title = makeBem(bem.LanguageSelector, 'title', 'h1')
+bem.LanguageSelector__searchBox = makeBem(bem.LanguageSelector, 'search-box')
+bem.LanguageSelector__list = makeBem(bem.LanguageSelector, 'list', 'ol')
+bem.LanguageSelector__language = makeBem(bem.LanguageSelector, 'language', 'button')
+
 type LanguageSelectorProps = {
+  /** replaces the title on top */
+  titleOverride?: string
+  /** jumpstarts the selector with a pre-selected language */
   preselectedValue?: string
+  /** triggered after the final language selection */
   onLanguageChange: Function
 }
 
