d933fb98f3fc1fe45a754ec0c7343def8ff0b213
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-11-04 02:12:26 +0000

    create button configurator

---
+++ jsapp/js/dev/buttonDemo.tsx
@@ -0,0 +1,255 @@
+import React, {ReactElement} from 'react'
+import Select from 'react-select'
+import bem from 'js/bem';
+import Checkbox from 'js/components/common/checkbox'
+import TextBox from 'js/components/common/textBox'
+import Button, {ButtonType, ButtonColor, ButtonSize} from 'js/components/common/button'
+import {IconNames} from 'jsapp/fonts/k-icons'
+
+const buttonTypes: ButtonType[] = ['bare', 'frame', 'full']
+const buttonColors: ButtonColor[] = ['blue', 'teal', 'green', 'red', 'orange', 'gray']
+const buttonSizes: ButtonSize[] = ['s', 'm', 'l']
+
+const iconNamesOptions: IconNameOption[] = []
+for (let iconName in IconNames) {
+  // Hacking TypeScript: https://stackoverflow.com/a/51281023/2311247
+  const typedIconName: IconNames = IconNames[iconName as keyof typeof IconNames]
+  iconNamesOptions.push({
+    label: typedIconName,
+    value: typedIconName
+  })
+}
+
+type ButtonDemoState = {
+  currentType: ButtonType
+  currentColor: ButtonColor
+  currentSize: ButtonSize
+  currentStartIcon: IconNameOption | null
+  currentEndIcon: IconNameOption | null
+  currentLabel: string
+  currentTooltip: string
+  isDisabled: boolean
+  isPending: boolean
+  isFullWidth: boolean
+}
+
+type SelectTypeOption = {
+  value: ButtonType,
+  label: ButtonType
+}
+
+type SelectColorOption = {
+  value: ButtonColor,
+  label: ButtonColor
+}
+
+type SelectSizeOption = {
+  value: ButtonSize,
+  label: ButtonSize
+}
+
+type IconNameOption = {
+  value: IconNames,
+  label: IconNames
+}
+
+const defaultLabel = 'click me'
+const defaultIcon: IconNameOption = {
+  label: IconNames.trash,
+  value: IconNames.trash
+}
+
+export default class DesignSystemRoute extends React.Component<{}, ButtonDemoState> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      currentType: buttonTypes[0],
+      currentColor: buttonColors[0],
+      currentSize: buttonSizes[0],
+      currentStartIcon: null,
+      currentEndIcon: null,
+      currentLabel: defaultLabel,
+      currentTooltip: '',
+      isDisabled: false,
+      isPending: false,
+      isFullWidth: false,
+    }
+  }
+
+  onTypeChange(newType: SelectTypeOption | null) {
+    this.setState({
+      currentType: newType === null ? buttonTypes[0] : newType.value
+    })
+  }
+
+  onColorChange(newColor: SelectColorOption | null) {
+    this.setState({
+      currentColor: newColor === null ? buttonColors[0] : newColor.value
+    })
+  }
+
+  onSizeChange(newSize: SelectSizeOption | null) {
+    this.setState({
+      currentSize: newSize === null ? buttonSizes[0] : newSize.value
+    })
+  }
+
+  onStartIconChange(newStartIcon: IconNameOption | null) {
+    this.setState({
+      currentStartIcon: newStartIcon ? newStartIcon : null,
+      // Only one of icons is allowed.
+      currentEndIcon: newStartIcon ? null : this.state.currentEndIcon
+    })
+  }
+
+  onEndIconChange(newEndIcon: IconNameOption | null) {
+    this.setState({
+      currentEndIcon: newEndIcon ? newEndIcon : null,
+      // Only one of icons is allowed.
+      currentStartIcon: newEndIcon ? null : this.state.currentStartIcon
+    })
+  }
+
+  onLabelChange(newLabel: string) {
+    this.setState({
+      currentLabel: newLabel,
+      // If there is no label, icon is required
+      currentStartIcon: newLabel === '' ? defaultIcon : this.state.currentStartIcon
+    })
+  }
+
+  onTooltipChange(newTooltip: string) {
+    this.setState({currentTooltip: newTooltip})
+  }
+
+  onIsDisabledChange(isChecked: boolean) {
+    this.setState({isDisabled: isChecked})
+  }
+
+  onIsPendingChange(isChecked: boolean) {
+    this.setState({isPending: isChecked})
+  }
+
+  onIsFullWidthChange(isChecked: boolean) {
+    this.setState({isFullWidth: isChecked})
+  }
+
+  render() {
+    return (
+      <section>
+        <bem.SimpleTable>
+          <bem.SimpleTable__header>
+            <bem.SimpleTable__row>
+              <bem.SimpleTable__cell>configuration</bem.SimpleTable__cell>
+              <bem.SimpleTable__cell>live view</bem.SimpleTable__cell>
+            </bem.SimpleTable__row>
+          </bem.SimpleTable__header>
+          <bem.SimpleTable__body>
+            <bem.SimpleTable__row>
+              <bem.SimpleTable__cell>
+                <form>
+                  <Select
+                    className='kobo-select'
+                    classNamePrefix='kobo-select'
+                    label='type'
+                    value={{
+                      value: this.state.currentType,
+                      label: this.state.currentType
+                    }}
+                    isClearable={false}
+                    options={buttonTypes.map(
+                      (type: ButtonType) => {return {value: type, label: type}}
+                    )}
+                    onChange={this.onTypeChange.bind(this)}
+                  />
+                  <Select
+                    className='kobo-select'
+                    classNamePrefix='kobo-select'
+                    label='color'
+                    value={{
+                      value: this.state.currentColor,
+                      label: this.state.currentColor
+                    }}
+                    isClearable={false}
+                    options={buttonColors.map(
+                      (type: ButtonColor) => {return {value: type, label: type}}
+                    )}
+                    onChange={this.onColorChange.bind(this)}
+                  />
+                  <Select
+                    className='kobo-select'
+                    classNamePrefix='kobo-select'
+                    label='size'
+                    value={{
+                      value: this.state.currentSize,
+                      label: this.state.currentSize
+                    }}
+                    isClearable={false}
+                    options={buttonSizes.map(
+                      (type: ButtonSize) => {return {value: type, label: type}}
+                    )}
+                    onChange={this.onSizeChange.bind(this)}
+                  />
+                  <Select
+                    label='start icon'
+                    value={this.state.currentStartIcon}
+                    isClearable={true}
+                    options={iconNamesOptions}
+                    onChange={this.onStartIconChange.bind(this)}
+                  />
+                  <Select
+                    label='end icon'
+                    value={this.state.currentEndIcon}
+                    isClearable={true}
+                    options={iconNamesOptions}
+                    onChange={this.onEndIconChange.bind(this)}
+                  />
+                  <TextBox
+                    label='text'
+                    onChange={this.onLabelChange.bind(this)}
+                    value={this.state.currentLabel}
+                  />
+                  <TextBox
+                    label='tooltip'
+                    onChange={this.onTooltipChange.bind(this)}
+                    value={this.state.currentTooltip}
+                  />
+                  <Checkbox
+                    label='is disabled'
+                    onChange={this.onIsDisabledChange.bind(this)}
+                    checked={this.state.isDisabled}
+                  />
+                  <Checkbox
+                    label='is pending'
+                    onChange={this.onIsPendingChange.bind(this)}
+                    checked={this.state.isPending}
+                  />
+                  <Checkbox
+                    label='is full width'
+                    onChange={this.onIsFullWidthChange.bind(this)}
+                    checked={this.state.isFullWidth}
+                  />
+                </form>
+              </bem.SimpleTable__cell>
+              <bem.SimpleTable__cell>
+                <Button
+                  type={this.state.currentType}
+                  color={this.state.currentColor}
+                  size={this.state.currentSize}
+                  startIcon={this.state.currentStartIcon?.value}
+                  endIcon={this.state.currentEndIcon?.value}
+                  label={this.state.currentLabel}
+                  tooltip={this.state.currentTooltip}
+                  isDisabled={this.state.isDisabled}
+                  isPending={this.state.isPending}
+                  isFullWidth={this.state.isFullWidth}
+                  onClick={() => {alert('Clicked!')}}
+                />
+              </bem.SimpleTable__cell>
+            </bem.SimpleTable__row>
+          </bem.SimpleTable__body>
+        </bem.SimpleTable>
+      </section>
+    )
+  }
+}
--- jsapp/js/dev/designSystemRoute.tsx
+++ jsapp/js/dev/designSystemRoute.tsx
@@ -1,9 +1,5 @@
-import React, {ReactElement} from 'react'
-import Button, {ButtonType, ButtonColor, ButtonSize} from 'js/components/common/button'
-
-const buttonTypes: ButtonType[] = ['bare', 'frame', 'full']
-const buttonColors: ButtonColor[] = ['blue', 'teal', 'green', 'red', 'orange', 'gray']
-const buttonSizes: ButtonSize[] = ['s', 'm', 'l']
+import React from 'react'
+import ButtonDemo from 'js/dev/buttonDemo'
 
 /**
  * This is a route for displaying our design system. It is meant as both
@@ -13,88 +9,12 @@ const buttonSizes: ButtonSize[] = ['s', 'm', 'l']
  * - don't use `t()` ever, just english strings
  */
 export default class DesignSystemRoute extends React.Component<{}> {
-  onAnyClick() {
-    alert('Button clicked!')
-  }
-
-  renderAllButtons() {
-    const buttonNodes: ReactElement[] = []
-
-    buttonTypes.forEach((buttonType) => {
-      buttonNodes.push(<h2>Type: {buttonType}</h2>)
-      buttonColors.forEach((buttonColor) => {
-        buttonSizes.forEach((buttonSize) => {
-          buttonNodes.push((
-            <Button
-              type={buttonType}
-              color={buttonColor}
-              size={buttonSize}
-              onClick={this.onAnyClick.bind(this)}
-              label={`I'm ${buttonType} ${buttonColor} ${buttonSize}`}
-            />
-          ))
-          buttonNodes.push((
-            <Button
-              type={buttonType}
-              color={buttonColor}
-              size={buttonSize}
-              isDisabled={true}
-              onClick={this.onAnyClick.bind(this)}
-              label={`I'm ${buttonType} ${buttonColor} ${buttonSize} disabled`}
-            />
-          ))
-          buttonNodes.push((
-            <Button
-              type={buttonType}
-              color={buttonColor}
-              size={buttonSize}
-              isPending={true}
-              onClick={this.onAnyClick.bind(this)}
-              label={`I'm ${buttonType} ${buttonColor} ${buttonSize} pending`}
-            />
-          ))
-          buttonNodes.push((
-            <Button
-              type={buttonType}
-              color={buttonColor}
-              size={buttonSize}
-              onClick={this.onAnyClick.bind(this)}
-              startIcon='trash'
-            />
-          ))
-          buttonNodes.push((
-            <Button
-              type={buttonType}
-              color={buttonColor}
-              size={buttonSize}
-              onClick={this.onAnyClick.bind(this)}
-              startIcon='trash'
-              label={`I'm ${buttonType} ${buttonColor} ${buttonSize} with start icon`}
-            />
-          ))
-          buttonNodes.push((
-            <Button
-              type={buttonType}
-              color={buttonColor}
-              size={buttonSize}
-              onClick={this.onAnyClick.bind(this)}
-              endIcon='trash'
-              label={`I'm ${buttonType} ${buttonColor} ${buttonSize} with end icon`}
-            />
-          ))
-        })
-      })
-    })
-
-    return buttonNodes
-  }
-
   render() {
     return (
       <section className='form-view form-view--fullscreen'>
         <div className='form-view__cell form-view__cell--padding'>
           <h1><code>Button</code> component</h1>
-          {this.renderAllButtons()}
+          <ButtonDemo/>
         </div>
       </section>
     )
