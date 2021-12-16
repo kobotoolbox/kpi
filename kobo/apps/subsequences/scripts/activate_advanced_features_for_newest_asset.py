b2cd8cc02f6960ade60d243956b9ea529e3423c6
Alex Dorey <dorey415@gmail.com>
2021-12-16 13:37:01 -0500

    activates most recent asset

--- kobo/apps/subsequences/scripts/activate_advanced_features_for_newest_asset.py
+++ kobo/apps/subsequences/scripts/activate_advanced_features_for_newest_asset.py
@@ -3,8 +3,7 @@ from pprint import pprint
 from kpi.models import Asset
 
 def run():
-    # Asset.objects.order_by('+date_created').first()
-    asset = Asset.objects.last()
+    asset = Asset.objects.order_by('date_created').last()
     asset.advanced_features = {'transcript': {}}
     asset.save()
     pprint(asset.get_advanced_submission_schema())
