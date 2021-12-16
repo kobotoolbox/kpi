e4ca85c720e8fd05dd8ddd6c4310c2e0a897bbe3
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-12-16 18:41:27 +0100

    work on options

--- jsapp/js/components/common/koboSelect.tsx
+++ jsapp/js/components/common/koboSelect.tsx
@@ -9,6 +9,7 @@ import './koboSelect.scss'
 // We can't use "kobo-select" as it is already being used for custom styling of `react-select`.
 bem.KoboSelect = makeBem(null, 'k-select')
 bem.KoboSelect__trigger = makeBem(bem.KoboSelect, 'trigger')
+bem.KoboSelect__clear = makeBem(bem.KoboSelect, 'clear')
 bem.KoboSelect__menu = makeBem(bem.KoboSelect, 'menu', 'menu')
 bem.KoboSelect__option = makeBem(bem.KoboSelect, 'option', 'button')
 
@@ -69,10 +70,48 @@ class KoboSelect extends React.Component<KoboSelectProps, {}> {
     super(props)
   }
 
+  /** Please make sure to pass the `selectedOption` prop back here. */
+  onOptionClick(newSelectedOption: string) {
+    this.props.onChange(newSelectedOption)
+  }
+
   renderTrigger() {
+    const foundSelectedOption = this.props.options.find((option) => (
+      this.props.selectedOption !== null &&
+      option.id === this.props.selectedOption
+    ))
+
+    // When one of the options is selected, we display it inside the trigger.
+    if (foundSelectedOption) {
+      return (
+        <bem.KoboSelect__trigger>
+          {foundSelectedOption.icon &&
+            <Icon
+              name={foundSelectedOption.icon}
+              size={ButtonToIconMap.get(this.props.size)}
+            />
+          }
+
+          <label>{foundSelectedOption.label}</label>
+
+          {this.props.isClearable &&
+            <bem.KoboSelect__clear>
+              <Icon
+                name='close'
+                size={ButtonToIconMap.get(this.props.size)}
+              />
+            </bem.KoboSelect__clear>
+          }
+
+          <Icon name='caret-down' size={ButtonToIconMap.get(this.props.size)}/>
+        </bem.KoboSelect__trigger>
+      )
+    }
+
+    // The default trigger for nothing selected.
     return (
       <bem.KoboSelect__trigger>
-        <label>Click me</label>
+        <label>{t('Select…')}</label>
         <Icon name='caret-down' size={ButtonToIconMap.get(this.props.size)}/>
       </bem.KoboSelect__trigger>
     )
@@ -81,15 +120,15 @@ class KoboSelect extends React.Component<KoboSelectProps, {}> {
   renderMenu() {
     return (
       <bem.KoboSelect__menu>
-        <bem.KoboSelect__option>
-          option 1
-        </bem.KoboSelect__option>
-        <bem.KoboSelect__option>
-          option 2
-        </bem.KoboSelect__option>
-        <bem.KoboSelect__option>
-          option 3
-        </bem.KoboSelect__option>
+        {this.props.options.map((option) => (
+          <bem.KoboSelect__option
+            key={option.id}
+            onClick={this.onOptionClick.bind(this, option.id)}
+          >
+            {option.icon && <Icon name={option.icon}/>}
+            <label>{option.label}</label>
+          </bem.KoboSelect__option>
+        ))}
       </bem.KoboSelect__menu>
     )
   }
@@ -99,6 +138,10 @@ class KoboSelect extends React.Component<KoboSelectProps, {}> {
       `size-${this.props.size}`,
       `type-${this.props.type}`
     ];
+
+    if (this.props.isFullWidth) {
+      modifiers.push('is-full-width')
+    }
 
     return (
       <bem.KoboSelect m={modifiers}>
--- jsapp/js/designSystem/koboSelectDemo.tsx
+++ jsapp/js/designSystem/koboSelectDemo.tsx
@@ -1,14 +1,69 @@
 import React from 'react'
 import bem from 'js/bem';
-import KoboSelect from 'js/components/common/koboSelect'
+import KoboSelect, {KoboSelectType} from 'js/components/common/koboSelect'
+import {ButtonSize} from 'js/components/common/button'
+import Checkbox from 'js/components/common/checkbox'
+import Radio from 'js/components/common/radio'
 
-export default class KoboDropdownDemo extends React.Component<{}, {}> {
+const koboSelectTypes: KoboSelectType[] = ['blue', 'gray', 'outline']
+const koboSelectSizes: ButtonSize[] = ['s', 'm', 'l']
+
+type KoboSelectDemoState = {
+  demoType: KoboSelectType
+  demoSize: ButtonSize
+  demoIsClearable: boolean
+  demoIsSearchable: boolean
+  demoIsDisabled: boolean
+  demoIsPending: boolean
+  demoIsFullWidth: boolean
+  demoSelectedOption: string | null
+}
+
+export default class KoboSelectDemo extends React.Component<{}, KoboSelectDemoState> {
   constructor(props: {}) {
     super(props)
+    this.state = {
+      demoType: 'blue',
+      demoSize: 'm',
+      demoIsClearable: false,
+      demoIsSearchable: false,
+      demoIsDisabled: false,
+      demoIsPending: false,
+      demoIsFullWidth: false,
+      demoSelectedOption: 'two'
+    }
   }
 
-  onChange(id: string | null) {
-    console.log('id', id)
+  onTypeChange({}: any, newType: KoboSelectType) {
+    this.setState({demoType: newType})
+  }
+
+  onSizeChange({}: any, newSize: ButtonSize) {
+    this.setState({demoSize: newSize})
+  }
+
+  onIsClearableChange(isChecked: boolean) {
+    this.setState({demoIsClearable: isChecked})
+  }
+
+  onIsSearchableChange(isChecked: boolean) {
+    this.setState({demoIsSearchable: isChecked})
+  }
+
+  onIsDisabledChange(isChecked: boolean) {
+    this.setState({demoIsDisabled: isChecked})
+  }
+
+  onIsPendingChange(isChecked: boolean) {
+    this.setState({demoIsPending: isChecked})
+  }
+
+  onIsFullWidthChange(isChecked: boolean) {
+    this.setState({demoIsFullWidth: isChecked})
+  }
+
+  onSelectChange(newSelectedOption: string | null) {
+    this.setState({demoSelectedOption: newSelectedOption})
   }
 
   render() {
@@ -26,18 +81,88 @@ export default class KoboDropdownDemo extends React.Component<{}, {}> {
           <bem.SimpleTable__body>
             <bem.SimpleTable__row>
               <bem.SimpleTable__cell>
-                TBD
+                <form>
+                  <div className='demo__form-row'>
+                    <div className='demo__form-config'>
+                      <Radio
+                        title='type'
+                        name='type'
+                        selected={this.state.demoType}
+                        options={koboSelectTypes.map(
+                          (type: KoboSelectType) => {return {value: type, label: type}}
+                        )}
+                        onChange={this.onTypeChange.bind(this)}
+                      />
+                    </div>
+
+                    <div className='demo__form-config'>
+                      <Radio
+                        title='size'
+                        name='size'
+                        selected={this.state.demoSize}
+                        options={koboSelectSizes.map(
+                          (type: ButtonSize) => {return {value: type, label: type}}
+                        )}
+                        onChange={this.onSizeChange.bind(this)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className='demo__form-row'>
+                    <div className='demo__form-config'>
+                      <Checkbox
+                        label='is clearable'
+                        onChange={this.onIsClearableChange.bind(this)}
+                        checked={this.state.demoIsClearable}
+                      />
+                    </div>
+
+                    <div className='demo__form-config'>
+                      <Checkbox
+                        label='is searchable'
+                        onChange={this.onIsSearchableChange.bind(this)}
+                        checked={this.state.demoIsSearchable}
+                      />
+                    </div>
+                  </div>
+
+                  <div className='demo__form-row'>
+                    <div className='demo__form-config'>
+                      <Checkbox
+                        label='is disabled'
+                        onChange={this.onIsDisabledChange.bind(this)}
+                        checked={this.state.demoIsDisabled}
+                      />
+                    </div>
+
+                    <div className='demo__form-config'>
+                      <Checkbox
+                        label='is pending'
+                        onChange={this.onIsPendingChange.bind(this)}
+                        checked={this.state.demoIsPending}
+                      />
+                    </div>
+
+                    <div className='demo__form-config'>
+                      <Checkbox
+                        label='is full width'
+                        onChange={this.onIsFullWidthChange.bind(this)}
+                        checked={this.state.demoIsFullWidth}
+                      />
+                    </div>
+                  </div>
+                </form>
               </bem.SimpleTable__cell>
               <bem.SimpleTable__cell>
                 <div className='demo__preview'>
                   <KoboSelect
-                    type='blue'
-                    size='s'
-                    isClearable={false}
-                    isSearchable={false}
-                    isDisabled={false}
-                    isPending={false}
-                    isFullWidth={false}
+                    type={this.state.demoType}
+                    size={this.state.demoSize}
+                    isClearable={this.state.demoIsClearable}
+                    isSearchable={this.state.demoIsSearchable}
+                    isDisabled={this.state.demoIsDisabled}
+                    isPending={this.state.demoIsPending}
+                    isFullWidth={this.state.demoIsFullWidth}
                     options={[
                       {
                         id: 'one',
@@ -48,10 +173,15 @@ export default class KoboDropdownDemo extends React.Component<{}, {}> {
                         id: 'two',
                         label: 'Two',
                         icon: 'information'
+                      },
+                      {
+                        id: 'three',
+                        label: 'Three',
+                        icon: 'information'
                       }
                     ]}
-                    selectedOption='two'
-                    onChange={this.onChange.bind(this)}
+                    selectedOption={this.state.demoSelectedOption}
+                    onChange={this.onSelectChange.bind(this)}
                   />
                 </div>
               </bem.SimpleTable__cell>
