64f78e16b4e856b3165d044ebc85d97b42505f44
Alex Dorey <dorey415@gmail.com>
2022-01-21 17:56:20 -0500

    pulls names from the latest deployed version rather than from the current asset version.

--- kpi/models/asset.py
+++ kpi/models/asset.py
@@ -447,7 +447,12 @@ class Asset(ObjectPermissionMixin,
         if len(self.advanced_features) == 0:
             NO_FEATURES_MSG = 'no advanced features activated for this form'
             return {'type': 'object', '$description': NO_FEATURES_MSG}
-        return advanced_submission_jsonschema(self.content,
+        last_deployed_version = self.deployed_versions.last()
+        if last_deployed_version is None:
+            NO_DEPLOYMENT_MSG = 'asset needs a deployment for this feature'
+            return {'type': 'object', '$description': NO_DEPLOYMENT_MSG}
+        content = last_deployed_version.version_content
+        return advanced_submission_jsonschema(content,
                                               self.advanced_features,
                                               url=url)
 
