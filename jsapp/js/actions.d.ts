094003f5d933f5d6bb4b5ae724b914a16d1b216a
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2022-02-15 22:11:01 +0100

    linter fixes

--- jsapp/js/actions.d.ts
+++ jsapp/js/actions.d.ts
@@ -4,97 +4,97 @@
  */
 
 interface GetSubmissionDefinition extends Function {
-  (assetUid: string, submissionIdOrUuid: string): void
-  completed: GetSubmissionCompletedDefinition
-  failed: GenericFailedDefinition
+  (assetUid: string, submissionIdOrUuid: string): void;
+  completed: GetSubmissionCompletedDefinition;
+  failed: GenericFailedDefinition;
 }
 
 interface GetSubmissionCompletedDefinition extends Function {
-  (response: SubmissionResponse): void
-  listen: (callback: (response: SubmissionResponse) => void) => Function
+  (response: SubmissionResponse): void;
+  listen: (callback: (response: SubmissionResponse) => void) => Function;
 }
 
 interface GetProcessingSubmissionsDefinition extends Function {
-  (assetUid: string, questionsPaths: string[]): void
-  completed: GetProcessingSubmissionsCompletedDefinition
-  failed: GenericFailedDefinition
+  (assetUid: string, questionsPaths: string[]): void;
+  completed: GetProcessingSubmissionsCompletedDefinition;
+  failed: GenericFailedDefinition;
 }
 
 interface GetProcessingSubmissionsCompletedDefinition extends Function {
-  (response: GetProcessingSubmissionsResponse): void
-  listen: (callback: (response: GetProcessingSubmissionsResponse) => void) => Function
+  (response: GetProcessingSubmissionsResponse): void;
+  listen: (callback: (response: GetProcessingSubmissionsResponse) => void) => Function;
 }
 
 interface LoadAssetDefinition extends Function {
-  (params: {id: string}): void
-  completed: LoadAssetCompletedDefinition
-  failed: GenericFailedDefinition
+  (params: {id: string}): void;
+  completed: LoadAssetCompletedDefinition;
+  failed: GenericFailedDefinition;
 }
 
 interface LoadAssetCompletedDefinition extends Function {
-  (response: AssetResponse): void
-  listen: (callback: (response: AssetResponse) => void) => Function
+  (response: AssetResponse): void;
+  listen: (callback: (response: AssetResponse) => void) => Function;
 }
 
 interface GenericFailedDefinition extends Function {
-  (response: FailResponse): void
-  listen: (callback: (response: FailResponse) => void) => Function
+  (response: FailResponse): void;
+  listen: (callback: (response: FailResponse) => void) => Function;
 }
 
 interface UpdateAssetDefinition extends Function {
-  (uid: string, values: any, params?: any): void
-  completed: UpdateAssetCompletedDefinition
-  failed: GenericFailedDefinition
+  (uid: string, values: any, params?: any): void;
+  completed: UpdateAssetCompletedDefinition;
+  failed: GenericFailedDefinition;
 }
 
 interface UpdateAssetCompletedDefinition extends Function {
-  (response: AssetResponse): void
-  listen: (callback: (response: AssetResponse) => void) => Function
+  (response: AssetResponse): void;
+  listen: (callback: (response: AssetResponse) => void) => Function;
 }
 
 // TODO: as you use more actions in your ts files, please extend this namespace
 export namespace actions {
-    const navigation: any
-    const auth: any
-    const survey: any
-    const search: any
+    const navigation: object;
+    const auth: object;
+    const survey: object;
+    const search: object;
     const resources: {
-      createImport: any
-      loadAsset: LoadAssetDefinition
-      deployAsset: any
-      setDeploymentActive: any
-      createSnapshot: any
-      cloneAsset: any
-      deleteAsset: any
-      listTags: any
-      createResource: any
-      updateAsset: UpdateAssetDefinition
-      updateSubmissionValidationStatus: any
-      removeSubmissionValidationStatus: any
-      deleteSubmission: any
-      duplicateSubmission: any
-      refreshTableSubmissions: any
-      getAssetFiles: any
+      createImport: Function;
+      loadAsset: LoadAssetDefinition;
+      deployAsset: Function;
+      setDeploymentActive: Function;
+      createSnapshot: Function;
+      cloneAsset: Function;
+      deleteAsset: Function;
+      listTags: Function;
+      createResource: Function;
+      updateAsset: UpdateAssetDefinition;
+      updateSubmissionValidationStatus: Function;
+      removeSubmissionValidationStatus: Function;
+      deleteSubmission: Function;
+      duplicateSubmission: Function;
+      refreshTableSubmissions: Function;
+      getAssetFiles: Function;
     }
-    const hooks: any
-    const misc: any
-    const reports: any
-    const table: any
-    const map: any
-    const permissions: any
-    const help: any
-    const library: any
+    const hooks: object;
+    const misc: object;
+    const reports: object;
+    const table: object;
+    const map: object;
+    const permissions: object;
+    const help: object;
+    const library: object;
     const submissions: {
-      getSubmission: GetSubmissionDefinition
-      getSubmissionByUuid: GetSubmissionDefinition
-      getSubmissions: any
-      getProcessingSubmissions: GetProcessingSubmissionsDefinition
-      bulkDeleteStatus: any
-      bulkPatchStatus: any
-      bulkPatchValues: any
-      bulkDelete: any
+      getSubmission: GetSubmissionDefinition;
+      getSubmissionByUuid: GetSubmissionDefinition;
+      getSubmissions: Function;
+      getProcessingSubmissions: GetProcessingSubmissionsDefinition;
+      bulkDeleteStatus: Function;
+      bulkPatchStatus: Function;
+      bulkPatchValues: Function;
+      bulkDelete: Function;
     }
-    const media: any
-    const exports: any
-    const dataShare: any
+    const media: object;
+    const exports: object;
+    const dataShare: object;
 }
