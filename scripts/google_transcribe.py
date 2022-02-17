b2f9f1031bd93712744784209200556dd1c2054a
jacquelinemorrissette <jacquelinemariemorrissette@gmail.com>
2022-02-16 20:48:05 -0500

    Updated google transcribe runscript

--- scripts/google_transcribe.py
+++ scripts/google_transcribe.py
@@ -1,143 +1,126 @@
-import os
-import subprocess
+from django.contrib.auth.models import User
+from google.cloud import speech_v1
+from google.cloud import storage
 
-from google.cloud import speech_v1 as speech, storage
-from moviepy import editor as mp
-
-
-class TranscriptEngineBase:
-    def __init__(self, audio_file, language_encoding):
-        self.audio_file = audio_file
-        # setting bucket here because it would probably be an env variable
-        self.bucket_name = os.environ.get('GOOGLE_STORAGE_BUCKET_NAME')
-        self.language_encoding = language_encoding
-        self.gcs_url = ''
-        self.file_ext = ''
-        self.response = ''
-
-    def check_flac_exists(self):
-        file = os.path.splitext(self.audio_file)[0] + '.flac'
-        if os.path.isfile(file) is True:
-            self.audio_file = file
-            return
-        else:
-            self.convert_file_type()
-
-    def convert_file_type(self):
-        converted_filename = f"{self.file_path}/{self.file_name}.flac"
-        subprocess.run(['ffmpeg', '-i', self.audio_file, '-ac', '1', '-ar', '16000', converted_filename])
-
-        self.audio_file = converted_filename
-        self.file_ext = '.flac'
-        return
-
-    @property
-    def file_name(self):
-        # returns the file name with no extension to maintain consistency
-        file = os.path.splitext(os.path.split(self.audio_file)[1])[0]
-        return file
-
-    @property
-    def file_path(self):
-        file_path = os.path.split(self.audio_file)[0]
-        return file_path
-
-    def file_type(self):
-        # FLAC and AMR files need to be converted
-        if self.file_ext != '.flac':
-            self.remove_video()
-            self.check_flac_exists()
-
-    def remove_video(self):
-        # Removing any video from the files reduces the time it takes to convert to flac
-        # No luck in finding a library that supports exporting the audio to flac
-        try:
-            video_file = mp.VideoFileClip(self.audio_file)
-            new_file = f"{self.file_path}/{self.file_name}.mp3"
-            video_file.audio.write_audiofile(new_file)
-            self.audio_file = new_file
-        except KeyError:
-            pass
-
-    def store_transcript(self, gsc_response):
-        # we will need to save the transcript somewhere
-        # saving it in txt files for now
-        transcript_file = open(
-            f"kpi/tests/{self.file_name}.txt",
-            "a"
-        )
-
-        for result in gsc_response.results:
-            alternatives = result.alternatives
-            print(alternatives[0].transcript, file=transcript_file)
-
-        transcript_file.close()
+from kpi.models.asset import Asset
 
 
-class GoogleTranscriptEngine(TranscriptEngineBase):
-    def gcs_store_file(self):
+class AutoTranscription:
+    """
+    The engine for transcribing audio files
+    """
+    def store_transcript(self, transcript, asset, submission_id):
+        pass
+
+
+class GoogleTranscribeEngine(AutoTranscription):
+    def __init__(self):
+        self.destination_path = None
+        self.bucket_name = 'kobo-transcription-test'
+
+    def delete_google_file(self):
         """
-        Stores the file in Google Cloud Storage to be used
-        for transcription
-
-        Returns:
-             str: url for the file in google storage
+        Files need to be deleted from google storage after
+        the transcript finished to avoid running up the bill
         """
-        destination_path = f"{self.file_name}{self.file_ext}"
-        source_filename = self.audio_file
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(self.bucket_name)
+        blob = bucket.blob(self.destination_path)
+        blob.delete()
+
+    def get_converted_audio(
+            self,
+            xpath: str,
+            asset_uid: str,
+            submission_id: int,
+            user: object
+    ):
+        asset = Asset.objects.get(uid=asset_uid)
+        attachment = asset.deployment.get_attachment(submission_id, user, xpath=xpath)
+        attachment_path = attachment.absolute_flac_path
+        filepath = attachment.media_file_basename
+        return attachment_path, filepath
+
+    def store_file(self, attachment_path: str, filename: str):
+        self.destination_path = f"{filename}.flac"
+
+        # send the audio file to google storage
         storage_client = storage.Client()
         bucket = storage_client.bucket(bucket_name=self.bucket_name)
-        destination = bucket.blob(destination_path)
+        destination = bucket.blob(self.destination_path)
         destination.upload_from_filename(
-            source_filename,
-            # Temp hardcode for debugging
-            content_type=f"audio/flac",
+            attachment_path,
+            content_type='audio/flac',
+        )
+        return self.destination_path
+
+    def transcribe_file(
+            self,
+            # request: object,
+            asset_uid: str,
+            xpath: str,
+            submission_id: int,
+            source: str,
+            user: object,
+    ):
+        # get the audio file in a Google supported format
+        path, filename = self.get_converted_audio(
+            xpath=xpath,
+            asset_uid=asset_uid,
+            submission_id=submission_id,
+            user=user
         )
 
-        self.gcs_url = f"gs://{self.bucket_name}/{destination_path}"
-        return self.gcs_url
+        # Make sure the file is stored in Google storage or long
+        # files won't run
+        gcs_path = self.store_file(path, filename)
 
-    def transcribe(self):
-        """
-        Transcribe the file using Google Speech-to-text
-
-        :return:
-            json: Response containing the transcribed text
-        """
-        self.file_type()
-        self.gcs_store_file()
-        client = speech.SpeechClient()
-        audio = speech.RecognitionAudio(uri=self.gcs_url)
-        config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
-            sample_rate_hertz=16000,
-            language_code=self.language_encoding,
+        # Create the parameters required for the transcription
+        speech_client = speech_v1.SpeechClient()
+        audio = speech_v1.RecognitionAudio(uri=f'gs://{self.bucket_name}/{gcs_path}')
+        config = speech_v1.RecognitionConfig(
+            encoding=speech_v1.RecognitionConfig.AudioEncoding.FLAC,
+            language_code=source,
+            audio_channel_count=2,
             enable_automatic_punctuation=True,
         )
-        response = client.long_running_recognize(
-            config=config,
-            audio=audio,
-        )
-        self.response = response
 
-        result = response.result()
-        self.store_transcript(result)
-        return response
+        transcript = []
+        speech_results = speech_client.long_running_recognize(config=config, audio=audio)
+        results = speech_results.result()
+
+        for result in results.results:
+            transcript.append(result)
+
+        # self.store_transcript(transcript)
+        self.delete_google_file()
+        return results
 
 
 def run(*args):
-    try:
-        file_path = args[0]
-        if not os.path.isfile(file_path):
-            raise Exception('A filepath is required for arg 1')
-    except Exception:
-        print(args, flush=True)
-        raise Exception('A filepath and language-region pair are required')
+    asset_uid = args[0]
+    submission_id = args[1]
+    xpath = args[2]
+    source = args[3]
+    user = User.objects.get(username=args[4])
 
-    try:
-        language_pair = args[1]
-    except Exception:
-        raise Exception('A language-region pair is required for arg 2')
+    google = GoogleTranscribeEngine()
+    transcript = google.transcribe_file(asset_uid, xpath, submission_id, source, user)
+    print("Here is the transcript: ", transcript)
+    return transcript
 
-    transcript = GoogleTranscriptEngine(file_path, language_pair)
-    transcript.transcribe()
+    # try:
+    #     file_path = args[0]
+    #     if not os.path.isfile(file_path):
+    #         raise Exception('A filepath is required for arg 1')
+    # except Exception:
+    #     print(args, flush=True)
+    #     raise Exception('A filepath and language-region pair are required')
+    #
+    # try:
+    #     language_pair = args[1]
+    # except Exception:
+    #     raise Exception('A language-region pair is required for arg 2')
+    #
+    # transcript = GoogleTranscriptEngine(file_path, language_pair)
+    # transcript.transcribe()
