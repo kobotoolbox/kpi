801275309ef88b3ea41494519cb231062a6eb1e8
Olivier Leger <olivierleger@gmail.com>
2022-04-06 15:49:07 -0400

    PyMongo 4 upgrade: j was deprecated in favor of journal

--- kobo/settings/base.py
+++ kobo/settings/base.py
@@ -787,7 +787,7 @@ if MONGO_DATABASE.get('USER') and MONGO_DATABASE.get('PASSWORD'):
 else:
     MONGO_CONNECTION_URL = "mongodb://%(HOST)s:%(PORT)s/%(NAME)s" % MONGO_DATABASE
 MONGO_CONNECTION = MongoClient(
-    MONGO_CONNECTION_URL, j=True, tz_aware=True, connect=False)
+    MONGO_CONNECTION_URL, journal=True, tz_aware=True, connect=False)
 MONGO_DB = MONGO_CONNECTION[MONGO_DATABASE['NAME']]
 
 # If a request or task makes a database query and then times out, the database
