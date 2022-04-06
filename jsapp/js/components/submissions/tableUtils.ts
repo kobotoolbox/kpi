eac73b038d641aaf18e5b157df39ff9ddf47ba84
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2022-04-06 15:14:21 +0200

    lint tableUtils

--- jsapp/js/components/submissions/tableUtils.ts
+++ jsapp/js/components/submissions/tableUtils.ts
@@ -1,16 +1,16 @@
 import {
   META_QUESTION_TYPES,
-  SUPPLEMENTAL_DETAILS_PROP
-} from 'js/constants'
+  SUPPLEMENTAL_DETAILS_PROP,
+} from 'js/constants';
 import {
   SUBMISSION_ACTIONS_ID,
   VALIDATION_STATUS_ID_PROP,
-} from 'js/components/submissions/tableConstants'
-import {getSurveyFlatPaths} from 'js/assetUtils'
+} from 'js/components/submissions/tableConstants';
+import {getSurveyFlatPaths} from 'js/assetUtils';
 import type {
   AssetResponse,
-  SurveyRow
-} from 'js/dataInterface'
+  SurveyRow,
+} from 'js/dataInterface';
 
 export function getColumnLabel(
   asset: AssetResponse,
@@ -19,26 +19,26 @@ export function getColumnLabel(
   translationIndex = 0
 ): string {
   if (asset.content?.survey === undefined) {
-    return key
+    return key;
   }
 
-  var question
-  var questionPath: string[] = []
+  let question;
+  let questionPath: string[] = [];
   if (key.includes('/')) {
-    questionPath = key.split('/')
+    questionPath = key.split('/');
     question = asset.content.survey.find((o) => (
       o.name === questionPath[questionPath.length - 1] ||
       o.$autoname === questionPath[questionPath.length - 1]
-    ))
+    ));
   } else {
-    question = asset.content.survey.find((o) => o.name === key || o.$autoname === key)
+    question = asset.content.survey.find((o) => o.name === key || o.$autoname === key);
   }
 
   if (
     question === undefined &&
     questionPath[0] === SUPPLEMENTAL_DETAILS_PROP
   ) {
-    const flatPaths = getSurveyFlatPaths(asset.content?.survey)
+    const flatPaths = getSurveyFlatPaths(asset.content?.survey);
     // Supplemental details keys are built like one of:
     // - prefix / source question name / transcript
     // - prefix / source question name / translated / language code
@@ -47,74 +47,74 @@ export function getColumnLabel(
       flatPaths[questionPath[1]],
       showGroupName,
       translationIndex
-    )
+    );
 
     if (questionPath[2] === 'transcript') {
-      return `${sourceQuestionLabel} - ${t('transcript')} (${questionPath[3]})`
+      return `${sourceQuestionLabel} - ${t('transcript')} (${questionPath[3]})`;
     } else if (questionPath[2] === 'translated') {
-      return `${sourceQuestionLabel} - ${t('translation')} (${questionPath[3]})`
+      return `${sourceQuestionLabel} - ${t('translation')} (${questionPath[3]})`;
     }
   }
 
 
   // NOTE: Some very old code has something to do with nonexistent/negative
   // translationIndex. No idea what is that. It does influences returned value.
-  const showLabels = translationIndex > -1
+  const showLabels = translationIndex > -1;
 
   if (key === SUBMISSION_ACTIONS_ID) {
-    return t('Multi-select checkboxes column')
+    return t('Multi-select checkboxes column');
   }
   if (key === VALIDATION_STATUS_ID_PROP) {
-    return t('Validation')
+    return t('Validation');
   }
 
-  var label = key
+  let label = key;
 
   if (key.includes('/')) {
-    var splitK = key.split('/')
-    label = splitK[splitK.length - 1]
+    const splitK = key.split('/');
+    label = splitK[splitK.length - 1];
   }
-  if (question && question.label && showLabels && question.label[translationIndex]) {
-    label = question.label[translationIndex]
+  if (question?.label && showLabels && question.label[translationIndex]) {
+    label = question.label[translationIndex];
   }
   // show Groups in labels, when selected
   if (showGroupName && questionPath && key.includes('/')) {
-    var gLabels = questionPath.join(' / ')
+    let gLabels = questionPath.join(' / ');
 
     if (showLabels) {
-      var gT = questionPath.map(function (g) {
-        var x = asset.content?.survey?.find((o) => o.name === g || o.$autoname === g)
-        if (x && x.label && x.label[translationIndex]) {
-          return x.label[translationIndex]
+      const gT = questionPath.map(function (g) {
+        const x = asset.content?.survey?.find((o) => o.name === g || o.$autoname === g);
+        if (x?.label && x.label[translationIndex]) {
+          return x.label[translationIndex];
         }
 
-        return g
-      })
-      gLabels = gT.join(' / ')
+        return g;
+      });
+      gLabels = gT.join(' / ');
     }
-    return gLabels
+    return gLabels;
   }
 
-  return label
+  return label;
 }
 
 export function getColumnHXLTags(survey: SurveyRow[], key: string) {
   const colQuestion: SurveyRow | undefined = survey.find((question) =>
     question.$autoname === key
-  )
+  );
   if (!colQuestion || !colQuestion.tags) {
-    return null
+    return null;
   }
-  const HXLTags: string[] = []
+  const HXLTags: string[] = [];
   colQuestion.tags.forEach((tag) => {
     if (tag.startsWith('hxl:')) {
-      HXLTags.push(tag.replace('hxl:', ''))
+      HXLTags.push(tag.replace('hxl:', ''));
     }
-  })
+  });
   if (HXLTags.length === 0) {
-    return null
+    return null;
   } else {
-    return HXLTags.join('')
+    return HXLTags.join('');
   }
 }
 
@@ -125,5 +125,5 @@ export function getColumnHXLTags(survey: SurveyRow[], key: string) {
 export function getBackgroundAudioQuestionName(asset: AssetResponse): string | null {
   return asset?.content?.survey?.find(
     (item) => item.type === META_QUESTION_TYPES['background-audio']
-  )?.name || null
+  )?.name || null;
 }
