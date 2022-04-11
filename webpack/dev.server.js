e5eae3474dee0e47b5bbdc8045522b0bf6889ce2
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2022-04-11 13:07:39 +0200

    linter cleanup

--- webpack/dev.server.js
+++ webpack/dev.server.js
@@ -2,7 +2,7 @@ process.traceDeprecation = true;
 const path = require('path');
 const webpack = require('webpack');
 const WebpackCommon = require('./webpack.common');
-const BundleTracker = require('webpack-bundle-tracker');
+require('webpack-bundle-tracker');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 var isPublicDomainDefined = process.env.KOBOFORM_PUBLIC_SUBDOMAIN &&
   process.env.PUBLIC_DOMAIN_NAME;
@@ -18,20 +18,20 @@ module.exports = WebpackCommon({
         commons: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
+          chunks: 'all',
+        },
+      },
+    },
   },
   entry: {
     app: ['react-hot-loader/patch', './jsapp/js/main.es6'],
-    browsertests: path.resolve(__dirname, '../test/index.js')
+    browsertests: path.resolve(__dirname, '../test/index.js'),
   },
   output: {
     library: 'KPI',
     path: path.resolve(__dirname, '../jsapp/compiled/'),
     publicPath: publicPath,
-    filename: '[name]-[contenthash].js'
+    filename: '[name]-[contenthash].js',
   },
   devServer: {
     devMiddleware: {
@@ -41,12 +41,12 @@ module.exports = WebpackCommon({
     hot: true,
     headers: {'Access-Control-Allow-Origin': '*'},
     port: 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
   },
   plugins: [
     new webpack.SourceMapDevToolPlugin({
       filename: '[file].map',
-      exclude: /vendors.*.*/
+      exclude: /vendors.*.*/,
     }),
     new webpack.HotModuleReplacementPlugin(),
     new CircularDependencyPlugin({
