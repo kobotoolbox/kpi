06b7f1db4431caeeae4f75eef22227c728dca708
JacquelineMorrissette <jackie@kobotoolbox.org>
2022-04-25 21:11:25 -0400

    retrieve database URL from 'KPI_DATABASE_URL' and fallback on 'DATABASE_URL'

--- kobo/settings/base.py
+++ kobo/settings/base.py
@@ -320,10 +320,13 @@ SKIP_HEAVY_MIGRATIONS = env.bool('SKIP_HEAVY_MIGRATIONS', False)
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
-    'default': env.db(default="sqlite:///%s/db.sqlite3" % BASE_DIR),
+    'default': env.db_url(
+        'KPI_DATABASE_URL' if 'KPI_DATABASE_URL' in os.environ else 'DATABASE_URL',
+        default='sqlite:///%s/db.sqlite3' % BASE_DIR
+    ),
 }
+
 if 'KC_DATABASE_URL' in os.environ:
     DATABASES['kobocat'] = env.db_url('KC_DATABASE_URL')
 
