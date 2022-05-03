7fd24661b1b060cc45475a7439f497303f5e477d
Olivier Leger <olivierleger@gmail.com>
2022-05-03 16:20:41 -0400

    Unblock deactivation of primary MFA method

--- kobo/apps/mfa/urls.py
+++ kobo/apps/mfa/urls.py
@@ -1,7 +1,15 @@
 # coding: utf-8
+import trench
 from django.urls import path, include
 
+from .command.deactivate_mfa_method import deactivate_mfa_method_command
 from .views import MFAListUserMethodsView
+
+# Monkey-patch `django-trench` to avoid blocking deactivation of primary
+# method.
+trench.command.deactivate_mfa_method.deactivate_mfa_method_command = (
+    deactivate_mfa_method_command
+)
 
 urlpatterns = [
     path('mfa/user-methods/', MFAListUserMethodsView.as_view(),
