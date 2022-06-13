65a5bb2e5ab8e3c0b2618e5b994fd91c9f811290
David Burke <david@burkesoftware.com>
2022-06-13 16:12:14 -0400

    Add env vars to manage celery autoscale. Remove contacting redis on app init.

--- kobo/apps/__init__.py
+++ kobo/apps/__init__.py
@@ -11,23 +11,6 @@ class KpiConfig(AppConfig):
     name = 'kpi'
 
     def ready(self, *args, **kwargs):
-        # Register signals only when the app is ready to avoid issues with models
-        # not loaded yet.
-        import kpi.signals
-
-        # Once it's okay to read from the database, apply the user-desired
-        # autoscaling configuration for Celery workers
-        from kobo.celery import update_concurrency_from_constance
-        try:
-            # Push this onto the task queue with `delay()` instead of calling
-            # it directly because a direct call in the absence of any Celery
-            # workers hangs indefinitely
-            update_concurrency_from_constance.delay()
-        except kombu.exceptions.OperationalError as e:
-            # It's normal for Django to start without access to a message
-            # broker, e.g. while running `./manage.py collectstatic`
-            # during a Docker image build
-            pass
         return super().ready(*args, **kwargs)
 
 
