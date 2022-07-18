5b6ed838c311721e3972b9b0fbe887e0ca53a7f6
Alex Dorey <dorey415@gmail.com>
2022-07-18 12:33:46 -0400

    populating qpaths in the form as the asset is saved

--- kpi/utils/absolute_paths.py
+++ kpi/utils/absolute_paths.py
@@ -5,11 +5,24 @@ def concat_paths(name, parent_names):
         [*parent_names, name]
     )
 
+def get_name(row):
+    return row.get('name',
+        row.get('$autoname')
+    )
+
 def insert_qpath_in_place(content):
     '''
     insert an absolute path to each question based on parent
     groups
 
-    the delimiter will be configurable
+    the delimiter is set in the DELIMITER variable
     '''
-    pass
+    hierarchy = []
+    for row in content.get('survey', []):
+        if row.get('type') == 'end_group':
+            hierarchy.pop()
+        else:
+            rowname = get_name(row)
+            row['$qpath'] = concat_paths(rowname, hierarchy)
+            if row.get('type') == 'begin_group':
+                hierarchy.append(get_name(row))
