6446030964b49614762a8a9570f5a909e8e89a4e
John N. Milner <john@tmoj.net>
2022-07-28 15:39:03 -0400

    Always regenerate secret when client activates MFA
    When the client requests creation of a new MFA method, *always* create a
    new secret. The default django-trench behavior reuses old secrets,
    meaning that a given user would always see the same QR code no matter
    how many times they enabled and disabled MFA. That default behavior
    would create a security weakness if a user lost their original device,
    because deactivating MFA and reconfiguring it on a new device would not
    prevent the lost device from continuing to generate valid TOTPs.

--- kobo/apps/mfa/serializers.py
+++ kobo/apps/mfa/serializers.py
@@ -1,6 +1,26 @@
 # coding: utf-8
 from rest_framework import serializers
-from trench.utils import get_mfa_model
+from trench.serializers import RequestMFAMethodActivationSerializer
+from trench.utils import create_secret, get_mfa_model
+
+
+class ActivateMfaMethodSerializer(RequestMFAMethodActivationSerializer):
+     def create(self, validated_data):
+        """
+        When the client requests creation of a new MFA method, *always* create
+        a new secret. The default django-trench behavior reuses old secrets,
+        meaning that without this override, a given user would always see the
+        same QR code no matter how many times they enabled and disabled MFA.
+        That default behavior would create a security weakness if a user lost
+        their original device, because deactivating MFA and reconfiguring it on
+        a new device would not prevent the lost device from continuing to
+        generate valid TOTPs.
+        """
+        mfa_method, created = super().create(validated_data)
+        if not created:
+            mfa_method.secret = create_secret()
+            mfa_method.save()
+        return mfa_method, created
 
 
 class UserMfaMethodSerializer(serializers.ModelSerializer):
