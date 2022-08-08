0b9fa68b57d3beabdd884fd132ce29a5febd4399
Olivier Leger <olivierleger@gmail.com>
2022-08-08 12:33:36 -0400

    Restore jnm's explanation for secret regenerations

--- kobo/apps/mfa/command/create_mfa_method.py
+++ kobo/apps/mfa/command/create_mfa_method.py
@@ -13,6 +13,15 @@ class CreateMfaMethodCommand(TrenchCreateMfAMethodCommand):
     Overload `django-trench` behaviour which keeps the same secret forever even
     if MFA method is disabled/enabled.
     See https://github.com/merixstudio/django-trench/blob/6a9f3f049b6c1d97fbfd05e0527b693e04c33891/trench/command/create_mfa_method.py#L14-L25
+
+    When the client requests creation of a new MFA method, *always* create
+    a new secret. The default django-trench behavior reuses old secrets,
+    meaning that without this override, a given user would always see the
+    same QR code no matter how many times they enabled and disabled MFA.
+    That default behavior would create a security weakness if a user lost
+    their original device, because deactivating MFA and reconfiguring it on
+    a new device would not prevent the lost device from continuing to
+    generate valid TOTPs.
     """
     def execute(self, user_id: int, name: str) -> MFAMethod:
         mfa, created = self._mfa_model.objects.get_or_create(
