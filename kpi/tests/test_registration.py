f657a900046be80fd83d195fa9ac7572ac691358
JacquelineMorrissette <jackie@kobotoolbox.org>
2022-08-22 23:26:41 -0400

    add constance support for custom error message if email domain is not in allowed list

--- kpi/tests/test_registration.py
+++ kpi/tests/test_registration.py
@@ -12,10 +12,10 @@ class RegistrationTestCase(TestCase):
     def valid_data(self):
         User = get_user_model()
         return {
-            User.USERNAME_FIELD: "alice",
-            "email": "alice@example.com",
-            "password1": "swordfish",
-            "password2": "swordfish",
+            User.USERNAME_FIELD: 'alice',
+            'email': 'alice@example.com',
+            'password1': 'swordfish',
+            'password2': 'swordfish',
         }
 
     def test_empty_string_allows_all_domains(self):
@@ -23,21 +23,21 @@ class RegistrationTestCase(TestCase):
             constance.config.REGISTRATION_ALLOWED_EMAIL_DOMAINS, ''
         )
         response = self.client.post(
-            reverse("registration_register"), data=self.valid_data
+            reverse('registration_register'), data=self.valid_data
         )
         self.assertRedirects(response, '/accounts/register/complete/')
 
     @override_config(REGISTRATION_ALLOWED_EMAIL_DOMAINS='foo.bar\nexample.com')
     def test_allowed_domain_can_register(self):
         response = self.client.post(
-            reverse("registration_register"), data=self.valid_data
+            reverse('registration_register'), data=self.valid_data
         )
         self.assertRedirects(response, '/accounts/register/complete/')
 
     @override_config(REGISTRATION_ALLOWED_EMAIL_DOMAINS='foo.bar\nbaz.qux')
     def test_disallowed_domain_cannot_register(self):
         response = self.client.post(
-            reverse("registration_register"), data=self.valid_data
+            reverse('registration_register'), data=self.valid_data
         )
         self.assertIn(
             t('This email domain is not allowed to create an account').encode(),
