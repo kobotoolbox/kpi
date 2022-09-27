044a62ce51798763b89e705b637104b4db104e44
Olivier Leger <olivierleger@gmail.com>
2022-09-27 15:16:32 -0400

    Update API documentation

--- kobo/apps/audit_log/views.py
+++ kobo/apps/audit_log/views.py
@@ -9,6 +9,42 @@ from .serializers import AuditLogSerializer
 
 class AuditLogViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
+    Audit logs
+
+    Lists the actions performed (delete, update, create) by users.
+    Only available for superusers.
+
+    <pre class="prettyprint">
+    <b>GET</b> /api/v2/audit-logs/
+    </pre>
+
+    > Example
+    >
+    >       curl -X GET https://[kpi-url]/audit-logs/
+
+    > Response 200
+
+    >       {
+    >           "count": 1,
+    >           "next": null,
+    >           "previous": null,
+    >           "results": [
+    >               {
+    >                    "app_label": "foo",
+    >                    "model_name": "bar",
+    >                    "object_id": 1,
+    >                    "user": "http://kf.kobo.local/users/kobo_user/",
+    >                    "method": "DELETE",
+    >               }
+    >           ]
+    >       }
+
+    Results from this endpoint can be filtered by a Boolean query specified in the `q` parameter.
+    For example: api/v2/audit-logs/?q=method:delete
+
+
+
+    ### CURRENT ENDPOINT
     """
 
     model = AuditLog
