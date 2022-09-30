0c0ce7317a0b8e058b02e454cb72a1f5a08b3b7f
Davis Raymond Muro <davisraymondmuro@outlook.com>
2022-09-30 18:18:11 +0300

    Disable celery autoscaling

--- kobo/apps/__init__.py
+++ kobo/apps/__init__.py
@@ -10,25 +10,25 @@ from kpi.utils.two_database_configuration_checker import \
 class KpiConfig(AppConfig):
     name = 'kpi'
 
-    def ready(self, *args, **kwargs):
-        # Register signals only when the app is ready to avoid issues with models
-        # not loaded yet.
-        import kpi.signals
+    # def ready(self, *args, **kwargs):
+    #     # Register signals only when the app is ready to avoid issues with models
+    #     # not loaded yet.
+    #     import kpi.signals
 
-        # Once it's okay to read from the database, apply the user-desired
-        # autoscaling configuration for Celery workers
-        from kobo.celery import update_concurrency_from_constance
-        try:
-            # Push this onto the task queue with `delay()` instead of calling
-            # it directly because a direct call in the absence of any Celery
-            # workers hangs indefinitely
-            update_concurrency_from_constance.delay()
-        except kombu.exceptions.OperationalError as e:
-            # It's normal for Django to start without access to a message
-            # broker, e.g. while running `./manage.py collectstatic`
-            # during a Docker image build
-            pass
-        return super().ready(*args, **kwargs)
+    #     # Once it's okay to read from the database, apply the user-desired
+    #     # autoscaling configuration for Celery workers
+    #     from kobo.celery import update_concurrency_from_constance
+    #     try:
+    #         # Push this onto the task queue with `delay()` instead of calling
+    #         # it directly because a direct call in the absence of any Celery
+    #         # workers hangs indefinitely
+    #         update_concurrency_from_constance.delay()
+    #     except kombu.exceptions.OperationalError as e:
+    #         # It's normal for Django to start without access to a message
+    #         # broker, e.g. while running `./manage.py collectstatic`
+    #         # during a Docker image build
+    #         pass
+    #     return super().ready(*args, **kwargs)
 
 
 register(TwoDatabaseConfigurationChecker().as_check(), Tags.database)
