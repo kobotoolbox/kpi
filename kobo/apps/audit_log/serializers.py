dd29b99148d8a75d45307f12a162c42e9a8ba87c
Olivier Leger <olivierleger@gmail.com>
2022-09-30 16:44:11 -0400

    Remove hack to force show method and datecreated in serializer

--- kobo/apps/audit_log/serializers.py
+++ kobo/apps/audit_log/serializers.py
@@ -43,19 +43,3 @@ class AuditLogSerializer(serializers.ModelSerializer):
 
     def get_date_created(self, audit_log):
         return audit_log.date_created.strftime('%Y-%m-%dT%H:%M:%SZ')
-
-    def get_uniqueness_extra_kwargs(self, field_names, declared_fields, extra_kwargs):
-        extra_kwargs, hidden_fields = super().get_uniqueness_extra_kwargs(
-            field_names, declared_fields, extra_kwargs
-        )
-        # Override DRF behaviour which ignores `method` field from the model
-        # because a SerializerField name `method` is also declared in this class.
-        # When this happens, DRF treats all fields included in unique constraints
-        # as hidden fields and do not display them in the response.
-        # See:
-        # - https://github.com/encode/django-rest-framework/blob/2de50818296b1b4bae68787626c0236752e35101/rest_framework/serializers.py#L1052
-        # - https://github.com/encode/django-rest-framework/blob/2de50818296b1b4bae68787626c0236752e35101/rest_framework/serializers.py#L1057-L1059
-        # - https://github.com/encode/django-rest-framework/blob/2de50818296b1b4bae68787626c0236752e35101/rest_framework/serializers.py#L1459-L1463
-        hidden_fields.pop('method', None)
-        hidden_fields.pop('date_created', None)
-        return extra_kwargs, hidden_fields
