9cd63fed1b229d035b41a422982ae24a329c6b2a
Alex Dorey <dorey415@gmail.com>
2022-10-05 17:56:11 -0400

    "python manage.py runscript repop_known_cols" works with no args

--- kobo/apps/subsequences/scripts/repop_known_cols.py
+++ kobo/apps/subsequences/scripts/repop_known_cols.py
@@ -5,6 +5,7 @@ Usage:
   python manage.py runscript repop_known_cols --script-args=<assetUid>
 '''
 from kpi.models.asset import Asset
+from kobo.apps.subsequences.models import SubmissionExtras
 from pprint import pprint
 
 from kobo.apps.subsequences.utils.parse_knowncols import parse_knowncols
@@ -13,13 +14,27 @@ from kobo.apps.subsequences.utils.determine_export_cols_with_values import (
 )
 
 
-def run(asset_uid):
-    asset = Asset.objects.get(uid=asset_uid)
-    print('Before:')
-    print(sorted(asset.known_cols))
+def repop_asset_knowncols(asset):
+    print(f'for_asset: {asset.uid}')
+    print('  before:')
+    print('   - ' + '\n   - '.join(sorted(asset.known_cols)))
     known_cols = determine_export_cols_with_values(asset.submission_extras.all())
     asset.known_cols = known_cols
     asset.save()
-    print('After:')
-    print(sorted(known_cols))
-    pprint(parse_knowncols(known_cols))
+    print('  after:')
+    print('   - ' + '\n   - '.join(sorted(known_cols)))
+
+def run(asset_uid=None):
+    if asset_uid is None:
+        id_key = 'asset_id'
+        asset_ids = list(
+            set(
+                [a['asset_id'] for a in SubmissionExtras.objects.all().values('asset_id')]
+            )
+        )
+        for asset_id in asset_ids:
+            asset = Asset.objects.get(id=asset_id)
+            repop_asset_knowncols(asset)
+    else:
+        asset = Asset.objects.get(asset_uid=asset_uid)
+        repop_asset_knowncols(asset)
