308786456167dae77459674bbf8dbf22e1ea82b4
David Burke <david@burkesoftware.com>
2022-11-16 11:01:31 -0500

    Remove unused code

--- jsapp/js/account/security/mfa/mfa.api.ts
+++
@@ -1,5 +0,0 @@
-import { ROOT_URL } from "jsapp/js/constants"
-
-export const getUserMethods = () => {
-  return fetch(`${ROOT_URL}/api/v2/auth/mfa/user-methods/`).then((response) => response.json())
-}
--- jsapp/js/router/lol.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/account",
-    element: <div>Hello world!</div>,
-  },
-]);
