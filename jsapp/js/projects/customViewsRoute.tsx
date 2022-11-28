ff8ce9edb54d547de243ef72db48f62e87bf7f98
Leszek <smutnyleszek@gmail.com>
2022-11-28 23:29:26 +0100

    move code to projects directory and stop misusing design system

--- jsapp/js/projects/customViewsRoute.tsx
+++ jsapp/js/projects/customViewsRoute.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import type {ProjectsFilterDefinition} from './projectsView/projectsViewConstants';
+import ProjectsFilter from './projectsView/projectsFilter';
 
 export default function CustomViewsRoute() {
+  const [filters, setFilters] = useState<ProjectsFilterDefinition[]>([]);
+
   return (
-    <div>
-      <h1>Custom Views route</h1>
-    </div>
+    <section>
+      <h1>Projects View temporary demo</h1>
+
+      <ProjectsFilter
+        onFiltersChange={setFilters}
+        filters={filters}
+      />
+    </section>
   );
 }
