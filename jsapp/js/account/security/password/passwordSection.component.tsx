317a8a31ac8bedf0ec66e8e0b2bedb5d22f31935
JacquelineMorrissette <jackie@kobotoolbox.org>
2022-12-09 13:30:13 -0500

    remove bem from password component

--- jsapp/js/account/security/password/passwordSection.component.tsx
+++ jsapp/js/account/security/password/passwordSection.component.tsx
@@ -1,47 +1,43 @@
 import React from 'react';
 import {ACCOUNT_ROUTES} from 'js/account/routes';
-import bem, {makeBem} from 'js/bem';
 import TextBox from 'js/components/common/textBox';
 import {PATHS} from 'js/router/routerConstants';
-
-bem.SecurityRow = makeBem(null, 'security-row');
-bem.SecurityRow__header = makeBem(bem.SecurityRow, 'header');
-bem.SecurityRow__title = makeBem(bem.SecurityRow, 'title', 'h2');
-bem.SecurityRow__buttons = makeBem(bem.SecurityRow, 'buttons');
-bem.SecurityRow__link = makeBem(bem.SecurityRow, 'link')
+import styles from './passwordSection.module.scss';
 
 const HIDDEN_TOKEN_VALUE = '*'.repeat(10);
 
 export default function PasswordSection () {
   return (
-    <bem.SecurityRow>
-      <bem.SecurityRow__header>
-        <bem.SecurityRow__title>
+    <div className={styles.root}>
+      <div className={styles.securityRowHeader}>
+        <div className={styles.title}>
           {t('Password')}
-        </bem.SecurityRow__title>
-      </bem.SecurityRow__header>
+        </div>
+      </div>
+      <div className={styles.securityRowDescription}>
       <TextBox
         customModifiers='on-white'
         type='password'
         value={HIDDEN_TOKEN_VALUE}
         readOnly
       />
-      <bem.SecurityRow__link>
+      </div>
+      <div className={styles.SecurityRowLink}>
         <a 
           href={PATHS.RESET}
         >
           {t('forgot password')}
         </a>
-      </bem.SecurityRow__link>
-      <bem.SecurityRow__buttons>
+      </div>
+      <div className={styles.securityRowButton}>
         <a
           href={`/#${ACCOUNT_ROUTES.CHANGE_PASSWORD}`}
           className='kobo-button kobo-button--blue'
         >
           {t('update')}
         </a>
-      </bem.SecurityRow__buttons>
+      </div>
 
-    </bem.SecurityRow>
+    </div>
   );
 }
