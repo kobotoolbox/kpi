3142a90fb057bf0567df976ee43d62a6952361ff
David Burke <david@burkesoftware.com>
2022-12-12 14:12:45 -0500

    refactor api fetch wrapper

--- jsapp/js/api.ts
+++ jsapp/js/api.ts
@@ -4,33 +4,31 @@ import {Json} from './components/common/common.interfaces';
 
 const JSON_HEADER = 'application/json';
 
-/** GET Kobo API at path */
-export const fetchGet = async <T>(path: string) => {
-  const response = await fetch(ROOT_URL + path, {
-    headers: {
-      Accept: JSON_HEADER,
-    },
-  });
-  return (await response.json()) as Promise<T>;
-};
+const fetchData = async <T>(path: string, method = 'GET', data?: Json) => {
+  const headers: {[key: string]: string} = {
+    Accept: JSON_HEADER,
+  };
 
-const fetchData = async <T>(path: string, method: string, data?: Json) => {
-  let csrfToken = '';
-  const csrfCookie = document.cookie.match(/csrftoken=(\w{64})/);
-  if (csrfCookie) {
-    csrfToken = csrfCookie[1];
+  if (data) {
+    const csrfCookie = document.cookie.match(/csrftoken=(\w{64})/);
+    if (csrfCookie) {
+      headers['X-CSRFToken'] = csrfCookie[1];
+    }
+
+    headers['Content-Type'] = JSON_HEADER;
   }
 
   const response = await fetch(ROOT_URL + path, {
     method: method,
-    headers: {
-      Accept: JSON_HEADER,
-      'Content-Type': JSON_HEADER,
-      'X-CSRFToken': csrfToken,
-    },
+    headers,
     body: JSON.stringify(data),
   });
   return (await response.json()) as Promise<T>;
+};
+
+/** GET Kobo API at path */
+export const fetchGet = async <T>(path: string) => {
+  return fetchData<T>(path);
 };
 
 /** POST data to Kobo API at path */
