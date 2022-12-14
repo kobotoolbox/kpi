5bc4e665b36b82c20bed371444f03465b5d88d41
Anji Tong <anjitong@hotmail.com>
2022-12-14 02:02:34 -0500

    Some styling cleanups
    TODO:
    - naming is all over the place
    - there are 2 types of buttons--use the framed Button, not <a>
    - change switch form MFA to a button
    - move the titles to the left side(?)
    - use a modal for password(?)

--- jsapp/js/account/security/password/passwordSection.component.tsx
+++ jsapp/js/account/security/password/passwordSection.component.tsx
@@ -2,42 +2,37 @@ import React from 'react';
 import {ACCOUNT_ROUTES} from 'js/account/routes';
 import TextBox from 'js/components/common/textBox';
 import {PATHS} from 'js/router/routerConstants';
+import bem from 'js/bem';
 import styles from './passwordSection.module.scss';
 
 const HIDDEN_TOKEN_VALUE = '*'.repeat(10);
 
-export default function PasswordSection () {
+export default function PasswordSection() {
   return (
     <div className={styles.root}>
       <div className={styles.securityRowHeader}>
-        <div className={styles.title}>
-          {t('Password')}
-        </div>
+        <h2 className={styles.title}>{t('Password')}</h2>
       </div>
-      <div className={styles.securityRowDescription}>
-      <TextBox
-        customModifiers='on-white'
-        type='password'
-        value={HIDDEN_TOKEN_VALUE}
-        readOnly
-      />
-      </div>
-      <div className={styles.SecurityRowLink}>
-        <a 
-          href={PATHS.RESET}
-        >
-          {t('forgot password')}
-        </a>
-      </div>
-      <div className={styles.securityRowButton}>
+
+      <bem.FormModal__item m='password'>
+        <TextBox
+          customModifiers='on-white'
+          type='password'
+          value={HIDDEN_TOKEN_VALUE}
+          readOnly
+        />
+
         <a
           href={`/#${ACCOUNT_ROUTES.CHANGE_PASSWORD}`}
           className='kobo-button kobo-button--blue'
         >
           {t('update')}
         </a>
-      </div>
+      </bem.FormModal__item>
 
+      <div className={styles.SecurityRowLink}>
+        <a href={PATHS.RESET}>{t('forgot password')}</a>
+      </div>
     </div>
   );
 }
--- jsapp/js/account/security/sso/ssoSection.component.tsx
+++ jsapp/js/account/security/sso/ssoSection.component.tsx
@@ -12,27 +12,29 @@ export default function SsoSection() {
   return (
     <div className={styles.root}>
       <div className={styles.securityRowHeader}>
-        <h2>{t('Single-Sign On')}</h2>
+        <h2 className={styles.title}>{t('Single-Sign On')}</h2>
       </div>
       {identities.length === 0 ? (
         <>
-          <div className={styles.securityDescription}>
-            {t(
-              "Connect your KoboToolbox account with your organization's identity provider for single-sign on (SSO). Afterwards, you will only " +
-                'be able to sign in via SSO unless you disable this setting here. This will also update your email address in case your current ' +
-                'address is different.'
-            )}
-          </div>
-          <div className={styles.securityButton}>
-            <a
-              href={
-                PATHS.MS_SSO +
-                '?process=connect&next=%2F%23%2Faccount%2Fsecurity'
-              }
-              className='kobo-button kobo-button--blue'
-            >
-              {t('Set up')}
-            </a>
+          <div className={styles.ssoSectionBody}>
+            <div className={styles.securityDescription}>
+              {t(
+                "Connect your KoboToolbox account with your organization's identity provider for single-sign on (SSO). Afterwards, you will only " +
+                  'be able to sign in via SSO unless you disable this setting here. This will also update your email address in case your current ' +
+                  'address is different.'
+              )}
+            </div>
+            <div className={styles.securityButton}>
+              <a
+                href={
+                  PATHS.MS_SSO +
+                  '?process=connect&next=%2F%23%2Faccount%2Fsecurity'
+                }
+                className='kobo-button kobo-button--blue'
+              >
+                {t('Set up')}
+              </a>
+            </div>
           </div>
         </>
       ) : (
