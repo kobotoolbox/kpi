f79171329d8acace51c341ae401c535699feb9f0
Anji Tong <anjitong@hotmail.com>
2022-12-14 01:25:23 -0500

    MVP styling

--- jsapp/js/account/security/email/emailSection.component.tsx
+++ jsapp/js/account/security/email/emailSection.component.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import sessionStore from 'js/stores/session';
-import { EmailResponse, getUserEmails, setUserEmail } from './emailSection.api';
-import style from './emailSection.module.scss'
+import {EmailResponse, getUserEmails, setUserEmail} from './emailSection.api';
+import style from './emailSection.module.scss';
 import Button from 'jsapp/js/components/common/button';
 import TextBox from 'jsapp/js/components/common/textBox';
+import Icon from 'jsapp/js/components/common/icon';
 
 interface EmailState {
   isLoading: boolean;
@@ -38,6 +39,7 @@ export default function EmailSection() {
         setEmail({
           ...email,
           emails: data.results,
+          newEmail: '',
         });
       });
     });
@@ -51,40 +53,46 @@ export default function EmailSection() {
   }
 
   const currentAccount = session.currentAccount;
-  function getUnverifiedEmail(): EmailResponse | undefined {
-    return email.emails.find((userEmail) => !userEmail.verified);
-  }
+  const unverifiedEmail = email.emails.find((userEmail) => !userEmail.verified);
 
   return (
     <div className={style['email-section']}>
-      <h2>Email</h2>
-      {!session.isPending &&
-        session.isInitialLoadComplete &&
-        'email' in currentAccount && (
-          <h2>{currentAccount.email}</h2>
-      )}
-      {getUnverifiedEmail() && (
-        <div>
-          <h2>Unverified email</h2>
-          <h2>{getUnverifiedEmail()?.email}</h2>
+      <div className={style['email-header']}>
+        <h2 className={style['email-title']}>Email</h2>
+      </div>
+
+      <div className={style['email-body']}>
+        {!session.isPending &&
+          session.isInitialLoadComplete &&
+          'email' in currentAccount && <p>{currentAccount.email}</p>}
+        <TextBox
+          customModifiers='on-white'
+          value={email.newEmail}
+          placeholder={t('Type new email address')}
+          onChange={onTextFieldChange.bind(onTextFieldChange)}
+        />
+        <Button
+          label='Change'
+          size='l'
+          color='blue'
+          type='frame'
+          onClick={setNewUserEmail.bind(setNewUserEmail, email.newEmail)}
+        />
+      </div>
+
+      {unverifiedEmail?.email && (
+        <div className={style['email-unverified']}>
+          <Icon name='alert' />
+
+          <p className={style['blurb']}>
+            <strong>
+              {t('Check your email ##UNVERIFIED_EMAIL##. ').replace('##UNVERIFIED_EMAIL##', unverifiedEmail.email)}
+            </strong>
+
+            {t('A verification link has been sent to confirm your ownership. Once confirmed, this address will replace ##UNVERIFIED_EMAIL##').replace('##UNVERIFIED_EMAIL##', unverifiedEmail.email)}
+          </p>
         </div>
       )}
-      <TextBox
-        customModifiers='on-white'
-        label={t('New email')}
-        value={email.newEmail}
-        placeholder={t(
-          'Type new email address'
-        )}
-        onChange={onTextFieldChange.bind(onTextFieldChange)}
-      />
-      <Button
-        label='Change'
-        size='l'
-        color='blue'
-        type='frame'
-        onClick={setNewUserEmail.bind(setNewUserEmail, email.newEmail)}
-      />
     </div>
   );
 }
