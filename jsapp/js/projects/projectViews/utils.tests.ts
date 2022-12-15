65cafc6982a0d82e34a0c070f5ead541ccfadf70
Leszek <smutnyleszek@gmail.com>
2022-12-15 21:42:15 +0100

    add some tests for buildQueriesFromFilters

--- jsapp/js/projects/projectViews/utils.tests.ts
+++ jsapp/js/projects/projectViews/utils.tests.ts
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import type {ProjectsFilterDefinition} from './constants';
-import {removeIncorrectFilters} from './utils';
+import {removeIncorrectFilters, buildQueriesFromFilters} from './utils';
 
 describe('projectViewsUtils', () => {
   describe('removeIncorrectFilters', () => {
@@ -26,6 +26,90 @@ describe('projectViewsUtils', () => {
       ];
       const test = removeIncorrectFilters(dirty);
       chai.expect(test).to.deep.equal(clean);
+    });
+  });
+
+  describe('buildQueriesFromFilters', () => {
+    it('should build as much queries as there are filters', () => {
+      const filters: ProjectsFilterDefinition[] = [
+        {fieldName: 'name', condition: 'isNot', value: 'Foo'},
+        {fieldName: 'name', condition: 'isNot', value: 'Bar'},
+        {fieldName: 'name', condition: 'isNot', value: 'Fum'},
+        {fieldName: 'name', condition: 'isNotEmpty'},
+      ];
+      const test = buildQueriesFromFilters(filters);
+      chai.expect(test.length).to.equal(filters.length);
+    });
+
+    it('should build multiple queries from multiple filters', () => {
+      const filters: ProjectsFilterDefinition[] = [
+        {fieldName: 'name', condition: 'is', value: 'Foo'},
+        {fieldName: 'sector', condition: 'isEmpty'},
+        {fieldName: 'countries', condition: 'isNotEmpty'},
+      ];
+      const queries = [
+        'name__iexact:"Foo"',
+        'settings__sector:""',
+        'NOT settings__country_codes[]:""',
+      ];
+      const test = buildQueriesFromFilters(filters);
+      chai.expect(test).to.deep.equal(queries);
+    });
+
+    describe('should buid proper query in different cases', () => {
+      const cases: Array<{in: ProjectsFilterDefinition; out: string}> = [
+        {
+          in: {fieldName: 'name', condition: 'doesNotContain', value: 'foo'},
+          out: 'NOT name__icontains:"foo"',
+        },
+        {
+          in: {fieldName: 'description', condition: 'startsWith', value: 'foo'},
+          out: 'settings__description__istartswith:"foo"',
+        },
+        {
+          in: {fieldName: 'status', condition: 'contains', value: 'foo bar'},
+          out: '_deployment_data__icontains:"foo bar"',
+        },
+        {
+          in: {fieldName: 'ownerUsername', condition: 'doesNotContain', value: 'foo'},
+          out: 'NOT owner__username__icontains:"foo"',
+        },
+        {
+          in: {fieldName: 'ownerFullName', condition: 'endsWith', value: 'foo'},
+          out: 'owner__extra_details__data__name__iendswith:"foo"',
+        },
+        {
+          in: {fieldName: 'ownerEmail', condition: 'is', value: 'foo'},
+          out: 'owner__email__iexact:"foo"',
+        },
+        {
+          in: {fieldName: 'ownerOrganization', condition: 'isEmpty'},
+          out: 'owner__extra_details__data__organization:""',
+        },
+        {
+          in: {fieldName: 'dateModified', condition: 'isNot', value: 'foo'},
+          out: 'NOT date_modified__date__iexact:"foo"',
+        },
+        {
+          in: {fieldName: 'sector', condition: 'isNotEmpty'},
+          out: 'NOT settings__sector:""',
+        },
+        {
+          in: {fieldName: 'countries', condition: 'startsWith', value: 'foo'},
+          out: 'settings__country_codes[]__istartswith:"foo"',
+        },
+        {
+          in: {fieldName: 'languages', condition: 'is', value: 'foo'},
+          out: 'summary__languages[]__iexact:"foo"',
+        },
+
+      ];
+      cases.forEach((testCase) => {
+        it(`should build "${testCase.out}" query from filter`, () => {
+          const test = buildQueriesFromFilters([testCase.in]);
+          chai.expect(test).to.deep.equal([testCase.out]);
+        });
+      });
     });
   });
 });
