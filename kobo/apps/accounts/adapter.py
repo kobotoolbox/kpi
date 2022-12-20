ea0a1cd7cfd1404a475af386ed5a840e73f9156d
David Burke <david@burkesoftware.com>
2022-12-20 15:58:09 -0500

    Save signup form data

--- kobo/apps/accounts/adapter.py
+++ kobo/apps/accounts/adapter.py
@@ -1,8 +1,7 @@
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.forms import SignupForm
 from constance import config
 from django.db import transaction
-
-from .forms import SignupForm
 
 
 class AccountAdapter(DefaultAccountAdapter):
@@ -10,6 +9,7 @@ class AccountAdapter(DefaultAccountAdapter):
         return config.REGISTRATION_OPEN
 
     def save_user(self, request, user, form, commit=True):
+        # Compare allauth SignupForm with our custom field
         standard_fields = set(SignupForm().fields.keys())
         extra_fields = set(form.fields.keys()).difference(standard_fields)
         with transaction.atomic():
