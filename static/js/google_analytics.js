12f9c663c33e0b5d247fed7bf40411bb4f6adc51
Leszek <smutnyleszek@gmail.com>
2023-03-08 21:50:59 +0100

    unwrap google code

--- static/js/google_analytics.js
+++ static/js/google_analytics.js
@@ -1,11 +1,9 @@
-(function(){
-  const gaTokenEl = document.head.querySelector('meta[name=google-analytics-token]');
-  if (gaTokenEl !== null) {
-    // Code from Google below
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+const gaTokenEl = document.head.querySelector('meta[name=google-analytics-token]');
+if (gaTokenEl !== null) {
+  // Code from Google below
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    gtag('config', gaTokenEl);
-  }
-})();
+  gtag('config', gaTokenEl);
+}
