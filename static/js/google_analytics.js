06bed86183da7a4bc6246bef5814c6d8cdb9d3b8
Leszek <smutnyleszek@gmail.com>
2023-03-08 22:24:34 +0100

    wrap google code in anonymous function in a better way

--- static/js/google_analytics.js
+++ static/js/google_analytics.js
@@ -1,9 +1,10 @@
-const gaTokenEl = document.head.querySelector('meta[name=google-analytics-token]');
-if (gaTokenEl !== null) {
-  // Code from Google below
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', gaTokenEl);
-}
+(function(){
+  const gaTokenEl = document.head.querySelector('meta[name=google-analytics-token]');
+  if (gaTokenEl !== null) {
+    // Code from Google below
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function(){dataLayer.push(arguments);};
+    window.gtag('js', new Date());
+    window.gtag('config', gaTokenEl);
+  }
+})();
