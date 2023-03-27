fbd28ea1b09729950bea3c366d737bc8bdc8f077
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2023-03-27 14:47:27 -0500

    make strings translatable

--- jsapp/js/account/successRoute.tsx
+++ jsapp/js/account/successRoute.tsx
@@ -4,23 +4,20 @@ import styles from './successRoute.module.scss';
 import Icon from 'jsapp/js/components/common/icon';
 import InlineMessage from "js/components/common/inlineMessage";
 
-const SuccessRoute = () => {
-  return (
-    <article className={styles.container}>
-      <a className={styles.backLink} href={ROOT_URL + '#/account/plan'}>
-        <Icon name={'arrow-left'}/>
-        <span>{'Go Back to Account Section'}</span>
-      </a>
-      <section className={styles.message}>
-        <h2>{'Thank you for your purchase!'}</h2>
-        <InlineMessage classNames={styles.details} type={'default'}
-          message={`Your account should be updated shortly.
-            Please check your e-mail and spam folder for updates from Stripe about the status of your transaction.`
-          }
-        />
-      </section>
-    </article>
-  )
-}
+const SuccessRoute = () => (
+  <article className={styles.container}>
+    <a className={styles.backLink} href={ROOT_URL + '#/account/plan'}>
+      <Icon name={'arrow-left'}/>
+      <span>{t('Go Back to Account Section')}</span>
+    </a>
+    <section className={styles.message}>
+      <h2>{t('Thank you for your purchase!')}</h2>
+      <InlineMessage classNames={styles.details} type={'default'}
+        message={t('Your account should be updated shortly. ' +
+          'Please check your e-mail and spam folder for updates from Stripe about the status of your transaction.')}
+      />
+    </section>
+  </article>
+);
 
 export default SuccessRoute;
