c34c78e80236803303bd73b4717a1b24bdf77884
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2023-07-06 15:20:12 -0500

    migration to clear old default values for FREE_TIER_THRESHOLDS

---
+++ kpi/migrations/0051_set_free_tier_thresholds_to_default.py
@@ -0,0 +1,27 @@
+from constance import config
+from django.db import migrations
+
+
+def reset_free_tier_thresholds(apps, schema_editor):
+    # The constance defaults for FREE_TIER_THRESHOLDS changed, so we set existing config to the new default value
+    thresholds = {
+        'storage': None,
+        'data': None,
+        'transcription_minutes': None,
+        'translation_chars': None,
+    }
+    setattr(config, 'FREE_TIER_THRESHOLDS', thresholds)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('kpi', '0050_add_indexes_to_import_and_export'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            reset_free_tier_thresholds,
+            migrations.RunPython.noop,
+        )
+    ]
