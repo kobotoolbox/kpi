5c00200666c4a406a6d733ddf6b6eeea87c9d315
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2023-07-17 12:08:10 -0500

    remove unused import

--- kobo/apps/accounts/mfa/tests/test_login.py
+++ kobo/apps/accounts/mfa/tests/test_login.py
@@ -2,7 +2,6 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.utils.http import urlencode
 from rest_framework import status
 from trench.utils import get_mfa_model
 
