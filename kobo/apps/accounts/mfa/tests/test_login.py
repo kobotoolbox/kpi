b3fca8fc2a52c05f27fad6105743cfb5fd8be962
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2023-07-17 12:05:40 -0500

    refactor mfa login tests

--- kobo/apps/accounts/mfa/tests/test_login.py
+++ kobo/apps/accounts/mfa/tests/test_login.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.http import urlencode
@@ -24,12 +25,18 @@ class LoginTests(KpiTestCase):
         )
         # Ensure `self.client` is not authenticated
         self.client.logout()
+        # Store the original value of STRIPE_ENABLED so we can override it
+        self.original_stripe_enabled = settings.STRIPE_ENABLED
+
+    def tearDown(self):
+        settings.STRIPE_ENABLED = self.original_stripe_enabled
 
     def test_login_with_mfa_enabled(self):
         """
         Validate that multi-factor authentication form is displayed after
         successful login
         """
+        settings.STRIPE_ENABLED = False
         data = {
             'login': 'someuser',
             'password': 'someuser',
