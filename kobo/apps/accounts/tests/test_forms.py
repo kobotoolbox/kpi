3f96fa63abc757e7c854476cc42ef4745533b34a
John N. Milner <john@tmoj.net>
2023-07-21 18:51:32 -0400

    Add test (1 failing) for custom user metadata…
    fields in the registration form

--- kobo/apps/accounts/tests/test_forms.py
+++ kobo/apps/accounts/tests/test_forms.py
@@ -1,6 +1,11 @@
+import json
+
+from constance.test import override_config
 from django.test import TestCase, override_settings
+from django.utils import translation
 from model_bakery import baker
 from pyquery import PyQuery
+
 from kobo.apps.accounts.forms import SocialSignupForm
 
 
@@ -25,3 +30,59 @@ class AccountFormsTestCase(TestCase):
         pq = PyQuery(str(form))
         assert (email_input := pq("[name=email]"))
         assert "readonly" not in email_input[0].attrib
+
+    @override_config(USER_METADATA_FIELDS='{}')
+    def test_only_configurable_fields_can_be_removed(self):
+        form = SocialSignupForm(sociallogin=self.sociallogin)
+        assert 'username' in form.fields
+        assert 'email' in form.fields
+
+    @override_config(
+        USER_METADATA_FIELDS=json.dumps(
+            [{'name': 'full_name', 'required': True}]
+        )
+    )
+    def test_field_without_custom_label_can_be_required(self):
+        form = SocialSignupForm(sociallogin=self.sociallogin)
+        assert form.fields['full_name'].required
+        assert form.fields['full_name'].label == 'Full name'
+
+    @override_config(
+        USER_METADATA_FIELDS=json.dumps(
+            [
+                {
+                    'name': 'full_name',
+                    'required': True,
+                    'label': {'default': 'Secret Agent ID'},
+                }
+            ]
+        )
+    )
+    def test_field_with_only_default_custom_label(self):
+        form = SocialSignupForm(sociallogin=self.sociallogin)
+        assert form.fields['full_name'].required
+        assert form.fields['full_name'].label == 'Secret Agent ID'
+
+    @override_config(
+        USER_METADATA_FIELDS=json.dumps(
+            [
+                {
+                    'name': 'full_name',
+                    'required': True,
+                    'label': {
+                        'default': 'Secret Agent ID',
+                        'es': 'ID de agente secreto',
+                    },
+                }
+            ]
+        )
+    )
+    def test_field_with_specific_and_default_custom_labels(self):
+        with translation.override('es'):
+            form = SocialSignupForm(sociallogin=self.sociallogin)
+            assert form.fields['full_name'].required
+            assert form.fields['full_name'].label == 'ID de agente secreto'
+        with translation.override('en'):
+            form = SocialSignupForm(sociallogin=self.sociallogin)
+            assert form.fields['full_name'].required
+            assert form.fields['full_name'].label == 'Secret Agent ID'
