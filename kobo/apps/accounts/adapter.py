0f0a7135b819ca7adf1c163128db23549c718a9d
LMNTL <jessica.thomas@kobotoolbox.org>
2023-07-25 13:15:56 -0500

    Restrict MFA based on subscription when Stripe is enabled (#4543)
    * disable MFA section in account settings when user doesn't have subscription and stripe is enabled
    
    * prevent 2FA flow on login for free users when Stripe is enabled
    
    * refactor mfa login tests
    
    * add tests for stripe-specific MFA features
    
    * remove unused import
    
    * refactor MFALoginView.form_valid
    
    * change back a conditional that got hardwired during testing
    
    * refactor hasActiveSubscription
    
    * don't error when checking MFA settings while Stripe is disabled
    
    * fix linting errors in stripe.utils.js
    
    * use @override_settings instead of directly changing settings
    
    * prevent MFA login without subscription when using SSO
    
    * merge in fixes from noliveleger
    
    * add test to ensure that skipped MFA login doesn't create a vulnerability
    
    * don't restrict MFA access for explicitly allowed users
    
    * add test for per-user availability

--- kobo/apps/accounts/adapter.py
+++ kobo/apps/accounts/adapter.py
@@ -9,31 +9,42 @@ from django.template.response import TemplateResponse
 from trench.utils import get_mfa_model, user_token_generator
 
 from .mfa.forms import MfaTokenForm
+from .mfa.models import MfaAvailableToUser
 from .mfa.views import MfaTokenView
+from .utils import user_has_paid_subscription
 
 
 class AccountAdapter(DefaultAccountAdapter):
-
     def pre_login(self, request, user, **kwargs):
-
         if parent_response := super().pre_login(request, user, **kwargs):
             # A response from the parent means the login process must be
             # interrupted, e.g. due to the user being inactive or not having
             # validated their email address
             return parent_response
 
-        # If MFA is activated, display the token form before let them in
-        if get_mfa_model().objects.filter(is_active=True, user=user).exists():
+        mfa_allowed = True
+        if settings.STRIPE_ENABLED:
+            mfa_allowed = (
+                user_has_paid_subscription(user.username)
+                or MfaAvailableToUser.objects.filter(user=user).exists()
+            )
 
+        # If MFA is activated and allowed for the user, display the token form before letting them in
+        if (
+            mfa_allowed
+            and get_mfa_model()
+            .objects.filter(is_active=True, user=user)
+            .exists()
+        ):
             ephemeral_token_cache = user_token_generator.make_token(user)
             mfa_token_form = MfaTokenForm(
                 initial={'ephemeral_token': ephemeral_token_cache}
             )
 
-            next_url = (
-                kwargs.get('redirect_url')
-                or resolve_url(settings.LOGIN_REDIRECT_URL)
+            next_url = kwargs.get('redirect_url') or resolve_url(
+                settings.LOGIN_REDIRECT_URL
             )
+
             context = {
                 REDIRECT_FIELD_NAME: next_url,
                 'view': MfaTokenView,
