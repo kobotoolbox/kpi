64c187164fd9c451f3822724c084b3ee1f19170f
Leszek <smutnyleszek@gmail.com>
2023-08-07 17:38:47 +0200

    hide form behind a button

--- jsapp/js/router/invalidatedPassword.component.tsx
+++ jsapp/js/router/invalidatedPassword.component.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Icon from '../components/common/icon';
+import Button from '../components/common/button';
 import UpdatePasswordForm from 'js/account/security/password/updatePasswordForm.component';
 import styles from './invalidatedPassword.module.scss';
 
 export default function InvalidatedPassword() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
   return (
     <div className={styles.root}>
       <header className={styles.header}>
@@ -11,10 +14,24 @@ export default function InvalidatedPassword() {
         <h1>{t('Temporary Access Restriction: Password Update Required')}</h1>
       </header>
 
-      <p className={styles.message}>{t('Access to the app has been temporarily restricted due to a weak password. To regain access and ensure the security of your data, please update your password promptly.')}</p>
+      <p className={styles.message}>
+        {t(
+          'Access to the app has been temporarily restricted due to a weak password. To regain access and ensure the security of your data, please update your password promptly.'
+        )}
+      </p>
 
       <footer className={styles.footer}>
-        <UpdatePasswordForm />
+        {!isFormVisible && (
+          <Button
+            size='l'
+            color='blue'
+            label={t('Update password')}
+            type='full'
+            onClick={() => setIsFormVisible(!isFormVisible)}
+          />
+        )}
+
+        {isFormVisible && <UpdatePasswordForm />}
       </footer>
     </div>
   );
