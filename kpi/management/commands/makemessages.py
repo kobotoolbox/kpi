336e50bbdb1ab3af22a70bab8c6004925e10f62b
Olivier Leger <olivierleger@gmail.com>
2023-08-09 12:52:44 -0400

    Remove node_modules from parsing, add support to `nt` (plural)

--- kpi/management/commands/makemessages.py
+++ kpi/management/commands/makemessages.py
@@ -3,4 +3,11 @@ from django.core.management.commands import makemessages
 
 class Command(makemessages.Command):
 
-    xgettext_options = makemessages.Command.xgettext_options + ['--keyword=t']
+    xgettext_options = makemessages.Command.xgettext_options + [
+        '--keyword=t',
+        '--keyword=nt',
+    ]
+
+    def handle(self, *args, **options):
+        options['ignore_patterns'].append('node_modules*')
+        super().handle(*args, **options)
