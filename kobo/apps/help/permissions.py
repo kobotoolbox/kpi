22c8fe9ec2fea446c778c490b1a63cef86209b70
Olivier Leger <olivierleger@gmail.com>
2023-08-16 14:24:10 -0400

    Allow access to app messages when password is unsafe

--- kobo/apps/help/permissions.py
+++ kobo/apps/help/permissions.py
@@ -4,11 +4,9 @@ from rest_framework import exceptions, permissions
 from kpi.mixins.validation_password_permission import ValidationPasswordPermissionMixin
 
 
-class InAppMessagePermissions(
-    ValidationPasswordPermissionMixin, permissions.BasePermission
-):
+class InAppMessagePermissions(permissions.BasePermission):
     def has_permission(self, request, view):
-        self.validate_password(request)
+
         if not request.user.is_authenticated:
             # Deny access to anonymous users
             return False
