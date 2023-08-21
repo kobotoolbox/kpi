c5a15a077f1d25848f9ac96f27690a25773fdd26
Philip Edwards <phil@edwards.io>
2023-08-21 17:59:20 -0400

    Show bulk actions on custom view route

--- jsapp/js/projects/customViewRoute.tsx
+++ jsapp/js/projects/customViewRoute.tsx
@@ -21,8 +21,9 @@ import styles from './projectViews.module.scss';
 import {toJS} from 'mobx';
 import {ROOT_URL} from 'js/constants';
 import {fetchPostUrl} from 'js/api';
-import ProjectQuickActions from './projectsTable/projectQuickActions';
 import ProjectQuickActionsEmpty from './projectsTable/projectQuickActionsEmpty';
+import ProjectQuickActions from './projectsTable/projectQuickActions';
+import ProjectBulkActions from './projectsTable/projectBulkActions';
 
 function CustomViewRoute() {
   const {viewUid} = useParams();
@@ -112,13 +113,21 @@ function CustomViewRoute() {
             <ProjectQuickActions asset={selectedAssets[0]} />
           </div>
         )}
+
+        {selectedAssets.length > 1 && (
+          <div className={styles.actions}>
+            <ProjectBulkActions assets={selectedAssets} />
+          </div>
+        )}
       </header>
 
       <ProjectsTable
         assets={customView.assets}
         isLoading={!customView.isFirstLoadComplete}
         highlightedFields={getFilteredFieldsNames()}
-        visibleFields={toJS(customView.fields) || customView.defaultVisibleFields}
+        visibleFields={
+          toJS(customView.fields) || customView.defaultVisibleFields
+        }
         orderableFields={DEFAULT_ORDERABLE_FIELDS}
         order={customView.order}
         onChangeOrderRequested={customView.setOrder.bind(customView)}
