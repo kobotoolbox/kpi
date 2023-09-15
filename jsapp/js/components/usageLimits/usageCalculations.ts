e0b804abe591a577b197c101f693da7f20815a90
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2023-09-15 13:42:19 -0500

    refactor usageCalculations

--- jsapp/js/components/usageLimits/usageCalculations.ts
+++ jsapp/js/components/usageLimits/usageCalculations.ts
@@ -1,12 +1,15 @@
-import {useEffect, useState, useMemo, useReducer} from 'react';
+import {useEffect, useState, useMemo, useReducer, useContext} from 'react';
 import {getUsageForOrganization} from '../../account/usage.api';
 import type {BaseSubscription, BasePrice} from '../../account/stripe.api';
-import {getSubscription, getProducts} from '../../account/stripe.api';
+import {getProducts} from '../../account/stripe.api';
 import type {FreeTierThresholds} from 'js/envStore';
 import envStore from 'js/envStore';
 import {truncateNumber} from 'js/utils';
 import {USAGE_WARNING_RATIO} from 'js/constants';
 import useWhenStripeIsEnabled from 'js/hooks/useWhenStripeIsEnabled.hook';
+import {when} from 'mobx';
+import subscriptionStore from 'js/account/subscriptionStore';
+import {UsageContext} from 'js/account/useUsage.hook';
 
 interface UsageState {
   storage: number;
@@ -30,17 +33,9 @@ function subscriptionReducer(state: SubscribedState, action: {prodData: any}) {
   return {...state, subscribedProduct: action.prodData};
 }
 
-export const getAllExceedingLimits = () => {
+export const useExceedingLimits = () => {
   const [state, dispatch] = useReducer(subscriptionReducer, initialState);
-  const [usage, setUsage] = useState<UsageState>({
-    storage: 0,
-    monthlySubmissions: 0,
-    yearlySubmissions: 0,
-    monthlyTranscriptionMinutes: 0,
-    yearlyTranscriptionMinutes: 0,
-    monthlyTranslationChars: 0,
-    yearlyTranslationChars: 0,
-  });
+  const usage = useContext(UsageContext);
 
   const [exceedList, setExceedList] = useState<string[]>([]);
   const [warningList, setWarningList] = useState<string[]>([]);
@@ -95,36 +90,14 @@ export const getAllExceedingLimits = () => {
 
   // Get subscription data
   useWhenStripeIsEnabled(() => {
-    getSubscription().then((data) => {
-      dispatch({
-        prodData: data.results,
-      });
-    });
-  }, []);
-
-  // Get current usage
-  useEffect(() => {
-    getUsageForOrganization().then((data) => {
-      if (!data) {
-        return;
+    return when(
+      () => subscriptionStore.isInitialised,
+      () => {
+        dispatch({
+          prodData: subscriptionStore.subscriptionResponse,
+        });
       }
-      setUsage({
-        ...usage,
-        storage: data.total_storage_bytes,
-        monthlySubmissions: data.total_submission_count['current_month'],
-        yearlySubmissions: data.total_submission_count['current_year'],
-        monthlyTranscriptionMinutes: Math.floor(
-          truncateNumber(data.total_nlp_usage['asr_seconds_current_month'] / 60)
-        ), // seconds to minutes
-        yearlyTranscriptionMinutes: Math.floor(
-          truncateNumber(data.total_nlp_usage['asr_seconds_current_year'] / 60)
-        ),
-        monthlyTranslationChars:
-          data.total_nlp_usage['mt_characters_current_month'],
-        yearlyTranslationChars:
-          data.total_nlp_usage['mt_characters_current_year'],
-      });
-    });
+    );
   }, []);
 
   function setLimitThresholds(
@@ -185,68 +158,19 @@ export const getAllExceedingLimits = () => {
 
   // Check if usage is more than limit
   useMemo(() => {
-    // Check yearly vs monthly / community
-    let interval;
-    if (state.subscribedProduct?.length > 0) {
-      interval = state.subscribedProduct?.[0].items[0].price.recurring.interval;
-    }
     isOverLimit(subscribedStorageLimit, usage.storage, 'storage');
-
-    // If subscribed plan is month or community plan
-    if (interval === 'month' || interval === undefined) {
-      isOverLimit(
-        subscribedSubmissionLimit,
-        usage.monthlySubmissions,
-        'submission'
-      );
-      isOverLimit(
-        subscribedTranscriptionMinutes,
-        usage.monthlyTranscriptionMinutes,
-        'automated transcription'
-      );
-      isOverLimit(
-        subscribedTranslationChars,
-        usage.monthlyTranslationChars,
-        'machine translation'
-      );
-    }
-    // If subscribed plan is year
-    if (interval === 'year') {
-      isOverLimit(
-        subscribedSubmissionLimit,
-        usage.yearlySubmissions,
-        'submission'
-      );
-      isOverLimit(
-        subscribedTranscriptionMinutes,
-        usage.yearlyTranscriptionMinutes,
-        'automated transcription'
-      );
-      isOverLimit(
-        subscribedTranslationChars,
-        usage.yearlyTranslationChars,
-        'machine translation'
-      );
-    }
+    isOverLimit(subscribedSubmissionLimit, usage.submissions, 'submission');
+    isOverLimit(
+      subscribedTranscriptionMinutes,
+      usage.transcriptionMinutes,
+      'automated transcription'
+    );
+    isOverLimit(
+      subscribedTranslationChars,
+      usage.translationChars,
+      'machine translation'
+    );
   }, [usage]);
 
   return {exceedList, warningList};
-};
-
-export const getPlanInterval = () => {
-  const [state, dispatch] = useReducer(subscriptionReducer, initialState);
-
-  useWhenStripeIsEnabled(() => {
-    getSubscription().then((data) => {
-      dispatch({
-        prodData: data.results,
-      });
-    });
-  }, []);
-
-  let interval;
-  if (state.subscribedProduct?.length > 0) {
-    return state.subscribedProduct[0].items[0].price.recurring.interval;
-  }
-  return 'month';
 };
