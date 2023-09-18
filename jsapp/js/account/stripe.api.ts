51d9250c5eddd6cce03399ef056302e0b5b8edf8
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2023-09-18 15:03:57 -0500

    get limit metadata from product, not price

--- jsapp/js/account/stripe.api.ts
+++ jsapp/js/account/stripe.api.ts
@@ -129,7 +129,7 @@ export async function getAccountLimits() {
   let metadata;
   if (activeSubscriptions.length) {
     // get limit data from the user's subscription
-    metadata = activeSubscriptions[0].items[0].price.metadata;
+    metadata = activeSubscriptions[0].items[0].price.product.metadata;
   } else {
     // the user has no subscription, so get limits from the free monthly price
     try {
@@ -148,6 +148,7 @@ export async function getAccountLimits() {
       metadata = {};
     }
   }
+
   const limits: AccountLimit = {
     submission_limit: 'unlimited',
     nlp_seconds_limit: 'unlimited',
