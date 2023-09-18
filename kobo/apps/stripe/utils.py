2a0ffaa347070933ec8c346c8a08952a691e03f5
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2023-09-18 16:27:09 -0500

    add missing stripe/utils.py

---
+++ kobo/apps/stripe/utils.py
@@ -0,0 +1,10 @@
+def get_default_add_on_limits():
+    return {
+        'submissions_limit': 0,
+        'asr_seconds_limit': 0,
+        'mt_characters_limit': 0,
+    }
+
+
+def get_default_valid_subscription_products():
+    return []
