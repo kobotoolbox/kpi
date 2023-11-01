db45cfff864ba3bc7796232d2f56509210f988ea
Leszek <smutnyleszek@gmail.com>
2023-11-01 16:11:01 +0100

    post merge fixes

--- jsapp/js/account/accountSettingsRoute.tsx
+++ jsapp/js/account/accountSettingsRoute.tsx
@@ -4,6 +4,7 @@ import {unstable_usePrompt as usePrompt} from 'react-router-dom';
 import bem, {makeBem} from 'js/bem';
 import sessionStore from 'js/stores/session';
 import './accountSettings.scss';
+import {notify, stringToColor} from 'js/utils';
 import envStore from '../envStore';
 import {dataInterface} from '../dataInterface';
 import AccountFieldsEditor from './accountFieldsEditor.component';
