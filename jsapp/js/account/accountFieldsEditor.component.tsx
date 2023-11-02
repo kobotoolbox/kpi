16f6724f7108f006c498be0b1aa1f1f30bf777a6
Leszek <smutnyleszek@gmail.com>
2023-11-03 00:13:18 +0100

    change fields order and columnize the layout in AccountFieldsEditor

--- jsapp/js/account/accountFieldsEditor.component.tsx
+++ jsapp/js/account/accountFieldsEditor.component.tsx
@@ -82,192 +82,206 @@ export default function AccountFieldsEditor(props: AccountFieldsEditorProps) {
 
   return (
     <div>
-      {/* Privacy */}
-      {isFieldToBeDisplayed('require_auth') && (
-        <div className={styles.row}>
-          <label>{t('Privacy')}</label>
+      <div className={styles.row}>
+        {/* Privacy */}
+        {isFieldToBeDisplayed('require_auth') && (
+          <div className={styles.field}>
+            <label>{t('Privacy')}</label>
 
-          <Checkbox
-            checked={props.values.require_auth}
-            onChange={(isChecked: boolean) =>
-              onAnyFieldChange('require_auth', isChecked)
-            }
-            label={t('Require authentication to see forms and submit data')}
-          />
-        </div>
-      )}
+            <Checkbox
+              checked={props.values.require_auth}
+              onChange={(isChecked: boolean) =>
+                onAnyFieldChange('require_auth', isChecked)
+              }
+              label={t('Require authentication to see forms and submit data')}
+            />
+          </div>
+        )}
+      </div>
 
-      {/* Full name */}
-      {isFieldToBeDisplayed('name') && (
-        <div className={styles.row}>
-          <TextBox
-            label={getLabel('name')}
-            onChange={onAnyFieldChange.bind(onAnyFieldChange, 'name')}
-            value={props.values.name}
-            errors={props.errors?.name}
-            placeholder={t('Use this to display your real name to other users')}
-          />
-        </div>
-      )}
+      <div className={styles.row}>
+        {/* Full name */}
+        {isFieldToBeDisplayed('name') && (
+          <div className={styles.field}>
+            <TextBox
+              label={getLabel('name')}
+              onChange={onAnyFieldChange.bind(onAnyFieldChange, 'name')}
+              value={props.values.name}
+              errors={props.errors?.name}
+              placeholder={t('Use this to display your real name to other users')}
+            />
+          </div>
+        )}
 
-      {/* Organization */}
-      {isFieldToBeDisplayed('organization') && (
-        <div className={styles.row}>
-          <TextBox
-            label={getLabel('organization')}
-            onChange={onAnyFieldChange.bind(onAnyFieldChange, 'organization')}
-            value={props.values.organization}
-            errors={props.errors?.organization}
-          />
-        </div>
-      )}
+        {/* Gender */}
+        {isFieldToBeDisplayed('gender') && (
+          <div className={styles.field}>
+            <KoboSelect
+              label={getLabel('gender')}
+              name='gender'
+              type='outline'
+              size='l'
+              isClearable
+              isSearchable
+              selectedOption={props.values.gender}
+              onChange={(value: string | null) =>
+                onAnyFieldChange('gender', value || '')
+              }
+              options={genderSelectOptions}
+              error={props.errors?.gender}
+            />
+          </div>
+        )}
+      </div>
 
-      {/* Organization Website */}
-      {isFieldToBeDisplayed('organization_website') && (
-        <div className={styles.row}>
-          <TextBox
-            label={getLabel('organization_website')}
-            value={props.values.organization_website}
-            onChange={onAnyFieldChange.bind(
-              onAnyFieldChange,
-              'organization_website'
+      <div className={styles.row}>
+        {/* Country */}
+        {isFieldToBeDisplayed('country') && (
+          <div className={styles.field}>
+            <KoboSelect
+              label={getLabel('country')}
+              name='country'
+              type='outline'
+              size='l'
+              isClearable
+              isSearchable
+              selectedOption={props.values.country}
+              onChange={(value: string | null) =>
+                onAnyFieldChange('country', value || '')
+              }
+              options={envStore.data.country_choices}
+              error={props.errors?.country}
+            />
+          </div>
+        )}
+
+        {/* City */}
+        {isFieldToBeDisplayed('city') && (
+          <div className={styles.field}>
+            <TextBox
+              label={getLabel('city')}
+              value={props.values.city}
+              onChange={onAnyFieldChange.bind(onAnyFieldChange, 'city')}
+              errors={props.errors?.city}
+            />
+          </div>
+        )}
+      </div>
+
+      <div className={styles.row}>
+        {/* Organization */}
+        {isFieldToBeDisplayed('organization') && (
+          <div className={styles.field}>
+            <TextBox
+              label={getLabel('organization')}
+              onChange={onAnyFieldChange.bind(onAnyFieldChange, 'organization')}
+              value={props.values.organization}
+              errors={props.errors?.organization}
+            />
+          </div>
+        )}
+
+        {/* Organization Website */}
+        {isFieldToBeDisplayed('organization_website') && (
+          <div className={styles.field}>
+            <TextBox
+              label={getLabel('organization_website')}
+              value={props.values.organization_website}
+              onChange={onAnyFieldChange.bind(
+                onAnyFieldChange,
+                'organization_website'
+              )}
+              errors={props.errors?.organization_website}
+            />
+          </div>
+        )}
+      </div>
+
+      <div className={styles.row}>
+        {/* Primary Sector */}
+        {isFieldToBeDisplayed('sector') && (
+          <div className={styles.field}>
+            <KoboSelect
+              label={getLabel('sector')}
+              name='sector'
+              type='outline'
+              size='l'
+              isClearable
+              isSearchable
+              selectedOption={props.values.sector}
+              onChange={(value: string | null) =>
+                onAnyFieldChange('sector', value || '')
+              }
+              options={envStore.data.sector_choices}
+              error={props.errors?.sector}
+            />
+          </div>
+        )}
+      </div>
+
+      <div className={styles.row}>
+        {/* Bio */}
+        {isFieldToBeDisplayed('bio') && (
+          <div className={styles.field}>
+            <TextBox
+              label={getLabel('bio')}
+              value={props.values.bio}
+              onChange={onAnyFieldChange.bind(onAnyFieldChange, 'bio')}
+              errors={props.errors?.bio}
+            />
+          </div>
+        )}
+      </div>
+
+      <div className={styles.row}>
+        {/* Social */}
+        {(isFieldToBeDisplayed('twitter') ||
+          isFieldToBeDisplayed('linkedin') ||
+          isFieldToBeDisplayed('instagram')) && (
+          <>
+            <div className={styles.socialLabel}>{t('Social')}</div>
+
+            {/* Twitter */}
+            {isFieldToBeDisplayed('twitter') && (
+              <div className={styles.field}>
+                <TextBox
+                  startIcon='logo-twitter'
+                  placeholder={getLabel('twitter')}
+                  value={props.values.twitter}
+                  onChange={onAnyFieldChange.bind(onAnyFieldChange, 'twitter')}
+                  errors={props.errors?.twitter}
+                />
+              </div>
             )}
-            errors={props.errors?.organization_website}
-          />
-        </div>
-      )}
 
-      {/* Primary Sector */}
-      {isFieldToBeDisplayed('sector') && (
-        <div className={styles.row}>
-          <label htmlFor='sector'>{getLabel('sector')}</label>
-          <KoboSelect
-            name='sector'
-            type='outline'
-            size='l'
-            isClearable
-            isSearchable
-            selectedOption={props.values.sector}
-            onChange={(value: string | null) =>
-              onAnyFieldChange('sector', value || '')
-            }
-            options={envStore.data.sector_choices}
-            error={props.errors?.sector}
-          />
-        </div>
-      )}
+            {/* LinkedIn */}
+            {isFieldToBeDisplayed('linkedin') && (
+              <div className={styles.field}>
+                <TextBox
+                  startIcon='logo-linkedin'
+                  placeholder={getLabel('linkedin')}
+                  value={props.values.linkedin}
+                  onChange={onAnyFieldChange.bind(onAnyFieldChange, 'linkedin')}
+                  errors={props.errors?.linkedin}
+                />
+              </div>
+            )}
 
-      {/* Gender */}
-      {isFieldToBeDisplayed('gender') && (
-        <div className={styles.row}>
-          <label htmlFor='gender'>{getLabel('gender')}</label>
-          <KoboSelect
-            name='gender'
-            type='outline'
-            size='l'
-            isClearable
-            isSearchable
-            selectedOption={props.values.gender}
-            onChange={(value: string | null) =>
-              onAnyFieldChange('gender', value || '')
-            }
-            options={genderSelectOptions}
-            error={props.errors?.gender}
-          />
-        </div>
-      )}
-
-      {/* Bio */}
-      {isFieldToBeDisplayed('bio') && (
-        <div className={styles.row}>
-          <TextBox
-            label={getLabel('bio')}
-            value={props.values.bio}
-            onChange={onAnyFieldChange.bind(onAnyFieldChange, 'bio')}
-            errors={props.errors?.bio}
-          />
-        </div>
-      )}
-
-      {/* Country */}
-      {isFieldToBeDisplayed('country') && (
-        <div className={styles.row}>
-          <label htmlFor='country'>{getLabel('country')}</label>
-          <KoboSelect
-            name='country'
-            type='outline'
-            size='l'
-            isClearable
-            isSearchable
-            selectedOption={props.values.country}
-            onChange={(value: string | null) =>
-              onAnyFieldChange('country', value || '')
-            }
-            options={envStore.data.country_choices}
-            error={props.errors?.country}
-          />
-        </div>
-      )}
-
-      {/* City */}
-      {isFieldToBeDisplayed('city') && (
-        <div className={styles.row}>
-          <TextBox
-            label={getLabel('city')}
-            value={props.values.city}
-            onChange={onAnyFieldChange.bind(onAnyFieldChange, 'city')}
-            errors={props.errors?.city}
-          />
-        </div>
-      )}
-
-      {/* Social */}
-      {(isFieldToBeDisplayed('twitter') ||
-        isFieldToBeDisplayed('linkedin') ||
-        isFieldToBeDisplayed('instagram')) && (
-        <>
-          <label>{t('Social')}</label>
-
-          {/* Twitter */}
-          {isFieldToBeDisplayed('twitter') && (
-            <div className={styles.row}>
-              <TextBox
-                startIcon='logo-twitter'
-                placeholder={getLabel('twitter')}
-                value={props.values.twitter}
-                onChange={onAnyFieldChange.bind(onAnyFieldChange, 'twitter')}
-                errors={props.errors?.twitter}
-              />
-            </div>
-          )}
-
-          {/* LinkedIn */}
-          {isFieldToBeDisplayed('linkedin') && (
-            <div className={styles.row}>
-              <TextBox
-                startIcon='logo-linkedin'
-                placeholder={getLabel('linkedin')}
-                value={props.values.linkedin}
-                onChange={onAnyFieldChange.bind(onAnyFieldChange, 'linkedin')}
-                errors={props.errors?.linkedin}
-              />
-            </div>
-          )}
-
-          {/* Instagram */}
-          {isFieldToBeDisplayed('instagram') && (
-            <div className={styles.row}>
-              <TextBox
-                startIcon='logo-instagram'
-                placeholder={getLabel('instagram')}
-                value={props.values.instagram}
-                onChange={onAnyFieldChange.bind(onAnyFieldChange, 'instagram')}
-                errors={props.errors?.instagram}
-              />
-            </div>
-          )}
-        </>
-      )}
+            {/* Instagram */}
+            {isFieldToBeDisplayed('instagram') && (
+              <div className={styles.field}>
+                <TextBox
+                  startIcon='logo-instagram'
+                  placeholder={getLabel('instagram')}
+                  value={props.values.instagram}
+                  onChange={onAnyFieldChange.bind(onAnyFieldChange, 'instagram')}
+                  errors={props.errors?.instagram}
+                />
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
