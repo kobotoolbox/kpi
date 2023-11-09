39c103f58d69aecc74730e2a20f6af6ec3d45a34
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2023-11-09 13:04:31 -0600

    don't allow 'kobo' as a provider_id

--- kobo/apps/django_allauth/admin.py
+++ kobo/apps/django_allauth/admin.py
@@ -1,6 +1,7 @@
 from allauth.socialaccount.admin import SocialAppForm, SocialAppAdmin
 from allauth.socialaccount.models import SocialApp
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 
 
 class RequireProviderIdSocialAppForm(SocialAppForm):
@@ -9,6 +10,12 @@ class RequireProviderIdSocialAppForm(SocialAppForm):
         # require the provider_id in the admin, since we can't make it required on allauth's model
         self.fields['provider_id'].required = True
 
+    def clean(self):
+        reserved_keywords = ['kobo']
+        provider_id = self.cleaned_data.get('provider_id')
+        # we check this already for the ID in kobo/apps/accounts/apps.py
+        if provider_id in reserved_keywords:
+            raise ValidationError(f'`{provider_id}` is not a valid value for the `provider_id` setting.')
 
 class RequireProviderIdSocialAppAdmin(SocialAppAdmin):
     form = RequireProviderIdSocialAppForm
