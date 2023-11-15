373bdfa45df7cdd8d6e2f77d4726a57041547088
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2023-11-15 11:42:04 -0600

    require provider_id and provider be unique at the admin level

--- kobo/apps/django_allauth/admin.py
+++ kobo/apps/django_allauth/admin.py
@@ -2,6 +2,7 @@ from allauth.socialaccount.admin import SocialAppForm, SocialAppAdmin
 from allauth.socialaccount.models import SocialApp
 from django.contrib import admin
 from django.core.exceptions import ValidationError
+from django.db.models import Q
 
 
 class RequireProviderIdSocialAppForm(SocialAppForm):
@@ -10,12 +11,31 @@ class RequireProviderIdSocialAppForm(SocialAppForm):
         # require the provider_id in the admin, since we can't make it required on allauth's model
         self.fields['provider_id'].required = True
 
-    def clean(self):
+    def clean_provider_id(self):
         reserved_keywords = ['kobo']
         provider_id = self.cleaned_data.get('provider_id')
         # we check this already for the ID in kobo/apps/accounts/apps.py
         if provider_id in reserved_keywords:
-            raise ValidationError(f'`{provider_id}` is not a valid value for the `provider_id` setting.')
+            raise ValidationError(
+                f'`{provider_id}` is not a valid value for the `provider_id` setting.'
+            )
+
+        """
+        By default, django-allauth only supports showing one provider on the login screen.
+        But OIDC providers allow multiple subproviders, so kpi has some additional code to display multiple providers.
+        Because of that, we need to make sure that the `provider` and `provider_id` fields are unique.
+        django-allauth (as of 0.57.0) technically enforces this on the model level, but in practice it's flawed.
+        """
+        if SocialApp.objects.filter(
+            Q(provider_id=provider_id) |
+            Q(provider=provider_id)
+        ).exists():
+            raise ValidationError(
+                """The Provider ID value must be unique and cannot match an existing Provider name.
+                Please use a different value."""
+            )
+        return provider_id
+
 
 class RequireProviderIdSocialAppAdmin(SocialAppAdmin):
     form = RequireProviderIdSocialAppForm
