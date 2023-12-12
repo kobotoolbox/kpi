970c2fa68cdf137590b92b57f9f3ac8fe8561c77
RuthShryock <ruthieshryock@gmail.com>
2023-12-12 14:53:09 -0500

    fixing name typo

--- kobo/apps/service_health/test_service_health_minimal.py
+++ kobo/apps/service_health/test_service_health_minimal.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 class ServiceHealthMinimalTestCase(TestCase):
     url = reverse('service-health-minimal')
 
-    def test_service_health_minimalhealth(self):
+    def test_service_health_minimal(self):
         res = self.client.get(self.url)
         self.assertEqual(res.status_code, 200)
 
