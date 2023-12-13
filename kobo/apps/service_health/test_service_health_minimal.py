fd5383d81f43e6627eb4252832f1804fb339128b
RuthShryock <ruthieshryock@gmail.com>
2023-12-13 09:10:35 -0500

    fixing format so that assertNumQueires is used as a context manager

--- kobo/apps/service_health/test_service_health_minimal.py
+++ kobo/apps/service_health/test_service_health_minimal.py
@@ -6,13 +6,9 @@ class ServiceHealthMinimalTestCase(TestCase):
     url = reverse('service-health-minimal')
 
     def test_service_health_minimal(self):
-        res = self.client.get(self.url)
-        self.assertEqual(res.status_code, 200)
+        with self.assertNumQueries(0):
+            res = self.client.get(self.url)
+            self.assertEqual(res.status_code, 200)
 
-        # Check that the response content is "ok"
-        expected_content = 'ok'
-        actual_content = res.content.decode('utf-8')
-        self.assertEqual(actual_content, expected_content)
-
-        # Ensure that no database queries were executed
-        self.assertNumQueries(0)
+            # Check that the response content contains "ok"
+            self.assertContains(res, 'ok')
