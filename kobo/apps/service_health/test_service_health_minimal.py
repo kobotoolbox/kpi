f5e6f57b21b9cb30a2e88fcb969a8718817bd530
RuthShryock <ruthieshryock@gmail.com>
2023-12-13 10:31:32 -0500

    removing unnecessary assertion  and context manager only needs to wrap line 10

--- kobo/apps/service_health/test_service_health_minimal.py
+++ kobo/apps/service_health/test_service_health_minimal.py
@@ -8,7 +8,6 @@ class ServiceHealthMinimalTestCase(TestCase):
     def test_service_health_minimal(self):
         with self.assertNumQueries(0):
             res = self.client.get(self.url)
-            self.assertEqual(res.status_code, 200)
 
-            # Check that the response content contains "ok"
-            self.assertContains(res, 'ok')
+        # Check that the response content contains "ok"
+        self.assertContains(res, 'ok')
