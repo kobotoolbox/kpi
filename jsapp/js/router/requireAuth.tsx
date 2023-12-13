70552f40b1ccfe22b151a4dc1f6b2fc4befb2a6b
RuthShryock <ruthieshryock@gmail.com>
2023-12-13 15:12:06 -0500

    added redirection for pages that require auth but are not logged in

--- jsapp/js/router/requireAuth.tsx
+++ jsapp/js/router/requireAuth.tsx
@@ -1,18 +1,39 @@
-import React, {ReactElement, Suspense, useState} from 'react';
+import React, {ReactElement, Suspense, useEffect, useState} from 'react';
 import {RouteObject} from 'react-router-dom';
 import sessionStore from 'js/stores/session';
-import AccessDenied from './accessDenied';
+import LoadingSpinner from '../components/common/loadingSpinner';
+import {PATHS} from './routerConstants';
 
 interface Props {
   children: RouteObject[] | undefined | ReactElement;
+  redirect?: boolean;
 }
 
-/** https://gist.github.com/mjackson/d54b40a094277b7afdd6b81f51a0393f */
-export default function RequireAuth({children}: Props) {
+export function getRedirectedLogin() {
+  let loginUrl = PATHS.LOGIN;
+  const loc = location.hash.split('#');
+  const currentLoc = loc.length > 1 ? loc[1] : '';
+
+  if (currentLoc) {
+    const nextUrl = encodeURIComponent(`/#${currentLoc}`);
+    loginUrl += `?next=${nextUrl}`;
+  }
+
+  window.location.href = loginUrl;
+}
+
+export default function RequireAuth({children, redirect = true}: Props) {
   const [session] = useState(() => sessionStore);
-  return session.isLoggedIn ? (
+
+  useEffect(() => {
+    if (redirect && !session.isLoggedIn) {
+      getRedirectedLogin();
+    }
+  }, [session.isLoggedIn, redirect]);
+
+  return redirect && session.isLoggedIn ? (
     <Suspense fallback={null}>{children}</Suspense>
   ) : (
-    <AccessDenied />
+    <LoadingSpinner />
   );
-};
+}
