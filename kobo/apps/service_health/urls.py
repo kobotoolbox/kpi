4858f0ec07da6898ebc272bf8dae52966d93f9db
RuthShryock <ruthieshryock@gmail.com>
2023-12-13 12:36:20 -0500

    cleaning up

--- kobo/apps/service_health/urls.py
+++ kobo/apps/service_health/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import service_health
-from .views import service_health_minimal
+from .views import service_health, service_health_minimal
 
 urlpatterns = [
     path('service_health/', service_health, name="service-health"),
