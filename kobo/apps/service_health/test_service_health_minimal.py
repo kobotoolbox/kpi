9e3ba84b2b7a370cb171f271fc471d47717ee199
RuthShryock <ruthieshryock@gmail.com>
2023-12-13 12:32:42 -0500

    adding docstring and correct django convention for urls

--- kobo/apps/service_health/test_service_health_minimal.py
+++ kobo/apps/service_health/test_service_health_minimal.py
@@ -3,11 +3,14 @@ from django.urls import reverse
 
 
 class ServiceHealthMinimalTestCase(TestCase):
+    """
+    Test for endpoint which makes no connections to databases or other external
+    services and is helpful for HTTP health checks.
+    """
+
     url = reverse('service-health-minimal')
 
     def test_service_health_minimal(self):
         with self.assertNumQueries(0):
             res = self.client.get(self.url)
-
-        # Check that the response content contains "ok"
         self.assertContains(res, 'ok')
--- kobo/apps/service_health/urls.py
+++ kobo/apps/service_health/urls.py
@@ -5,5 +5,9 @@ from .views import service_health_minimal
 
 urlpatterns = [
     path('service_health/', service_health, name="service-health"),
-    path('service_health/minimal', service_health_minimal, name="service-health-minimal"),
+    path(
+        'service_health/minimal/',
+        service_health_minimal,
+        name="service-health-minimal",
+    ),
 ]
