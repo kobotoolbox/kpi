acc89d2ad4914003e7f5d52af4ddb500ac135810
RuthShryock <ruthieshryock@gmail.com>
2023-12-13 12:35:04 -0500

    fixing docstring location

--- kobo/apps/service_health/test_service_health_minimal.py
+++ kobo/apps/service_health/test_service_health_minimal.py
@@ -3,14 +3,13 @@ from django.urls import reverse
 
 
 class ServiceHealthMinimalTestCase(TestCase):
-    """
-    Test for endpoint which makes no connections to databases or other external
-    services and is helpful for HTTP health checks.
-    """
-
     url = reverse('service-health-minimal')
 
     def test_service_health_minimal(self):
+        """
+        Test for endpoint which makes no connections to databases or other external
+        services and is helpful for HTTP health checks.
+        """
         with self.assertNumQueries(0):
             res = self.client.get(self.url)
         self.assertContains(res, 'ok')
