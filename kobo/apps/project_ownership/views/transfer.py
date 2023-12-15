05fb0f5cd1901d93618ba998ef447b81cf9e0e69
Olivier Leger <olivierleger@gmail.com>
2023-12-15 10:50:20 -0500

    API doc

--- kobo/apps/project_ownership/views/transfer.py
+++ kobo/apps/project_ownership/views/transfer.py
@@ -8,6 +8,60 @@ from ..serializers import TransferDetailSerializer
 
 class TransferViewSet(viewsets.ReadOnlyModelViewSet):
 
+    """
+    ## List of transfers
+
+    <span class='label label-danger'>Not implemented</span> Refer to invite list instead.
+
+    ## Transfer detail
+
+    It provides more details on error
+
+    <pre class="prettyprint">
+    <b>GET</b> /api/v2/project-ownership/transfers/&lt;transfer_uid&gt;/
+    </pre>
+
+    > Example
+    >
+    >       curl -X GET https://[kpi]/api/v2/project-ownership/transfers/pot54pTqM5qwKdZ4wnNdiwDY/
+
+    <pre class="prettyprint">
+    <b>HTTP 200 OK</b>
+    {
+       "url": "https://[kpi]/api/v2/project-ownership/transfers/pot54pTqM5qwKdZ4wnNdiwDY/",
+       "asset": "https://[kpi]/api/v2/assets/a8rg3w7ZNL5Nwj7iHzKiyX/",
+       "status": "in_progress",
+       "error": null,
+       "date_modified": "2023-12-14T21:17:29Z",
+       "statuses": [
+            {
+                "status": "success",
+                "status_type": "submissions",
+                "error": null
+            },
+            {
+                "status": "success",
+                "status_type": "media_files",
+                "error": null
+            },
+            {
+                "status": "in_progress",
+                "status_type": "attachments",
+                "error": null
+            },
+            {
+                "status": "in_progress",
+                "status_type": "global",
+                "error": null
+            }
+        ]
+    }
+    </pre>
+
+
+    ### CURRENT ENDPOINT
+    """
+
     model = Transfer
     lookup_field = 'uid'
     permission_classes = (IsAuthenticated,)
