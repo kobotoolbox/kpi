8d0e2f577fecc736bee26068825a3106815e1d50
James Kiger <james.kiger@gmail.com>
2023-12-20 07:40:17 -0500

    Add invitation acceptance modal styling

--- jsapp/js/components/transferProjectsInvite.component.tsx
+++ jsapp/js/components/transferProjectsInvite.component.tsx
@@ -5,7 +5,7 @@ import KoboModalFooter from 'js/components/modals/koboModalFooter';
 import Button from 'js/components/common/button';
 import Icon from 'js/components/common/icon';
 
-import styles from './transferProjects.module.scss';
+import styles from './transferProjectsInvite.module.scss';
 
 export default function TransferProjectsInvite() {
   const [isModalOpen, setIsModalOpen] = useState(true);
@@ -15,45 +15,65 @@ export default function TransferProjectsInvite() {
     setIsModalOpen(!isModalOpen);
   };
 
+  let noteClass = styles.note;
+  if (isDeclined) {
+    noteClass = [noteClass, styles.declinedNote].join(' ');
+  }
+
   return (
     <KoboModal isOpen={isModalOpen} onRequestClose={toggleModal} size='medium'>
-      <KoboModalHeader>
+      <KoboModalHeader onRequestCloseByX={toggleModal}>
         {isDeclined
           ? t('Project transfer declined')
           : 'Accept ownership transfer'}
       </KoboModalHeader>
-      {!isDeclined && (
-        <div>
-          <section className={styles.modalBody}>
-            <p className={styles.modalCopy}>
+      <div>
+        <section className={styles.modalBody}>
+          {isDeclined ? (
+            <p>
+              {t(
+                'You have declined the request of transfer ownership for {project name}.'
+              )}
+            </p>
+          ) : (
+            <p>
               {t(
                 'When you accept the ownership transfer of project {project name}, all of the submissions, data storage, and transcription and translation usage for the project will be transferred to you and count against your plan limits.'
               )}
-              <br />
-              <br />
-              <strong>
-                {t(
-                  'Once you accept, the transfer might take a few minutes to complete.'
-                )}
-              </strong>
             </p>
+          )}
 
-            <div className={styles.warning}>
-              <Icon
-                name='information'
-                size='s'
-                color='red'
-                classNames={[styles.warningIcon]}
-              />
-
-              <div className={styles.warningCopy}>
+          {!isDeclined && (
+            <strong>
+              {t(
+                'Once you accept, the transfer might take a few minutes to complete.'
+              )}
+            </strong>
+          )}
+          <div className={noteClass}>
+            <Icon
+              name='information'
+              size='s'
+              color='blue'
+              classNames={[styles.noteIcon]}
+            />
+            {isDeclined ? (
+              <div>
+                {t(
+                  '{current owner name} will receive a notification that the transfer was incomplete. {current owner name} will remain the project owner.'
+                )}
+              </div>
+            ) : (
+              <div>
                 {t(
                   'Note: The previous owner has permissions to manage the project. You can change user permissions in the project sharing settings.'
                 )}
               </div>
-            </div>
-          </section>
+            )}
+          </div>
+        </section>
 
+        {!isDeclined && (
           <KoboModalFooter alignment='end'>
             <Button
               label={t('Decline')}
@@ -72,33 +92,8 @@ export default function TransferProjectsInvite() {
               size='l'
             />
           </KoboModalFooter>
-        </div>
-      )}
-
-      {isDeclined && (
-        <section className={styles.modalBody}>
-          <p className={styles.modalCopy}>
-            {t(
-              'You have declined the request of transfer ownership for {project name}.'
-            )}
-          </p>
-
-          <div className={styles.warning}>
-            <Icon
-              name='information'
-              size='s'
-              color='red'
-              classNames={[styles.warningIcon]}
-            />
-
-            <div className={styles.warningCopy}>
-              {t(
-                '{current owner name} will receive a notification that the transfer was incomplete. {current owner name} will remain the project owner.'
-              )}
-            </div>
-          </div>
-        </section>
-      )}
+        )}
+      </div>
     </KoboModal>
   );
 }
