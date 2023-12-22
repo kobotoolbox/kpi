e09b91837c6868d4619277d35183bdfb796bdc76
RuthShryock <ruthieshryock@gmail.com>
2023-12-22 10:15:18 -0800

    adding organization user as inline

--- kobo/apps/organizations/admin.py
+++ kobo/apps/organizations/admin.py
@@ -18,9 +18,14 @@ class OwnerInline(BaseOwnerInline):
     model = OrganizationOwner
 
 
+class OrgUserInline(admin.StackedInline):
+    model = OrganizationUser
+    extra = 0
+
+
 @admin.register(Organization)
 class OrgAdmin(BaseOrganizationAdmin):
-    inlines = [OwnerInline]
+    inlines = [OwnerInline, OrgUserInline]
     readonly_fields = ['id']
 
 
