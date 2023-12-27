c73e801c5c53a2c5b3413939b56a9228d906d9b1
James Kiger <james.kiger@gmail.com>
2023-12-27 15:51:38 -0500

    Add unit tests, some broken

--- kobo/apps/project_ownership/tests/api/v2/test_api.py
+++ kobo/apps/project_ownership/tests/api/v2/test_api.py
@@ -1,3 +1,5 @@
+import pytest
+
 from constance.test import override_config
 from django.contrib.auth import get_user_model
 from rest_framework import status
@@ -8,6 +10,12 @@ from kpi.models import Asset
 from kpi.tests.kpi_test_case import KpiTestCase
 from kpi.urls.router_api_v2 import URL_NAMESPACE as ROUTER_URL_NAMESPACE
 
+from kobo.apps.project_ownership.models import (
+    Invite,
+    InviteStatusChoices,
+    Transfer,
+)
+
 
 class ProjectOwnershipAPITestCase(KpiTestCase):
 
@@ -17,7 +25,12 @@ class ProjectOwnershipAPITestCase(KpiTestCase):
 
         super().setUp()
         User = get_user_model()  # noqa
+        self.someuser = User.objects.get(username='someuser')
         self.anotheruser = User.objects.get(username='anotheruser')
+        self.thirduser =  User.objects.create_user(
+            username='thirduser', password='thirduser', email='thirduser@example.com'
+        )
+
         self.invite_url = reverse(self._get_endpoint('project-ownership-invite-list'))
         self.asset = Asset.objects.get(pk=1)
 
@@ -48,40 +61,147 @@ class ProjectOwnershipAPITestCase(KpiTestCase):
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     def test_cannot_create_invite_as_regular_user(self):
-        pass
+        
+        self.client.login(username='thirduser', password='thirduser')
+        payload = {
+            'recipient': self.absolute_reverse(
+                self._get_endpoint('user-detail'),
+                args=[self.anotheruser.username]
+            ),
+            'assets': [self.asset.uid]
+        }
+        response = self.client.post(self.invite_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+class ProjectOwnershipInviteAPITestCase(KpiTestCase):
+
+    URL_NAMESPACE = ROUTER_URL_NAMESPACE
+
+    def setUp(self) -> None:
+
+        super().setUp()
+        User = get_user_model()  # noqa
+        self.someuser = User.objects.get(username='someuser')
+        self.anotheruser = User.objects.get(username='anotheruser')
+        self.thirduser =  User.objects.create_user(
+            username='thirduser', password='thirduser', email='thirduser@example.com'
+        )
+
+        self.asset = Asset.objects.get(pk=1)
+        self.invite = Invite.objects.create(sender=self.someuser, recipient=self.anotheruser)
+        self.transfer = Transfer.objects.create(invite=self.invite, asset=self.asset)
+        self.invite_detail_url = reverse(self._get_endpoint('project-ownership-invite-detail'), args=[self.invite.uid])
 
     def test_can_cancel_invite_as_sender(self):
-        pass
+
+        self.client.login(username='someuser', password='someuser')
+        payload = {
+            'status': InviteStatusChoices.CANCELLED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()["status"] == InviteStatusChoices.CANCELLED.value
 
     def test_cannot_cancel_invite_as_regular_sender(self):
-        pass
+
+        self.client.login(username='thirduser', password='thirduser')
+        payload = {
+            'status': InviteStatusChoices.CANCELLED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_404_NOT_FOUND
 
     def test_cannot_cancel_invite_as_recipient(self):
-        pass
+
+        self.client.login(username='anotheruser', password='anotheruser')
+        payload = {
+            'status': InviteStatusChoices.CANCELLED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
 
     def test_can_accept_invite_as_recipient(self):
-        pass
+
+        self.client.login(username='anotheruser', password='anotheruser')
+        payload = {
+            'status': InviteStatusChoices.ACCEPTED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()["status"] == "in_progress"
 
     def test_can_decline_invite_as_recipient(self):
-        pass
+
+        self.client.login(username='anotheruser', password='anotheruser')
+        payload = {
+            'status': InviteStatusChoices.DECLINED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()["status"] == "declined"
 
     def test_cannot_accept_invite_as_sender(self):
-        pass
+
+        self.client.login(username='someuser', password='someuser')
+        payload = {
+            'status': InviteStatusChoices.ACCEPTED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
 
     def test_cannot_decline_invite_as_sender(self):
-        pass
+
+        self.client.login(username='someuser', password='someuser')
+        payload = {
+            'status': InviteStatusChoices.DECLINED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
 
     def test_cannot_change_in_progress_invite(self):
-        pass
+        
+        self.invite.status = InviteStatusChoices.IN_PROGRESS.value
+        self.invite.save()
+
+        self.client.login(username='anotheruser', password='anotheruser')
+        payload = {
+            'status': InviteStatusChoices.DECLINED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     def test_cannot_change_complete_invite(self):
-        pass
+        self.invite.status = InviteStatusChoices.COMPLETE.value
+        self.invite.save()
+
+        self.client.login(username='anotheruser', password='anotheruser')
+        payload = {
+            'status': InviteStatusChoices.DECLINED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     def test_cannot_change_failed_invite(self):
-        pass
+        self.invite.status = InviteStatusChoices.FAILED.value
+        self.invite.save()
+
+        self.client.login(username='anotheruser', password='anotheruser')
+        payload = {
+            'status': InviteStatusChoices.ACCEPTED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     def test_cannot_change_expired_invite(self):
-        pass
+        self.invite.status = InviteStatusChoices.EXPIRED.value
+        self.invite.save()
+
+        self.client.login(username='anotheruser', password='anotheruser')
+        payload = {
+            'status': InviteStatusChoices.ACCEPTED.value
+        }
+        response = self.client.patch(self.invite_detail_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
 class ProjectOwnershipAccountUsageAPITestCase(KpiTestCase):
@@ -139,16 +259,72 @@ class ProjectOwnershipInAppMessageAPITestCase(KpiTestCase):
         in_app_response = self.client.get(reverse('inappmessage-list'))
         assert in_app_response.status_code == status.HTTP_200_OK
         assert in_app_response.data['count'] == 1
-
+    
+    @override_config(PROJECT_OWNERSHIP_AUTO_ACCEPT_INVITES=True)
     def test_other_users_do_not_receive_in_app_message(self):
-        # same test as test_shared_users_receive_in_app_message but do not give
-        # alice 'view_asset' permission
-        pass
+        self.client.login(username='alice', password='alice')
+        
+        in_app_response = self.client.get(reverse('inappmessage-list'))
+        assert in_app_response.status_code == status.HTTP_200_OK
+        assert in_app_response.data['count'] == 0
 
+        self.client.login(username='someuser', password='someuser')
+        payload = {
+            'recipient': self.absolute_reverse(
+                self._get_endpoint('user-detail'),
+                args=[self.anotheruser.username]
+            ),
+            'assets': [self.asset.uid]
+        }
+        response = self.client.post(self.invite_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+        self.client.login(username='alice', password='alice')
+        in_app_response = self.client.get(reverse('inappmessage-list'))
+        assert in_app_response.status_code == status.HTTP_200_OK
+        assert in_app_response.data['count'] == 0
+
+    @override_config(PROJECT_OWNERSHIP_AUTO_ACCEPT_INVITES=True)
     def test_previous_owner_do_not_receive_in_app_message(self):
 
-        pass
+        self.client.login(username='someuser', password='someuser')
+        in_app_response = self.client.get(reverse('inappmessage-list'))
+        assert in_app_response.status_code == status.HTTP_200_OK
+        assert in_app_response.data['count'] == 0
+
+        payload = {
+            'recipient': self.absolute_reverse(
+                self._get_endpoint('user-detail'),
+                args=[self.anotheruser.username]
+            ),
+            'assets': [self.asset.uid]
+        }
+        response = self.client.post(self.invite_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+        in_app_response = self.client.get(reverse('inappmessage-list'))
+        assert in_app_response.status_code == status.HTTP_200_OK
+        assert in_app_response.data['count'] == 0
 
     def test_new_owner_do_not_receive_in_app_message(self):
 
-        pass
+        self.client.login(username='anotheruser', password='anotheruser')
+        in_app_response = self.client.get(reverse('inappmessage-list'))
+        assert in_app_response.status_code == status.HTTP_200_OK
+        assert in_app_response.data['count'] == 0
+
+        self.client.login(username='someuser', password='someuser')
+        payload = {
+            'recipient': self.absolute_reverse(
+                self._get_endpoint('user-detail'),
+                args=[self.anotheruser.username]
+            ),
+            'assets': [self.asset.uid]
+        }
+        response = self.client.post(self.invite_url, data=payload, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+        self.client.login(username='anotheruser', password='anotheruser')
+        in_app_response = self.client.get(reverse('inappmessage-list'))
+        assert in_app_response.status_code == status.HTTP_200_OK
+        assert in_app_response.data['count'] == 0
