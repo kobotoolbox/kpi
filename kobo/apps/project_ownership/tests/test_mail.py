c08fa655212082a246427110a1a7956509114ee0
James Kiger <james.kiger@gmail.com>
2023-12-28 14:42:26 -0500

    Finish email tests

--- kobo/apps/project_ownership/tests/test_mail.py
+++ kobo/apps/project_ownership/tests/test_mail.py
@@ -1,3 +1,4 @@
+from constance.test import override_config
 from django.contrib.auth import get_user_model
 from django.core import mail
 from rest_framework.reverse import reverse
@@ -6,7 +7,8 @@ from kpi.models import Asset
 from kpi.tests.kpi_test_case import KpiTestCase
 from kpi.urls.router_api_v2 import URL_NAMESPACE as ROUTER_URL_NAMESPACE
 
-from kobo.apps.project_ownership.models import Invite, InviteStatusChoices
+from ..models import Invite, InviteStatusChoices, Transfer, TransferStatusChoices
+from ..tasks import mark_as_expired
 
 
 class ProjectOwnershipMailTestCase(KpiTestCase):
@@ -65,8 +67,26 @@ class ProjectOwnershipMailTestCase(KpiTestCase):
             mail.outbox[0].subject, 'KoboToolbox project ownership transfer incomplete'
         )
 
+    @override_config(PROJECT_OWNERSHIP_INVITE_EXPIRY=0)
     def test_sender_receives_expired_notification(self):
-        raise NotImplementedError('To be implemented')
+        Invite.objects.create(sender=self.someuser, recipient=self.anotheruser)
 
+        mark_as_expired()
+
+        self.assertEqual(mail.outbox[0].to[0], self.someuser.email)
+        self.assertEqual(mail.outbox[0].subject, 'Invite has expired')
+
+    @override_config(PROJECT_OWNERSHIP_ADMIN_EMAIL='admin@admin.com')
     def test_admins_receive_failure_report(self):
-        raise NotImplementedError('To be implemented')
+        asset = Asset.objects.get(pk=1)
+        invite = Invite.objects.create(sender=self.someuser, recipient=self.anotheruser)
+        transfer = Transfer.objects.create(invite=invite, asset=asset)
+
+        transfer.status = TransferStatusChoices.FAILED.value
+        transfer.save()
+
+        self.assertEqual(mail.outbox[0].to[0], 'admin@admin.com')
+        self.assertEqual(
+            mail.outbox[0].subject,
+            'KoboToolbox Notifications: Project ownership transfer failure',
+        )
