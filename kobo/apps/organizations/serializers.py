371b2b96a26e9261612629081fbf348bed873a1d
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2024-01-04 16:25:12 -0600

    add owner to /api/v2/organizations/ response

--- kobo/apps/organizations/serializers.py
+++ kobo/apps/organizations/serializers.py
@@ -1,13 +1,34 @@
 from rest_framework import serializers
 
-from kobo.apps.organizations.models import Organization, create_organization
+from kobo.apps.organizations.models import (
+    create_organization,
+    Organization,
+    OrganizationOwner,
+    OrganizationUser,
+)
+
+
+class OrganizationUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrganizationUser
+        fields = ['user', 'organization']
+
+
+class OrganizationOwnerSerializer(serializers.ModelSerializer):
+    organization_user = OrganizationUserSerializer()
+
+    class Meta:
+        model = OrganizationOwner
+        fields = ['organization_user']
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
+    owner = OrganizationOwnerSerializer()
+
     class Meta:
         model = Organization
-        fields = ['id', 'name', 'is_active', 'created', 'modified', 'slug']
-        read_only_fields = ["id", "slug"]
+        fields = ['id', 'name', 'is_active', 'created', 'modified', 'slug', 'owner']
+        read_only_fields = ['id', 'slug', 'owner']
 
     def create(self, validated_data):
         user = self.context['request'].user
