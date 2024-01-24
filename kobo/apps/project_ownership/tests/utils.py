97fa9adf1c3af5bcf23e221d82036448c0678cfd
Olivier Leger <olivierleger@gmail.com>
2024-01-24 15:03:39 -0500

    Fix unit tests after merge conflicts

--- kobo/apps/project_ownership/tests/utils.py
+++ kobo/apps/project_ownership/tests/utils.py
@@ -1,10 +1,15 @@
+from django.db.models import F
 from kpi.models.asset import Asset
 
 
 class MockServiceUsageSerializer:
 
     def _get_storage_usage(self):
-        assets = Asset.objects.filter(owner__in=self._user_ids)
+
+        assets = Asset.objects.annotate(user_id=F('owner_id')).filter(
+            self._user_id_query
+        )
+
         self._total_storage_bytes = 0
         for asset in assets:
             if asset.has_deployment:
@@ -19,7 +24,9 @@ class MockServiceUsageSerializer:
             'current_year': 0,
             'current_month': 0,
         }
-        assets = Asset.objects.filter(owner__in=self._user_ids)
+        assets = Asset.objects.annotate(user_id=F('owner_id')).filter(
+            self._user_id_query
+        )
         for asset in assets:
             if asset.has_deployment:
                 submissions = asset.deployment.get_submissions(asset.owner)
