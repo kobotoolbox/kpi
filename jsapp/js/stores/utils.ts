4a6500065c6477a3106799da251aa4d38d1a55b2
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2024-02-22 10:12:38 -0600

    fix some typescript errors in cacheAction

--- jsapp/js/stores/utils.ts
+++ jsapp/js/stores/utils.ts
@@ -3,26 +3,30 @@
  * Cached function will only execute once per page load, or when `refresh` is true
  * Prevents multiple copies of a listener from firing at the same time for the same arguments
  **/
-export const cacheAction = (callback, onDone, onFail) => {
-  // we initialize the cache here so it gets stored as a closure with the returned function
-  const responseCache = {};
-  const cachedCallback = (params, refresh = false) => {
+export const cacheAction = (
+  callback: (params: Record<string, any>) => JQuery.jqXHR<any>,
+  onDone: Function,
+  onFail: Function
+) => {
+  // we initialize the cache here so that it gets stored as a closure with the returned function
+  const responseCache: Record<string, any> = {};
+  const cachedCallback = (params: Record<string, any>, refresh = false) => {
     // get a hash of the parameters so we can construct a bounded-length cache key
-    const paramsHash = JSON.stringify(params);
-    if (refresh || !(paramsHash in responseCache)) {
+    const cacheKey = JSON.stringify(params);
+    if (refresh || !(cacheKey in responseCache)) {
       // initialize the cache entry with an empty value, or evict stale cached entry for this asset
       // we use a string instead of null/undefined to distinguish from null responses from the server, etc.
-      responseCache[paramsHash] = 'pending';
+      responseCache[cacheKey] = 'pending';
       callback(params)
         .done((res) => {
           // save the fully loaded response to the cache
-          responseCache[paramsHash] = res;
+          responseCache[cacheKey] = res;
           onDone(res);
         })
-        .fail(onFail);
-    } else if (responseCache[paramsHash] !== 'pending') {
+        .fail((res) => onFail(res));
+    } else if (responseCache[cacheKey] !== 'pending') {
       // we have a cache entry, use it
-      onDone(responseCache[paramsHash]);
+      onDone(responseCache[cacheKey]);
     }
     // the cache entry for this action is currently pending, do nothing
   };
