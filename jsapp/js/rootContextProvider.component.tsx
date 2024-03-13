be55c5efb11907826cbe69dc2ef3f7a5bf7ac96a
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2024-03-13 12:40:59 -0500

    comment RootContextProvider

--- jsapp/js/rootContextProvider.component.tsx
+++ jsapp/js/rootContextProvider.component.tsx
@@ -1,6 +1,11 @@
 import React, {PropsWithChildren} from 'react';
 import {OrganizationWrapper} from 'js/account/organizations/useOrganization.hook';
 
+/* This is a context provider that wraps the root element (in ./app.js)
+ * Use this if you need to share context between, for example, the sidebar and the main page
+ * *Don't* use this as a catch-all for context providers - make sure to wrap them around
+ * the closest common parent component/route
+ */
 export const RootContextProvider = ({children}: PropsWithChildren<never>) => {
   return <OrganizationWrapper>{children}</OrganizationWrapper>;
 };
