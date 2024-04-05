b04a8ebca36160de7a5e667b04809779f5561011
Jessica Thomas <jessica.thomas@kobotoolbox.org>
2024-04-05 09:13:54 -0500

    make html error templates only show if html has been requested

--- kobo/urls.py
+++ kobo/urls.py
@@ -1,10 +1,14 @@
 # coding: utf-8
 from django.conf import settings
+from django.conf.urls import handler404, handler500
 from django.contrib import admin
 from django.contrib.admin.views.decorators import staff_member_required
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import include, path, re_path
 from django.views.generic.base import RedirectView
-
+from rest_framework import status
+from rest_framework.exceptions import server_error
 
 admin.autodiscover()
 admin.site.login = staff_member_required(
@@ -38,3 +42,22 @@ if settings.ENABLE_METRICS:
     urlpatterns.append(
         path('', include('django_prometheus.urls')),
     )
+
+
+def render500(request):
+    format = request.GET.get('format') or request.POST.get('format')
+    if not request.accepts('text/html') or (format and format != 'api'):
+        # fall back to the DRF 500 handler if they're requesting a data route
+        return server_error(request)
+    return render(request, 'custom_500.html', status=500)
+
+
+def render404(request, exception):
+    format = request.GET.get('format') or request.POST.get('format')
+    if not request.accepts('text/html') or (format and format != 'api'):
+        return HttpResponse('Page not found (404)', status=status.HTTP_404_NOT_FOUND)
+    return render(request, 'custom_404.html', status=404)
+
+
+handler404 = render404
+handler500 = render500
