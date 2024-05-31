c47101f6bc0542b58e29d73b3cb821ba8049bec3
RuthShryock <ruthieshryock@gmail.com>
2024-05-31 17:07:22 -0400

    rounding to nearest first decimal

--- jsapp/js/account/usage/usageProjectBreakdown.tsx
+++ jsapp/js/account/usage/usageProjectBreakdown.tsx
@@ -180,9 +180,9 @@ const ProjectBreakdown = () => {
                 </td>
                 <td>{prettyBytes(project.storage_bytes)}</td>
                 <td>
-                  {Math.round(
-                    project.nlp_usage_current_month.total_nlp_asr_seconds / 60
-                  ).toLocaleString()}
+                  {(project.nlp_usage_current_month.total_nlp_asr_seconds / 60)
+                    .toFixed(1)
+                    .toLocaleString()}
                 </td>
                 <td>
                   {project.nlp_usage_current_month.total_nlp_mt_characters.toLocaleString()}
