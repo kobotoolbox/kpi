4d31241c9c2c17447f4b3c4176d3457e7a1bd35c
David Burke <david@burkesoftware.com>
2024-08-05 14:26:41 -0400

    Make healthcheck avoid middleware

--- kobo/apps/service_health/test_service_health_minimal.py
+++ kobo/apps/service_health/test_service_health_minimal.py
@@ -1,14 +1,14 @@
-from django.test import TestCase
-from django.urls import reverse
+from django.test import TestCase, override_settings
 
 
 class ServiceHealthMinimalTestCase(TestCase):
-    url = reverse('service-health-minimal')
+    url = "/service_health/minimal/"
 
+    @override_settings(ALLOWED_HOSTS=["nope"])
     def test_service_health_minimal(self):
         """
         Test for endpoint which makes no connections to databases or other external
-        services and is helpful for HTTP health checks.
+        services, ingores ALLOWED_HOSTS, and is helpful for HTTP health checks.
         """
         with self.assertNumQueries(0):
             res = self.client.get(self.url)
