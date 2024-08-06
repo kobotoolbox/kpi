078b971592e17d8682bd71252f3e282df521b236
David Burke <david@burkesoftware.com>
2024-08-06 14:25:59 -0400

    Address feedback, remove openrose service health check

--- kobo/apps/service_health/test_service_health_minimal.py
+++ kobo/apps/service_health/test_service_health_minimal.py
@@ -2,13 +2,13 @@ from django.test import TestCase, override_settings
 
 
 class ServiceHealthMinimalTestCase(TestCase):
-    url = '/service_health/minimal/'
+    url = '/service_health/minimal/'  # Path checked in middleware, can't use reverse
 
     @override_settings(ALLOWED_HOSTS=['nope'])
     def test_service_health_minimal(self):
         """
         Test for endpoint which makes no connections to databases or other external
-        services, ingores ALLOWED_HOSTS, and is helpful for HTTP health checks.
+        services, ignores ALLOWED_HOSTS, and is helpful for HTTP health checks.
         """
         with self.assertNumQueries(0):
             res = self.client.get(self.url)
