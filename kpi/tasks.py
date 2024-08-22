f5e1f923f6f6ba2529b38e4184ed6ddf33da8ea6
Olivier Leger <olivierleger@gmail.com>
2024-08-22 18:12:43 -0400

    Add TODO comment

--- kpi/tasks.py
+++ kpi/tasks.py
@@ -81,6 +81,7 @@ def sync_media_files(asset_uid):
         # 🙈 Race condition: Celery task starts too fast and does not see
         # the deployment data, even if asset has been saved prior to call this
         # task
+        # TODO Find why the race condition happens and remove `time.sleep(1)`
         time.sleep(1)
         asset.refresh_from_db(fields=['_deployment_data'])
 
