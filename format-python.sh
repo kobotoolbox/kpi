499a7c048cacf3689ed83119b77a8bea0b85574d
Olivier Leger <olivierleger@gmail.com>
2024-09-09 18:35:36 -0400

    Fix bug reformatting whole code base when Python code is not changed

--- format-python.sh
+++ format-python.sh
@@ -2,9 +2,6 @@
 
 BASE_REVISION=$1
 
-echo $BASE_REVISION
-
-
 if [ -z "$BASE_REVISION" ]; then
     BASE_REVISION="beta"
 elif [ "$BASE_REVISION" == "-l" ] || [ "$BASE_REVISION" == "--last" ]; then
@@ -20,9 +17,14 @@ fi
 #  - Unnecessary parentheses after class definition (--select UP039)
 #  - Indentation warning (--select W1)
 #  - No newline at end of file (--select W292)
-echo "Using ruff..."
-git diff --name-only "$BASE_REVISION" | grep '.py' | xargs ruff check --select Q --select I --select F401 --select UP026 --select UP034 --select UP039 --select W292 --fix
+PYTHON_CHANGES=$(git diff --name-only "$BASE_REVISION" | grep '\.py')
+if [ -n "$PYTHON_CHANGES" ]; then
+    echo "Using ruff..."
+    git diff --name-only "$BASE_REVISION" | grep '\.py' | xargs ruff check --select Q --select I --select F401 --select UP026 --select UP034 --select UP039 --select W292 --fix
 
-# Applying Black format on changes (since $BASE_REVISION)
-echo "Using darker..."
-darker -i -r "$BASE_REVISION"
+    # Applying Black format on changes (since $BASE_REVISION)
+    echo "Using darker..."
+    darker -i -r "$BASE_REVISION"
+else
+    echo "No Python changes detected!"
+fi
