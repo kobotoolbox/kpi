8b53e787596408c7bd449cfdd8c87b7e6b8efc79
Olivier Leger <olivierleger@gmail.com>
2024-09-09 17:01:52 -0400

    Utility to format Python code before pushing new commits

---
+++ format-python.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BASE_REVISION=$1
+
+echo $BASE_REVISION
+
+
+if [ -z "$BASE_REVISION" ]; then
+    BASE_REVISION="beta"
+elif [ "$BASE_REVISION" == "-l" ] || [ "$BASE_REVISION" == "--last" ]; then
+    BASE_REVISION=$(git log --oneline| head -n 1 | awk '{ print $1}')
+fi
+
+# First, do not touch formatting but fix:
+#  - single/double quotes (--select Q)
+#  - sort imports (--select I)
+#  - unused imports (--select F401)
+#  - deprecated `mock` (--select UP026)
+#  - extraneous-parentheses (--select UP034)
+#  - Unnecessary parentheses after class definition (--select UP039)
+#  - Indentation warning (--select W1)
+#  - No newline at end of file (--select W292)
+echo "Using ruff..."
+git diff --name-only "$BASE_REVISION" | grep '.py' | xargs ruff check --select Q --select I --select F401 --select UP026 --select UP034 --select UP039 --select W292 --fix
+
+# Applying Black format on changes (since $BASE_REVISION)
+echo "Using darker..."
+darker -i -r "$BASE_REVISION"
