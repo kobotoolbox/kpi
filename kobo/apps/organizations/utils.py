fd312c083e69ad5739bdc9095de543816c56ccf8
James Kiger <james.kiger@gmail.com>
2024-09-24 10:27:37 -0400

    Linting

--- kobo/apps/organizations/utils.py
+++ kobo/apps/organizations/utils.py
@@ -16,7 +16,9 @@ def get_monthly_billing_dates(organization: Union[Organization, None]):
     """Returns start and end dates of an organization's monthly billing cycle"""
 
     now = timezone.now().replace(tzinfo=ZoneInfo('UTC'))
-    first_of_this_month = datetime(now.year, now.month, 1, tzinfo=ZoneInfo('UTC'))
+    first_of_this_month = datetime(
+        now.year, now.month, 1, tzinfo=ZoneInfo('UTC')
+    )
     first_of_next_month = (
         first_of_this_month
         + relativedelta(months=1)
@@ -25,7 +27,7 @@ def get_monthly_billing_dates(organization: Union[Organization, None]):
     # If no organization, just use the calendar month
     if not organization:
         return first_of_this_month, first_of_next_month
-    
+
     # If no active subscription, check for canceled subscription 
     if not (billing_details := organization.active_subscription_billing_details()):
         if not (
@@ -33,7 +35,7 @@ def get_monthly_billing_dates(organization: Union[Organization, None]):
             := organization.canceled_subscription_billing_cycle_anchor()
         ):
             return first_of_this_month, first_of_next_month
-        
+
         canceled_subscription_anchor = canceled_subscription_anchor.replace(
             tzinfo=ZoneInfo('UTC')
         )
@@ -46,7 +48,7 @@ def get_monthly_billing_dates(organization: Union[Organization, None]):
             canceled_subscription_anchor,
         )
         return period_start, period_end
-    
+
     if not billing_details.get('billing_cycle_anchor'):
         return first_of_this_month, first_of_next_month
 
@@ -61,7 +63,9 @@ def get_monthly_billing_dates(organization: Union[Organization, None]):
         return period_start, period_end
 
     # Subscription is billed yearly - count backwards from the end of the current billing year
-    period_start = billing_details.get('current_period_end').replace(tzinfo=ZoneInfo('UTC'))
+    period_start = billing_details.get('current_period_end').replace(
+        tzinfo=ZoneInfo('UTC')
+    )
     while period_start > now:
         period_start -= relativedelta(months=1)
     period_end = period_start + relativedelta(months=1)
@@ -93,8 +97,8 @@ def get_yearly_billing_dates(organization: Union[Organization, None]):
 
     # Subscription is monthly, calculate this year's start based on anchor date
     period_start = anchor_date.replace(
-        tzinfo=ZoneInfo('UTC')) + relativedelta(years=1
-    )
+        tzinfo=ZoneInfo('UTC')
+    ) + relativedelta(years=1)
     while period_start < now:
         anchor_date += relativedelta(years=1)
     period_end = period_start + relativedelta(years=1)
