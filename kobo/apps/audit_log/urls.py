8e1bdedd362300bfe478a8ec4323cb6d1f373eba
rgraber <becca.graber@kobotoolbox.org>
2024-09-30 08:59:29 -0400

    fixup!: oops s

--- kobo/apps/audit_log/urls.py
+++ kobo/apps/audit_log/urls.py
@@ -5,7 +5,7 @@ from .views import AccessLogViewSet, AllAccessLogViewSet, AuditLogViewSet
 
 router = DefaultRouter()
 router.register(r'audit-logs', AuditLogViewSet, basename='audit-log')
-router.register(r'access-logs', AllAccessLogViewSet, basename='all-access-log')
+router.register(r'access-logs', AllAccessLogViewSet, basename='all-access-logs')
 router.register(
     r'access-logs/me', AccessLogViewSet, basename='access-logs'
 )
