93aabe91774e244962e1ac464a4267e5f48b4b7d
rgraber <becca.graber@kobotoolbox.org>
2024-10-01 16:03:54 -0400

    fixup!: format

--- kobo/apps/audit_log/urls.py
+++ kobo/apps/audit_log/urls.py
@@ -1,15 +1,22 @@
-from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import AccessLogViewSet, AllAccessLogViewSet, AuditLogViewSet, AllProjectHistoryLogViewSet
+from .views import (
+    AccessLogViewSet,
+    AllAccessLogViewSet,
+    AllProjectHistoryLogViewSet,
+    AuditLogViewSet,
+)
 
 router = DefaultRouter()
 router.register(r'audit-logs', AuditLogViewSet, basename='audit-log')
 router.register(r'access-logs', AllAccessLogViewSet, basename='all-access-logs')
-router.register(r'project-history-logs', AllProjectHistoryLogViewSet, basename='all-project-logs')
-
 router.register(
     r'access-logs/me', AccessLogViewSet, basename='access-log'
+)
+# the route for project history logs for a specific asset is registered in router_api_v2
+# because it acts as a subroute of /assets
+router.register(
+    r'project-history-logs', AllProjectHistoryLogViewSet, basename='all-project-logs'
 )
 
 urlpatterns = []
