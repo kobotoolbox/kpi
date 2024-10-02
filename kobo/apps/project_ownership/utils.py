3ef23010da0563c07528afaeae9d1191b3cf6a02
Olivier Leger <olivierleger@gmail.com>
2024-10-02 09:38:02 -0400

    Use bulk update for attachments and media files… when moving files from one user's storage to another

--- kobo/apps/project_ownership/utils.py
+++ kobo/apps/project_ownership/utils.py
@@ -1,9 +1,11 @@
 import os
+import time
 from typing import Optional
 
 from django.apps import apps
 from django.utils import timezone
 
+from kobo.apps.openrosa.apps.logger.models.attachment import Attachment
 from kpi.deployment_backends.kc_access.shadow_models import (
     KobocatAttachment,
     KobocatMetadata,
@@ -11,6 +13,7 @@ from kpi.deployment_backends.kc_access.shadow_models import (
 from kpi.models.asset import AssetFile
 from .models.choices import TransferStatusChoices, TransferStatusTypeChoices
 from .exceptions import AsyncTaskException
+from .constants import ASYNC_TASK_HEARTBEAT, FILE_MOVE_CHUNK_SIZE
 
 
 def get_target_folder(
@@ -59,27 +62,39 @@ def move_attachments(transfer: 'project_ownership.Transfer'):
         instance_id__in=submission_ids
     ).exclude(media_file__startswith=f'{transfer.asset.owner.username}/')
 
-    for attachment in attachments.iterator():
-        # Pretty slow but it should run in celery task. We want to be the
-        # path of the file is saved right away. It lets us resume when it stopped
-        # in case of failure.
-        if not (
-            target_folder := get_target_folder(
-                transfer.invite.sender.username,
-                transfer.invite.recipient.username,
-                attachment.media_file.name,
-            )
-        ):
-            continue
-        else:
-            attachment.media_file.move(target_folder)
-            attachment.save(update_fields=['media_file'])
+    attachments_to_update = []
+    try:
+        heartbeat = int(time.time())
+        # Moving files is pretty slow, thus it should run in a celery task.
+        for attachment in attachments.iterator():
+            if not (
+                target_folder := get_target_folder(
+                    transfer.invite.sender.username,
+                    transfer.invite.recipient.username,
+                    attachment.media_file.name,
+                )
+            ):
+                continue
+            else:
+                # We want to be sure the path of the file is saved no matter what.
+                # Thanks to try/finally block, if updates are still pending, they
+                # should be saved in case of errors.
+                # It lets us resume when it stopped in case of failure.
+                attachment.media_file.move(target_folder)
+                attachments_to_update.append(attachment)
 
-            # We only need to update `date_modified` to update task heart beat.
-            # No need to use `TransferStatus.update_status()` and
-            # its lock mechanism.
-            transfer.statuses.filter(status_type=async_task_type).update(
-                date_modified=timezone.now()
+                if len(attachments_to_update) > FILE_MOVE_CHUNK_SIZE:
+                    Attachment.objects.bulk_update(
+                        attachments_to_update, fields=['media_file']
+                    )
+                    attachments_to_update = []
+
+                heartbeat = _update_heartbeat(heartbeat, transfer, async_task_type)
+
+    finally:
+        if attachments_to_update:
+            Attachment.objects.bulk_update(
+                attachments_to_update, fields=['media_file']
             )
 
     _mark_task_as_successful(transfer, async_task_type)
@@ -102,41 +117,53 @@ def move_media_files(transfer: 'project_ownership.Transfer'):
             )
         }
 
-    for media_file in media_files:
-        if not (
-            target_folder := get_target_folder(
-                transfer.invite.sender.username,
-                transfer.invite.recipient.username,
-                media_file.content.name,
-            )
-        ):
-            continue
-        else:
-            # Pretty slow but it should run in celery task. We want to be sure the
-            # path of the file is saved right away. It lets us resume when it stopped
-            # in case of failure.
-            media_file.content.move(target_folder)
-            old_md5 = media_file.metadata.pop('hash', None)
-            media_file.set_md5_hash()
-            if old_md5 in kc_files.keys():
-                kc_obj = kc_files[old_md5]
-                if kc_target_folder := get_target_folder(
+    media_files_to_update = []
+    metadata_to_update = []
+    try:
+        heartbeat = int(time.time())
+        # Moving files is pretty slow, thus it should run in a celery task.
+        for media_file in media_files:
+            if not (
+                target_folder := get_target_folder(
                     transfer.invite.sender.username,
                     transfer.invite.recipient.username,
-                    kc_obj.data_file.name,
-                ):
-                    kc_obj.data_file.move(kc_target_folder)
-                    kc_obj.file_hash = media_file.md5_hash
-                    kc_obj.save(update_fields=['data_file', 'file_hash'])
+                    media_file.content.name,
+                )
+            ):
+                continue
+            else:
+                # We want to be sure the path of the file is saved no matter what.
+                # Thanks to try/finally block, if updates are still pending, they
+                # should be saved in case of errors.
+                # It lets us resume when it stopped in case of failure.
+                media_file.content.move(target_folder)
+                old_md5 = media_file.metadata.pop('hash', None)
+                media_file.set_md5_hash()
+                if old_md5 in kc_files.keys():
+                    kc_obj = kc_files[old_md5]
+                    if kc_target_folder := get_target_folder(
+                        transfer.invite.sender.username,
+                        transfer.invite.recipient.username,
+                        kc_obj.data_file.name,
+                    ):
+                        kc_obj.data_file.move(kc_target_folder)
+                        kc_obj.file_hash = media_file.md5_hash
+                        metadata_to_update.append(kc_obj)
 
-            media_file.save(update_fields=['content', 'metadata'])
+                media_files_to_update.append(media_file)
+                heartbeat = _update_heartbeat(heartbeat, transfer, async_task_type)
 
-        # We only need to update `date_modified` to update task heart beat.
-        # No need to use `TransferStatus.update_status()` and
-        # its lock mechanism.
-        transfer.statuses.filter(status_type=async_task_type).update(
-            date_modified=timezone.now()
-        )
+    finally:
+        # No need to use chunk size for media files like we do for attachments,
+        # because the odds are pretty low that more than 100 media files are
+        # linked to the project.
+        if metadata_to_update:
+            media_files_to_update.append(media_file)
+
+        if media_files_to_update:
+            AssetFile.objects.bulk_update(
+                media_files_to_update, update_fields=['content', 'metadata']
+            )
 
     _mark_task_as_successful(transfer, async_task_type)
 
@@ -166,3 +193,19 @@ def _mark_task_as_successful(
     TransferStatus.update_status(
         transfer.pk, TransferStatusChoices.SUCCESS, async_task_type
     )
+
+
+def _update_heartbeat(
+    heartbeat: int, transfer: 'project_ownership.Transfer', async_task_type: str
+) -> int:
+
+    if heartbeat + ASYNC_TASK_HEARTBEAT >= time.time():
+        # We only need to update `date_modified` to update task heartbeat.
+        # No need to use `TransferStatus.update_status()` and
+        # its lock mechanism.
+        transfer.statuses.filter(status_type=async_task_type).update(
+            date_modified=timezone.now()
+        )
+        return int(time.time())
+
+    return heartbeat
