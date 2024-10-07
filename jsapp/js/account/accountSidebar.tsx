a13c3bed356b5664710f4dafef683e3aebfacb01
Paulo Amorim <pauloamorimbr@gmail.com>
2024-10-07 15:36:38 -0300

    sample migration from useOrganization to react query

--- jsapp/js/account/accountSidebar.tsx
+++ jsapp/js/account/accountSidebar.tsx
@@ -3,12 +3,12 @@ import {NavLink} from 'react-router-dom';
 import {observer} from 'mobx-react-lite';
 import bem from 'js/bem';
 import Icon from 'js/components/common/icon';
-import {IconName} from 'jsapp/fonts/k-icons';
+import type {IconName} from 'jsapp/fonts/k-icons';
 import Badge from '../components/common/badge';
 import subscriptionStore from 'js/account/subscriptionStore';
 import './accountSidebar.scss';
 import useWhenStripeIsEnabled from 'js/hooks/useWhenStripeIsEnabled.hook';
-import {OrganizationContext} from 'js/account/organizations/useOrganization.hook';
+import {useOrganizationQuery} from 'js/account/organizations/useOrganization.hook';
 import {ACCOUNT_ROUTES} from 'js/account/routes.constants';
 
 interface AccountNavLinkProps {
@@ -34,9 +34,12 @@ function AccountNavLink(props: AccountNavLinkProps) {
 
 function AccountSidebar() {
   const [showPlans, setShowPlans] = useState(false);
-  const [organization, _] = useContext(OrganizationContext);
 
-  const isOrgOwner = useMemo(() => organization?.is_owner, [organization]);
+  const {
+    data: organizationData,
+    isLoading: isLoadingOrganization,
+    isError,
+  } = useOrganizationQuery();
 
   useWhenStripeIsEnabled(() => {
     if (!subscriptionStore.isInitialised) {
@@ -45,12 +48,20 @@ function AccountSidebar() {
     setShowPlans(true);
   }, [subscriptionStore.isInitialised]);
 
-  const showAddOnsLink = useMemo(() => {
-    return !subscriptionStore.planResponse.length;
-  }, [subscriptionStore.isInitialised]);
+  const showAddOnsLink = useMemo(
+    () => !subscriptionStore.planResponse.length,
+    [subscriptionStore.isInitialised]
+  );
 
   return (
     <bem.FormSidebar m='account'>
+      <div>
+        {isLoadingOrganization ? (
+          <div>Loading...</div>
+        ) : (
+          <pre>{JSON.stringify(organizationData, null, 2)}</pre>
+        )}
+      </div>
       <AccountNavLink
         iconName='user'
         name={t('Profile')}
@@ -61,7 +72,7 @@ function AccountSidebar() {
         name={t('Security')}
         to={ACCOUNT_ROUTES.SECURITY}
       />
-      {isOrgOwner && (
+      {organizationData?.is_owner && (
         <>
           <AccountNavLink
             iconName='reports'
@@ -80,7 +91,7 @@ function AccountSidebar() {
                   iconName='plus'
                   name={t('Add-ons')}
                   to={ACCOUNT_ROUTES.ADD_ONS}
-                  isNew={true}
+                  isNew
                 />
               )}
             </>
--- jsapp/js/account/organizations/useOrganization.hook.tsx
+++ jsapp/js/account/organizations/useOrganization.hook.tsx
@@ -2,6 +2,7 @@ import React, {createContext} from 'react';
 import {getOrganization} from 'js/account/stripe.api';
 import type {Organization} from 'js/account/stripe.types';
 import {useApiFetcher, withApiFetcher} from 'js/hooks/useApiFetcher.hook';
+import {useQuery} from '@tanstack/react-query';
 
 const loadOrganization = async () => {
   const response = await getOrganization();
@@ -18,9 +19,19 @@ const INITIAL_ORGANIZATION_STATE: Organization = Object.freeze({
   is_owner: false,
 });
 
+/**
+ * @deprecated - use useOrganizationQuery instead
+ */
 export const useOrganization = () =>
   useApiFetcher(loadOrganization, INITIAL_ORGANIZATION_STATE);
-
+/**
+ * @deprecated - use useOrganizationQuery instead
+ */
 export const OrganizationContext = createContext(
   withApiFetcher(INITIAL_ORGANIZATION_STATE)
 );
+
+export const useOrganizationQuery = () => useQuery({
+  queryFn: loadOrganization,
+  queryKey: ['organization'],
+});
