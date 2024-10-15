8a1597f361b7b27eb340cb3fe3d33bd4f20fe6ba
Paulo Amorim <pauloamorimbr@gmail.com>
2024-10-15 14:04:01 -0300

    Cleanup and addition of QueryKeys enum

--- jsapp/js/query/queries/activityLog.query.ts
+++ jsapp/js/query/queries/activityLog.query.ts
@@ -1,6 +1,8 @@
 import {keepPreviousData, useQuery} from '@tanstack/react-query';
+import type {KoboSelectOption} from 'jsapp/js/components/common/koboSelect';
 import type {PaginatedResponse} from 'jsapp/js/dataInterface';
 import moment from 'moment';
+import {QueryKeys} from '../queryKeys';
 
 export interface ActivityLogsItem {
   id: number;
@@ -11,6 +13,12 @@ export interface ActivityLogsItem {
 }
 
 // MOCK DATA GENERATION
+const mockOptions: KoboSelectOption[] = [
+  {value: '1', label: 'Option 1'},
+  {value: '2', label: 'Option 2'},
+  {value: '3', label: 'Option 3'},
+];
+
 const getRandomMockDescriptionData = () => {
   const who = ['Trent', 'Jane', 'Alice', 'Bob', 'Charlie'];
   const action = ['created', 'updated', 'deleted', 'added', 'removed'];
@@ -33,6 +41,12 @@ const mockData: ActivityLogsItem[] = Array.from({length: 150}, (_, index) => {
 });
 // END OF MOCK GENERATION
 
+/**
+ * Fetches the activity logs from the server.
+ * @param limit Pagination parameter: number of items per page
+ * @param offset Pagination parameter: offset of the page
+ * @returns
+ */
 const getActivityLogs = async (limit: number, offset: number) =>
   new Promise<PaginatedResponse<ActivityLogsItem>>((resolve) => {
     setTimeout(
@@ -47,9 +61,36 @@ const getActivityLogs = async (limit: number, offset: number) =>
     );
   });
 
+/**
+ * Fetches the filter options for the activity logs.
+ * @returns
+ */
+const getFilterOptions = async () =>
+  new Promise<KoboSelectOption[]>((resolve) => {
+    setTimeout(() => resolve(mockOptions), 1000);
+  });
+
+/**
+ *
+ *  This is a hook that fetches activity logs from the server.
+ *
+ * @param itemLimit Pagination parameter: number of items per page
+ * @param pageOffset Pagination parameter: offset of the page
+ * @returns
+ */
 export const useActivityLogsQuery = (itemLimit: number, pageOffset: number) =>
   useQuery({
-    queryKey: ['accessLogs', itemLimit, pageOffset],
+    queryKey: [QueryKeys.activityLog, itemLimit, pageOffset],
     queryFn: () => getActivityLogs(itemLimit, pageOffset),
     placeholderData: keepPreviousData,
+  });
+
+/**
+ * This is a hook to fetch the filter options for the activity logs.
+ * @returns
+ */
+export const useActivityLogsFilterOptionsQuery = () =>
+  useQuery({
+    queryKey: [QueryKeys.activityLogsFilter],
+    queryFn: () => getFilterOptions(),
   });
