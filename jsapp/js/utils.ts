7413c8aaf08a17358848eda8100a52b7f45c918b
Leszek <smutnyleszek@gmail.com>
2024-10-15 15:27:41 +0200

    update Avatar component (add size option, stories, better color function, etc.) and use it everywhere we display avatar

--- jsapp/js/utils.ts
+++ jsapp/js/utils.ts
@@ -254,42 +254,6 @@ export function getLangString(obj: LangObject): string | undefined {
   }
 }
 
-export function stringToColor(str: string, prc?: number) {
-  // Higher prc = lighter color, lower = darker
-  prc = typeof prc === 'number' ? prc : -15;
-  const hash = function (word: string) {
-    let h = 0;
-    for (let i = 0; i < word.length; i++) {
-      h = word.charCodeAt(i) + ((h << 5) - h);
-    }
-    return h;
-  };
-  const shade = function (color: string, prc2: number) {
-    const num = parseInt(color, 16);
-    const amt = Math.round(2.55 * prc2);
-    const R = (num >> 16) + amt;
-    const G = ((num >> 8) & 0x00ff) + amt;
-    const B = (num & 0x0000ff) + amt;
-    return (
-      0x1000000 +
-      (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
-      (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 +
-      (B < 255 ? (B < 1 ? 0 : B) : 255)
-    )
-      .toString(16)
-      .slice(1);
-  };
-  const intToRgba = function (i: number) {
-    const color =
-      ((i >> 24) & 0xff).toString(16) +
-      ((i >> 16) & 0xff).toString(16) +
-      ((i >> 8) & 0xff).toString(16) +
-      (i & 0xff).toString(16);
-    return color;
-  };
-  return shade(intToRgba(hash(str)), prc);
-}
-
 export function isAValidUrl(url: string) {
   try {
     new URL(url);
