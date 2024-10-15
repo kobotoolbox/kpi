46622e75ed0133f5720bb92ecba4edab1273291b
Paulo Amorim <pauloamorimbr@gmail.com>
2024-10-15 05:13:57 -0300

    apply review suggestions

--- jsapp/js/components/activity/formActivity.tsx
+++ jsapp/js/components/activity/formActivity.tsx
@@ -3,13 +3,15 @@ import {useState} from 'react';
 import '../../../scss/components/_kobo.form-view.scss';
 import type {KoboSelectOption} from '../common/koboSelect';
 import KoboSelect from '../common/koboSelect';
-import './formActivity.scss';
 import type {UniversalTableColumn} from 'jsapp/js/universalTable/universalTable.component';
 import Button from '../common/button';
 import PaginatedQueryUniversalTable from 'jsapp/js/universalTable/paginatedQueryUniversalTable.component';
 import type {ActivityLogItem} from 'jsapp/js/query/queries/activityLog.query';
-import {useActivityLogQuery} from 'jsapp/js/query/queries/activityLog.query';
+import {useActivityLogsQuery} from 'jsapp/js/query/queries/activityLog.query';
 import moment from 'moment';
+import styles from './formActivity.module.scss';
+import cx from 'classnames';
+import {formatTime, stringToColor} from 'jsapp/js/utils';
 
 interface TableDataItem {
   description: ReactNode;
@@ -34,6 +36,10 @@ const columns: UniversalTableColumn[] = [
   },
 ];
 
+const UserAvatar = ({name}: {name: string}) => (
+  <div style={{background: `#${stringToColor(name)}`}} className={styles.profilePicture}>{name.slice(0, 1)}</div>
+);
+
 const EventDescription = ({
   who,
   action,
@@ -43,17 +49,15 @@ const EventDescription = ({
   action: string;
   what: string;
 }) => (
-  <div className='event-description'>
-    <div className='profile-picture'>{who.slice(0, 1)}</div>
-    <span className='description'>
-      <span className='who'>{who}</span>{' '}
-      <span className='action'>{action}</span> {what}
-    </span>
-    <button className='see-details'>{t('See details')}</button>
+  <div className={styles.eventDescription}>
+    <UserAvatar name={who} />
+    <span className={styles.who}>{who}</span>{' '}
+    <span className={styles.action}>{action}</span> {what}
+    <button className={styles.seeDetails}>{t('See details')}</button>
   </div>
 );
 
-const EventDate = ({dateTime}: {dateTime: string}) => moment(dateTime).calendar();
+const EventDate = ({dateTime}: {dateTime: string}) => formatTime(dateTime);
 
 export default function FormActivity() {
   const [filterOptions, setFilterOptions] =
@@ -81,13 +85,13 @@ export default function FormActivity() {
   };
 
   return (
-    <div className='form-view main-container'>
-      <div className='header'>
+    <div className={cx('form-view', styles.mainContainer)}>
+      <div className={styles.header}>
         <h1>{t('Recent Project Activity')}</h1>
-        <div className='header-actions'>
+        <div className={styles.headerActions}>
           <KoboSelect
             isClearable
-            className='filter-select'
+            className={styles.filterSelect}
             selectedOption={selectedFilterOption?.value || ''}
             onChange={handleFilterChange}
             type='outline'
@@ -104,10 +108,10 @@ export default function FormActivity() {
           />
         </div>
       </div>
-      <div className='table-container'>
+      <div className={styles.tableContainer}>
         <PaginatedQueryUniversalTable<ActivityLogItem, TableDataItem>
           columns={columns}
-          queryHook={useActivityLogQuery}
+          queryHook={useActivityLogsQuery}
           rowRenderer={rowRenderer}
         />
       </div>
