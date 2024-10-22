87ea767267fe86b22da9082fc2f6c6b017c391bc
Paulo Amorim <pauloamorimbr@gmail.com>
2024-10-22 14:49:31 -0300

    add queries and be mock

--- jsapp/js/query/queryKeys.ts
+++ jsapp/js/query/queryKeys.ts
@@ -7,5 +7,6 @@
 export enum QueryKeys {
   accessLogs = 'accessLogs',
   activityLogs = 'activityLogs',
+  activityLogsExportStatus = 'activityLogsExportStatus',
   activityLogsFilter = 'activityLogsFilter',
 }
