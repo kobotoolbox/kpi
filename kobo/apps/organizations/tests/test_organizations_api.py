484e0772142e4dc15e406801380ca70f7a0d1975
rajpatel24 <rajpatel24998@gmail.com>
2024-10-28 11:54:45 +0530

    Add tests for is_mmo field in organization API response

--- kobo/apps/organizations/tests/test_organizations_api.py
+++ kobo/apps/organizations/tests/test_organizations_api.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from model_bakery import baker
 from rest_framework import status
+from unittest.mock import patch
 
 from kobo.apps.kobo_auth.shortcuts import User
 from kobo.apps.organizations.models import Organization
@@ -13,14 +14,22 @@ from kpi.utils.fuzzy_int import FuzzyInt
 class OrganizationTestCase(BaseTestCase):
     fixtures = ['test_data']
     URL_NAMESPACE = URL_NAMESPACE
+    DEFAULT_SUBSCRIPTION_DETAILS = {
+        'current_period_start': '2024-01-01',
+        'current_period_end': '2024-12-31'
+    }
 
     def setUp(self):
         self.user = User.objects.get(username='someuser')
         self.client.force_login(self.user)
         self.url_list = reverse(self._get_endpoint('organizations-list'))
 
-    def _insert_data(self):
-        self.organization = baker.make(Organization, id='org_abcd1234')
+    def _insert_data(self, mmo_override=False):
+        self.organization = baker.make(
+            Organization,
+            id='org_abcd1234',
+            mmo_override=mmo_override
+        )
         self.organization.add_user(user=self.user, is_admin=True)
         self.url_detail = reverse(
             self._get_endpoint('organizations-detail'),
@@ -65,3 +74,62 @@ class OrganizationTestCase(BaseTestCase):
         self.organization.add_user(user=user)
         res = self.client.patch(self.url_detail, data)
         self.assertEqual(res.status_code, 403)
+
+    def test_api_response_includes_is_mmo_with_mmo_override(self):
+        """
+        Test that is_mmo is True when mmo_override is enabled.
+        """
+        self._insert_data(mmo_override=True)
+        response = self.client.get(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['is_mmo'], True)
+
+    @patch.object(
+        Organization,
+        'active_subscription_billing_details',
+        return_value=DEFAULT_SUBSCRIPTION_DETAILS
+    )
+    def test_api_response_includes_is_mmo_with_subscription(
+        self, mock_active_subscription
+    ):
+        """
+        Test that is_mmo is True when there is an active subscription.
+        """
+        self._insert_data(mmo_override=False)
+        response = self.client.get(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['is_mmo'], True)
+
+    @patch.object(
+        Organization,
+        'active_subscription_billing_details',
+        return_value=None
+    )
+    def test_api_response_includes_is_mmo_with_no_override_and_no_subscription(
+        self, mock_active_subscription
+    ):
+        """
+        Test that is_mmo is False when neither mmo_override nor active
+        subscription is present.
+        """
+        self._insert_data(mmo_override=False)
+        response = self.client.get(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['is_mmo'], False)
+
+    @patch.object(
+        Organization,
+        'active_subscription_billing_details',
+        return_value=DEFAULT_SUBSCRIPTION_DETAILS
+    )
+    def test_api_response_includes_is_mmo_with_override_and_subscription(
+        self, mock_active_subscription
+    ):
+        """
+        Test that is_mmo is True when both mmo_override and active
+        subscription are present.
+        """
+        self._insert_data(mmo_override=True)
+        response = self.client.get(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['is_mmo'], True)
