5db7da32566a3a762de5f2f7c12f837ae73e52c5
Leszek <smutnyleszek@gmail.com>
2024-10-28 10:58:55 +0100

    improve getSupplementalPathParts to return the source row path (not only name)
    and add more tests

--- jsapp/js/components/processing/processingUtils.ts
+++ jsapp/js/components/processing/processingUtils.ts
@@ -1,10 +1,10 @@
 import type {LanguageCode} from 'js/components/languages/languagesStore';
-import {QuestionTypeName} from 'js/constants';
+import {QuestionTypeName, SUPPLEMENTAL_DETAILS_PROP} from 'js/constants';
 
 /** Single Processing is capable of handling these question types. */
 export const PROCESSING_QUESTION_TYPES = [
   QuestionTypeName.audio,
-  QuestionTypeName['background-audio']
+  QuestionTypeName['background-audio'],
 ];
 
 type SupplementalPathPartsType = 'transcript' | 'translation' | 'qual';
@@ -12,10 +12,15 @@ type SupplementalPathPartsType = 'transcript' | 'translation' | 'qual';
 interface SupplementalPathParts {
   sourceRowName: string;
   /**
-   * What kind of data the path refers to. Can be of transcript, translation or
-   * qualitative analysis question.
+   * Includes groups. Will be the same as `sourceRowName` if there are no groups.
    */
-  type: SupplementalPathPartsType;
+  sourceRowPath: string;
+  /**
+   * What kind of data the path refers to. Can be of transcript, translation or
+   * qualitative analysis question. `null` is being returned when path is not
+   * recognized.
+   */
+  type: SupplementalPathPartsType | null;
   /**
    * Applicable only for transcripts and translations. This is the language code
    * of the text.
@@ -27,16 +32,27 @@ interface SupplementalPathParts {
 
 /**
  * Receives one of paths leading to a supplemental detail and breaks it down to
- * a more meaningful data.
+ * a more meaningful data. e.g. `_supplementalDetails/Who_was_that/translation_pl` or
+ * `_supplementalDetails/outer_group/middle_group/inner_group/What_did_you_hear/transcript_pl`
  */
 export function getSupplementalPathParts(path: string): SupplementalPathParts {
   const pathArr = path.split('/');
-  const path2Arr = pathArr[2].split('_');
+  const lastEl = pathArr.pop();
+  const lastElArr = lastEl?.split('_') || [];
+
+  // Handles path that is not a supplemental path
+  if (pathArr[0] !== SUPPLEMENTAL_DETAILS_PROP) {
+    return {
+      sourceRowName: path,
+      sourceRowPath: path,
+      type: null,
+    };
+  }
 
   let pathType: SupplementalPathPartsType;
-  if (path2Arr[0] === 'transcript') {
+  if (lastElArr[0] === 'transcript') {
     pathType = 'transcript';
-  } else if (path2Arr[0] === 'translation') {
+  } else if (lastElArr[0] === 'translation') {
     pathType = 'translation';
   } else {
     // For now the only other type of data here is the qualitative analysis
@@ -45,18 +61,20 @@ export function getSupplementalPathParts(path: string): SupplementalPathParts {
   }
 
   const output: SupplementalPathParts = {
-    sourceRowName: pathArr[1],
+    sourceRowName: pathArr[pathArr.length - 1],
+    // We start from second element, because first one is `SUPPLEMENTAL_DETAILS_PROP`
+    sourceRowPath: pathArr.slice(1, pathArr.length).join('/'),
     type: pathType,
   };
 
   // For transx we add the language code
   if (pathType === 'transcript' || pathType === 'translation') {
-    output.languageCode = path2Arr[1];
+    output.languageCode = lastElArr[1];
   }
 
   // For qualitative analysis questions, we store the uuid of the question
   if (pathType === 'qual') {
-    output.analysisQuestionUuid = pathArr[2];
+    output.analysisQuestionUuid = lastEl;
   }
 
   return output;
