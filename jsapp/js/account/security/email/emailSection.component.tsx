500897fcea2828bb6e21c8c4c81652399477d25e
Anji Tong <ollejna@gmail.com>
2024-10-31 16:56:46 -0400

    WIP moving elements around

--- jsapp/js/account/security/email/emailSection.component.tsx
+++ jsapp/js/account/security/email/emailSection.component.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import cx from 'classnames';
 
 // Stores and email related
@@ -22,6 +22,7 @@ import {formatTime, notify} from 'js/utils';
 // Styles
 import styles from './emailSection.module.scss';
 import securityStyles from 'js/account/security/securityRoute.module.scss';
+import {useOrganizationQuery} from '../../stripe.api';
 
 interface EmailState {
   emails: EmailResponse[];
@@ -32,6 +33,8 @@ interface EmailState {
 
 export default function EmailSection() {
   const [session] = useState(() => sessionStore);
+
+  const orgQuery = useOrganizationQuery();
 
   let initialEmail = '';
   if ('email' in session.currentAccount) {
@@ -116,24 +119,22 @@ export default function EmailSection() {
   const unverifiedEmail = email.emails.find(
     (userEmail) => !userEmail.verified && !userEmail.primary
   );
+  const verifiedEmail = email.emails.find(
+    (userEmail) => userEmail.verified && userEmail.primary
+  );
 
   return (
     <section className={securityStyles.securitySection}>
       <div className={securityStyles.securitySectionTitle}>
-        <h2 className={securityStyles.securitySectionTitleText}>{t('Email address')}</h2>
+        <h2 className={securityStyles.securitySectionTitleText}>
+          {t('Email address')}
+        </h2>
+        <div>
+          {verifiedEmail?.email}
+        </div>
       </div>
 
       <div className={cx(securityStyles.securitySectionBody, styles.body)}>
-        {!session.isPending &&
-          session.isInitialLoadComplete &&
-          'email' in currentAccount && (
-            <TextBox
-              value={email.newEmail}
-              placeholder={t('Type new email address')}
-              onChange={onTextFieldChange.bind(onTextFieldChange)}
-              type='email'
-            />
-          )}
 
         {unverifiedEmail?.email &&
           !session.isPending &&
@@ -186,20 +187,33 @@ export default function EmailSection() {
           )}
       </div>
 
-      <form
-        className={styles.options}
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <Button
-          label='Change'
-          size='m'
-          type='primary'
-          onClick={handleSubmit}
-        />
-      </form>
+      <div>
+        {!session.isPending &&
+          session.isInitialLoadComplete &&
+          'email' in currentAccount && (
+            <TextBox
+              value={email.newEmail}
+              placeholder={t('Type new email address')}
+              onChange={onTextFieldChange.bind(onTextFieldChange)}
+              type='email'
+            />
+          )}
+
+        <form
+          className={styles.options}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <Button
+            label='Change'
+            size='m'
+            type='primary'
+            onClick={handleSubmit}
+          />
+        </form>
+      </div>
     </section>
   );
 }
