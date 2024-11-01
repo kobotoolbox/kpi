802649f38249787ec5a550e5bb9e3b3fbc0d25db
James Kiger <james.kiger@gmail.com>
2024-11-01 11:06:39 -0400

    Rewrite RequireOrgRole wrapper component to handle more complicated role and org requirements

--- jsapp/js/account/organizations/validateOrgPermissions.component.tsx
+++ jsapp/js/account/organizations/validateOrgPermissions.component.tsx
@@ -3,15 +3,32 @@ import {useNavigate} from 'react-router-dom';
 import LoadingSpinner from 'js/components/common/loadingSpinner';
 import {ACCOUNT_ROUTES} from 'js/account/routes.constants';
 import {useOrganizationQuery} from 'js/account/stripe.api';
+import {OrganizationUserRole} from '../stripe.types';
 
 interface Props {
   children: React.ReactNode;
+  validRoles?: OrganizationUserRole[];
+  mmoOnly?: boolean;
   redirect?: boolean;
 }
 
-export const RequireOrgOwner = ({children, redirect = true}: Props) => {
+/**
+ * Use to handle display of pages that should only be accessible to certain user roles
+ * or members of MMOs. Defaults to allowing access for all users, so you must supply
+ * any restrictions.
+ */
+export const ValidateOrgPermissions = ({
+  children,
+  validRoles = undefined,
+  mmoOnly = false,
+  redirect = true,
+}: Props) => {
   const navigate = useNavigate();
   const orgQuery = useOrganizationQuery();
+  const hasValidRole = validRoles ? validRoles.includes(
+    orgQuery.data?.request_user_role ?? OrganizationUserRole.member
+  ) : true;
+  const hasValidOrg = mmoOnly ? orgQuery.data?.is_mmo : true;
 
   // Redirect to Account Settings if you're not the owner
   useEffect(() => {
@@ -19,13 +36,14 @@ export const RequireOrgOwner = ({children, redirect = true}: Props) => {
       redirect &&
       !orgQuery.isPending &&
       orgQuery.data &&
-      !orgQuery.data.is_owner
+      !hasValidRole &&
+      !hasValidOrg
     ) {
       navigate(ACCOUNT_ROUTES.ACCOUNT_SETTINGS);
     }
   }, [redirect, orgQuery.isSuccess, orgQuery.data, navigate]);
 
-  return redirect && orgQuery.data?.is_owner ? (
+  return redirect && hasValidRole && hasValidOrg ? (
     <Suspense fallback={null}>{children}</Suspense>
   ) : (
     <LoadingSpinner />
