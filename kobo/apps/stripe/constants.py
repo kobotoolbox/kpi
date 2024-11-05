4fef35863127266f37dff771cfd2136e9ea6491e
James Kiger <68701146+jamesrkiger@users.noreply.github.com>
2024-11-05 11:08:47 -0500

    refactor(billingAddons): normalize NLP usage naming TASK-1015 (#5222)
    ## Checklist
    
    1. [ ] If you've added code that should be tested, add tests
    2. [ ] If you've changed APIs, update (or create!) the documentation
    3. [ ] Ensure the tests pass
    4. [ ] Run `./python-format.sh` to make sure that your code lints and
    that you've followed [our coding
    style](https://github.com/kobotoolbox/kpi/blob/main/CONTRIBUTING.md)
    5. [x] Write a title and, if necessary, a description of your work
    suitable for publishing in our [release
    notes](https://community.kobotoolbox.org/tag/release-notes)
    6. [x] Mention any related issues in this repository (as #ISSUE) and in
    other repositories (as kobotoolbox/other#ISSUE)
    7. [ ] Open an issue in the
    [docs](https://github.com/kobotoolbox/docs/issues/new) if there are
    UI/UX changes
    8. [ ] Create a testing plan for the reviewer and add it to the Testing
    section
    9. [ ] Add frontend or backend tag and any other appropriate tags to
    this pull request
    
    ## Description
    
    Previously we were using 'nlp_character_limit' and 'nlp_minutes_limit'
    as the keys for nlp usage limits in Stripe metadata. Elsewhere in the
    code we have been using 'asr_minutes' and 'mt_characters'. This PR
    removes the Stripe-specific language for consistency. A separate PR will
    be submitted to update the frontend accordingly.

--- kobo/apps/stripe/constants.py
+++ kobo/apps/stripe/constants.py
@@ -27,10 +27,3 @@ USAGE_LIMIT_MAP = {
     'storage': 'storage_bytes',
     'submission': 'submission',
 }
-
-USAGE_LIMIT_MAP_STRIPE = {
-    'characters': 'nlp_character',
-    'seconds': 'nlp_seconds',
-    'storage': 'storage_bytes',
-    'submission': 'submission',
-}
