b9e6409a95c79ba04bf36cff9a259d469e0a030e
Kalvis Kalniņš <kalvis.kalnins@kobotoolbox.org>
2024-11-13 13:43:44 +0200

    fix(api): nicier url /api/schema/swagger

--- kobo/urls.py
+++ kobo/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     # YOUR PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # https://github.com/stochastic-technologies/django-loginas
