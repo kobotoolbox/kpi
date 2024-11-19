6393a25e2139535142c9ec7a70f19af537ee13ee
Phil Edwards <phil@edwards.io>
2024-11-19 15:34:05 -0500

    docs: add t() comment, expand combined line comments
    https://github.com/kobotoolbox/kpi/pull/5264#pullrequestreview-2445687782

--- jsapp/jest/setupUnitTest.ts
+++ jsapp/jest/setupUnitTest.ts
@@ -1,12 +1,17 @@
 import chai from 'chai';
 import $ from 'jquery';
 
-import 'whatwg-fetch'; // polyfill global fetch (for Node 20 and older)
+// Polyfill global fetch (for Node 20 and older)
+import 'whatwg-fetch';
 
+// Add global t() mock (see /static/js/global_t.js)
 global.t = (str: string) => str;
 
-// @ts-expect-error: ℹ️ Add chai globals for BDD-style tests
-global.chai = chai; global.expect = chai.expect;
+// @ts-expect-error: ℹ️ Add chai global for BDD-style tests
+global.chai = chai;
+
+// @ts-expect-error: ℹ️ Use chai's version of `expect`
+global.expect = chai.expect;
 
 // @ts-expect-error: ℹ️ Add jQuery globals for xlform code
 global.jQuery = global.$ = $;
