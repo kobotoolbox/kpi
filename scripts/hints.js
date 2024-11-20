8b9d15a2186c4ae2666c1a92f320e8bff44b16db
Phil Edwards <phil@edwards.io>
2024-11-20 13:51:21 -0500

    chore: drop mocha, /browser_tests/, "test-mocha-autobuild"

--- scripts/hints.js
+++ scripts/hints.js
@@ -28,21 +28,6 @@ const hints = {
            ${s.darkblue}Enjoy a quicker-launching dev server!
   `,
 
-  'test-mocha-autobuild': `
-    This will rebuild the js tests on change.
-
-    Open ${s.underline}file://${process.cwd()}/test/tests.html${s.nounderline}
-    to see the test results in your browser.
-
-    Reload the page to re-run the tests.
-
-    If you don't need to inspect objects interactively
-    using the browser console,
-
-    you can use 'npm run test-watch' to watch
-    or filter tests in the terminal console.
-  `,
-
   SKIP_TS_CHECK: `${s.red}
      Skipping TypeScript check (${s.magenta}SKIP_TS_CHECK${s.red})
   ${s.normal}`,
