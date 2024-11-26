f9d33e4ddf9a6bacb46290be8333060a880057ee
rgraber <becca.graber@kobotoolbox.org>
2024-11-26 17:07:13 -0500

    feat: create endpoints for project history logs

--- kobo/apps/audit_log/urls.py
+++ kobo/apps/audit_log/urls.py
@@ -1,10 +1,22 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import AccessLogViewSet, AllAccessLogViewSet, AuditLogViewSet
+from .views import (
+    AccessLogViewSet,
+    AllAccessLogViewSet,
+    AllProjectHistoryLogViewSet,
+    AuditLogViewSet,
+)
 
 router = DefaultRouter()
 router.register(r'audit-logs', AuditLogViewSet, basename='audit-log')
 router.register(r'access-logs', AllAccessLogViewSet, basename='all-access-logs')
 router.register(r'access-logs/me', AccessLogViewSet, basename='access-log')
+# routes for PH logs for individual assets are registered in router_api_v2.py
+router.register(
+    r'project-history-logs',
+    AllProjectHistoryLogViewSet,
+    basename='all-project-history-logs',
+)
+
 
 urlpatterns = []
