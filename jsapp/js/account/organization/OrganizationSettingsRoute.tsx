147365ae3fc9c1a1a93d64584384254092242a58
Leszek <smutnyleszek@gmail.com>
2024-12-04 15:09:58 +0100

    some code review fixes

--- jsapp/js/account/organization/OrganizationSettingsRoute.tsx
+++ jsapp/js/account/organization/OrganizationSettingsRoute.tsx
@@ -2,10 +2,11 @@
 import {useState, useEffect} from 'react';
 
 // Partial components
-import OrganizationSettingsField from './OrganizationSettingsField';
 import LoadingSpinner from 'jsapp/js/components/common/loadingSpinner';
 import InlineMessage from 'jsapp/js/components/common/inlineMessage';
 import Button from 'jsapp/js/components/common/button';
+import TextBox from 'jsapp/js/components/common/textBox';
+import KoboSelect from 'jsapp/js/components/common/koboSelect';
 
 // Stores, hooks and utilities
 import useWhenStripeIsEnabled from 'js/hooks/useWhenStripeIsEnabled.hook';
@@ -19,12 +20,7 @@ import {ORGANIZATION_TYPES, type OrganizationTypeName} from 'jsapp/js/account/or
 
 // Styles
 import styles from 'js/account/organization/organizationSettingsRoute.module.scss';
-
-interface State {
-  name: string;
-  website?: string;
-  type?: OrganizationTypeName;
-}
+import NlpUsageLimitBlockModal from 'jsapp/js/components/processing/nlpUsageLimitBlockModal/nlpUsageLimitBlockModal.component';
 
 /**
  * Renders few fields with organization related settings, like name or website
@@ -34,12 +30,16 @@ interface State {
 export default function OrganizationSettingsRoute() {
   const orgQuery = useOrganizationQuery();
   const [subscriptions] = useState(() => subscriptionStore);
-  const [state, setState] = useState<State>({name: ''});
+  const [name, setName] = useState<string>('');
+  const [website, setWebsite] = useState<string>('');
+  const [orgType, setOrgType] = useState<OrganizationTypeName | null>(null);
   const [isStripeEnabled, setIsStripeEnabled] = useState(false);
 
   useEffect(() => {
     if (orgQuery.data) {
-      setState({name: orgQuery.data.name});
+      setName(orgQuery.data.name);
+      setWebsite(orgQuery.data.website);
+      setOrgType(orgQuery.data.organization_type);
     }
   }, [orgQuery.data]);
 
@@ -55,30 +55,19 @@ export default function OrganizationSettingsRoute() {
 
   const isPendingOrgPatch = orgQuery.data && orgQuery.isPending;
 
-  function handleSave() {
+  function handleSave(e: React.FormEvent) {
+    e.preventDefault();
     // TODO: call the API endpoint
     // to be done while doing: https://www.notion.so/kobotoolbox/Add-react-query-mutation-hook-for-org-changes-1307e515f6548010b5d3c087b634f01a
-    console.log('save');
+    console.log('save', name, website);
   }
 
-  function handleChangeName(name: string) {
-    setState((prevState) => {return {...prevState, name};});
+  function handleChangeName(newName: string) {
+    setName(newName);
   }
 
-  function handleChangeWebsite(website: string) {
-    setState((prevState) => {return {...prevState, website};});
-  }
-
-  function isNameValueValid(currentName: string) {
-    return !currentName;
-  }
-
-  function isWebsiteValueValid(currentWebsite: string) {
-    return !currentWebsite;
-  }
-
-  function getTypeLabel(typeName: OrganizationTypeName) {
-    return ORGANIZATION_TYPES[typeName]?.label || typeName;
+  function handleChangeWebsite(newWebsite: string) {
+    setWebsite(newWebsite);
   }
 
   const mmoLabel = getSimpleMMOLabel(
@@ -89,12 +78,22 @@ export default function OrganizationSettingsRoute() {
   );
   const mmoLabelLowercase = mmoLabel.toLowerCase();
 
-  if (!orgQuery.data) {
+  if (orgQuery.isLoading) {
     return <LoadingSpinner />;
   }
 
+  let deletionMessage = t('To delete this ##team/org##, please contact the server administrator.')
+    .replaceAll('##team/org##', mmoLabelLowercase);
+  if (isStripeEnabled) {
+    deletionMessage = t("To delete this ##team/org##, you need to cancel your current ##plan name## plan. At the end of the plan period your ##team/org##'s projects will be converted to projects owned by your personal account.")
+    .replaceAll('##team/org##', mmoLabelLowercase)
+    .replace('##plan name##', subscriptions.planName);
+  }
+
+  const currentTypeLabel = orgType === null ? '' : ORGANIZATION_TYPES[orgType]?.label;
+
   return (
-    <div className={styles.orgSettingsRoot}>
+    <form className={styles.orgSettingsRoot} onSubmit={handleSave}>
       <header className={styles.orgSettingsHeader}>
         <h2 className={styles.orgSettingsHeaderText}>
           {t('##team/org## details').replace('##team/org##', mmoLabel)}
@@ -102,52 +101,75 @@ export default function OrganizationSettingsRoute() {
       </header>
 
       <section className={styles.fieldsRow}>
-        <OrganizationSettingsField
+        {/*
+          On all instances, both owner and admins should be able to edit
+          organization name.
+        */}
+        <TextBox
+          className={styles.field}
           label={t('##team/org## name').replace('##team/org##', mmoLabel)}
+          value={name}
+          required
           onChange={handleChangeName}
-          value={state.name}
-          validateValue={isNameValueValid}
-          isDisabled={!isUserAdminOrOwner || isPendingOrgPatch}
+          disabled={!isUserAdminOrOwner || isPendingOrgPatch}
+          errors={name === ''}
         />
 
-        {isStripeEnabled && state.website && (
-          <OrganizationSettingsField
+        {/*
+          On Stripe-enabled instances, both owner and admins should be able to
+          edit organization website. On non-Stripe enabled instances it is not
+          visible.
+        */}
+        {isStripeEnabled && (
+          <TextBox
+            className={styles.field}
+            type='url'
             label={t('##team/org## website').replace('##team/org##', mmoLabel)}
+            value={website}
+            required
             onChange={handleChangeWebsite}
-            value={state.website}
-            validateValue={isWebsiteValueValid}
-            isDisabled={!isUserAdminOrOwner || isPendingOrgPatch}
+            disabled={!isUserAdminOrOwner || isPendingOrgPatch}
+            errors={website === ''}
           />
         )}
       </section>
+
+      {/*
+        On Stripe-enabled instances, both owner and admins should be able to
+        view organization type. On non-Stripe enabled instances it is not
+        visible.
+      */}
+      {isStripeEnabled && orgType && (
+        <section className={styles.fieldsRow}>
+          <KoboSelect
+            className={styles.fieldLong}
+            name='org-settings-type'
+            type='outline'
+            size='l'
+            isDisabled // always disabled
+            label={t('##team/org## type').replace('##team/org##', mmoLabel)}
+            options={[{
+              value: 'orgType',
+              label: currentTypeLabel,
+            }]}
+            selectedOption='orgType'
+            onChange={() => null}
+          />
+        </section>
+      )}
 
       <section className={styles.fieldsRow}>
-        {isStripeEnabled && state.type && (
-          <OrganizationSettingsField
-            label={t('##team/org## type').replace('##team/org##', mmoLabel)}
-            value={getTypeLabel(state.type)}
-            isDisabled
-          />
-        )}
+        <Button
+          type='primary'
+          size='m'
+          label={t('Save')}
+          isDisabled={!isUserAdminOrOwner}
+          isPending={isPendingOrgPatch}
+          isSubmit
+        />
       </section>
 
-      <Button
-        type='primary'
-        size='m'
-        onClick={handleSave}
-        label={t('Save')}
-        isDisabled={!isUserAdminOrOwner}
-        isPending={isPendingOrgPatch}
-      />
-
-      <InlineMessage
-        type='default'
-        message={
-          t("To delete this ##team/org##, you need to cancel your current ##plan name## plan. At the end of the plan period your ##team/org##'s projects will be converted to projects owned by your personal account.")
-            .replaceAll('##team/org##', mmoLabelLowercase)
-            .replace('##plan name##', subscriptions.planName)
-        }
-      />
-    </div>
+      <InlineMessage type='default' message={deletionMessage} />
+    </form>
   );
 }
