6cac659824d7c3fb1c5a748c352e58a8ad7d3a76
Rebecca Graber <becca.graber@kobotoolbox.org>
2024-12-05 10:07:25 -0500

    feat(projectHistoryLogs): add endpoints for viewing project history logs TASK-973 (#5319)
    ### 📣 Summary
    Add two new endpoints for viewing project history logs, one for all
    logs, and one for logs for a given project.
    
    
    ### 📖 Description
    Adds two new endpoints: `/api/v2/project-history-logs` (all project
    history logs) and `/api/v2/assets/<uid>/history` (logs for the specific
    project).
    The former is only available to superusers, the latter is available to
    anyone with the manage_asset permission for the project.
    Both endpoints can be searched by a all fields, most importantly
    `action`, `metadata__asset_uid`, and `username`. A full list of
    available searchable fields is in the endpoint documentation.
    
    
    ### 👀 Preview steps
    
    
    Feature/no-change template:
    1. ℹ️ have two accounts, one super and one not. Have at least two
    projects.
    2. Change the name of both projects to generate some logs.
    3. Log in as the superuser.
    4. Go to `localhost/api/v2/project-history-logs`
    5. 🟢 You should see logs for both projects
    6. Grant the non-superuser the manage_project permission for one of the
    projects (they will have this if they are the owner, or you can assign
    it from Settings > Sharing)
    7. Log out
    8. 🟢 Go to `localhost/api/v2/project-history-logs`. You should get a
    401.
    9. Log in as the non-superuser.
    10. 🟢 Go to `localhost/api/v2/project-history-logs`. You should get a
    403.
    11. Go to `localhost/api/v2/assets/<asset_uid>/history`
    12. 🟢 You should see project history logs for just that project
    13. Revoke the manage_project permission for the user. You can do this
    by transferring ownership or just removing the singular permission (will
    require you to log back in as the superuser).
    14. 🟢 Reload `localhost/api/v2/assets/<asset_uid>/history`. You should
    get a 403.

--- kobo/apps/audit_log/serializers.py
+++ kobo/apps/audit_log/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from kpi.fields import RelativePrefixHyperlinkedRelatedField
-from .models import AuditLog
+from .models import AuditLog, ProjectHistoryLog
 
 
 class AuditLogSerializer(serializers.ModelSerializer):
@@ -64,3 +64,39 @@ class AccessLogSerializer(serializers.Serializer):
 
     def get_date_created(self, audit_log):
         return audit_log['date_created'].strftime('%Y-%m-%dT%H:%M:%SZ')
+
+
+class ProjectHistoryLogSerializer(serializers.ModelSerializer):
+    user = serializers.HyperlinkedRelatedField(
+        queryset=get_user_model().objects.all(),
+        lookup_field='username',
+        view_name='user-kpi-detail',
+    )
+    date_created = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ProjectHistoryLog
+        fields = (
+            'user',
+            'user_uid',
+            'username',
+            'action',
+            'metadata',
+            'date_created',
+        )
+
+        read_only_fields = (
+            'user',
+            'user_uid',
+            'username',
+            'action',
+            'metadata',
+            'date_created',
+        )
+
+    def get_date_created(self, audit_log):
+        return audit_log.date_created.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    def get_username(self, audit_log):
+        return audit_log.user.username
