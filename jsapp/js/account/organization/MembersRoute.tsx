af28266bb3f87c754e4191bc57a2eb32886d72c1
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2024-12-11 12:53:19 +0100

    feat(MembersRoute): add actions dropdown to the table of organization members TASK-987 TASK-990 (#5309)
    ### 📣 Summary
    In Members Table (from Account → Team/Organization → Members) add
    actions dropdown with one action. The action allows removing a member or
    leaving the organization.
    
    ### 📖 Description
    The availability of "Remove" action is based on the role of the logged
    in user. When trying to remove user (or leave organization) a
    confirmation prompt is being shown.
    
    ### 👀 Preview steps
    1. ℹ️ have multiple different users
    2. for one of the users (e.g. "joe"), use
    http://kf.kobo.local/admin/organizations/organization/ to add multiple
    users into joe's organization
    3. For one of the users (e.g. "sue") set the role to "admin"
    4. enable "Multi-members override" for joe's organization
    5. enable feature flag `mmosEnabled`
    6. navigate to `#/account/organization/members`
    7. 🟢 notice that a table of organization members displays a list of
    users added in step 2
    8. 🟢 notice that in the table "joe" (as owner) has an ability to remove
    all organization members (excluding themselves) including "sue" who's an
    admin
    
    As continuation for above steps:
    1. log in as "sue"
    2. navigate to `#/account/organization/members`
    3. 🟢 notice that in the table "joe" (the owner) has no actions available
    4. 🟢 notice that any "member" user in the table has actions available,
    and if you click "Remove" a confirmation prompt will appear
    5. 🟢 notice that for "sue" the action available is "Leave team"/"Leave
    organization", and if you click it, a confirmation prompt will appear -
    and it will have different content than confirmation prompt for removing
    a member
    
    As continuation for above steps:
    1. pick a "member" user and use "Remove" action
    2. confirm prompt
    3. 🟢 verify that user was removed successfuly
    
    ### 💭 Notes
    Build atop #5281

--- jsapp/js/account/organization/MembersRoute.tsx
+++ jsapp/js/account/organization/MembersRoute.tsx
@@ -6,10 +6,11 @@ import PaginatedQueryUniversalTable from 'js/universalTable/paginatedQueryUniver
 import LoadingSpinner from 'js/components/common/loadingSpinner';
 import Avatar from 'js/components/common/avatar';
 import Badge from 'jsapp/js/components/common/badge';
+import MemberActionsDropdown from './MemberActionsDropdown';
 
 // Stores, hooks and utilities
 import {formatTime} from 'js/utils';
-import {useOrganizationQuery} from './organizationQuery';
+import {OrganizationUserRole, useOrganizationQuery} from './organizationQuery';
 import useOrganizationMembersQuery from './membersQuery';
 
 // Constants and types
@@ -21,10 +22,88 @@ import styles from './membersRoute.module.scss';
 export default function MembersRoute() {
   const orgQuery = useOrganizationQuery();
 
-  if (!orgQuery.data?.id) {
+  if (!orgQuery.data) {
     return (
       <LoadingSpinner />
     );
+  }
+
+  const columns = [
+    {
+      key: 'user__extra_details__name',
+      label: t('Name'),
+      cellFormatter: (member: OrganizationMember) => (
+        <Avatar
+          size='m'
+          username={member.user__username}
+          isUsernameVisible
+          email={member.user__email}
+          // We pass `undefined` for the case it's an empty string
+          fullName={member.user__extra_details__name || undefined}
+        />
+      ),
+      size: 360,
+    },
+    {
+      key: 'invite',
+      label: t('Status'),
+      size: 120,
+      cellFormatter: (member: OrganizationMember) => {
+        if (member.invite?.status) {
+          return member.invite.status;
+        } else {
+          return <Badge color='light-green' size='s' label={t('Active')} />;
+        }
+        return null;
+      },
+    },
+    {
+      key: 'date_joined',
+      label: t('Date added'),
+      size: 140,
+      cellFormatter: (member: OrganizationMember) => formatTime(member.date_joined),
+    },
+    {
+      key: 'role',
+      label: t('Role'),
+      size: 120,
+    },
+    {
+      key: 'user__has_mfa_enabled',
+      label: t('2FA'),
+      size: 90,
+      cellFormatter: (member: OrganizationMember) => {
+        if (member.user__has_mfa_enabled) {
+          return <Badge size='s' color='light-blue' icon='check' />;
+        }
+        return <Badge size='s' color='light-storm' icon='minus' />;
+      },
+    },
+  ];
+
+  // Actions column is only for owner and admins.
+  if (
+    orgQuery.data.request_user_role === OrganizationUserRole.admin ||
+    orgQuery.data.request_user_role === OrganizationUserRole.owner
+  ) {
+    columns.push({
+      key: 'url',
+      label: '',
+      size: 64,
+      cellFormatter: (member: OrganizationMember) => {
+        // There is no action that can be done on an owner
+        if (member.role === OrganizationUserRole.owner) {
+          return null;
+        }
+
+        return (
+          <MemberActionsDropdown
+            targetUsername={member.user__username}
+            currentUserRole={orgQuery.data.request_user_role}
+          />
+        );
+      },
+    });
   }
 
   return (
@@ -35,67 +114,7 @@ export default function MembersRoute() {
 
       <PaginatedQueryUniversalTable<OrganizationMember>
         queryHook={useOrganizationMembersQuery}
-        columns={[
-          {
-            key: 'user__extra_details__name',
-            label: t('Name'),
-            cellFormatter: (member: OrganizationMember) => (
-              <Avatar
-                size='m'
-                username={member.user__username}
-                isUsernameVisible
-                email={member.user__email}
-                // We pass `undefined` for the case it's an empty string
-                fullName={member.user__extra_details__name || undefined}
-              />
-            ),
-            size: 360,
-          },
-          {
-            key: 'invite',
-            label: t('Status'),
-            size: 120,
-            cellFormatter: (member: OrganizationMember) => {
-              if (member.invite?.status) {
-                return member.invite.status;
-              } else {
-                return <Badge color='light-green' size='s' label={t('Active')} />;
-              }
-              return null;
-            },
-          },
-          {
-            key: 'date_joined',
-            label: t('Date added'),
-            size: 140,
-            cellFormatter: (member: OrganizationMember) => formatTime(member.date_joined),
-          },
-          {
-            key: 'role',
-            label: t('Role'),
-            size: 120,
-          },
-          {
-            key: 'user__has_mfa_enabled',
-            label: t('2FA'),
-            size: 90,
-            cellFormatter: (member: OrganizationMember) => {
-              if (member.user__has_mfa_enabled) {
-                return <Badge size='s' color='light-blue' icon='check' />;
-              }
-              return <Badge size='s' color='light-storm' icon='minus' />;
-            },
-          },
-          {
-            // We use `url` here, but the cell would contain interactive UI
-            // element
-            key: 'url',
-            label: '',
-            size: 64,
-            // TODO: this will be added soon
-            cellFormatter: () => (' '),
-          },
-        ]}
+        columns={columns}
       />
     </div>
   );
