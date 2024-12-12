3e8ff0ad3f63bdbfb8d6df06181f35d8aee79649
Paulo Amorim <pauloamorimbr@gmail.com>
2024-12-12 08:56:06 -0300

    feat(projectHistoryLogs): fetch data from API TASK-881 TASK-1156 (#5343)
    ### 💭 Notes
    This PR fetches data from the backend:
    - Some modifications in the `PaginatedQueryUniversalTable` were needed
    to accommodate passing extra data for the query, that affected existent
    queries. I tried making it a minimal impact though.
    - Filter selector and export button actions will be finished in a future
    task
    
    ### 👀 Preview steps
    1. ℹ️ have account and a project
    2. Have the activity logs feature flag on with:
    `?ff_activityLogsEnabled=true`
    3. Make some changes to the project, like editing the form and
    redeploying
    4. Navigate to Project > Settings > Activity
    5. 🟢 Changes made to the project should appear in the list

--- jsapp/js/api.endpoints.ts
+++ jsapp/js/api.endpoints.ts
@@ -1,4 +1,5 @@
 export const endpoints = {
+  ASSET_HISTORY: '/api/v2/assets/:asset_uid/history/',
   ASSET_URL: '/api/v2/assets/:uid/',
   ORG_ASSETS_URL: '/api/v2/organizations/:organization_id/assets/',
   ME_URL: '/me/',
@@ -11,6 +12,7 @@ export const endpoints = {
   CHECKOUT_URL: '/api/v2/stripe/checkout-link',
   /** Expected parameter: organization_id  **/
   PORTAL_URL: '/api/v2/stripe/customer-portal',
+  PROJECT_HISTORY_LOGS: '/api/v2/project-history-logs/',
   /** Expected parameters: price_id and subscription_id **/
   CHANGE_PLAN_URL: '/api/v2/stripe/change-plan',
   ACCESS_LOGS_URL: '/api/v2/access-logs/me',
