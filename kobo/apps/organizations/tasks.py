1f10bd1d4aa34508774740537e7844d1184f0572
Olivier Leger <olivierleger@gmail.com>
2024-12-13 09:08:27 -0500

    Exclude existing transfers when transferring members data

--- kobo/apps/organizations/tasks.py
+++ kobo/apps/organizations/tasks.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from more_itertools import chunked
 
 from kobo.apps.kobo_auth.shortcuts import User
+from kobo.apps.project_ownership.models import Transfer
 from kobo.apps.project_ownership.utils import create_invite
 from kobo.celery import celery_app
 
@@ -14,7 +15,15 @@ from kobo.celery import celery_app
 def transfer_member_data_ownership_to_org(user_id: int):
     sender = User.objects.get(pk=user_id)
     recipient = sender.organization.owner_user_object
-    user_assets = sender.assets.only('pk', 'uid').iterator()
+    user_assets = (
+        sender.assets.only('pk', 'uid')
+        .exclude(
+            pk__in=Transfer.objects.values_list('asset_id', flat=True).filter(
+                invite__sender=sender, invite__recipient=recipient
+            )
+        )
+        .iterator()
+    )
 
     # Splitting assets into batches to avoid creating a long-running
     # Celery task that could exceed Celery's soft time limit or consume
