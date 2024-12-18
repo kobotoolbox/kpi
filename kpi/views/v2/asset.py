f831d10c92a0e5a833a6da539356f72d761f1f74
rgraber <becca.graber@kobotoolbox.org>
2024-12-18 13:03:34 -0500

    temp: bad comment

--- kpi/views/v2/asset.py
+++ kpi/views/v2/asset.py
@@ -420,6 +420,7 @@ class AssetViewSet(
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
+        # this comment is tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo long
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
