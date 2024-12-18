ad2e5a73fa0a784a6b5fbe5feaa70fae60a5c9a2
Leszek <smutnyleszek@gmail.com>
2024-12-18 14:45:26 +0100

    update SimpleTable stories to use global Mantine setup

--- jsapp/js/components/common/SimpleTable.stories.tsx
+++ jsapp/js/components/common/SimpleTable.stories.tsx
@@ -1,6 +1,4 @@
 import type {Meta, StoryObj} from '@storybook/react';
-import {MantineProvider} from '@mantine/core';
-import {themeKobo} from 'jsapp/js/theme';
 import SimpleTable from './SimpleTable';
 
 const meta: Meta<React.ComponentProps<typeof SimpleTable>> = {
@@ -9,37 +7,35 @@ const meta: Meta<React.ComponentProps<typeof SimpleTable>> = {
   argTypes: {},
   args: {},
   render: ({...args}) => (
-    <MantineProvider theme={themeKobo}>
-      <SimpleTable
-        {...args}
-        head={['Element position', 'Atomic mass', 'Symbol', 'Element name']}
-        body={
+    <SimpleTable
+      {...args}
+      head={['Element position', 'Atomic mass', 'Symbol', 'Element name']}
+      body={
+        [
+          [6, 12.011, 'C', 'Carbon'],
+          [7, 14.007, 'N', 'Nitrogen'],
+          [39, 88.906, 'Y', 'Yttrium'],
+          [56, 137.33, 'Ba', 'Barium'],
           [
-            [6, 12.011, 'C', 'Carbon'],
-            [7, 14.007, 'N', 'Nitrogen'],
-            [39, 88.906, 'Y', 'Yttrium'],
-            [56, 137.33, 'Ba', 'Barium'],
-            [
-              'n/a',
-              'n/a',
-              '??',
-              (
-                <div key='test'>
-                  This is just a DIV. It has a button and an input:
-                  <br/><br/>
-                  <button>button</button>
-                  <br/><br/>
-                  <input type='email'/>
-                  <br/><br/>
-                  It shows you can have any <code>React.ReactNode</code> here.
-                </div>
-              ),
-            ],
-            [58, 140.12, 'Ce', 'Cerium'],
-          ]
-        }
-      />
-    </MantineProvider>
+            'n/a',
+            'n/a',
+            '??',
+            (
+              <div key='test'>
+                This is just a DIV. It has a button and an input:
+                <br/><br/>
+                <button>button</button>
+                <br/><br/>
+                <input type='email'/>
+                <br/><br/>
+                It shows you can have any <code>React.ReactNode</code> here.
+              </div>
+            ),
+          ],
+          [58, 140.12, 'Ce', 'Cerium'],
+        ]
+      }
+    />
   ),
 };
 
