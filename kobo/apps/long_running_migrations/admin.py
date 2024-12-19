088f29e52131eeaf9bf1f760f9064a66509ccefa
Olivier Leger <olivierleger@gmail.com>
2024-12-19 17:57:08 -0500

    Improve Admin for long-running migrations

--- kobo/apps/long_running_migrations/admin.py
+++ kobo/apps/long_running_migrations/admin.py
@@ -5,4 +5,10 @@ from .models import LongRunningMigration
 
 @admin.register(LongRunningMigration)
 class LongRunningMigrationAdmin(admin.ModelAdmin):
-    readonly_fields=('date_created', 'date_modified')
+
+    list_display = ('name', 'status', 'date_modified')
+    readonly_fields = ('name', 'date_created', 'attempts', 'date_modified')
+    fields = ('status', 'attempts', 'date_created', 'date_modified')
+
+    def has_add_permission(self, request, obj=None):
+        return False
