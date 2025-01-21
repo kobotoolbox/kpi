0085b161484f0b6215d47e5ce8098bca5e1cd04a
Anji Tong <ollejna@gmail.com>
2025-01-21 16:03:39 -0500

    WIP tests for 003 and 004

--- kobo/apps/long_running_migrations/tests/test_migrations.py
+++ kobo/apps/long_running_migrations/tests/test_migrations.py
@@ -60,3 +60,80 @@ class LongRunningMigrationTestCase(TestCase):
         migration.execute()
         metadata.refresh_from_db()
         assert str(metadata.data_file).startswith(metadata.xform.user.username)
+
+    def test_0003_migration(self):
+        bob = User.objects.create_user(username='bob', password='bob')
+        alice = User.objects.create_user(username='alice', password='alice')
+        asset = Asset.objects.create(
+            content={
+                'survey': [
+                    {
+                        'type': 'text',
+                        'label': 'Question 1',
+                        'name': 'q1',
+                        '$kuid': 'abc',
+                    },
+                    {
+                        'type': 'text',
+                        'label': 'Question 2',
+                        'name': 'q2',
+                        '$kuid': 'def',
+                    },
+                ],
+                'settings': {},
+            },
+            owner=bob,
+            asset_type='survey',
+        )
+        AssetFile.objects.create(
+            asset=asset, user=bob)
+        )
+        asset.deploy(backend='mock')
+
+        # Testing 003 involves checking if Bob's assets' data are owned by
+        # Alice's MMO
+        # - give bob an asset with data
+        # - make alice an owner of an MMO
+        # - invite bob to alice's MMO
+        # - assert that bob's assets' data are not owned by alice's MMO
+        # - run migration 003
+        # - assert that bob's assets' data are owned by alice's MMO
+
+    def test_0004_migration(self):
+        bob = User.objects.create_user(username='bob', password='bob')
+        alice = User.objects.create_user(username='alice', password='alice')
+        asset = Asset.objects.create(
+            content={
+                'survey': [
+                    {
+                        'type': 'text',
+                        'label': 'Question 1',
+                        'name': 'q1',
+                        '$kuid': 'abc',
+                    },
+                    {
+                        'type': 'text',
+                        'label': 'Question 2',
+                        'name': 'q2',
+                        '$kuid': 'def',
+                    },
+                ],
+                'settings': {},
+            },
+            owner=bob,
+            asset_type='survey',
+        )
+        AssetFile.objects.create(
+            asset=asset, user=bob)
+        )
+        asset.deploy(backend='mock')
+
+        # Testing 004 involves checking if Bob's assets' search fields are
+        # labled with alice's organization
+        # -  make alice an owner of an MMO
+        # -  invite bob to alice's MMO
+        # -  assert that bob's assets' search fields are not owned by alice's
+        #    organization
+        # -  run migration 003
+        # -  assert that bob's assets' search fields are owned by alice's
+        #    organization
