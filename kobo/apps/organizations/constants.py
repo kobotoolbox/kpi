c087fefb87828a274fd9b45dab84d50e54fa1b68
Olivier Leger <olivierleger@gmail.com>
2025-01-23 18:53:39 +0100

    Test linter

--- kobo/apps/organizations/constants.py
+++ kobo/apps/organizations/constants.py
@@ -14,11 +14,13 @@ INVITE_MEMBER_ERROR = (
     'same email address. If you do not already have another account, you can '
     'create one.'
 )
+
 INVITE_ALREADY_ACCEPTED_ERROR = 'Invite has already been accepted.'
 INVITE_NOT_FOUND_ERROR = 'Invite not found.'
 ORG_ADMIN_ROLE = 'admin'
 ORG_EXTERNAL_ROLE = 'external'
 ORG_MEMBER_ROLE = 'member'
 ORG_OWNER_ROLE = 'owner'
-USER_DOES_NOT_EXIST_ERROR = \
+USER_DOES_NOT_EXIST_ERROR = (
     'User with username or email {invitee} does not exist or is not active.'
+)
