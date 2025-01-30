87a380f4bcabab53bb365e182e4ace33577f97e4
Leszek <smutnyleszek@gmail.com>
2025-01-30 21:24:41 +0100

    remove url from banner for now

--- jsapp/js/account/organization/invites/OrgInviteAcceptedBanner.tsx
+++ jsapp/js/account/organization/invites/OrgInviteAcceptedBanner.tsx
@@ -4,12 +4,9 @@ import React from 'react';
 import Alert from 'js/components/common/alert';
 // Stores, hooks and utilities
 import {useOrganizationMemberDetailQuery} from 'jsapp/js/account/organization/membersQuery';
-import { replaceBracketsWithLink } from 'jsapp/js/textUtils';
 import { useOrganizationQuery } from '../organizationQuery';
 // Constants and types
 import {MemberInviteStatus} from 'js/account/organization//membersInviteQuery';
-
-const INVITE_HELP_URL = 'TODO: Add URL here';
 
 export default function OrgInviteAcceptedBanner(props: {orgId: string; username: string}) {
   const organizationMemberDetailQuery = useOrganizationMemberDetailQuery(props.orgId, props.username);
@@ -28,18 +25,11 @@ export default function OrgInviteAcceptedBanner(props: {orgId: string; username:
     // We wrap it in a div to avoid flexbox squashing the content.
     <div>
     <Alert type='info' mt='md' ml='md' mr='md' iconName='information'>
-      <span
-      dangerouslySetInnerHTML={{
-        __html: replaceBracketsWithLink(
-          t(
-            'This account is now managed by ##TEAM_OR_ORGANIZATION_NAME##. All projects previously owned by your account ' +
-            'are currently being transfered and will be owned by ##TEAM_OR_ORGANIZATION_NAME##. This process can take up ' +
-            'to a few minutes to complete. Learn more about teams [here].'
-          ).replaceAll('##TEAM_OR_ORGANIZATION_NAME##', orgQuery.data.name),
-          INVITE_HELP_URL
-        ),
-      }}
-      />
+      {t(
+        'This account is now managed by ##TEAM_OR_ORGANIZATION_NAME##. All projects previously owned by your account ' +
+        'are currently being transfered and will be owned by ##TEAM_OR_ORGANIZATION_NAME##. This process can take up ' +
+        'to a few minutes to complete.'
+      ).replaceAll('##TEAM_OR_ORGANIZATION_NAME##', orgQuery.data.name)}
     </Alert>
     </div>
   );
