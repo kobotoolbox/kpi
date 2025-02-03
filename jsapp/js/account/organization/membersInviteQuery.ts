bdc8c97405106e3b93ca319a045550a71ae0c041
James Kiger <james.kiger@gmail.com>
2025-02-03 11:15:47 -0500

    Initial adjustments to members table for handling org invitees

--- jsapp/js/account/organization/membersInviteQuery.ts
+++ jsapp/js/account/organization/membersInviteQuery.ts
@@ -30,7 +30,7 @@ import {type Json} from 'jsapp/js/components/common/common.interfaces';
  * The source of truth of statuses are at `OrganizationInviteStatusChoices` in
  * `kobo/apps/organizations/models.py`. This enum should be kept in sync.
  */
-enum MemberInviteStatus {
+export enum MemberInviteStatus {
   accepted = 'accepted',
   cancelled = 'cancelled',
   complete = 'complete',
@@ -65,6 +65,10 @@ interface MemberInviteRequestBase {
 interface SendMemberInviteParams extends MemberInviteRequestBase {
   /** List of usernames. */
   invitees: string[];
+}
+
+interface MemberInviteUpdate extends MemberInviteRequestBase{
+  status: MemberInviteStatus
 }
 
 interface MemberInviteUpdate extends MemberInviteRequestBase{
