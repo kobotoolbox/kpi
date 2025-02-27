505c4b082caa6560e0771470e5a90c197ee9fd1a
Leszek <smutnyleszek@gmail.com>
2025-02-27 10:45:29 +0100

    update todo comment

--- jsapp/js/components/map/map.tsx
+++ jsapp/js/components/map/map.tsx
@@ -471,7 +471,9 @@ export class FormMap extends React.Component<FormMapProps, FormMapState> {
               .then((response) => {
                 if (isValidGeoJSON(JSON.stringify(response))) {
                   overlayLayer = omnivore
-                    // TODO: should this be .parse?
+                    // We have already loaded file content, but unfortunately omnivore doesn't support parsing JSON
+                    // strings for GeoJSON (it does support most of other types though…). So unfortunately we need to
+                    // make it load the file second time (`.geojson` does a call to fetch URL)
                     .geojson(layer.content)
                     .on('error', () => {
                       notify.error(OVERLAY_ERROR_OMNIVORE.replace('##name##', layer.description))
@@ -483,7 +485,8 @@ export class FormMap extends React.Component<FormMapProps, FormMapState> {
                   notify.error(OVERLAY_ERROR_INVALID_GEOJSON.replace('##name##', layer.description))
                 }
               })
-              .catch(() => {
+              .catch((err) => {
+                console.error(err)
                 notify.error(OVERLAY_ERROR.replace('##name##', layer.description) + ' 1')
               })
           } catch (err) {
