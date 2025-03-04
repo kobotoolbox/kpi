7224c3692498e037adf4f1b50ed4dd5a919b9a52
Leszek <smutnyleszek@gmail.com>
2025-03-04 16:29:08 +0100

    update checkbox, koboDropdown, and koboRange stories

--- jsapp/js/components/common/checkbox.stories.tsx
+++ jsapp/js/components/common/checkbox.stories.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import type { ComponentStory, ComponentMeta } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
 import Checkbox from './checkbox'
 
-export default {
+const meta: Meta<typeof Checkbox> = {
   title: 'common/Checkbox',
   component: Checkbox,
-  argTypes: {},
-} as ComponentMeta<typeof Checkbox>
+  argTypes: {
+    label: { type: 'string' },
+    checked: { type: 'boolean' },
+    onChange: { action: 'changed' },
+  },
+}
 
-const Template: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} onChange={() => {}} />
+export default meta
 
-export const Primary = Template.bind({})
-Primary.args = {
-  label: 'I approve',
+type Story = StoryObj<typeof Checkbox>
+
+export const Default: Story = {
+  args: {
+    label: 'Checkbox',
+    checked: false,
+  },
 }
