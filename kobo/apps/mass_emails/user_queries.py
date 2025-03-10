3729098f86e10024be910fbc0fd90272306301a8
Raj Patel <51355159+rajpatel24@users.noreply.github.com>
2025-03-10 22:48:15 +0530

    feat(massEmail): query for identifying inactive users based on login, form activity, and submissions TASK-1581 (#5569)
    ### 📣 Summary
    Detect and return a list of users who have been inactive for a specified
    period based on login activity, project updates, and submissions.
    
    
    ### 📖 Description
    This update introduces a method to identify inactive users who have not:
    
    - Logged into their account.
    - Modified their projects or had modifications made by others.
    - Received submissions to their projects.
    
    By default, the inactivity period is set to 365 days but can be
    configured dynamically.
    
    
    ### 👀 Preview steps
    
    1. Run `get_inactive_users(days=1)` in the shell, users inactive for 1+
    days should be listed.
    2. Modify `inactivity_threshold = now() - timedelta(minutes=2)`
    3. Log in & log out, run `get_inactive_users()`, user should NOT be
    listed. Wait 2 mins, run again, user should be listed now.
    4. Log in, create a project, run `get_inactive_users()`, user should NOT
    be listed. Wait 2 mins, run again, user should be listed now.
    5. Change the name of the project, run `get_inactive_users()`, user
    should NOT be listed. Wait 2 mins, run again, user should be listed now.
    6. Add a submission, run `get_inactive_users()`, user should NOT be
    listed. Wait 2 mins, run again, user should be listed.

--- kobo/apps/mass_emails/user_queries.py
+++ kobo/apps/mass_emails/user_queries.py
@@ -1,11 +1,59 @@
+from datetime import timedelta
 from math import inf
 
-from django.db.models import QuerySet
+from django.db.models import Q, QuerySet
+from django.utils.timezone import now
 
 from kobo.apps.kobo_auth.shortcuts import User
 from kobo.apps.organizations.models import Organization
+from kobo.apps.openrosa.apps.logger.models import Instance
 from kobo.apps.stripe.utils import get_organization_plan_limits
+from kpi.models import Asset
 from kpi.utils.usage_calculator import get_storage_usage_by_user_id
+
+
+def get_inactive_users(days: int = 365) -> QuerySet:
+    """
+    Retrieve users who have been inactive for a specified number of days.
+
+    A user is considered inactive if:
+    - They have not logged in within the given period (or never logged in).
+    - They have not modified or created an asset within the given period.
+    - They have not modified or have a submission within the given period.
+
+    Note: Users created within the given period who never logged in are not
+    considered inactive.
+
+    :param days: int: Number of days to determine inactivity (default: 365 days)
+
+    :return: A queryset of inactive users
+    """
+    # Calculate the user inactivity threshold date
+    days = days or 365
+    inactivity_threshold = now() - timedelta(days=days)
+
+    # Identify users who have not logged in within the given period
+    inactive_users = User.objects.filter(
+        Q(last_login__lt=inactivity_threshold) |
+        (Q(last_login__isnull=True) & Q(date_joined__lt=inactivity_threshold))
+    )
+
+    # Find users who have active projects within the given period
+    active_asset_owners = Asset.objects.filter(
+        Q(date_modified__gt=inactivity_threshold) |
+        Q(date_created__gt=inactivity_threshold)
+    ).values_list('owner_id', flat=True)
+
+    # Find users who have active submissions within the given period
+    active_submission_owners = Instance.objects.filter(
+        Q(date_modified__gt=inactivity_threshold) |
+        Q(date_created__gt=inactivity_threshold)
+    ).values_list('xform__user_id', flat=True)
+
+    active_users = set(active_asset_owners) | set(active_submission_owners)
+    return inactive_users.exclude(
+        Q(id__in=active_users) | Q(username='AnonymousUser')
+    )
 
 
 def get_users_within_range_of_usage_limit(
