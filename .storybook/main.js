kobotoolbox/kpi

commit d5796bd4e412aac55c519bf629ae650ff25af4d5

style(frontend): organize imports with Biome TASK-1640 (#5568)
### 💭 Notes

See `package.json`, `tsconfig.json`, `biome.jsonc`, `webpack.common.js`,
`kpi.code-workspace` and `js/k-icons.ts` files. The rest are
semi-auto-generated.

Organize imports using Biome, automatically on save in VSCode workspace.
As Biome explains:
> Import statements are sorted by “distance”. Modules that are “farther”
from the user are put on the top, modules “closer” to the user are put
on the bottom.

For smarter distance calculation Biome treats `#` prefixed paths as
internal paths (as [implemented by nodejs
itself](https://nodejs.org/docs/latest-v14.x/api/packages.html#packages_imports)),
and so I also did a simple search/replace refactor to consolidate
aliases and remap `jsapp/js/*` to `#/*`.

Biome supports [grouped
imports](https://biomejs.dev/analyzer/import-sorting/#grouped-imports).
Note that manually set grouped imports conflicts with auto-import
functionality - auto-imports just add the new import at the end of
imports and so at the end of last import group instead of the correct
import group, and then either we commit wrong grouping or the import has
to be adjusted manually anyways. Usually the first, but both defeats the
purpose of either auto-imports or import group. Unless, it's ok to most
of time to automatically add to only the last group, while only manually
to others! Here's my compromise:
- first group for side-effect imports (e.g. `import './example.scss'`),
we want the order of them manually reviewed anyways. And we will have
less of them over time.
- second group for `import { ... } from 'react'` only, because it looks
like all of like to see react import at the top of other imports,
because almost all files already had it there. It's only one line (or
none), there wont be second line.
- third group for everything else, auto-import safe and auto-sorted.

### 👀 Preview steps

Developers:
- open repo in the VSCode workspace

.storybook/preview.js
@@ -1,11 +1,13 @@
 import 'jsapp/scss/main.scss'
-import 'js/bemComponents'
+import '#/bemComponents'
 import '@mantine/core/styles.css'
+
 import { useEffect } from 'react'
+
+import { MantineProvider, useMantineColorScheme } from '@mantine/core'
 import { addons } from '@storybook/preview-api'
 import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode'
-import { MantineProvider, useMantineColorScheme } from '@mantine/core'
-import { themeKobo } from 'jsapp/js/theme'
+import { themeKobo } from '#/theme'
 
 const channel = addons.getChannel()
 

jsapp/js/main.js
@@ -2,18 +2,19 @@
  * The Project Management app bundle file. All the required setup is done here
  * plus it is the file that is handling the root rendering.
  */
-
 import 'jquery-ui/ui/widgets/sortable'
-import moment from 'moment'
-import AllRoutes from 'js/router/allRoutes'
-import RegistrationPasswordApp from './registrationPasswordApp'
+import '../scss/main.scss'
+
 import React from 'react'
+
+import * as Sentry from '@sentry/react'
+import moment from 'moment'
 import { Cookies } from 'react-cookie'
 import { createRoot } from 'react-dom/client'
-import * as Sentry from '@sentry/react'
-import { csrfSafeMethod, currentLang } from 'utils'
-import '../scss/main.scss'
 import Modal from 'react-modal'
+import AllRoutes from '#/router/allRoutes'
+import { csrfSafeMethod, currentLang } from '#/utils'
+import RegistrationPasswordApp from './registrationPasswordApp'
 
 const sentryDsnEl = document.head.querySelector('meta[name=sentry-dsn]')
 if (sentryDsnEl !== null) {

commit 6f7c217a371fb75f3c8e43928be5008d96b0cbe8

fix(storybook): handle the new '#' path alias
Storybook was broken since by d5796bd4e, this fixes it.

.storybook/main.js
@@ -34,11 +34,9 @@ module.exports = {
       resolve: {
         extensions: ['.jsx', '.js', '.coffee', '.ts', '.tsx', '.scss'],
         alias: {
-          app: path.join(__dirname, '../app'),
-          jsapp: path.join(__dirname, '../jsapp'),
+          '#': path.join(__dirname, '../jsapp/js'),
           js: path.join(__dirname, '../jsapp/js'),
           scss: path.join(__dirname, '../jsapp/scss'),
-          utils: path.join(__dirname, '../jsapp/js/utils'),
         },
       },
     })

.storybook/preview.js
@@ -1,4 +1,4 @@
-import 'jsapp/scss/main.scss'
+import 'scss/main.scss'
 import '#/bemComponents'
 import '@mantine/core/styles.css'
 