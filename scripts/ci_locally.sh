bc5f1c76a2eb5ce54b19e8551a6ac1a0a19e13d3
Olivier Leger <olivierleger@gmail.com>
2025-03-18 15:15:21 +0100

    Improve first pytest pass output

--- scripts/ci_locally.sh
+++ scripts/ci_locally.sh
@@ -12,6 +12,35 @@ if [ "$WHOAMI" != "$OWNER" ]; then
     GOSU_USER=$OWNER
 fi
 
+run_parallel_pytest() {
+    echo -n 'Running initial test pass '
+    # Run pytest in the background and redirect output to a temporary file
+    pytest -vv --disable-warnings -n auto > /tmp/pytest_output.log 2>&1 &
+    pid=$!
+
+    spinner='|/-\'
+    progress="0%"
+
+    # Loop while pytest is still running
+    while kill -0 $pid 2>/dev/null; do
+        # Extract the latest progress percentage from the pytest output log
+        latest_progress=$(grep -o '[0-9]\+%' /tmp/pytest_output.log | tail -n 1)
+        if [[ ! -z "$latest_progress" ]]; then
+            progress=$latest_progress
+        fi
+
+        # Display the spinner and the current progress
+        for i in `seq 0 3`; do
+          echo -ne "\rRunning initial test pass ${spinner:i:1} $progress"
+          sleep 0.2
+        done
+
+    done
+
+    echo -e '\rRunning initial test pass ✔ 100%'
+    rm -f /tmp/pytest_output.log
+}
+
 echo -e '\n\n# Run CI jobs locally'
 echo 'Disclaimer: local environment may differ from Github Actions environment.'
 
@@ -40,24 +69,24 @@ else
     darker --check --isort -L "flake8 --max-line-length=88 --extend-ignore=F821" kpi kobo hub -r "origin/$CURRENT_BRANCH"
 fi
 
-
 echo -e '\n\n## Job: Pytest'
 
 echo -e '\n\n### Step: Update translations'
 if [ -n "$GOSU_USER" ]; then
     gosu "$GOSU_USER" git submodule init
     gosu "$GOSU_USER" git submodule update --remote
-    gosu "$GOSU_USER" python manage.py compilemessages
+    gosu "$GOSU_USER" python manage.py compilemessages -v 0
 else
-    git submodule init && git submodule update --remote && python manage.py compilemessages
+    git submodule init && git submodule update --remote && python manage.py compilemessages -v 0
 fi
 echo -e '\n\n### Step: Test back-end code'
 echo 'Disclaimer: CI uses pytest with coverage option, this script does not.'
 # Speed-up pytest by running tests in parallel
-pytest -q --disable-warnings -n auto || true
+run_parallel_pytest
 # Run only the last failed tests sequentially.
 # Tests that previously failed due to concurrency should pass now.
 # If they still fail, it indicates genuine test failures.
+echo -e '\nRunning final test pass'
 pytest -q --disable-warnings --lf -rf
 
 echo -e '\n\n### Step: Run coveralls for back-end code'
