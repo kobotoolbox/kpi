2ad7d41b1e4406dcb33f09fc7f7b4b7234774fc7
Olivier Leger <olivierleger@gmail.com>
2025-03-18 11:49:24 +0100

    Fix permission issues under linux env

--- scripts/ci_locally.sh
+++ scripts/ci_locally.sh
@@ -34,17 +34,23 @@ echo 'Disclaimer: CI runs Darker only on the last commit and the remote branch'
 
 if [ -n "$GOSU_USER" ]; then
     CURRENT_BRANCH=$(gosu "$GOSU_USER" git rev-parse --abbrev-ref HEAD)
+    gosu $GOSU_USER darker --check --isort -L "flake8 --max-line-length=88 --extend-ignore=F821" kpi kobo hub -r "origin/$CURRENT_BRANCH"
 else
     CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    darker --check --isort -L "flake8 --max-line-length=88 --extend-ignore=F821" kpi kobo hub -r "origin/$CURRENT_BRANCH"
 fi
 
-darker --check --isort -L "flake8 --max-line-length=88 --extend-ignore=F821" kpi kobo hub -r "origin/$CURRENT_BRANCH"
 
 echo -e '\n\n## Job: Pytest'
 
 echo -e '\n\n### Step: Update translations'
-git submodule init && git submodule update --remote && python manage.py compilemessages
-
+if [ -n "$GOSU_USER" ]; then
+    gosu "$GOSU_USER" git submodule init
+    gosu "$GOSU_USER" git submodule update --remote
+    gosu "$GOSU_USER" python manage.py compilemessages
+else
+    git submodule init && git submodule update --remote && python manage.py compilemessages
+fi
 echo -e '\n\n### Step: Test back-end code'
 echo 'Disclaimer: CI uses pytest with coverage option, this script does not.'
 # Speed-up pytest by running tests in parallel
