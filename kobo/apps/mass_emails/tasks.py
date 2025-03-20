34166ff9ec8fd9adee206d8d453d339bf50c7f4b
Raj Patel <rajpatel24998@gmail.com>
2025-03-20 18:12:03 +0530

    Add admin action to enqueue mass email records

--- kobo/apps/mass_emails/tasks.py
+++ kobo/apps/mass_emails/tasks.py
@@ -2,9 +2,16 @@ from datetime import timedelta
 
 from constance import config
 from django.utils.timezone import now
+from django.conf import settings
 
 from kobo.celery import celery_app
-from kobo.apps.mass_emails.models import EmailStatus, MassEmailRecord
+from kobo.apps.mass_emails.models import (
+    EmailStatus,
+    MassEmailConfig,
+    MassEmailRecord,
+    MassEmailJob,
+    USER_QUERIES
+)
 from kpi.utils.log import logging
 
 
@@ -25,4 +32,29 @@ def mark_old_enqueued_mass_email_record_as_failed():
     logging.info(
         f'Updated {updated_records} MassEmailRecord(s) from `enqueued` to `failed` '
         f'that were older than {threshold_date}.'
+    )
+
+
+@celery_app.task(
+    queue='kpi_low_priority_queue',
+    soft_time_limit=settings.CELERY_LONG_RUNNING_TASK_SOFT_TIME_LIMIT,
+    time_limit=settings.CELERY_LONG_RUNNING_TASK_TIME_LIMIT,
+)
+def enqueue_mass_email_records(email_config_id: int):
+    """
+    Creates a email job and enqueues email records for users based on query
+    """
+    email_config = MassEmailConfig.objects.get(id=email_config_id)
+    job = MassEmailJob.objects.create(email_config=email_config)
+    users = USER_QUERIES.get(email_config.query, lambda: [])()
+
+    records = [
+        MassEmailRecord(user=user, email_job=job, status=EmailStatus.ENQUEUED)
+        for user in users
+    ]
+    MassEmailRecord.objects.bulk_create(records)
+
+    logging.info(
+        f'Created {len(records)} MassEmailRecord(s) for {email_config.name} '
+        f'with query {email_config.query}'
     )
