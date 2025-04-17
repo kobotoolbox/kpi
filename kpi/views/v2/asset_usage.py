8bf5efa7881b79e5324b133db9392259670d6097
zoegodin-admin <2133178@cshawi.ca>
2025-04-17 14:54:18 -0400

    added basic tags distinctions for asset_subscription, asset_usage, assets

--- kpi/views/v2/asset_usage.py
+++ kpi/views/v2/asset_usage.py
@@ -8,7 +8,9 @@ from kpi.permissions import IsAuthenticated
 from kpi.serializers.v2.service_usage import AssetUsageSerializer
 
 
-
+@extend_schema(
+    tags=['asset-usage'],
+)
 class AssetUsageViewSet(ListModelMixin, viewsets.GenericViewSet):
     """
     ## Asset Usage Tracker
