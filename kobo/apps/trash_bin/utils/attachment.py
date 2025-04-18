cfe910a8ba0764e26128e2810f4f916966372e17
Olivier Leger <olivierleger@gmail.com>
2025-04-18 10:38:29 -0400

    fix attachments deletion

--- kobo/apps/trash_bin/utils/attachment.py
+++ kobo/apps/trash_bin/utils/attachment.py
@@ -17,8 +17,9 @@ def delete_attachment(attachment_trash: AttachmentTrash):
         # Delete the file on storage but do not persist the change in DB.
         # Otherwise, the filename will be lost.
         attachment.media_file.delete(save=False)
-        Attachment.objects.filter(id=attachment_id).update(
+        Attachment.all_objects.filter(id=attachment_id).update(
             delete_status=AttachmentDeleteStatus.DELETED,
             date_modified=timezone.now(),
         )
+        # TODO log deletion
         attachment_trash.delete()
