d4a92371cce4c376c103e840141392341562b7b6
zoegodin-admin <2133178@cshawi.ca>
2025-04-23 10:32:53 -0400

    Added documentation for asset usage endpoint

--- kpi/views/v2/asset_usage.py
+++ kpi/views/v2/asset_usage.py
@@ -6,45 +6,14 @@ from kpi.models.asset import Asset
 from kpi.paginators import AssetUsagePagination
 from kpi.permissions import IsAuthenticated
 from kpi.serializers.v2.service_usage import AssetUsageSerializer
-
+from kpi.views.docs.asset_usage.asset_usage_docs import assset_usage_documentation
 
 @extend_schema(
     tags=['asset-usage'],
+    description=assset_usage_documentation,
 )
 class AssetUsageViewSet(ListModelMixin, viewsets.GenericViewSet):
-    """
-    ## Asset Usage Tracker
-    Tracks the total and monthly submissions per asset, as well as NLP usage and total storage use
 
-    <pre class="prettyprint">
-    <b>GET</b> /api/v2/asset_usage/
-    </pre>
-
-    > Example
-    >
-    >       curl -X GET https://[kpi]/api/v2/asset_usage/
-    >       {
-    >           "count": {integer},
-    >           "next": {url_to_next_page},
-    >           "previous": {url_to_previous_page},
-    >           "results": [
-    >               {
-    >                   "asset": {asset_url},
-    >                   "asset_name": {string},
-    >                   "nlp_usage_current_period": {
-    >                       "total_asr_seconds": {integer},
-    >                       "total_mt_characters": {integer},
-    >                   }
-    >                   "nlp_usage_all_time": {
-    >                       "total_asr_seconds": {integer},
-    >                       "total_mt_characters": {integer},
-    >                   }
-    >                   "storage_bytes": {integer},
-    >                   "submission_count_current_period": {integer},
-    >                   "submission_count_all_time": {integer},
-    >               },{...}
-    >           ]
-    """
     renderer_classes = (
         renderers.BrowsableAPIRenderer,
         renderers.JSONRenderer,
