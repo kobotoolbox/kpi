28046eb9702161027cf6e88fc7508f90b4976ebb
zoegodin-admin <2133178@cshawi.ca>
2025-04-23 10:20:54 -0400

    Added asset subscription endpoint documentation

--- kpi/views/v2/user_asset_subscription.py
+++ kpi/views/v2/user_asset_subscription.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets
 
 from kpi.models import UserAssetSubscription
@@ -7,9 +7,37 @@ from kpi.serializers.v2.user_asset_subscription import (
     UserAssetSubscriptionSerializer,
 )
 from kpi.utils.object_permission import get_database_user
+from kpi.views.docs.asset_subscription.asset_subscription_doc import (
+    asset_subscription_create,
+    asset_subscription_destroy,
+    asset_subscription_list,
+    asset_subscription_get,
+    asset_subscription_update,
+    asset_subscription_partial_update
+)
 
 @extend_schema(
     tags=['asset-subscriptions'],
+)
+@extend_schema_view(
+    create=extend_schema(
+        description=asset_subscription_create,
+    ),
+    destroy=extend_schema(
+        description=asset_subscription_destroy,
+    ),
+    list=extend_schema(
+        description=asset_subscription_list,
+    ),
+    retrieve=extend_schema(
+        description=asset_subscription_get,
+    ),
+    update=extend_schema(
+        description=asset_subscription_update,
+    ),
+    partial_update=extend_schema(
+        description=asset_subscription_partial_update,
+    ),
 )
 class UserAssetSubscriptionViewSet(viewsets.ModelViewSet):
     queryset = UserAssetSubscription.objects.none()
