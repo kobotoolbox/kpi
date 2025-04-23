8c9e43038bb56d044379fb94bfc6c80ee261c5f6
Leszek <smutnyleszek@gmail.com>
2025-04-23 11:57:31 +0200

    remove unused file

--- jsapp/js/storybookUtils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * To be used inside storybook test (inside `play` function) to wait for an API call to finish between actions.
- * For example, `await sleep(2000)`
- * @param ms - milliseconds to wait
- */
-export function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
