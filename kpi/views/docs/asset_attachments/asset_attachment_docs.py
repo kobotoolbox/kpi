2f3013df2dd1e06a6ec4de5d27a4dab4c3770ac6
zoegodin-admin <2133178@cshawi.ca>
2025-04-24 14:25:49 -0400

    added documentation for attachment-thumb endpoint

--- kpi/views/docs/asset_attachments/asset_attachment_docs.py
+++ kpi/views/docs/asset_attachments/asset_attachment_docs.py
@@ -43,4 +43,33 @@ Convert audio and video files. Only conversions to MP3 is supported for this fea
 """
 
 asset_attachment_thumb = """
+## GET attachment file with specified thumbnail size
+
+<pre class="prettyprint">
+<b>GET</b>  /api/v2/assets/{parent_lookup_asset}/data/{parent_lookup_data}/attachments/{id}/{sufix}
+</pre>
+
+<sup>*</sup>`data_id` can be the primary key of the submission or its `uuid`.
+Please note that using the `uuid` may match **several** submissions, only
+the first match will be returned.
+
+<sup>*</sup>`suffix` is an optional parameter.
+If unspecified, the original thumbnail size will be returned.
+
+> Example original size
+>
+>       curl -X GET https://[kpi]/api/v2/assets/{parent_lookup_asset}/data/{parent_lookup_data}/attachments/{id}/
+
+> Example small size
+>
+>        curl -X GET https://[kpi]/api/v2/assets/{parent_lookup_asset}/data/{parent_lookup_data}/attachments/{id}/small/
+
+> Example medium size
+>
+>        curl -X GET https://[kpi]/api/v2/assets/{parent_lookup_asset}/data/{parent_lookup_data}/attachments/{id}/medium/
+
+> Example large size
+>
+>        curl -X GET https://[kpi]/api/v2/assets/{parent_lookup_asset}/data/{parent_lookup_data}/attachments/{id}/large/
+
 """
