9216adfabc24517b462fc15cb10a2f734b3da2ff
zoegodin-admin <2133178@cshawi.ca>
2025-04-24 12:11:11 -0400

    removed unescessary import

--- kobo/apps/audit_log/views.py
+++ kobo/apps/audit_log/views.py
@@ -1,6 +1,5 @@
 from django.db import transaction
 from drf_spectacular.utils import extend_schema, extend_schema_view
-from jsonschema.validators import create
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
