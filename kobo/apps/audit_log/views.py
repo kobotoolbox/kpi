559bebb7ac44ffb2c896e8841987ab5b76ba105c
zoegodin-admin <2133178@cshawi.ca>
2025-04-25 10:35:17 -0400

    added back original comments for future python doc

--- kobo/apps/audit_log/views.py
+++ kobo/apps/audit_log/views.py
@@ -185,6 +185,74 @@ class AuditLogViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     description=access_logs_get
 )
 class AllAccessLogViewSet(AuditLogViewSet):
+    """
+    Access logs
+    Lists all access logs for all users. Only available to superusers.
+    Submissions will be grouped together by user by hour
+    <pre class="prettyprint">
+    <b>GET</b> /api/v2/access-logs/
+    </pre>
+    > Example
+    >
+    >       curl -X GET https://[kpi-url]/access-logs/
+    > Response 200
+    >       {
+    >           "count": 10,
+    >           "next": null,
+    >           "previous": null,
+    >           "results": [
+    >                {
+    >                    "user": "http://localhost/api/v2/users/admin/",
+    >                    "user_uid": "u12345",
+    >                    "username": "admin",
+    >                    "metadata": {
+    >                        "source": "Firefox (Ubuntu)",
+    >                        "auth_type": "digest",
+    >                        "ip_address": "172.18.0.6"
+    >                   },
+    >                    "date_created": "2024-08-19T16:48:58Z",
+    >                },
+    >                {
+    >                    "user": "http://localhost/api/v2/users/someuser/",
+    >                    "user_uid": "u5678",
+    >                    "username": "someuser",
+    >                    "metadata": {
+    >                        "auth_type": "submission-group",
+    >                    },
+    >                    "date_created": "2024-08-19T16:00:00Z"
+    >                },
+    >                ...
+    >           ]
+    >       }
+    Results from this endpoint can be filtered by a Boolean query
+    specified in the `q` parameter.
+    **Filterable fields:**
+    1. date_created
+    2. user_uid
+    3. user__*
+        a. user__username
+        b. user__email
+        c. user__is_superuser
+    4. metadata__*
+        a. metadata__auth_type
+            available auth types:
+            i. django-loginas
+            ii. token
+            iii. digest
+            iv. basic
+            v. submission-group
+            vi. kpi.backends.ModelBackend
+            vii. authorized-application
+            viii. oauth2
+            ix. unknown
+        b. metadata__source
+        c. metadata__ip_address
+        d. metadata__initial_user_uid
+        e. metadata__initial_user_username
+        f. metadata__authorized_app_name
+    This endpoint can be paginated with 'offset' and 'limit' parameters, eg
+    >      curl -X GET https://[kpi-url]/access-logs/?offset=100&limit=50
+    """
     queryset = AccessLog.objects.with_submissions_grouped().order_by('-date_created')
     serializer_class = AccessLogSerializer
 
@@ -194,6 +262,49 @@ class AllAccessLogViewSet(AuditLogViewSet):
     description=access_logs_me,
 )
 class AccessLogViewSet(AuditLogViewSet):
+    """
+    Access logs
+    Lists all access logs for the authenticated user
+    Submissions will be grouped together by hour
+    <pre class="prettyprint">
+    <b>GET</b> /api/v2/access-logs/me/
+    </pre>
+    > Example
+    >
+    >       curl -X GET https://[kpi-url]/access-logs/me/
+    > Response 200
+    >       {
+    >           "count": 10,
+    >           "next": null,
+    >           "previous": null,
+    >           "results": [
+    >                {
+    >                    "user": "http://localhost/api/v2/users/admin/",
+    >                    "user_uid": "u12345",
+    >                    "username": "admin",
+    >                    "metadata": {
+    >                        "source": "Firefox (Ubuntu)",
+    >                        "auth_type": "Digest",
+    >                        "ip_address": "172.18.0.6"
+    >                    },
+    >                    "date_created": "2024-08-19T16:48:58Z"
+    >                },
+    >                {
+    >                    "user": "http://localhost/api/v2/users/admin/",
+    >                    "user_uid": "u12345",
+    >                    "username": "admin",
+    >                    "metadata": {
+    >                        "auth_type": "submission-group",
+    >                    },
+    >                    "date_created": "2024-08-19T16:00:00Z"
+    >                },
+    >                ...
+    >           ]
+    >       }
+    This endpoint can be paginated with 'offset' and 'limit' parameters, eg
+    >      curl -X GET https://[kpi-url]/access-logs/me/?offset=100&limit=50
+    will return entries 100-149
+    """
 
     queryset = AccessLog.objects.with_submissions_grouped().order_by('-date_created')
     permission_classes = (IsAuthenticated,)
@@ -666,7 +777,44 @@ class BaseAccessLogsExportViewSet(viewsets.ViewSet):
     ),
 )
 class AccessLogsExportViewSet(BaseAccessLogsExportViewSet):
-
+    """
+    Access logs export
+    Lists all access logs export tasks for the authenticated user
+    <pre class="prettyprint">
+    <b>GET</b> /api/v2/access-logs/me/export
+    </pre>
+    > Example
+    >
+    >       curl -X GET https://[kpi-url]/access-logs/me/export
+    > Response 200
+    >
+    >       [
+    >           {
+    >               "uid": "aleooVUrhe3cRrLY5urRhxLA",
+    >               "status": "complete",
+    >               "date_created": "2024-11-26T21:27:08.403181Z"
+    >           },
+    >           {
+    >               "uid": "aleMzK7RnuaPokb86TZF2N4d",
+    >               "status": "complete",
+    >               "date_created": "2024-11-26T20:18:55.982974Z"
+    >           }
+    >       ]
+    ### Creates an export task
+    <pre class="prettyprint">
+    <b>POST</b> /api/v2/access-log/me/export
+    </pre>
+    > Example
+    >
+    >       curl -X POST https://[kpi-url]/access-logs/me/export
+    > Response 202
+    >
+    >       [
+    >           "status: created"
+    >       ]
+    >
+    """
+    
     def create(self, request, *args, **kwargs):
         if AccessLogExportTask.objects.filter(
             user=request.user,
