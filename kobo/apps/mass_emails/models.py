d006e03ac16b9c0417281651609b3609a9748f4b
rgraber <becca.graber@kobotoolbox.org>
2025-05-07 15:53:28 -0400

    feat(massEmails): add 80% queries

--- kobo/apps/mass_emails/models.py
+++ kobo/apps/mass_emails/models.py
@@ -5,6 +5,8 @@ from django.db import models
 from kobo.apps.kobo_auth.shortcuts import User
 from kobo.apps.mass_emails.user_queries import (
     get_inactive_users,
+    get_users_over_80_percent_of_storage_limit,
+    get_users_over_80_percent_of_submission_limit,
     get_users_over_90_percent_of_nlp_limits,
     get_users_over_90_percent_of_storage_limit,
     get_users_over_90_percent_of_submission_limit,
@@ -16,9 +18,11 @@ from kpi.fields import KpiUidField
 from kpi.models.abstract_models import AbstractTimeStampedModel
 
 USER_QUERIES: dict[str, Callable] = {
+    'users_above_80_percent_storage': get_users_over_80_percent_of_storage_limit,
     'users_above_90_percent_storage': get_users_over_90_percent_of_storage_limit,
     'users_above_100_percent_storage': get_users_over_100_percent_of_storage_limit,
     'users_inactive_for_365_days': get_inactive_users,
+    'users_above_80_percent_submissions': get_users_over_80_percent_of_submission_limit,
     'users_above_90_percent_submissions': get_users_over_90_percent_of_submission_limit,
     'users_above_100_percent_submissions': get_users_over_100_percent_of_submission_limit,  # noqa
     'users_above_90_percent_nlp_usage': get_users_over_90_percent_of_nlp_limits,
