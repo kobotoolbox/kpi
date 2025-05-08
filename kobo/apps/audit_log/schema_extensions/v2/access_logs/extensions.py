6fb6cb809524a96e4c235eed621167c4607b5f13
zoegodin-admin <2133178@cshawi.ca>
2025-05-08 13:31:49 -0400

    added lacking URL fields to access-logs schema examples

--- kobo/apps/audit_log/schema_extensions/v2/access_logs/extensions.py
+++ kobo/apps/audit_log/schema_extensions/v2/access_logs/extensions.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from drf_spectacular.extensions import OpenApiSerializerFieldExtension
 from drf_spectacular.plumbing import build_basic_type
 from drf_spectacular.types import OpenApiTypes
-
+from rest_framework.reverse import reverse
 
 # This drf-extension made for the metadata field of AccessLog targets the external class
 # and tells it what it should return when generating the schema.
@@ -19,4 +20,20 @@ class AccessLogMetadataFieldExtension(OpenApiSerializerFieldExtension):
                 'initial_user_username': build_basic_type(OpenApiTypes.STR),
                 'authorized_app_name': build_basic_type(OpenApiTypes.STR),
             },
+        }
+
+
+class AccessLogUserFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kobo.apps.audit_log.schema_extensions.v2.access_logs.fields.AccessLogUserURLField'  # noqa
+
+    def map_serializer_field(self, auto_schema, direction):
+        example_url = settings.KOBOFORM_URL + reverse(
+            'api_v2:user-kpi-detail',
+            kwargs={'username': 'bob'}
+        )
+
+        return {
+            'type': 'string',
+            'format': 'url',
+            'example': example_url
         }
