35e964fe8337e09bf554bb11bda85aa7458e856c
Olivier Leger <olivierleger@gmail.com>
2025-05-27 10:43:37 -0400

    Move trench monkey-patch into KPI appConfig to make it load first

--- kobo/apps/__init__.py
+++ kobo/apps/__init__.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+import trench
 from django.apps import AppConfig
 from django.core.checks import Tags, register
 
@@ -10,6 +10,23 @@ class KpiConfig(AppConfig):
     name = 'kpi'
 
     def ready(self, *args, **kwargs):
+        # These imports cannot be at the top until the app is loaded.
+        from kobo.apps.accounts.mfa.command import (
+            create_mfa_method_command,
+            deactivate_mfa_method_command,
+        )
+        # Monkey-patch `django-trench` to avoid duplicating lots of code in views,
+        # and serializers just for few line changes.
+
+        # Changed behaviours:
+        # 1. Stop blocking deactivation of primary method
+        trench.command.deactivate_mfa_method.deactivate_mfa_method_command = (
+            deactivate_mfa_method_command
+        )
+        # 2. Resetting secret on reactivation
+        trench.command.create_mfa_method.create_mfa_method_command = (
+            create_mfa_method_command
+        )
 
         # Load all schema extension modules to register them
         import kpi.schema_extensions.imports  # noqa F401
