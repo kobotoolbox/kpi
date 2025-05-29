250f2a7bb8e2f768d8f55c550c49123c468c5ee7
Anji Tong <ollejna@gmail.com>
2025-05-29 11:27:42 -0400

    Add tooltip to component

--- jsapp/js/components/common/ActionIcon.tsx
+++ jsapp/js/components/common/ActionIcon.tsx
@@ -1,5 +1,6 @@
-import { ActionIcon as ActionIconMantine, createPolymorphicComponent } from '@mantine/core'
+import { ActionIcon as ActionIconMantine, Tooltip, createPolymorphicComponent } from '@mantine/core'
 import type { ActionIconProps as ActionIconPropsMantine } from '@mantine/core/lib/components'
+import type { TooltipProps } from '@mantine/core/lib/components'
 import { forwardRef } from 'react'
 import type { IconName } from '#/k-icons'
 import Icon, { type IconSize } from './icon'
@@ -7,18 +8,32 @@ import Icon, { type IconSize } from './icon'
 export interface ActionIconProps extends Omit<ActionIconPropsMantine, 'size'> {
   iconName: IconName
   size: 'sm' | 'md' | 'lg'
+  tooltip?: React.ReactNode
+  tooltipProps?: Partial<Omit<TooltipProps, 'label'>>
 }
 
-const ActionIcon = forwardRef<HTMLButtonElement, ActionIconProps>(({ iconName, ...props }, ref) => {
-  // Currently, our icon sizes only use a single letter instead of
-  // Mantine's 'sm', 'md', etc. So here we grab the first letter.
-  const iconSize = props.size[0] as IconSize
-  return (
-    <ActionIconMantine {...props} ref={ref}>
-      <Icon name={iconName} size={iconSize} />
-    </ActionIconMantine>
-  )
-})
+const ActionIcon = forwardRef<HTMLButtonElement, ActionIconProps>(
+  ({ iconName, tooltip, tooltipProps, ...others }, ref) => {
+    // Currently, our icon sizes only use a single letter instead of
+    // Mantine's 'sm', 'md', etc. So here we grab the first letter.
+    const iconSize = others.size[0] as IconSize
+    if (!tooltip) {
+      return (
+        <ActionIconMantine {...others} ref={ref}>
+          <Icon name={iconName} size={iconSize} />
+        </ActionIconMantine>
+      )
+    }
+
+    return (
+      <Tooltip label={tooltip} {...tooltipProps}>
+        <ActionIconMantine {...others} ref={ref}>
+          <Icon name={iconName} size={iconSize} />
+        </ActionIconMantine>
+      </Tooltip>
+    )
+  },
+)
 ActionIcon.displayName = 'ActionIcon'
 
 export default createPolymorphicComponent<'button', ActionIconProps>(ActionIcon)
