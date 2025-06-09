52dc74396705b7bb0ec1bcbbac1ef9e2a7459518
rgraber <becca.graber@kobotoolbox.org>
2025-06-09 08:49:19 -0400

    fixup!: base

--- kobo/settings/base.py
+++ kobo/settings/base.py
@@ -1419,7 +1419,7 @@ if os.environ.get('EMAIL_PORT'):
 if os.environ.get('EMAIL_USE_TLS'):
     EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 
-MAX_MASS_EMAILS_PER_DAY = 1
+MAX_MASS_EMAILS_PER_DAY = 1000
 MASS_EMAIL_THROTTLE_PER_SECOND = 40
 MASS_EMAIL_SLEEP_SECONDS = 1000
 # change the interval between "daily" email sends for testing. this will set both
