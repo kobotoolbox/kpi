kobotoolbox/kpi

commit 924c3fb00f5ff7e62b50a90f3f48dce250c27b88

feat(nlp): disable ASR for deleted attachment TASK-573 (#5629)
### 📣 Summary

Creating automated transcript or translation is no longer possible after
attachment is deleted.

### Notes

Things changed here:
- `getAttachmentForProcessing` function no longer requires
`assetContent` to be passed into it, instead it get's it by itself
- `TransxAutomaticButton` now has `disabled` property
- And the button is being disabled for transcript and translation flow
when attachment is deleted

### 👀 Preview steps

Testing:
1. ℹ️ Have NLP features set up (see
https://www.notion.so/kobotoolbox/Setting-up-NLP-and-Languages-bb607e3fed114993bb14338fe56c6ed4?pvs=4)
2. ℹ️ Have account and a project with `audio` question
3. ℹ️ Make a submission
4. Go to Project → Data → Table and use "Open" button for the audio
question response (to open NLP view)
5. After using "Begin" button to create transcript, choose a language
(one that has ASR features, e.g. "German")
6. 🟢 notice that "automatic" button is enabled
7. mock `is_deleted` flag on the attachment and visit point 5 again
- To mock it, you can override submision object in
`submissionsActions.getSubmissionByUuid.listen` from
`jsapp/js/actions/submissions.ts` file
9. 🟢 notice that "automatic" button is disabled

---------

Co-authored-by: Anji Tong <ollejna@gmail.com>

jsapp/js/components/processing/translations/stepConfig.component.tsx
@@ -13,6 +13,7 @@ import TransxAutomaticButton from '#/components/processing/transxAutomaticButton
 import { useExceedingLimits } from '#/components/usageLimits/useExceedingLimits.hook'
 import envStore from '#/envStore'
 import NlpUsageLimitBlockModal from '../nlpUsageLimitBlockModal/nlpUsageLimitBlockModal.component'
+import { getAttachmentForProcessing } from '../transcript/transcript.utils'
 
 export default function StepConfig() {
   const [usage] = useContext(UsageContext)
@@ -84,6 +85,7 @@ export default function StepConfig() {
 
   const draft = singleProcessingStore.getTranslationDraft()
   const isAutoEnabled = envStore.data.asr_mt_features_enabled
+  const attachment = getAttachmentForProcessing()
 
   return (
     <div className={cx(bodyStyles.root, bodyStyles.stepConfig)}>
@@ -118,6 +120,7 @@ export default function StepConfig() {
             onClick={onAutomaticButtonClick}
             selectedLanguage={draft?.languageCode}
             type='translation'
+            disabled={typeof attachment === 'string' || attachment.is_deleted}
           />
           <NlpUsageLimitBlockModal
             isModalOpen={isLimitBlockModalOpen}

jsapp/js/components/processing/transcript/stepConfig.component.tsx
@@ -13,6 +13,7 @@ import TransxAutomaticButton from '#/components/processing/transxAutomaticButton
 import { useExceedingLimits } from '#/components/usageLimits/useExceedingLimits.hook'
 import envStore from '#/envStore'
 import NlpUsageLimitBlockModal from '../nlpUsageLimitBlockModal/nlpUsageLimitBlockModal.component'
+import { getAttachmentForProcessing } from './transcript.utils'
 
 export default function StepConfig() {
   const [usage] = useContext(UsageContext)
@@ -77,6 +78,7 @@ export default function StepConfig() {
     singleProcessingStore.getProcessedFileLabel(),
   )
   const isAutoEnabled = envStore.data.asr_mt_features_enabled
+  const attachment = getAttachmentForProcessing()
 
   return (
     <div className={cx(bodyStyles.root, bodyStyles.stepConfig)}>
@@ -109,6 +111,7 @@ export default function StepConfig() {
             onClick={onAutomaticButtonClick}
             selectedLanguage={draft?.languageCode}
             type='transcript'
+            disabled={typeof attachment === 'string' || attachment.is_deleted}
           />
           <NlpUsageLimitBlockModal
             isModalOpen={isLimitBlockModalOpen}

jsapp/js/components/processing/sidebar/sidebarSubmissionMedia.tsx
@@ -18,12 +18,12 @@ export default function SidebarSubmissionMedia(props: SidebarSubmissionMediaProp
   // We need submission data.
   const [store] = useState(() => singleProcessingStore)
 
-  // We need `assetContent` to proceed.
-  if (!props.asset?.content) {
+  // We need `asset` to proceed.
+  if (!props.asset) {
     return null
   }
 
-  const attachment = getAttachmentForProcessing(props.asset.content)
+  const attachment = getAttachmentForProcessing()
   if (typeof attachment === 'string') {
     return null
   }

jsapp/js/components/processing/transcript/stepConfigAuto.component.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 
 import cx from 'classnames'
 import clonedeep from 'lodash.clonedeep'
-import assetStore from '#/assetStore'
 import Button from '#/components/common/button'
 import LoadingSpinner from '#/components/common/loadingSpinner'
 import type { LanguageCode } from '#/components/languages/languagesStore'
@@ -24,14 +23,11 @@ export default function StepConfigAuto() {
   // When polling for transcript, we need to calculate the estimated time
   useEffect(() => {
     if (singleProcessingStore.data.isPollingForTranscript) {
-      const asset = assetStore.getAsset(singleProcessingStore.currentAssetUid)
-      if (asset?.content) {
-        const attachment = getAttachmentForProcessing(asset.content)
-        if (typeof attachment !== 'string') {
-          getAudioDuration(attachment.download_url).then((length: number) => {
-            setEstimate(secondsToTranscriptionEstimate(length))
-          })
-        }
+      const attachment = getAttachmentForProcessing()
+      if (typeof attachment !== 'string') {
+        getAudioDuration(attachment.download_url).then((length: number) => {
+          setEstimate(secondsToTranscriptionEstimate(length))
+        })
       }
     } else {
       setEstimate(NO_ESTIMATED_MINUTES)

jsapp/js/components/processing/transcript/transcript.utils.ts
@@ -1,30 +1,33 @@
+import assetStore from '#/assetStore'
 import singleProcessingStore from '#/components/processing/singleProcessingStore'
 import { getMediaAttachment, getQuestionXPath, getRowData } from '#/components/submissions/submissionUtils'
-import type { AssetContent, SubmissionAttachment } from '#/dataInterface'
+import type { SubmissionAttachment } from '#/dataInterface'
 import { convertSecondsToMinutes } from '#/utils'
 
 /**
  * Returns an error string or the attachment. It's basically a wrapper function
  * over `getMediaAttachment` for DRY purposes in `singleProcessingStore` context.
  */
-export function getAttachmentForProcessing(assetContent: AssetContent): string | SubmissionAttachment {
+export function getAttachmentForProcessing(): string | SubmissionAttachment {
   const errorMessage = 'Insufficient data'
+
+  const asset = assetStore.getAsset(singleProcessingStore.currentAssetUid)
 
   const submissionData = singleProcessingStore.getSubmissionData()
   const currentQuestionName = singleProcessingStore.currentQuestionName
   // We need `assetContent` with survey, submission data, and question name to
   // go further.
-  if (!assetContent.survey || !submissionData || !currentQuestionName) {
+  if (!asset?.content?.survey || !submissionData || !currentQuestionName) {
     return errorMessage
   }
 
-  const rowData = getRowData(currentQuestionName, assetContent.survey, submissionData)
+  const rowData = getRowData(currentQuestionName, asset.content.survey, submissionData)
   // We need row data to go further. And we are expecting a string (filename).
   if (!rowData || typeof rowData !== 'string') {
     return errorMessage
   }
 
-  const questionXPath = getQuestionXPath(assetContent.survey, currentQuestionName)
+  const questionXPath = getQuestionXPath(asset.content.survey, currentQuestionName)
 
   return getMediaAttachment(submissionData, rowData, questionXPath)
 }

jsapp/js/components/processing/transxAutomaticButton.tsx
@@ -11,6 +11,7 @@ interface TransxAutomaticButtonProps {
   selectedLanguage?: LanguageCode
   /** Which type of service the button should check availability for. */
   type: 'transcript' | 'translation'
+  disabled?: boolean
 }
 
 interface TransxAutomaticButtonState {
@@ -108,7 +109,7 @@ export default class TransxAutomaticButton extends React.Component<
           size='m'
           label={t('automatic')}
           onClick={this.props.onClick}
-          isDisabled={!this.state.isAvailable}
+          isDisabled={this.props.disabled || !this.state.isAvailable}
           isPending={singleProcessingStore.data.isFetchingData || this.state.isLoading}
         />
       )

commit c2fa99afac671908d53ca788e4ea9f0ef28c6e17

fix(removingAttachments): don't disable automatic translation button DEV-588 (#5858)
### 📣 Summary

Don't disable automatic translation for deleted attachments.

### 💭 Notes

This was a mistake introduced in #5629. We added disabling of automatic
transcript button (correct) and then did the same thing for automatic
translation (incorrect).

### 👀 Preview steps

Testing:
1. ℹ️ Have NLP features set up (see
https://www.notion.so/kobotoolbox/Setting-up-NLP-and-Languages-bb607e3fed114993bb14338fe56c6ed4?pvs=4)
2. ℹ️ Have account and a project with `audio` question
3. ℹ️ Make a submission
4. Go to Project → Data → Table and use "Open" button for the audio
question response (to open NLP view)
5. Create a transcript
6. On the right side use "…" button next to audio player and choose
"Delete" option
7. Open "Translations" tab and start creating translation ("Begin"
button, then choose some language)
8. 🔴 on `main` notice that the "Automatic" button is disabled (bug)
9. 🟢 on the PR branch notice that "Automatic" button is enabled
correctly

jsapp/js/components/processing/translations/stepConfig.component.tsx
@@ -14,7 +14,6 @@ import TransxAutomaticButton from '#/components/processing/transxAutomaticButton
 import { useExceedingLimits } from '#/components/usageLimits/useExceedingLimits.hook'
 import envStore from '#/envStore'
 import NlpUsageLimitBlockModal from '../nlpUsageLimitBlockModal/nlpUsageLimitBlockModal.component'
-import { getAttachmentForProcessing } from '../transcript/transcript.utils'
 
 export default function StepConfig() {
   const [usage] = useContext(UsageContext)
@@ -87,7 +86,6 @@ export default function StepConfig() {
 
   const draft = singleProcessingStore.getTranslationDraft()
   const isAutoEnabled = envStore.data.asr_mt_features_enabled
-  const attachment = getAttachmentForProcessing()
 
   return (
     <div className={cx(bodyStyles.root, bodyStyles.stepConfig)}>
@@ -122,7 +120,6 @@ export default function StepConfig() {
             onClick={onAutomaticButtonClick}
             selectedLanguage={draft?.languageCode}
             type='translation'
-            disabled={typeof attachment === 'string' || attachment.is_deleted}
           />
           <NlpUsageLimitBlockModal
             isModalOpen={isLimitBlockModalOpen}