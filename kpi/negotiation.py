0471b05ca0217f53f5f08f49971cad6eddef7c1c
zoegodin-admin <zhodin9@gmail.com>
2025-06-25 07:59:33 -0400

    Added a custom negotation class so that only the available format shows on the api documentation

--- kpi/negotiation.py
+++ kpi/negotiation.py
@@ -1,21 +1,22 @@
-from rest_framework.negotiation import BaseContentNegotiation
-from rest_framework.renderers import JSONRenderer
+from rest_framework.negotiation import DefaultContentNegotiation as UpstreamDefaultContentNegociation
 
 
-class DefaultContentNegotiation(BaseContentNegotiation):
-    def select_renderer(self, request, renderers, format_suffix):
+class DefaultContentNegotiation(UpstreamDefaultContentNegociation):
+
+    def select_renderer(self, request, renderers, format_suffix=None):
         """
         This override the default rest framework select renderer in favor of supporting
-        the recieved suffix first- so if we have it in our available renderer this one
+        the received suffix first- so if we have it in our available renderer this one
         gets chosen, then the default serializer if there are no suffix and, if there are
         no renderers, we give the default for the app which is a JSONRenderer.
         """
+
+        accepts = self.get_accept_list(request)
+
         if format_suffix is not None:
             for r in renderers:
                 if r.format == format_suffix:
-                    return (r, r.media_type)
+                    return r, r.media_type
 
-        if renderers is not None:
-            return (renderers[0], renderers[0].media_type)
+        return renderers[0], renderers[0].media_type
 
-        return (JSONRenderer(), JSONRenderer.media_type)
