2e8def275ffbc7c2a12d19b715d686721c48a7c1
zoegodin-admin <zhodin9@gmail.com>
2025-07-01 10:21:50 -0400

    Fixed field wrong type

--- kpi/schema_extensions/v2/export_settings/extensions.py
+++ kpi/schema_extensions/v2/export_settings/extensions.py
@@ -81,24 +81,7 @@ class ExportSettingsFieldExtensions(OpenApiSerializerFieldExtension):
             properties={
                 'lang': build_basic_type(OpenApiTypes.STR),
                 'type': build_basic_type(OpenApiTypes.STR),
-                'fields': build_object_type(
-                    properties={
-                        '0': build_basic_type(OpenApiTypes.STR),
-                        '1': build_basic_type(OpenApiTypes.STR),
-                        '2': build_basic_type(OpenApiTypes.STR),
-                        '3': build_basic_type(OpenApiTypes.STR),
-                        '4': build_basic_type(OpenApiTypes.STR),
-                        '5': build_basic_type(OpenApiTypes.STR),
-                        '6': build_basic_type(OpenApiTypes.STR),
-                        '7': build_basic_type(OpenApiTypes.STR),
-                        '8': build_basic_type(OpenApiTypes.STR),
-                        '9': build_basic_type(OpenApiTypes.STR),
-                        '10': build_basic_type(OpenApiTypes.STR),
-                        '11': build_basic_type(OpenApiTypes.STR),
-                        '12': build_basic_type(OpenApiTypes.STR),
-                        '13': build_basic_type(OpenApiTypes.STR),
-                    }
-                ),
+                'fields': build_array_type(schema=build_basic_type(OpenApiTypes.STR)),
                 'group_sep': build_basic_type(OpenApiTypes.STR),
                 'multiple_select': build_basic_type(OpenApiTypes.STR),
                 'include_media_url': build_basic_type(OpenApiTypes.BOOL),
