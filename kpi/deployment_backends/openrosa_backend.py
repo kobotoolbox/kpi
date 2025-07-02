fa6fdbc1ce3c4c5b74ec4f0c0154abb43fdc2d6c
Olivier Leger <olivierleger@gmail.com>
2025-07-02 16:01:35 -0400

    Update tests

--- kpi/deployment_backends/openrosa_backend.py
+++ kpi/deployment_backends/openrosa_backend.py
@@ -14,7 +14,7 @@ from django.conf import settings
 from django.core.cache.backends.base import InvalidCacheBackendError
 from django.core.files import File
 from django.core.files.base import ContentFile
-from django.db.models import F, Q, Sum
+from django.db.models import F, Sum
 from django.db.models.functions import Coalesce
 from django.db.models.query import QuerySet
 from django.utils import timezone
