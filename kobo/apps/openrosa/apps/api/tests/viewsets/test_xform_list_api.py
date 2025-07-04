8611a79847bfb43d28a145bf9482dc361034c54a
Guillermo <lgar89@gmail.com>
2025-07-04 16:39:38 -0600

    Fix all broken tests in openrosa

--- kobo/apps/openrosa/apps/api/tests/viewsets/test_xform_list_api.py
+++ kobo/apps/openrosa/apps/api/tests/viewsets/test_xform_list_api.py
@@ -4,7 +4,6 @@ import re
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django_digest.test import DigestAuth
-from guardian.models import UserObjectPermission
 from rest_framework import status
 from rest_framework.reverse import reverse
 
@@ -660,9 +659,6 @@ class TestXFormListAsOrgAdminApiBase(TestXFormListApiBase):
         alice_profile = self._create_user_profile(alice_data)
         alice = alice_profile.user
         bob_organization.add_user(alice, is_admin=True)
-
-        # Make sure alice has no explicit permissions
-        assert UserObjectPermission.objects.filter(user=alice).exists()
 
     def test_head_xform_list(self):
         request = self.factory.head('/')
--- kobo/apps/openrosa/libs/permissions.py
+++ kobo/apps/openrosa/libs/permissions.py
@@ -6,6 +6,14 @@ from django_request_cache import cache_for_request
 from kobo.apps.openrosa.libs.utils.guardian import (
     get_users_with_perms as guardian_get_users_with_perms,
 )
+from kobo.apps.openrosa.libs.constants import (
+    CAN_VIEW_XFORM,
+    CAN_CHANGE_XFORM,
+    CAN_ADD_SUBMISSIONS,
+    CAN_DELETE_DATA_XFORM,
+    CAN_VALIDATE_XFORM,
+    CAN_DELETE_XFORM,
+)
 from kpi.constants import (
     PERM_ADD_SUBMISSIONS,
     PERM_CHANGE_ASSET,
@@ -16,12 +24,12 @@ from kpi.constants import (
 )
 
 KPI_PERMISSIONS_MAP = {  # keys are KPI's codenames, values are KC's
-    'change_xform': PERM_CHANGE_ASSET,  # "Can change XForm" in KC shell
-    'view_xform': PERM_VIEW_ASSET,  # "Can view XForm" in KC shell
-    'report_xform': PERM_ADD_SUBMISSIONS,  # "Can make submissions to the form" in KC shell  # noqa
-    'delete_data_xform': PERM_DELETE_SUBMISSIONS,  # "Can delete submissions" in KC shell  # noqa
-    'validate_xform': PERM_VALIDATE_SUBMISSIONS,  # "Can validate submissions" in KC shell  # noqa
-    'delete_xform': PERM_DELETE_ASSET,  # "Can delete XForm" in KC shell
+    CAN_CHANGE_XFORM: PERM_CHANGE_ASSET,  # "Can change XForm" in KC shell
+    CAN_VIEW_XFORM: PERM_VIEW_ASSET,  # "Can view XForm" in KC shell
+    CAN_ADD_SUBMISSIONS: PERM_ADD_SUBMISSIONS,  # "Can make submissions to the form" in KC shell  # noqa
+    CAN_DELETE_DATA_XFORM: PERM_DELETE_SUBMISSIONS,  # "Can delete submissions" in KC shell  # noqa
+    CAN_VALIDATE_XFORM: PERM_VALIDATE_SUBMISSIONS,  # "Can validate submissions" in KC shell  # noqa
+    CAN_DELETE_XFORM: PERM_DELETE_ASSET,  # "Can delete XForm" in KC shell
 }
 
 # Wrapper functions for KPI object permissions system
@@ -32,32 +40,35 @@ XFORM_MODELS_NAMES = [
     'datadictionary',
 ]
 
-def assign_perm(perm, user, instance):
-    if instance._meta.model_name in XFORM_MODELS_NAMES:
-        instance = instance.asset  # XForms permissions are based on the asset's
+def assign_perm(perm, user, obj):
+    if obj._meta.model_name in XFORM_MODELS_NAMES:
+        obj = obj.asset  # XForms permissions are based on the asset's
         perm = KPI_PERMISSIONS_MAP[perm.replace('logger.', '')]
-    instance.assign_perm(user, perm)
+    obj.assign_perm(user, perm)
 
 
-def get_users_with_perms(instance, attach_perms=False):
-    return instance.get_users_with_perms(attach_perms)
+def get_users_with_perms(obj, attach_perms=False):
+    return obj.get_users_with_perms(attach_perms)
 
 
-def remove_perm(perm, user, instance):
-    if instance._meta.model_name == 'xform':
-        instance = instance.asset  # XForms permissions are based on the asset's
+def remove_perm(perm, user, obj):
+    if obj._meta.model_name in XFORM_MODELS_NAMES:
+        obj = obj.asset  # XForms permissions are based on the asset's
         perm = KPI_PERMISSIONS_MAP[perm.replace('logger.', '')]
-    instance.remove_perm(user, perm)
+    obj.remove_perm(user, perm)
 
 
-def get_xform_ids_for_user(user):
+def get_xform_ids_for_user(user, perm=CAN_VIEW_XFORM):
     from kpi.utils.object_permission import (
         get_objects_for_user as kpi_get_objects_for_user,
     )
 
     XForm = apps.get_model('logger.xform')
+    perm = KPI_PERMISSIONS_MAP[perm.replace('logger.', '')]
     # By default kpi.utils.object_permissions.get_objects_for_user works for Asset model
-    qs_assets = kpi_get_objects_for_user(user, [PERM_VIEW_ASSET])
+    qs_assets = kpi_get_objects_for_user(
+        user, [perm]
+    )
     uids = [row[0] for row in qs_assets.values_list('uid')]
     xform_ids = [
         row[0] for row in XForm.objects.values_list('id').filter(kpi_asset_uid__in=uids)
