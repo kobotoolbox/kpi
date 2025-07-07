b9741455f03b712d15da90b8863fb697bb7394d6
Anji Tong <ollejna@gmail.com>
2025-07-07 17:54:49 -0400

    Linter

--- jsapp/xlform/src/view.js
+++ jsapp/xlform/src/view.js
@@ -1,11 +1,11 @@
 // TODO: This file was created by bulk-decaffeinate.
 // Sanity-check the conversion and remove this comment.
-(function() {
-  const $viewTemplates = require('./view.templates');
-  const $surveyApp = require('./view.surveyApp');
+;(() => {
+  const $viewTemplates = require('./view.templates')
+  const $surveyApp = require('./view.surveyApp')
 
-  return{
+  return {
     surveyApp: $surveyApp,
-    viewTemplates: $viewTemplates
-  };
-})();
+    viewTemplates: $viewTemplates,
+  }
+})()
