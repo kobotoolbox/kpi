7065d889e3f6737c78fb89b0461e15b60ec90d3b
decaffeinate <ollejna@gmail.com>
2025-07-07 17:48:18 -0400

    decaffeinate: Run post-processing cleanups on view.coffee

--- jsapp/xlform/src/view.js
+++ jsapp/xlform/src/view.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 (function() {
   const $viewTemplates = require('./view.templates');
   const $surveyApp = require('./view.surveyApp');
