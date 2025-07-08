ae332546cf8efcaa5e3302b1b154b6a6ee3c18f5
decaffeinate <ollejna@gmail.com>
2025-07-08 10:06:04 -0400

    decaffeinate: Run post-processing cleanups on model.rowDetail.coffee

--- jsapp/xlform/src/model.rowDetail.js
+++ jsapp/xlform/src/model.rowDetail.js
@@ -1,5 +1,7 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 const $base = require('./model.base');
 
-module.exports = ((() => ({
+module.exports = ((() => {return {
   RowDetail: $base.RowDetail
-})))();
+}}))();
