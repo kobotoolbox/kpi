2680f9b53f58955180e650e44fa8b3d20508ee38
Anji Tong <ollejna@gmail.com>
2025-07-08 10:40:24 -0400

    run biome

--- jsapp/xlform/src/model.rowDetail.js
+++ jsapp/xlform/src/model.rowDetail.js
@@ -1,7 +1,7 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-const $base = require('./model.base');
+const $base = require('./model.base')
 
-module.exports = ((() => {return {
-  RowDetail: $base.RowDetail
-}}))();
+module.exports = (() => {
+  return {
+    RowDetail: $base.RowDetail,
+  }
+})()
