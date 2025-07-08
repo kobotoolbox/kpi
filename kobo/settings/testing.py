fc0abb528a1f7c7c9699dd26f5a868c7d9cf5e43
James Kiger <james.kiger@gmail.com>
2025-07-08 10:08:33 -0400

    Add noqa for linter issue

--- kobo/settings/testing.py
+++ kobo/settings/testing.py
@@ -41,8 +41,8 @@ ENKETO_INTERNAL_URL = 'http://enketo.mock'
 # cached values
 CONSTANCE_DATABASE_CACHE_BACKEND = None
 
-if 'djstripe' not in INSTALLED_APPS:
-    INSTALLED_APPS += ('djstripe', 'kobo.apps.stripe.StripeAppConfig')
+if 'djstripe' not in INSTALLED_APPS:  # noqa F405
+    INSTALLED_APPS += ('djstripe', 'kobo.apps.stripe.StripeAppConfig') # noqa F405
 STRIPE_ENABLED = True
 
 WEBPACK_LOADER['DEFAULT'][
