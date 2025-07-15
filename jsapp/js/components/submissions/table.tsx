kobotoolbox/kpi

commit 7ea9a05cb10946019b831e0a1a9c6cb7e3f55253

fix(table): Improve the table error message DEV-692 (#5903)
### 📣 Summary
Stops the table view from spitting out raw html when an error occurs.
Replaces it with a more user friendly error

### 💭 Notes
Utilizes `handleApiFail` to display a helpful toast with the specific
error "plucked" out. Changes the error handling JSX code in the table
without getting too complicated and editing other files.

### 👀 Preview steps

1. ℹ️ have an account and a project
2. submit some data
3. go to `data.py` and under the `def list ...` line add `1/0` to throw
an error
4. navigate to the data tab
5. 🔴 [on release] notice that the error message is raw html
6. 🟢 [on PR] notice that the error message is more user friendly

jsapp/js/components/submissions/table.tsx
@@ -5,9 +5,11 @@ import React from 'react'
 import clonedeep from 'lodash.clonedeep'
 import isEqual from 'lodash.isequal'
 import { DebounceInput } from 'react-debounce-input'
+import Markdown from 'react-markdown'
 import ReactTable from 'react-table'
 import type { CellInfo } from 'react-table'
 import { actions } from '#/actions'
+import { handleApiFail } from '#/api'
 import type { SurveyFlatPaths } from '#/assetUtils'
 import { getQuestionOrChoiceDisplayName, getRowName, getSurveyFlatPaths, renderQuestionTypeIcon } from '#/assetUtils'
 import bem from '#/bem'
@@ -88,6 +90,7 @@ import type {
   ValidationStatusResponse,
 } from '#/dataInterface'
 import enketoHandler from '#/enketoHandler'
+import envStore from '#/envStore'
 import pageState from '#/pageState.store'
 import type { PageStateStoreState } from '#/pageState.store'
 import { stores } from '#/stores'
@@ -324,22 +327,11 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
 
   onGetSubmissionsFailed(error: FailResponse) {
     if (error?.responseText) {
-      let displayedError
-      try {
-        displayedError = JSON.parse(error.responseText)
-      } catch {
-        displayedError = error.responseText
-      }
+      handleApiFail(error)
+    }
 
-      if (displayedError.detail) {
-        this.setState({ error: displayedError.detail, loading: false })
-      } else {
-        this.setState({ error: displayedError, loading: false })
-      }
-    } else if (error?.statusText) {
-      this.setState({ error: error.statusText, loading: false })
-    } else {
-      this.setState({ error: t('Error: could not load data.'), loading: false })
+    if (error?.status && error.statusText) {
+      this.setState({ error: `${error.status} ${error.statusText}` })
     }
   }
 
@@ -1329,9 +1321,25 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
 
   render() {
     if (this.state.error && typeof this.state.error === 'string') {
+      const supportMessage = t(
+        'Please try again later, or [contact the support team](##SUPPORT_URL##) if this happens repeatedly.',
+      ).replace('##SUPPORT_URL##', envStore.data.support_url)
       return (
         <bem.FormView m='ui-panel'>
-          <CenteredMessage message={this.state.error} />
+          <CenteredMessage
+            message={
+              <div>
+                <h2>{t('Oops! Something went wrong on our end.')}</h2>
+                <div>
+                  <Markdown>{supportMessage}</Markdown>
+                </div>
+                <br />
+                <div>
+                  {t('Response details:')} {this.state.error}
+                </div>
+              </div>
+            }
+          />
         </bem.FormView>
       )
     }

commit a8172f54ffe0e31cd2fc9e4209f4673d55998e41

fix(table): show hard coded error message for `500`'s DEV-707 (#5918)
### 📣 Summary
Fixes a regression introduced in #5903 where the error message intended
for `500`'s was displaying for empty forms, undeployed forms, and/or
deployed forms with no data. Reverts the behaviour to show the response
text in these cases

### 👀 Preview steps

1. ℹ️ have an account and 2 projects
2. leave both projects empty
3. deploy one project
4. check the data table for both projects
6. 🔴 [on main] notice that the "Oops!" message exists for both these
projects
7. 🟢 [on PR] notice that the error messages are correct and relevant to
the issue (not deployed, or no submissions) and there is no toast
notification
8. add `1/0` to line 444 on `data.py`
9. 🟢 notice that the data table still does not show an a mess of html
and there is a detailed toast notification
10. remove the `1/0` in `data.py`
11. submit some data in a form, or navigate to a form with data
9. 🟢 notice that the data table displays as expected

---------

Co-authored-by: Paulo Amorim <pauloamorimbr@gmail.com>

jsapp/js/components/submissions/table.tsx
@@ -113,6 +113,8 @@ interface DataTableState {
   pageSize: number
   currentPage: number
   error: string | boolean
+  errorNumber: number | null
+  errorStatus: string | null
   showLabels: boolean
   translationIndex: number
   showGroupName: boolean
@@ -161,6 +163,8 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
       pageSize: 30,
       currentPage: 0,
       error: false,
+      errorNumber: null,
+      errorStatus: null,
       showLabels: true,
       translationIndex: 0,
       showGroupName: true,
@@ -325,13 +329,39 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
     }
   }
 
+  // The table view needs to handle the following errors differently:
+  // - 500 response from the backend will give a raw html response, so we display something else instead
+  // - non-500 response which contains some "detail" attribute after parsing the JSON response text, so we pluck it out
   onGetSubmissionsFailed(error: FailResponse) {
-    if (error?.responseText) {
-      handleApiFail(error)
+    if (error?.status) {
+      this.setState({ errorNumber: error.status })
+
+      if (error.status === 500 && error?.responseText) {
+        handleApiFail(error)
+      }
+
+      // If the error is not a 500 we parse the response and pluck out the "detail" to display
+      if (error.status !== 500 && error?.responseText) {
+        let displayedError
+
+        try {
+          displayedError = JSON.parse(error.responseText)
+        } catch {
+          displayedError = error.responseText
+        }
+
+        if (displayedError.detail) {
+          this.setState({ error: displayedError.detail, loading: false })
+        } else {
+          this.setState({ error: displayedError, loading: false })
+        }
+      } else if (error.status !== 500 && !error?.responseText) {
+        this.setState({ error: t('Error: could not load data.'), loading: false })
+      }
     }
 
-    if (error?.status && error.statusText) {
-      this.setState({ error: `${error.status} ${error.statusText}` })
+    if (error?.statusText) {
+      this.setState({ errorStatus: error.statusText })
     }
   }
 
@@ -1320,7 +1350,7 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
   }
 
   render() {
-    if (this.state.error && typeof this.state.error === 'string') {
+    if (this.state.errorNumber === 500) {
       const supportMessage = t(
         'Please try again later, or [contact the support team](##SUPPORT_URL##) if this happens repeatedly.',
       ).replace('##SUPPORT_URL##', envStore.data.support_url)
@@ -1335,11 +1365,19 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
                 </div>
                 <br />
                 <div>
-                  {t('Response details:')} {this.state.error}
+                  {t('Response details:')} {`${this.state.errorNumber} ${this.state.errorStatus}`}
                 </div>
               </div>
             }
           />
+        </bem.FormView>
+      )
+    }
+
+    if (typeof this.state.error === 'string') {
+      return (
+        <bem.FormView m='ui-panel'>
+          <CenteredMessage message={this.state.error} />
         </bem.FormView>
       )
     }