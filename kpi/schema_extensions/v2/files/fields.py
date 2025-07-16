2fe0fd5caf51ce9e6b7cd57510bcadc3e7fffd43
Zoé Godin <157842997+ZoeGodin@users.noreply.github.com>
2025-07-16 11:45:14 -0400

    docs(api): adding API endpoints for files in KPI documentation DEV-530  (#5880)
    ### 📣 Summary
    Added API documentation for `/api/v2/docs/files` endpoint.
    
    ### 📖 Description
    This PR introduces the endpoint documentation that will be used for the
    auto-generating API documentation of the files endpoint. With these
    changes, users will be able to understand, test and use the `files`
    endpoint more easily.
    
    ### 👀 Preview steps
    To see the changes, visit `http://kf.kobo.local/api/v2/docs/`. The page
    of the Kobo API will now be visible and generated by swagger. To see the
    changes added to `files` endpoint, scroll down to the category of the
    same name or make a research in the search bar. Documentation for the
    endpoint will be provided with a HTTP code, a body and/or a response
    when necessary.

---
+++ kpi/schema_extensions/v2/files/fields.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+
+class AssetUrlField(serializers.URLField):
+    pass
+
+
+class ContentURlField(serializers.URLField):
+    pass
+
+
+class FileUrlField(serializers.URLField):
+    pass
+
+
+class MetadataField(serializers.JSONField):
+    pass
+
+
+class MetadataCreateField(serializers.JSONField):
+    pass
+
+
+class UserUrlField(serializers.URLField):
+    pass
