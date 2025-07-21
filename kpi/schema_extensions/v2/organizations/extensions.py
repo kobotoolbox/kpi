233e0ec311fb57a12ed61c7a73906d1d25d9238e
Zoé Godin <157842997+ZoeGodin@users.noreply.github.com>
2025-07-21 06:37:29 -0400

    docs(api): adding API endpoints for organizations in KPI documentation DEV-540 (#5917)
    ### 📣 Summary
    Added API documentation for `/api/v2/docs/organizations` endpoint.
    
    ### 📖 Description
    This PR introduces the endpoint documentation that will be used for the
    auto-generating API documentation of the `organizations` endpoint. With
    these changes, users will be able to understand, test and use the
    `organizations` endpoint more easily.
    
    ### 👀 Preview steps
    To see the changes, visit `http://kf.kobo.local/api/v2/docs/`. The page
    of the Kobo API will now be visible and generated by swagger. To see the
    changes added to `organizations` endpoint, scroll down to the category
    of the same name or make a research in the search bar. Documentation for
    the endpoint will be provided with a HTTP code, a body and/or a response
    when necessary.

---
+++ kpi/schema_extensions/v2/organizations/extensions.py
@@ -0,0 +1,136 @@
+from drf_spectacular.extensions import OpenApiSerializerFieldExtension
+from drf_spectacular.plumbing import build_basic_type, build_object_type
+from drf_spectacular.types import OpenApiTypes
+
+from kpi.utils.schema_extensions.url_builder import build_url_type
+from .schema import BALANCE_FIELDS_SCHEMA
+
+
+class AssetFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.AssetField'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_url_type(
+            'api_v2:organizations-assets',
+            id='orgzeph7Ub8tVmJ82JBbH96n',
+        )
+
+
+class AssetUsageFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.AssetUsageField'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_url_type(
+            'api_v2:organizations-asset-usage',
+            id='orgzeph7Ub8tVmJ82JBbH96n',
+        )
+
+
+class BalanceFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.BalanceField'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_object_type(
+            properties={
+                'submission': BALANCE_FIELDS_SCHEMA,
+                'storage_bytes': BALANCE_FIELDS_SCHEMA,
+                'asr_seconds': BALANCE_FIELDS_SCHEMA,
+                'mt_characters': BALANCE_FIELDS_SCHEMA,
+            }
+        )
+
+
+class IsOwnerFieldExtensions(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.IsOwnerField'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_basic_type(OpenApiTypes.BOOL)
+
+
+class MembersFieldExtensions(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.MembersField'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_url_type(
+            'api_v2:organization-members-list',
+            organization_id='orgzeph7Ub8tVmJ82JBbH96n',
+        )
+
+
+class NlpUsageAllTimeExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.NlpUsageAllTime'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_object_type(
+            properties={
+                'total_nlp_asr_seconds': build_basic_type(OpenApiTypes.INT),
+                'total_nlp_mt_characters': build_basic_type(OpenApiTypes.INT),
+            }
+        )
+
+
+class NlpUsageCurrentPeriodExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.NlpUsageCurrentPeriod'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_object_type(
+            properties={
+                'total_nlp_asr_seconds': build_basic_type(OpenApiTypes.INT),
+                'total_nlp_mt_characters': build_basic_type(OpenApiTypes.INT),
+            }
+        )
+
+
+class RequestUserRoleFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.RequestUserRoleField'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_basic_type(OpenApiTypes.STR)
+
+
+class ServiceUsageFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.ServiceUsageField'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_url_type(
+            'api_v2:organizations-service-usage',
+            id='orgzeph7Ub8tVmJ82JBbH96n',
+        )
+
+
+class TotalNlpUsageFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.TotalNlpUsageField'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_object_type(
+            properties={
+                'asr_seconds_current_period': build_basic_type(OpenApiTypes.INT),
+                'mt_characters_current_period': build_basic_type(OpenApiTypes.INT),
+                'asr_seconds_all_time': build_basic_type(OpenApiTypes.INT),
+                'mt_characters_all_time': build_basic_type(OpenApiTypes.INT),
+            }
+        )
+
+
+class TotalSubmissionCountFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = (
+        'kpi.schema_extensions.v2.organizations.fields.TotalSubmissionCountField'
+    )
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_object_type(
+            properties={
+                'all_time': build_basic_type(OpenApiTypes.INT),
+                'current_period': build_basic_type(OpenApiTypes.INT),
+            }
+        )
+
+
+class UrlFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kpi.schema_extensions.v2.organizations.fields.UrlField'
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_url_type(
+            'api_v2:organizations-detail',
+            id='orgzeph7Ub8tVmJ82JBbH96n',
+        )
