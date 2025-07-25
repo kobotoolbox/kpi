4a5f2821d5eddb6948d27bf5e263c7119da91f49
Guillermo <lgar89@gmail.com>
2025-07-24 23:50:01 -0600

    Avoid errors for tests without db

--- kobo/conftest.py
+++ kobo/conftest.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from pytest import fixture
 
 from kpi.utils.object_permission import get_anonymous_user
@@ -5,6 +6,10 @@ from kpi.utils.object_permission import get_anonymous_user
 
 @fixture(autouse=True)
 def anonymous_user():
+
+    if connection.connection is None:
+        return
+
     anon = get_anonymous_user()
 
     return anon
