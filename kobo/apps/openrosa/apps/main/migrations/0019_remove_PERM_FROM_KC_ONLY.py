e774e24373ca4c359db5cbb1e7d406e0929827cc
Guillermo <lgar89@gmail.com>
2025-07-28 17:06:21 -0600

    Migration to remove PERM_FROM_KC_ONLY from the object permissions table

---
+++ kobo/apps/openrosa/apps/main/migrations/0019_remove_PERM_FROM_KC_ONLY.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+from kpi.models.object_permission import ObjectPermission
+
+PERM_FROM_KC_ONLY = 'from_kc_only'
+
+
+def noop(apps, schema_editor):
+    # Irreversible operation
+    pass
+
+
+def remove_kc_only_perm(apps, schema_editor):
+    deleted = ObjectPermission.objects.filter(
+        permission__codename=PERM_FROM_KC_ONLY
+    ).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0018_increase_metadata_data_file_max_length'),
+    ]
+
+    operations = [migrations.RunPython(remove_kc_only_perm, noop)]
