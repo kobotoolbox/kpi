a85a99b4dc02dca4e33fd35c4ca1bdca7552bc15
Kalvis Kalniņš <kalvis.kalnins@kobotoolbox.org>
2025-07-29 19:52:22 +0300

    feat(frontend): generate API schema with Orval

---
+++ orval.config.js
@@ -0,0 +1,32 @@
+import { operationName } from './orval.config.operationName';
+
+module.exports = {
+  'kpi-v2': {
+    output: {
+      mode: 'tags',
+      workspace: './jsapp/js/api/',
+      target: '.',
+      schemas: './models',
+      clean: true,
+
+      client: 'react-query',
+      httpClient: 'fetch',
+      namingConvention: 'PamelCase',
+      mock: true,
+      indexFiles: false,
+      biome: true,
+
+      // baseUrl: 'https://api.example.com', // prepend https://api.example.com to all api calls
+      urlEncodeParameters: true,
+
+      override: {
+        title: (title) => ``,
+        operationName: operationName,
+        enumGenerationType: 'enum',
+      },
+    },
+    input: {
+      target: 'http://kf.kobo.local/api/v2/schema',
+    },
+  },
+}
