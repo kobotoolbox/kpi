513cd70844811c18f59ee1efc8190aa72f56269e
Olivier Leger <olivierleger@gmail.com>
2025-07-31 12:17:23 -0400

    fix wrong urlpattern for social accounts

--- kobo/apps/accounts/urls.py
+++ kobo/apps/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 
 from .views import EmailAddressViewSet, SocialAccountViewSet
@@ -7,9 +7,19 @@ from .tos import TOSView
 
 router = routers.SimpleRouter()
 router.register(r'emails', EmailAddressViewSet)
-router.register(r'social-accounts', SocialAccountViewSet)
+
+socialaccount_list = SocialAccountViewSet.as_view({'get': 'list'})
+socialaccount_detail = SocialAccountViewSet.as_view(
+    {'get': 'retrieve', 'delete': 'destroy'}
+)
 
 urlpatterns = [
     path('me/', include(router.urls)),
+    path('me/social-accounts/', socialaccount_list, name='socialaccount-list'),
+    re_path(
+        rf'^me/social-accounts/{SocialAccountViewSet.lookup_value_regex}/$',
+        socialaccount_detail,
+        name='socialaccount-detail',
+    ),
     path('me/tos/', TOSView.as_view(), name='tos'),
 ]
