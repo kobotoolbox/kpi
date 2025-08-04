4dae8052f715d4d0607a453b5817cda0f97f2a21
Zoé Godin <157842997+ZoeGodin@users.noreply.github.com>
2025-08-04 07:19:39 -0400

    docs(api): adding API endpoints for transfer in KPI documentation DEV-545 (#5999)
    ### 📣 Summary
    Added API documentation for `/api/v2/docs/transfer` endpoint.
    
    ### 📖 Description
    This PR introduces the endpoint documentation that will be used for the
    auto-generating API documentation of the `transfer` endpoint. With these
    changes, users will be able to understand, test and use the `transfer`
    endpoint more easily.
    
    ### 👀 Preview steps
    To see the changes, visit `http://kf.kobo.local/api/v2/docs/`. The page
    of the Kobo API will now be visible and generated by swagger. To see the
    changes added to `transfer` endpoint, scroll down to the category of the
    same name or make a research in the search bar. Documentation for the
    endpoint will be provided with a HTTP code, a body and/or a response
    when necessary.

---
+++ kobo/apps/project_ownership/schema_extensions/v2/project_ownership/transfers/extensions.py
@@ -0,0 +1,45 @@
+from drf_spectacular.extensions import OpenApiSerializerFieldExtension
+from drf_spectacular.plumbing import (
+    build_array_type,
+    build_basic_type,
+    build_object_type,
+)
+from drf_spectacular.types import OpenApiTypes
+
+from kpi.utils.schema_extensions.url_builder import build_url_type
+
+
+class AssetUrlFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kobo.apps.project_ownership.schema_extensions.v2.project_ownership.transfers.fields.AssetUrlField'  # noqa
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_url_type(
+            'api_v2:assets-detail',
+            uid='aTPPUDScaFZkvBzd8FyK4Q'
+        )
+
+
+class StatusesFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kobo.apps.project_ownership.schema_extensions.v2.project_ownership.transfers.fields.StatusesField'  # noqa
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_array_type(
+            schema=build_object_type(
+                properties={
+                    'status': build_basic_type(OpenApiTypes.STR),
+                    'status_type': build_basic_type(OpenApiTypes.STR),
+                    'error':  build_basic_type(OpenApiTypes.STR),
+                }
+            )
+        )
+
+
+class TransferUrlFieldExtension(OpenApiSerializerFieldExtension):
+    target_class = 'kobo.apps.project_ownership.schema_extensions.v2.project_ownership.transfers.fields.TransferUrlField'  # noqa
+
+    def map_serializer_field(self, auto_schema, direction):
+        return build_url_type(
+            'api_v2:project-ownership-transfer-detail',
+            parent_lookup_invite_uid='poi52fGkwDjQeZkUxcaou39q',
+            uid='pot54pTqM5qwKdZ4wnNdiwDY'
+        )
