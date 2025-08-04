e3a73d2862ccd39c65431f759f55d8a6f34d0293
zoegodin-admin <zhodin9@gmail.com>
2025-08-04 07:24:45 -0400

    Fixed git error by changing folder names

---
+++ kobo/apps/accounts/extend_schemas/api/v2/email/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from kpi.utils.schema_extensions.serializers import inline_serializer_class
+
+EmailRequestPayload = inline_serializer_class(
+    name='EmailRequestPayload',
+    fields={
+        "email": serializers.EmailField(),
+    },
+)
