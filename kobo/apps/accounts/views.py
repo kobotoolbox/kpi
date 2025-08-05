be6654016e4341971db9055307745e8006796798
zoegodin-admin <zhodin9@gmail.com>
2025-08-05 14:19:28 -0400

    Added error examples to endpoint

--- kobo/apps/accounts/views.py
+++ kobo/apps/accounts/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from kpi.permissions import IsAuthenticated
 from kpi.utils.schema_extensions.markdown import read_md
 from kpi.utils.schema_extensions.response import open_api_200_ok_response, \
-    open_api_201_created_response
+    open_api_201_created_response, open_api_204_empty_response
 from kpi.versioning import APIV2Versioning
 from .extend_schemas.api.v2.email.serializers import EmailRequestPayload
 from .mixins import MultipleFieldLookupMixin
@@ -79,12 +79,27 @@ class EmailAddressViewSet(
 @extend_schema_view(
     destroy=extend_schema(
         description=read_md('accounts', 'social/delete.md'),
+        responses=open_api_204_empty_response(
+            raise_access_forbidden=False,
+            validate_payload=False,
+        )
     ),
     list=extend_schema(
         description=read_md('accounts', 'social/list.md'),
+        responses=open_api_200_ok_response(
+            SocialAccountSerializer,
+            raise_not_found=False,
+            raise_access_forbidden=False,
+            validate_payload=False,
+        )
     ),
     retrieve=extend_schema(
         description=read_md('accounts', 'social/retrieve.md'),
+        responses=open_api_200_ok_response(
+            SocialAccountSerializer,
+            raise_access_forbidden=False,
+            validate_payload=False,
+        )
     ),
 )
 class SocialAccountViewSet(
