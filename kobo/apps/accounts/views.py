339058795a426768164f1dbcbef6d2324962cec1
Zoé Godin <157842997+ZoeGodin@users.noreply.github.com>
2025-08-06 13:25:56 -0400

    docs(api): adding API endpoints for social in KPI documentation DEV-832 (#6047)
    ### 📣 Summary
    Added API documentation for `/me/social/` endpoint.
    
    ### 📖 Description
    This PR introduces the endpoint documentation that will be used for the
    auto-generating API documentation of the `social` endpoint. With these
    changes, users will be able to understand, test and use the `social`
    endpoint more easily.
    
    ### 👀 Preview steps
    To see the changes, visit `http://kf.kobo.local/api/v2/docs/`. The page
    of the Kobo API will now be visible and generated by swagger. To see the
    changes added to `social` endpoint, scroll down to the `Me` category or
    make a research in the search bar. Documentation for the endpoint will
    be provided with a HTTP code, a body and/or a response when necessary.

--- kobo/apps/accounts/views.py
+++ kobo/apps/accounts/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from kpi.permissions import IsAuthenticated
 from kpi.utils.schema_extensions.markdown import read_md
 from kpi.utils.schema_extensions.response import open_api_200_ok_response, \
-    open_api_201_created_response
+    open_api_201_created_response, open_api_204_empty_response
 from kpi.versioning import APIV2Versioning
 from .extend_schemas.api.v2.email.serializers import EmailRequestPayload
 from .mixins import MultipleFieldLookupMixin
@@ -20,7 +20,7 @@ from .serializers import EmailAddressSerializer, SocialAccountSerializer
 )
 @extend_schema_view(
     list=extend_schema(
-        description=read_md('accounts', 'email/list.md'),
+        description=read_md('accounts', 'me/email/list.md'),
         responses=open_api_200_ok_response(
             EmailAddressSerializer,
             raise_not_found=False,
@@ -29,7 +29,7 @@ from .serializers import EmailAddressSerializer, SocialAccountSerializer
         )
     ),
     create=extend_schema(
-        description=read_md('accounts', 'email/create.md'),
+        description=read_md('accounts', 'me/email/create.md'),
         request={'application/json': EmailRequestPayload},
         responses=open_api_201_created_response(
             EmailAddressSerializer,
@@ -51,8 +51,8 @@ class EmailAddressViewSet(
     - create         → CREATE    /me/
 
     Documentation:
-    - docs/api/v2/me/list.md
-    - docs/api/v2/me/create.md
+    - docs/api/v2/me/email/list.md
+    - docs/api/v2/me/email/create.md
     """
 
     queryset = EmailAddress.objects.all()
@@ -73,6 +73,35 @@ class EmailAddressViewSet(
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+@extend_schema(
+    tags=['Me']
+)
+@extend_schema_view(
+    destroy=extend_schema(
+        description=read_md('accounts', 'me/social/delete.md'),
+        responses=open_api_204_empty_response(
+            raise_access_forbidden=False,
+            validate_payload=False,
+        )
+    ),
+    list=extend_schema(
+        description=read_md('accounts', 'me/social/list.md'),
+        responses=open_api_200_ok_response(
+            SocialAccountSerializer,
+            raise_not_found=False,
+            raise_access_forbidden=False,
+            validate_payload=False,
+        )
+    ),
+    retrieve=extend_schema(
+        description=read_md('accounts', 'me/social/retrieve.md'),
+        responses=open_api_200_ok_response(
+            SocialAccountSerializer,
+            raise_access_forbidden=False,
+            validate_payload=False,
+        )
+    ),
+)
 class SocialAccountViewSet(
     MultipleFieldLookupMixin,
     mixins.ListModelMixin,
@@ -80,12 +109,27 @@ class SocialAccountViewSet(
     mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
+    """
+    Viewset for managing current user's socials
+
+    Available actions:
+    - destroy        → DELETE   /me/social-accounts/{provider}/{uid}/
+    - list           → GET      /me/social-accounts/
+    - retrieve       → GET      /me/social-accounts/{provider}/{uid}/
+
+    Documentation:
+    - docs/api/v2/me/social/destroy.md
+    - docs/api/v2/me/social/list.md
+    - docs/api/v2/me/social/retrieve.md
+    """
+
     lookup_value_regex = r'(?P<provider>[^/.]+)/(?P<uid>[-\w]+)'
     lookup_fields = ['provider', 'uid']
     queryset = SocialAccount.objects.all()
     serializer_class = SocialAccountSerializer
     permission_classes = (IsAuthenticated,)
     versioning_class = APIV2Versioning
+    renderer_classes = [JSONRenderer,]
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
