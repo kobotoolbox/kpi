baeaa28ac277eb852da857edd1bd968c864497f4
zoegodin-admin <zhodin9@gmail.com>
2025-08-06 09:30:30 -0400

    Initial commit for documenting tos endpoint

--- kpi/views/v2/tos.py
+++ kpi/views/v2/tos.py
@@ -1,10 +1,23 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer
 
 from hub.models import SitewideMessage
 from kpi.serializers.v2.tos import TermsOfServiceSerializer
 
 
+@extend_schema(
+    tags=['Terms of Services']
+)
+@extend_schema_view(
+    list=extend_schema(
+        description='list',
+    ),
+    retrieve=extend_schema(
+        description='retrieve',
+    )
+)
 class TermsOfServiceViewSet(viewsets.ReadOnlyModelViewSet):
     """
     TBC, Terms of service readonly endpoint
@@ -16,3 +29,6 @@ class TermsOfServiceViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = TermsOfServiceSerializer
     pagination_class = None
     permission_classes = (IsAuthenticated,)
+    renderer_classes = [
+        JSONRenderer,
+    ]
