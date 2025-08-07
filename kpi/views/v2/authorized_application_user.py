90fcce0a3e5e772c97f808678659f6e5561b0664
Zoé Godin <157842997+ZoeGodin@users.noreply.github.com>
2025-08-07 09:14:03 -0400

    docs(api): exclude API endpoints for authorized-applications in KPI documentation DEV-845 (#6059)
    ### 📣 Summary
    Excluded API documentation for `/api/v2/docs/authorized-applications`
    endpoint.
    
    ### 👀 Preview steps
    To see the changes, visit `http://kf.kobo.local/api/v2/docs/`. The page
    of the Kobo API will now be visible and generated by swagger. To see the
    changes added, try and look for `authorized-applications` endpoint,
    either with search bar of by scrolling down, but there shouldn't be
    anything.

--- kpi/views/v2/authorized_application_user.py
+++ kpi/views/v2/authorized_application_user.py
@@ -1,6 +1,8 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import exceptions, mixins, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 from kobo.apps.audit_log.models import AccessLog
@@ -10,8 +12,12 @@ from kpi.models import AuthorizedApplication
 from kpi.models.authorized_application import ApplicationTokenAuthentication
 from kpi.serializers import AuthorizedApplicationUserSerializer
 from kpi.serializers.v2.create_user import CreateUserSerializer
+from kpi.versioning import APIV2Versioning
 
 
+@extend_schema(
+    exclude=True,
+)
 class AuthorizedApplicationUserViewSet(
     mixins.CreateModelMixin, viewsets.GenericViewSet
 ):
@@ -19,6 +25,10 @@ class AuthorizedApplicationUserViewSet(
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
     lookup_field = 'username'
+    versioning_class = APIV2Versioning
+    renderer_classes = [
+        JSONRenderer,
+    ]
 
     @action(detail=False, methods=['POST'])
     def authenticate_user(self, request):
