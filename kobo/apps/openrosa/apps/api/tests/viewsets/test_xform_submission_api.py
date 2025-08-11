f0f681a7697c49921abd96171d80d1eaea6c3bac
Guillermo <lgar89@gmail.com>
2025-08-11 09:19:27 -0600

    Increase expected query counts due to permissions merge changes

--- kobo/apps/openrosa/apps/api/tests/viewsets/test_xform_submission_api.py
+++ kobo/apps/openrosa/apps/api/tests/viewsets/test_xform_submission_api.py
@@ -75,7 +75,7 @@ class TestXFormSubmissionApi(TestAbstractViewSet):
             # USAGE_LIMIT_ENFORCEMENT variable. But we use caching
             # so should find a way to keep that out of this count
             if settings.STRIPE_ENABLED:
-                expected_queries = FuzzyInt(79, 86)
+                expected_queries = FuzzyInt(80, 87)
             with self.assertNumQueries(expected_queries):
                 self.view(request)
 
