452dad968cd5533d8b27ad0653da2f7d34c88cfc
Kalvis Kalniņš <kalvis.kalnins@kobotoolbox.org>
2025-08-13 20:39:12 +0300

    ci(biome): fix filter syntax

--- kobo/apps/form_disclaimer/static/admin/js/disable_per_asset_message.js
+++ kobo/apps/form_disclaimer/static/admin/js/disable_per_asset_message.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hide_default_checkbox = document.querySelector('input[id=id_hidden]')
   const overrideFieldset = document.querySelectorAll('fieldset')[1]
-  if(hide_default_checkbox.checked) {
+  if (hide_default_checkbox.checked) {
     overrideFieldset.classList.add('hidden')
   }
   hide_default_checkbox.addEventListener('change', () => {
