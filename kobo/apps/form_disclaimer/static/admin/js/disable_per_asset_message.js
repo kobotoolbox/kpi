384eb6f6c9222047a9713cd0b97a4f09945f23ce
rgraber <becca.graber@kobotoolbox.org>
2025-08-13 08:44:46 -0400

    fixup!: hide fieldset altogether

--- kobo/apps/form_disclaimer/static/admin/js/disable_per_asset_message.js
+++ kobo/apps/form_disclaimer/static/admin/js/disable_per_asset_message.js
@@ -1,20 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const hide_default_checkbox = document.querySelector("input[id=id_hidden]")
-  const languageSelector= document.querySelector("select[id=id_language]")
-  const message = document.querySelector("textarea[id=id_message]")
+  const hide_default_checkbox = document.querySelector('input[id=id_hidden]')
+  const languageSelector= document.querySelector('select[id=id_language]')
+  const message = document.querySelector('textarea[id=id_message]')
+  const overrideFieldset = document.querySelectorAll('fieldset')[1]
   if(hide_default_checkbox.checked) {
     languageSelector.disabled = true
     message.disabled = true
+    overrideFieldset.classList.add('hidden')
   }
   hide_default_checkbox.addEventListener('change', () => {
-    const overrideMessage = document.querySelectorAll('fieldset')[1]
-
     if (hide_default_checkbox.checked) {
       languageSelector.disabled = true
       message.disabled = true
+      overrideFieldset.classList.add('hidden')
     } else {
       languageSelector.disabled = false
       message.disabled = false
+      overrideFieldset.classList.remove('hidden')
     }
   })
 })
