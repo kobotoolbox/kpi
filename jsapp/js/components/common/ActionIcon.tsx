85ffaae9e7ed0615ba69853366d903b5457024fe
Anji <ollejna@gmail.com>
2025-08-14 01:41:17 -0400

    Add tooltip wrapper to ActionIcon

--- jsapp/js/components/common/ActionIcon.tsx
+++ jsapp/js/components/common/ActionIcon.tsx
@@ -1,10 +1,14 @@
-import { ActionIcon as ActionIconMantine, createPolymorphicComponent } from '@mantine/core'
+import { ActionIcon as ActionIconMantine, createPolymorphicComponent, Tooltip } from '@mantine/core'
 import type { ActionIconProps as ActionIconPropsMantine } from '@mantine/core/lib/components'
 import { forwardRef } from 'react'
 import type { IconName } from '#/k-icons'
 import Icon, { type IconSize } from './icon'
+import { TooltipProps } from '@mantine/core/lib/components'
 
 export interface ActionIconProps extends Omit<ActionIconPropsMantine, 'size'> {
+  tooltip?: React.ReactNode
+  tooltipProps?: Partial<Omit<TooltipProps, 'label'>>
+
   iconName: IconName
   size: 'sm' | 'md' | 'lg'
 }
@@ -13,10 +17,21 @@ const ActionIcon = forwardRef<HTMLButtonElement, ActionIconProps>(({ iconName, .
   // Currently, our icon sizes only use a single letter instead of
   // Mantine's 'sm', 'md', etc. So here we grab the first letter.
   const iconSize = props.size[0] as IconSize
+
+  if (!props.tooltip) {
+    return (
+      <ActionIconMantine {...props} ref={ref}>
+        <Icon name={iconName} size={iconSize} />
+      </ActionIconMantine>
+    )
+  }
+
   return (
-    <ActionIconMantine {...props} ref={ref}>
-      <Icon name={iconName} size={iconSize} />
-    </ActionIconMantine>
+    <Tooltip label={props.tooltip} {...props.tooltipProps}>
+      <ActionIconMantine {...props} ref={ref}>
+        <Icon name={iconName} size={iconSize} />
+      </ActionIconMantine>
+    </Tooltip>
   )
 })
 ActionIcon.displayName = 'ActionIcon'
