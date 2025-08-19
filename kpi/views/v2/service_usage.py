d984933d8a327a494b04ca8a599aa39248cf7184
zoegodin-admin <zhodin9@gmail.com>
2025-08-19 13:35:44 -0400

    cleaned parentisis for service usage renderers

--- kpi/views/v2/service_usage.py
+++ kpi/views/v2/service_usage.py
@@ -35,7 +35,9 @@ class ServiceUsageViewSet(viewsets.GenericViewSet):
     - docs/api/v2/service_usage/list.md
     """
 
-    renderer_classes = (renderers.JSONRenderer,)
+    renderer_classes = [
+        renderers.JSONRenderer,
+    ]
     pagination_class = None
     permission_classes = (IsAuthenticated,)
 
