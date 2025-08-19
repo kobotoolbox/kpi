1867805ca5948d35706761d6be4b4fcec09fb9db
Akuukis <Akuukis@users.noreply.github.com>
2025-08-19 18:14:02 +0300

    refactor(UniversalTable): handle future generated API helpers DEV-895 (#6134)
    ### 💭 Notes
    
    Restructured UniversalTable to handle generated API helpers in the
    future. It should be possible to swap queries without more changes to
    the UniversalTable. Notably (see commit by commit):
    - Extracted the pagination state and query hook to parent component for
    simplicity. The future generated API hooks and fetchers would replace
    all of the `*.query` file contents. With previous `queryHook` approach
    it would require an extra boilerplate to re-wrap each hook to conform to
    the `queryHook` signature and complex typing gymnastics (I tried and
    failed), therefore I settled for an equal amount of boilerplate of
    raising pagination state along much simpler UniversalTable types and
    removing the mental complexity of passing a hook as a parameter.
    - unrelated but along the way, renamed UniversalTable files according to
    the frontend coding style guidelines. Note that nowhere UniversalTable
    was used directly, so I renamed the PaginatedQueryUniversalTable to
    UniversalTable and UniversalTable to UniversalTableCore as an internal
    component.
    
    
    ### 👀 Preview steps
    1. ℹ️ have an account and a project
    2. 🟢 [on PR] notice that organization member table still works
    3. 🟢 [on PR] notice that form activity table still works as before
    4. 🟢 [on PR] notice that access logs table still works

---
+++ jsapp/js/UniversalTable/index.tsx
@@ -0,0 +1,115 @@
+import React, { useMemo } from 'react'
+
+import type { UseQueryResult } from '@tanstack/react-query'
+import UniversalTableCore from './UniversalTableCore'
+import type { UniversalTableColumn } from './UniversalTableCore'
+
+export { UniversalTableColumn } from './UniversalTableCore'
+
+export interface Pagination {
+  limit: number
+  offset: number
+}
+
+export interface PaginatedListResponseData<Datum = never> {
+  count: number
+  /** @nullable */
+  next?: string | null
+  /** @nullable */
+  previous?: string | null
+  results: Datum[]
+}
+
+export type PaginatedListResponse<Datum = never> = PaginatedListResponse.Ok<Datum> | PaginatedListResponse.NotFound
+export namespace PaginatedListResponse {
+  export interface Ok<Datum = never> {
+    data: PaginatedListResponseData<Datum>
+    status: 200
+  }
+  export interface NotFound {
+    data: unknown
+    status: 404
+  }
+}
+
+interface UniversalTableProps<Datum> {
+  // Below are props from `UniversalTable` that should come from the parent
+  // component (these are kind of "configuration" props). The other
+  // `UniversalTable` props are being handled here internally.
+  columns: Array<UniversalTableColumn<Datum>>
+  queryResult: UseQueryResult<PaginatedListResponse<Datum>>
+  pagination: Pagination
+  setPagination: (pagination: Pagination) => unknown
+}
+
+export const PAGE_SIZES = [10, 30, 50, 100]
+export const DEFAULT_PAGE_SIZE = PAGE_SIZES[0]
+
+/**
+ * Displays paginated data directly from a `react-query` hook.
+ * For displaying custom data structures use UniversalTableCore directly.
+ *
+ * Boilerplate:
+ *
+ * ```tsx
+ * import UniversalTable, { DEFAULT_PAGE_SIZE } from '#/UniversalTable'
+ *
+ * const Example = () => {
+ *   const [pagination, setPagination] = useState({
+ *     limit: DEFAULT_PAGE_SIZE,
+ *     offset: 0,
+ *   })
+ *   const queryResult = useQuery({
+ *     ...
+ *   })
+ *   return <UniversalTable<Datum>
+ *     pagination={pagination}
+ *     setPagination={setPagination}
+ *     queryResult={queryResult}
+ *     columns={[
+ *       { key: 'name', label: t('Name'), cellFormatter: (datum: Datum) => datum.name.toLowerCase() },
+ *       ...
+ *     ]}
+ *   />
+ * }
+ * ```
+ */
+export default function UniversalTable<Datum>({
+  columns,
+  pagination,
+  queryResult,
+  setPagination,
+}: UniversalTableProps<Datum>) {
+  const availablePages = useMemo(
+    () => (queryResult.data?.status === 200 ? Math.ceil(queryResult.data?.data?.count / pagination.limit) : 0),
+    [pagination.limit, queryResult.data?.status, (queryResult.data?.data as PaginatedListResponseData<Datum>).count],
+  )
+
+  const currentPageIndex = useMemo(() => Math.ceil(pagination.offset / pagination.limit), [pagination])
+
+  if (queryResult.data?.status !== 200) return null
+
+  return (
+    <UniversalTableCore<Datum>
+      columns={columns}
+      data={queryResult.data?.data.results ?? []}
+      isSpinnerVisible={queryResult.isFetching}
+      pageIndex={currentPageIndex}
+      pageCount={availablePages}
+      pageSize={pagination.limit}
+      pageSizeOptions={PAGE_SIZES}
+      onRequestPaginationChange={(newPageInfo, oldPageInfo) => {
+        // Calculate new offset and limit from what we've got
+        let newOffset = newPageInfo.pageIndex * newPageInfo.pageSize
+        const newLimit = newPageInfo.pageSize
+
+        // If we change page size, we switch back to first page
+        if (newPageInfo.pageSize !== oldPageInfo.pageSize) {
+          newOffset = 0
+        }
+
+        setPagination({ limit: newLimit, offset: newOffset })
+      }}
+    />
+  )
+}
