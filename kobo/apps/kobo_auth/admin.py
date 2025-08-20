9c0dd3d81a396c9bf028dddd1efef8e938b5f57f
rgraber <becca.graber@kobotoolbox.org>
2025-08-20 10:55:52 -0400

    fixup!: well that was fun

--- kobo/apps/kobo_auth/admin.py
+++ kobo/apps/kobo_auth/admin.py
@@ -2,8 +2,10 @@ from django import forms
 from django.contrib import admin, messages
 
 from kobo.apps.kobo_auth.models import DataCollector, DataCollectorGroup
-from kpi.constants import ASSET_TYPE_SURVEY
+from kobo.apps.kobo_auth.shortcuts import User
+from kpi.constants import ASSET_TYPE_SURVEY, PERM_MANAGE_ASSET
 from kpi.models import Asset
+from kpi.utils.object_permission import get_objects_for_user
 
 
 class DataCollectorGroupAddForm(forms.ModelForm):
@@ -13,29 +15,41 @@ class DataCollectorGroupAddForm(forms.ModelForm):
         widget=forms.CheckboxSelectMultiple
     )
     name = forms.CharField()
+    owner = forms.ModelChoiceField(queryset=User.objects.all())
 
     class Meta:
         model = DataCollectorGroup
-        fields = ['name', 'assets']
+        fields = ['owner', 'name', 'assets']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if self.instance:
+        self.fields['owner'].widget.attrs['disabled'] = 'disabled'
+        if self.instance and self.instance.pk:
             self.fields['assets'].initial = self.instance.assets.all()
+            self.fields['owner'].initial = self.instance.owner
 
 
 @admin.register(DataCollectorGroup)
 class DataCollectorGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner')
-    fields = ['name', 'assets']
     form = DataCollectorGroupAddForm
 
     def save_model(self, request, obj, form, change):
         if not obj.pk:
             obj.owner = request.user
         assets = form.cleaned_data['assets']
-        obj.assets.set(assets)
         super().save_model(request, obj, form, change)
+        obj.assets.set(assets)
+
+    def get_form(self, request, obj=..., change=..., **kwargs):
+        form = super().get_form(request, obj, change, **kwargs)
+        owner = obj.owner if obj else request.user
+        if not obj:
+            form.base_fields['owner'].initial = owner
+        available_assets = get_objects_for_user(owner, perms=[PERM_MANAGE_ASSET])
+        form.base_fields['assets'].queryset = available_assets
+        return form
+
 
 @admin.register(DataCollector)
 class DataCollectorAdmin(admin.ModelAdmin):
