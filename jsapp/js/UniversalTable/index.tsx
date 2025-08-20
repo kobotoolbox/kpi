kobotoolbox/kpi

commit 36e38deda6e31549f4794145e9e55cb9a155b771

refactor(UniversalTable): handle future generated API helpers DEV-895 (#6134)
### 💭 Notes

Restructured UniversalTable to handle generated API helpers in the
future. It should be possible to swap queries without more changes to
the UniversalTable. Notably (see commit by commit):
- Extracted the pagination state and query hook to parent component for
simplicity. The future generated API hooks and fetchers would replace
all of the `*.query` file contents. With previous `queryHook` approach
it would require an extra boilerplate to re-wrap each hook to conform to
the `queryHook` signature and complex typing gymnastics (I tried and
failed), therefore I settled for an equal amount of boilerplate of
raising pagination state along much simpler UniversalTable types and
removing the mental complexity of passing a hook as a parameter.
- unrelated but along the way, renamed UniversalTable files according to
the frontend coding style guidelines. Note that nowhere UniversalTable
was used directly, so I renamed the PaginatedQueryUniversalTable to
UniversalTable and UniversalTable to UniversalTableCore as an internal
component.


### 👀 Preview steps
1. ℹ️ have an account and a project
2. 🟢 [on PR] notice that organization member table still works
3. 🟢 [on PR] notice that form activity table still works as before
4. 🟢 [on PR] notice that access logs table still works

jsapp/js/UniversalTable/index.tsx
@@ -1,0 +1,115 @@
+import React, { useMemo } from 'react'
+
+import type { UseQueryResult } from '@tanstack/react-query'
+import UniversalTableCore from './UniversalTableCore'
+import type { UniversalTableColumn } from './UniversalTableCore'
+
+export { UniversalTableColumn } from './UniversalTableCore'
+
+export interface Pagination {
+  limit: number
+  offset: number
+}
+
+export interface PaginatedListResponseData<Datum = never> {
+  count: number
+  /** @nullable */
+  next?: string | null
+  /** @nullable */
+  previous?: string | null
+  results: Datum[]
+}
+
+export type PaginatedListResponse<Datum = never> = PaginatedListResponse.Ok<Datum> | PaginatedListResponse.NotFound
+export namespace PaginatedListResponse {
+  export interface Ok<Datum = never> {
+    data: PaginatedListResponseData<Datum>
+    status: 200
+  }
+  export interface NotFound {
+    data: unknown
+    status: 404
+  }
+}
+
+interface UniversalTableProps<Datum> {
+  // Below are props from `UniversalTable` that should come from the parent
+  // component (these are kind of "configuration" props). The other
+  // `UniversalTable` props are being handled here internally.
+  columns: Array<UniversalTableColumn<Datum>>
+  queryResult: UseQueryResult<PaginatedListResponse<Datum>>
+  pagination: Pagination
+  setPagination: (pagination: Pagination) => unknown
+}
+
+export const PAGE_SIZES = [10, 30, 50, 100]
+export const DEFAULT_PAGE_SIZE = PAGE_SIZES[0]
+
+/**
+ * Displays paginated data directly from a `react-query` hook.
+ * For displaying custom data structures use UniversalTableCore directly.
+ *
+ * Boilerplate:
+ *
+ * ```tsx
+ * import UniversalTable, { DEFAULT_PAGE_SIZE } from '#/UniversalTable'
+ *
+ * const Example = () => {
+ *   const [pagination, setPagination] = useState({
+ *     limit: DEFAULT_PAGE_SIZE,
+ *     offset: 0,
+ *   })
+ *   const queryResult = useQuery({
+ *     ...
+ *   })
+ *   return <UniversalTable<Datum>
+ *     pagination={pagination}
+ *     setPagination={setPagination}
+ *     queryResult={queryResult}
+ *     columns={[
+ *       { key: 'name', label: t('Name'), cellFormatter: (datum: Datum) => datum.name.toLowerCase() },
+ *       ...
+ *     ]}
+ *   />
+ * }
+ * ```
+ */
+export default function UniversalTable<Datum>({
+  columns,
+  pagination,
+  queryResult,
+  setPagination,
+}: UniversalTableProps<Datum>) {
+  const availablePages = useMemo(
+    () => (queryResult.data?.status === 200 ? Math.ceil(queryResult.data?.data?.count / pagination.limit) : 0),
+    [pagination.limit, queryResult.data?.status, (queryResult.data?.data as PaginatedListResponseData<Datum>).count],
+  )
+
+  const currentPageIndex = useMemo(() => Math.ceil(pagination.offset / pagination.limit), [pagination])
+
+  if (queryResult.data?.status !== 200) return null
+
+  return (
+    <UniversalTableCore<Datum>
+      columns={columns}
+      data={queryResult.data?.data.results ?? []}
+      isSpinnerVisible={queryResult.isFetching}
+      pageIndex={currentPageIndex}
+      pageCount={availablePages}
+      pageSize={pagination.limit}
+      pageSizeOptions={PAGE_SIZES}
+      onRequestPaginationChange={(newPageInfo, oldPageInfo) => {
+        // Calculate new offset and limit from what we've got
+        let newOffset = newPageInfo.pageIndex * newPageInfo.pageSize
+        const newLimit = newPageInfo.pageSize
+
+        // If we change page size, we switch back to first page
+        if (newPageInfo.pageSize !== oldPageInfo.pageSize) {
+          newOffset = 0
+        }
+
+        setPagination({ limit: newLimit, offset: newOffset })
+      }}
+    />
+  )
+}

jsapp/js/UniversalTable/UniversalTableCore.mocks.tsx
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker'
 import type { ColumnPinningPosition } from '@tanstack/react-table'
 import Avatar from '#/components/common/avatar'
 import { formatDate } from '#/utils'
-import type { UniversalTableColumn } from './universalTable.component'
+import type { UniversalTableColumn } from './UniversalTableCore'
 
 /**
  * This function is useful to generate a (big) test data for the table.

jsapp/js/UniversalTable/UniversalTableCore.stories.tsx
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react-webpack5'
-import UniversalTable, { type UniversalTableColumn } from './universalTable.component'
+import UniversalTable, { type UniversalTableColumn } from './UniversalTableCore'
 import {
   type UniversalTableExampleDataItem,
   createUniversalTableExampleDataColumns,
   mockData,
-} from './universalTable.mocks'
+} from './UniversalTableCore.mocks'
 
 interface CustomArgs {
   hasColumnsPinnedLeft: 'none' | 'one' | 'multiple'

jsapp/js/UniversalTable/UniversalTableCore.tsx
@@ -16,7 +16,7 @@ import KoboSelect from '#/components/common/koboSelect'
 import LoadingSpinner from '#/components/common/loadingSpinner'
 import { useViewportSize } from '#/hooks/useViewportSize'
 import { generateUuid } from '#/utils'
-import styles from './universalTable.module.scss'
+import styles from './UniversalTableCore.module.scss'
 
 export interface UniversalTableColumn<DataItem> {
   /**
@@ -97,7 +97,7 @@ const DEFAULT_COLUMN_SIZE = {
  * expect to get user pagination requests through the callback function named
  * `onRequestPaginationChange`.
  */
-export default function UniversalTable<DataItem>(props: UniversalTableProps<DataItem>) {
+export default function UniversalTableCore<DataItem>(props: UniversalTableProps<DataItem>) {
   // We need table height for the resizers
   const [tableHeight, setTableHeight] = useState(0)
   const [hasHorizontalScrollbar, setHasHorizontalScrollbar] = useState(false)

jsapp/js/account/organization/MembersRoute.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Box, Divider, Group, Stack, Text, Title } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import UniversalTable, { DEFAULT_PAGE_SIZE, type UniversalTableColumn } from '#/UniversalTable'
 import InviteModal from '#/account/organization/InviteModal'
 import { getSimpleMMOLabel } from '#/account/organization/organization.utils'
 import subscriptionStore from '#/account/subscriptionStore'
@@ -11,21 +13,39 @@ import Avatar from '#/components/common/avatar'
 import Badge from '#/components/common/badge'
 import LoadingSpinner from '#/components/common/loadingSpinner'
 import envStore from '#/envStore'
-import PaginatedQueryUniversalTable from '#/universalTable/paginatedQueryUniversalTable.component'
-import type { UniversalTableColumn } from '#/universalTable/universalTable.component'
+import { QueryKeys } from '#/query/queryKeys'
 import { formatDate } from '#/utils'
 import InviteeActionsDropdown from './InviteeActionsDropdown'
 import MemberActionsDropdown from './MemberActionsDropdown'
 import MemberRoleSelector from './MemberRoleSelector'
-import useOrganizationMembersQuery from './membersQuery'
-import type { OrganizationMember, OrganizationMemberListItem } from './membersQuery'
+import { type OrganizationMember, type OrganizationMemberListItem, getOrganizationMembers } from './membersQuery'
 import styles from './membersRoute.module.scss'
 import { OrganizationUserRole, useOrganizationQuery } from './organizationQuery'
 
 export default function MembersRoute() {
   const orgQuery = useOrganizationQuery()
+  const orgId = orgQuery.data?.id
+
   const [opened, { open, close }] = useDisclosure(false)
   const mmoLabel = getSimpleMMOLabel(envStore.data, subscriptionStore.activeSubscriptions[0])
+
+  const [pagination, setPagination] = useState({
+    limit: DEFAULT_PAGE_SIZE,
+    offset: 0,
+  })
+
+  const queryResult = useQuery({
+    queryKey: [QueryKeys.organizationMembers, pagination.limit, pagination.offset, orgId],
+    // `orgId!` because it's ensured to be there in `enabled` property :ok:
+    queryFn: () => getOrganizationMembers(pagination.limit, pagination.offset, orgId!),
+    placeholderData: keepPreviousData,
+    enabled: !!orgId,
+    // We might want to improve this in future, for now let's not retry
+    retry: false,
+    // The `refetchOnWindowFocus` option is `true` by default, I'm setting it
+    // here so we don't forget about it.
+    refetchOnWindowFocus: true,
+  })
 
   /**
    * Checks whether object should be treated as organization member or invitee.
@@ -211,9 +231,11 @@ export default function MembersRoute() {
         </Box>
       )}
 
-      <PaginatedQueryUniversalTable<OrganizationMemberListItem>
-        queryHook={useOrganizationMembersQuery}
+      <UniversalTable<OrganizationMemberListItem>
         columns={columns}
+        queryResult={queryResult}
+        pagination={pagination}
+        setPagination={setPagination}
       />
     </div>
   )

jsapp/js/account/organization/membersQuery.ts
@@ -1,4 +1,4 @@
-import { keepPreviousData, useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { fetchDelete, fetchGet, fetchPatch } from '#/api'
 import { endpoints } from '#/api.endpoints'
 import type { Json } from '#/components/common/common.interfaces'
@@ -6,7 +6,6 @@ import type { Nullable } from '#/constants'
 import type { PaginatedResponse } from '#/dataInterface'
 import { QueryKeys } from '#/query/queryKeys'
 import { useSession } from '#/stores/useSession'
-import type { PaginatedQueryHookParams } from '#/universalTable/paginatedQueryUniversalTable.component'
 import type { MemberInvite } from './membersInviteQuery'
 import { type OrganizationUserRole, useOrganizationQuery } from './organizationQuery'
 
@@ -121,7 +120,7 @@ export function useRemoveOrganizationMember() {
  * This is mainly needed for `useOrganizationMembersQuery`, so you most probably
  * would use it through that hook rather than directly.
  */
-async function getOrganizationMembers(limit: number, offset: number, orgId: string) {
+export async function getOrganizationMembers(limit: number, offset: number, orgId: string) {
   const params = new URLSearchParams({
     limit: limit.toString(),
     offset: offset.toString(),
@@ -129,31 +128,13 @@ async function getOrganizationMembers(limit: number, offset: number, orgId: stri
 
   const apiUrl = endpoints.ORGANIZATION_MEMBERS_URL.replace(':organization_id', orgId)
 
-  return fetchGet<PaginatedResponse<OrganizationMemberListItem>>(apiUrl + '?' + params, {
-    errorMessageDisplay: t('There was an error getting the list.'),
-  })
-}
-
-/**
- * A hook that gives you paginated list of organization members. Uses
- * `useOrganizationQuery` to get the id.
- */
-export default function useOrganizationMembersQuery({ limit, offset }: PaginatedQueryHookParams) {
-  const orgQuery = useOrganizationQuery()
-  const orgId = orgQuery.data?.id
-
-  return useQuery({
-    queryKey: [QueryKeys.organizationMembers, limit, offset, orgId],
-    // `orgId!` because it's ensured to be there in `enabled` property :ok:
-    queryFn: () => getOrganizationMembers(limit, offset, orgId!),
-    placeholderData: keepPreviousData,
-    enabled: !!orgId,
-    // We might want to improve this in future, for now let's not retry
-    retry: false,
-    // The `refetchOnWindowFocus` option is `true` by default, I'm setting it
-    // here so we don't forget about it.
-    refetchOnWindowFocus: true,
-  })
+  // Note: little crust ahead of time to make a simpler transition to generated react-query helpers.
+  return {
+    status: 200 as const,
+    data: await fetchGet<PaginatedResponse<OrganizationMemberListItem>>(apiUrl + '?' + params, {
+      errorMessageDisplay: t('There was an error getting the list.'),
+    }),
+  }
 }
 
 export function useOrganizationMemberDetailQuery(username: string, notifyAboutError = true) {

jsapp/js/account/security/accessLogs/accessLogs.query.ts
@@ -1,9 +1,6 @@
-import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import { fetchGet, fetchPost } from '#/api'
 import { endpoints } from '#/api.endpoints'
 import type { FailResponse, PaginatedResponse } from '#/dataInterface'
-import { QueryKeys } from '#/query/queryKeys'
-import type { PaginatedQueryHookParams } from '#/universalTable/paginatedQueryUniversalTable.component'
 
 export interface AccessLog {
   /** User URL */
@@ -23,27 +20,18 @@ export interface AccessLog {
   count: number
 }
 
-async function getAccessLogs(limit: number, offset: number) {
+export async function getAccessLogs(limit: number, offset: number) {
   const params = new URLSearchParams({
     limit: limit.toString(),
     offset: offset.toString(),
   })
-  return fetchGet<PaginatedResponse<AccessLog>>(endpoints.ACCESS_LOGS_URL + '?' + params, {
-    errorMessageDisplay: t('There was an error getting the list.'),
-  })
-}
-
-export default function useAccessLogsQuery({ limit, offset }: PaginatedQueryHookParams) {
-  return useQuery({
-    queryKey: [QueryKeys.accessLogs, limit, offset],
-    queryFn: () => getAccessLogs(limit, offset),
-    placeholderData: keepPreviousData,
-    // We might want to improve this in future, for now let's not retry
-    retry: false,
-    // The `refetchOnWindowFocus` option is `true` by default, I'm setting it
-    // here so we don't forget about it.
-    refetchOnWindowFocus: true,
-  })
+  // Note: little crust ahead of time to make a simpler transition to generated react-query helpers.
+  return {
+    status: 200 as const,
+    data: await fetchGet<PaginatedResponse<AccessLog>>(endpoints.ACCESS_LOGS_URL + '?' + params, {
+      errorMessageDisplay: t('There was an error getting the list.'),
+    }),
+  }
 }
 
 /**

commit fae2cb54540a481a6cb240791b60f1dd8187b5ec

fix(UniversalTable): don't crash while data is loading DEV-895
Bug introduced in 36e38deda6e31549f4794145e9e55cb9a155b771

jsapp/js/UniversalTable/index.tsx
@@ -82,7 +82,7 @@ export default function UniversalTable<Datum>({
 }: UniversalTableProps<Datum>) {
   const availablePages = useMemo(
     () => (queryResult.data?.status === 200 ? Math.ceil(queryResult.data?.data?.count / pagination.limit) : 0),
-    [pagination.limit, queryResult.data?.status, (queryResult.data?.data as PaginatedListResponseData<Datum>).count],
+    [pagination.limit, queryResult.data?.status, (queryResult.data?.data as PaginatedListResponseData<Datum>)?.count],
   )
 
   const currentPageIndex = useMemo(() => Math.ceil(pagination.offset / pagination.limit), [pagination])