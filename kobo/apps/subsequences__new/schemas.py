0eef8596c8d5424b1b595e69e4d145201b299813
John N. Milner <john@tmoj.net>
2025-08-22 17:10:06 -0400

    Add forgotten file…again

---
+++ kobo/apps/subsequences__new/schemas.py
@@ -0,0 +1,57 @@
+import jsonschema
+
+from .actions import ACTION_IDS_TO_CLASSES, ACTIONS
+
+# not the full complexity of XPath, but a slash-delimited path of valid XML tag
+# names to convey group hierarchy
+QUESTION_XPATH_PATTERN = '^([A-Za-z_][A-Za-z0-9_-]*)(/[A-Za-z_][A-Za-z0-9_-]*)*$'
+
+ACTION_PARAMS_SCHEMA = {
+    'additionalProperties': False,
+    'properties': {
+        '_actionConfigs': {
+            'additionalProperties': False,
+            'patternProperties': {
+                QUESTION_XPATH_PATTERN: {
+                    'additionalProperties': False,
+                    'properties': {a.ID: a.params_schema for a in ACTIONS},
+                    'type': 'object',
+                }
+            },
+            'type': 'object',
+        },
+        '_version': {'const': '20250820'},
+    },
+    'type': 'object',
+}
+
+
+def validate_submission_supplement(asset: 'kpi.models.Asset', supplement: dict):
+    jsonschema.validate(get_submission_supplement_schema(asset), supplement)
+
+
+def get_submission_supplement_schema(asset: 'kpi.models.Asset') -> dict:
+    if asset.advanced_features.get('_version') != '20250820':
+        # TODO: migrate from old per-asset schema
+        raise NotImplementedError
+
+    submission_supplement_schema = {
+        'additionalProperties': False,
+        'properties': {'_version': {'const': '20250820'}},
+        'type': 'object',
+    }
+
+    for (
+        question_xpath,
+        action_configs_for_this_question,
+    ) in asset.advanced_features['_actionConfigs'].items():
+        for (
+            action_id,
+            action_params,
+        ) in action_configs_for_this_question.items():
+            action = ACTION_IDS_TO_CLASSES[action_id](question_xpath, action_params)
+            submission_supplement_schema['properties'].setdefault(question_xpath, {})[
+                action_id
+            ] = action.result_schema
+
+    return submission_supplement_schema
