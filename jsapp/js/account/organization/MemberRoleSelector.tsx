7fc4f2964f38f55b930b9b9a8cec3ea4cac0781a
Kalvis Kalniņš <kalvis.kalnins@kobotoolbox.org>
2025-08-25 20:34:19 +0300

    wip: optimistic updates

--- jsapp/js/account/organization/MemberRoleSelector.tsx
+++ jsapp/js/account/organization/MemberRoleSelector.tsx
@@ -1,15 +1,9 @@
 import { LoadingOverlay } from '@mantine/core'
-import {
-  getOrganizationsInvitesListQueryKey,
-  getOrganizationsInvitesRetrieveQueryKey,
-  useOrganizationsInvitesPartialUpdate,
-} from '#/api/react-query/organization-invites'
 import Select from '#/components/common/Select'
-import { queryClient } from '#/query/queryClient'
-import { QueryKeys } from '#/query/queryKeys'
 import { inviteGuidFromUrl } from './common'
-import { type OrganizationMemberListItem, usePatchOrganizationMember } from './membersQuery'
+import { usePatchOrganizationMember } from './membersQuery'
 import { OrganizationUserRole, useOrganizationQuery } from './organizationQuery'
+import useOrganizationsInvitesPartialUpdate from './useOrganizationsInvitesPartialUpdate'
 
 interface MemberRoleSelectorProps {
   username: string
@@ -28,35 +22,6 @@ export default function MemberRoleSelector({ username, role, inviteUrl }: Member
   const patchMember = usePatchOrganizationMember(username)
 
   const orgInvitesPatchMutation = useOrganizationsInvitesPartialUpdate({
-    mutation: {
-      onSuccess: (_data, variables) => {
-        queryClient.invalidateQueries({ queryKey: getOrganizationsInvitesListQueryKey(variables.organizationId) })
-        queryClient.invalidateQueries({
-          queryKey: getOrganizationsInvitesRetrieveQueryKey(variables.organizationId, variables.guid),
-        })
-        queryClient.invalidateQueries({ queryKey: [QueryKeys.organizationMembers] })
-        queryClient.invalidateQueries({ queryKey: [QueryKeys.organizationMemberDetail] })
-      },
-      onMutate: async ({ data }) => {
-        if (!('role' in data)) return
-
-        // If we are updating the invitee's role, we want to optimistically update their role in queries for
-        // the members table list. So we look for their unique invite url and update the relevant query accordingly
-        const qData = queryClient.getQueriesData({ queryKey: [QueryKeys.organizationMembers] })
-        const query = qData.find((q) =>
-          (q[1] as any)?.results?.find((m: OrganizationMemberListItem) => m.invite?.url === inviteUrl),
-        )
-
-        if (!query) return
-
-        const queryKey = query[0]
-        const queryData = query[1]
-        const item = (queryData as any).results.find((m: OrganizationMemberListItem) => m.invite?.url === inviteUrl)
-
-        item.invite.invitee_role = data.role
-        queryClient.setQueryData(queryKey, queryData)
-      },
-    },
     request: {
       errorMessageDisplay: t('There was an error updating this invitation.'),
     },
