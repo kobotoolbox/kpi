e0ea597ebb4e5f236e5b0df3c07f6835f344dc95
Guillermo <lgar89@gmail.com>
2025-09-02 15:07:42 -0600

    Remove django-reversion usages

--- kpi/tasks.py
+++ kpi/tasks.py
@@ -7,7 +7,6 @@ from django.core import mail
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management import call_command
 from django.db.models import Min
-from reversion.models import Version
 
 from kobo.apps.kobo_auth.shortcuts import User
 from kobo.apps.markdownx_uploader.tasks import remove_unused_markdown_files
@@ -128,15 +127,3 @@ def perform_maintenance():
     remove_unused_markdown_files()
     remove_old_import_tasks()
     remove_old_asset_snapshots()
-
-
-@celery_app.task(time_limit=30, soft_time_limit=30)
-def remove_old_versions():
-    while min_id := Version.objects.aggregate(Min('pk'))['pk__min']:
-        queryset = Version.objects.filter(
-            pk__lt=min_id + settings.VERSION_DELETION_BATCH_SIZE
-        )
-        deleted = queryset.delete()
-        # log at debug level so we don't flood the logs
-        logging.debug(f'Deleted {deleted[0]} version objects with pk < {min_id}')
-        time.sleep(10)
