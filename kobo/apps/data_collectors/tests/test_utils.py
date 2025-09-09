8f147a602e52ab16bca506b3e6626140a15c34eb
rgraber <becca.graber@kobotoolbox.org>
2025-09-09 08:45:55 -0400

    fix(dataCollectors): set correct redis entries DEV-798

--- kobo/apps/data_collectors/tests/test_utils.py
+++ kobo/apps/data_collectors/tests/test_utils.py
@@ -3,12 +3,15 @@ import string
 from unittest.mock import MagicMock, patch
 
 import fakeredis
-from django.conf import settings
 from django.test import TestCase
 
 from kobo.apps.data_collectors.constants import DC_ENKETO_URL_TEMPLATE
 from kobo.apps.data_collectors.models import DataCollectorGroup
 from kobo.apps.data_collectors.utils import (
+    URL_REGEX,
+    get_all_redis_entries_for_token,
+    get_redis_key_for_token_and_xform,
+    get_url_for_enketo_key,
     remove_data_collector_enketo_links,
     rename_data_collector_enketo_links,
     set_data_collector_enketo_links,
@@ -43,8 +46,9 @@ class TestDataCollectorUtils(TestCase):
     def fake_enketo_redis_actions(self, data):
         server_url = data['server_url']
         form_id = data['form_id']
+        cleaned_url = URL_REGEX.match(server_url).groups()[1]
         fake_enketo_id = ''.join(random.choices(string.ascii_letters, k=8))
-        self.redis_client.hset(f'or:{server_url}', form_id, fake_enketo_id)
+        self.redis_client.set(f'or:{cleaned_url},{form_id}', fake_enketo_id)
         self.redis_client.hset(f'id:{fake_enketo_id}', 'openRosaServer', server_url)
         mock_response = MagicMock()
         mock_response.json = lambda: {'enketo_id': fake_enketo_id}
@@ -52,9 +56,9 @@ class TestDataCollectorUtils(TestCase):
 
     def _check_expected_redis_entries(self, token, form_id):
         expected_url = DC_ENKETO_URL_TEMPLATE.format(token)
-        enketo_id = self.redis_client.hget(f'or:{expected_url}', form_id).decode(
-            'utf-8'
-        )
+        enketo_key_url = get_url_for_enketo_key(expected_url)
+        enketo_key = f'or:{enketo_key_url},{form_id}'
+        enketo_id = self.redis_client.get(enketo_key).decode('utf-8')
         open_rosa_server = self.redis_client.hget(
             f'id:{enketo_id}', 'openRosaServer'
         ).decode('utf-8')
@@ -72,12 +76,12 @@ class TestDataCollectorUtils(TestCase):
         self._check_expected_redis_entries('1', 'b12345')
 
         expected_url = DC_ENKETO_URL_TEMPLATE.format('1')
-        enketo_id_to_remove = self.redis_client.hget(f'or:{expected_url}', 'a12345')
+        enketo_id_to_remove = self.redis_client.get(f'or:{expected_url},a12345')
 
         remove_data_collector_enketo_links('1', ['a12345'])
         # make sure the data for a12345 has been removed
         expected_url = DC_ENKETO_URL_TEMPLATE.format('1')
-        assert self.redis_client.hget(f'or:{expected_url}', 'a12345') is None
+        assert self.redis_client.get(f'or:{expected_url},a12345') is None
         assert self.redis_client.hgetall(f'id:{enketo_id_to_remove}') == {}
 
         # make sure the data for b12345 is still there
@@ -89,9 +93,9 @@ class TestDataCollectorUtils(TestCase):
         self._check_expected_redis_entries('1', 'a12345')
         self._check_expected_redis_entries('1', 'b12345')
 
-        expected_url = DC_ENKETO_URL_TEMPLATE.format('1')
-        enketo_id_to_remove_a = self.redis_client.hget(f'or:{expected_url}', 'a12345')
-        enketo_id_to_remove_b = self.redis_client.hget(f'or:{expected_url}', 'b12345')
+        expected_url = get_url_for_enketo_key(DC_ENKETO_URL_TEMPLATE.format('1'))
+        enketo_id_to_remove_a = self.redis_client.get(f'or:{expected_url},a12345')
+        enketo_id_to_remove_b = self.redis_client.get(f'or:{expected_url},b12345')
 
         remove_data_collector_enketo_links('1')
         # make sure all data for this DC has been removed
@@ -101,27 +105,33 @@ class TestDataCollectorUtils(TestCase):
 
     def test_remove_non_existent_links(self):
         set_data_collector_enketo_links('1', ['a12345', 'b12345'])
-        # non-existent uid
-        remove_data_collector_enketo_links('1', 'c12345')
-        # non-existent token
-        remove_data_collector_enketo_links('2')
+
+        with self.assertLogs(logger=logging.name, level='WARNING') as log_context:
+            # non-existent asset uid
+            remove_data_collector_enketo_links('1', ['c12345'])
+            expected_key = get_redis_key_for_token_and_xform('1', 'c12345')
+            log_message = f'No redis entry found for key {expected_key}'
+            assert log_message in log_context.output[0]
+
+            # non-existent token
+            remove_data_collector_enketo_links('2')
+            log_message = f'No redis entries found for data collector token 2'
+            assert log_message in log_context.output[1]
 
     def test_rename_data_collector_links(self):
         set_data_collector_enketo_links('1', ['a12345', 'b12345'])
         # make sure we set something in redis
-        expected_url = DC_ENKETO_URL_TEMPLATE.format('1')
         self._check_expected_redis_entries('1', 'a12345')
         self._check_expected_redis_entries('1', 'b12345')
 
         rename_data_collector_enketo_links('1', '2')
         self._check_expected_redis_entries('2', 'a12345')
         self._check_expected_redis_entries('2', 'b12345')
-        assert self.redis_client.hgetall(f'or:{expected_url}') == {}
+        assert get_all_redis_entries_for_token(self.redis_client, '1') == []
 
     def test_rename_non_existent_links(self):
+        rename_data_collector_enketo_links('1', '2')
         with self.assertLogs(logger=logging.name, level='WARNING') as log_context:
             rename_data_collector_enketo_links('1', '2')
-            log_message = (
-                f'Attempt to rename non-existent key or:{settings.KOBOCAT_URL}/key/1'
-            )
+            log_message = f'No redis entries found for data collector token 1'
             assert log_message in log_context.output[0]
