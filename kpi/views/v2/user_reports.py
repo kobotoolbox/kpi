4f6f0f86e33820001ede6ac8e1d92901c72cb3dd
RuthShryock <ruthieshryock@gmail.com>
2025-09-10 12:59:59 -0400

    add documentation for /user-reports endpoint

--- kpi/views/v2/user_reports.py
+++ kpi/views/v2/user_reports.py
@@ -1,12 +1,38 @@
-from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework import mixins, viewsets
+
 from kobo.apps.audit_log.permissions import SuperUserPermission
 from kpi.models.user_reports import UserReports
 from kpi.paginators import LimitStartPagination
 from kpi.permissions import IsAuthenticated
+from kpi.schema_extensions.v2.user_reports.serializers import UserReportsListResponse
 from kpi.serializers.v2.user_reports import UserReportsSerializer
+from kpi.utils.schema_extensions.markdown import read_md
+from kpi.utils.schema_extensions.response import open_api_200_ok_response
 
 
-class UserReportsViewSet(viewsets.ModelViewSet):
+@extend_schema(
+    tags=['User Reports'],
+)
+@extend_schema_view(
+    list=extend_schema(
+        description=read_md('kpi', 'user_reports/list.md'),
+        responses=open_api_200_ok_response(
+            UserReportsListResponse,
+            require_auth=False,
+            raise_not_found=False,
+        ),
+    ),
+)
+class UserReportsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """
+    Available actions:
+    - list          → GET     /api/v2/user-reports/
+
+    Documentation:
+    - docs/api/v2/users_reports/list.md
+    """
+
     queryset = UserReports.objects.all()
     serializer_class = UserReportsSerializer
     pagination_class = LimitStartPagination
