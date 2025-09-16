d00bba58781a47de5dbac9195a2acbfa4c1d71a3
Raj Patel <rajpatel24998@gmail.com>
2025-09-16 13:37:49 +0530

    use datetime for exceeded limit counters to enforce grace period correctly

--- kobo/apps/stripe/tests/test_celery_tasks.py
+++ kobo/apps/stripe/tests/test_celery_tasks.py
@@ -41,3 +41,77 @@ class StripeSignalsTestCase(BaseTestCase):
         ) as patched_update:
             update_exceeded_limit_counters()
             patched_update.assert_called_once_with(test_counter)
+
+
+class ExceededLimitCountersTestCase(BaseTestCase):
+    fixtures = ['test_data']
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.org = baker.make(Organization, id='123456abcdef')
+        cls.user = User.objects.get(username='someuser')
+        cls.org.add_user(cls.user, is_admin=True)
+
+    def test_counter_not_updated_before_24h(self):
+        """
+        A counter modified 23h ago should not be updated yet
+        """
+        recent = baker.make(
+            ExceededLimitCounter,
+            user=self.user,
+            date_modified=timezone.now() - timedelta(hours=23),
+        )
+        with patch(
+            'kobo.apps.stripe.tasks.update_or_remove_limit_counter'
+        ) as patched_update:
+            update_exceeded_limit_counters()
+            patched_update.assert_not_called()
+
+    def test_counter_updated_at_24h(self):
+        """
+        A counter modified 24h ago should be updated
+        """
+        old = baker.make(
+            ExceededLimitCounter,
+            user=self.user,
+            date_modified=timezone.now() - timedelta(hours=24),
+        )
+        with patch(
+            'kobo.apps.stripe.tasks.update_or_remove_limit_counter'
+        ) as patched_update:
+            update_exceeded_limit_counters()
+            patched_update.assert_called_once_with(old)
+
+    def test_counter_updated_after_28h(self):
+        """
+        A counter modified 28h ago should also be updated
+        """
+        old = baker.make(
+            ExceededLimitCounter,
+            user=self.user,
+            date_modified=timezone.now() - timedelta(hours=28),
+        )
+        with patch(
+            'kobo.apps.stripe.tasks.update_or_remove_limit_counter'
+        ) as patched_update:
+            update_exceeded_limit_counters()
+            patched_update.assert_called_once_with(old)
+
+    def test_counter_deleted_if_not_exceeded(self):
+        """
+        update_counter should delete if the user is no longer exceeding
+        """
+        counter = baker.make(
+            ExceededLimitCounter,
+            user=self.user,
+            date_modified=timezone.now() - timedelta(hours=28),
+        )
+        with patch(
+            'kobo.apps.stripe.utils.limit_enforcement.ServiceUsageCalculator.get_usage_balances',  # noqa
+            return_value={counter.limit_type: {'exceeded': False}},
+        ):
+            update_exceeded_limit_counters()
+            self.assertFalse(
+                ExceededLimitCounter.objects.filter(id=counter.id).exists()
+            )
