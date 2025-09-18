757c6ed4e2cb2ee139dcd8ed32fb10c294b3bbfb
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2025-09-18 20:01:29 +0100

    Update main.js
    Co-authored-by: Paulo Amorim <pauloamorimbr@gmail.com>

--- jsapp/js/main.js
+++ jsapp/js/main.js
@@ -17,7 +17,7 @@ import '@fontsource/roboto/500.css'
 // Roboto doesn't have semibold (600) version. We do use 600 font weight a lot in our styles, but they all are rendered
 // as weight 700. To have true 600, we would need to switch to variable font Roboto Flex - but this would require more
 // refactor, and AFAIK would also not play nicely with Mantine which uses "Roboto" as default font and doesn't seem
-// ready for usng a variable font.
+// ready for using a variable font.
 // import '@fontsource/roboto/600.css'
 import '@fontsource/roboto/700.css'
 import '../scss/main.scss'
