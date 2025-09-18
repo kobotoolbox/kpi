0772fad97797b54d3bd55e2ff1b7032670d9abb3
Anji <ollejna@gmail.com>
2025-09-18 19:21:22 -0400

    fix(assetUsage): migrate asset usage table

--- jsapp/js/account/usage/assetUsage.api.ts
+++ jsapp/js/account/usage/assetUsage.api.ts
@@ -4,13 +4,6 @@ import { PROJECT_FIELDS } from '#/projects/projectViews/constants'
 import type { ProjectFieldName } from '#/projects/projectViews/constants'
 import type { ProjectsTableOrder } from '#/projects/projectsTable/projectsTable'
 
-export interface AssetUsage {
-  count: string
-  next: string | null
-  previous: string | null
-  results: AssetWithUsage[]
-}
-
 export interface AssetWithUsage {
   asset: string
   uid: string
@@ -32,29 +25,6 @@ export interface AssetWithUsage {
 const ORG_ASSET_USAGE_URL = '/api/v2/organizations/:organization_id/asset_usage/'
 
 export async function getOrgAssetUsage(
-  pageNumber: number | string,
-  organizationId: string,
-  order?: ProjectsTableOrder,
-) {
-  const apiUrl = ORG_ASSET_USAGE_URL.replace(':organization_id', organizationId)
-
-  const params = new URLSearchParams({
-    page: pageNumber.toString(),
-  })
-
-  if (order?.fieldName && order.direction && (order.direction === 'ascending' || order.direction === 'descending')) {
-    const orderingPrefix = order.direction === 'ascending' ? '' : '-'
-    const fieldDefinition = PROJECT_FIELDS[order.fieldName as ProjectFieldName]
-    params.set('ordering', orderingPrefix + fieldDefinition.apiOrderingName)
-  }
-
-  return fetchGet<AssetUsage>(`${apiUrl}?${params}`, {
-    includeHeaders: true,
-    errorMessageDisplay: t('There was an error fetching asset usage data.'),
-  })
-}
-
-export async function getOrgAssetUsage2(
   limit: number,
   offset: number,
   organizationId: string,
