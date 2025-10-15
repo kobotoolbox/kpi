d104c810c203c94322eba4d190700b50a5525358
Kalvis Kalniņš <kalvis.kalnins@kobotoolbox.org>
2025-10-15 13:33:17 +0300

    fix(reactQuery): after-rebase fixes

--- jsapp/js/account/security/accessLogs/accessLogsSection.component.tsx
+++ jsapp/js/account/security/accessLogs/accessLogsSection.component.tsx
@@ -5,11 +5,7 @@ import UniversalTable, { DEFAULT_PAGE_SIZE } from '#/UniversalTable'
 import securityStyles from '#/account/security/securityRoute.module.scss'
 import type { AccessLogResponse } from '#/api/models/accessLogResponse'
 import type { ErrorDetail } from '#/api/models/errorDetail'
-import {
-  getAccessLogsMeListQueryKey,
-  useAccessLogsMeExportCreate,
-  useAccessLogsMeList,
-} from '#/api/react-query/access-logs'
+import { getAccessLogsMeListQueryKey, useAccessLogsMeExportCreate, useAccessLogsMeList } from '#/api/react-query/logging'
 import Button from '#/components/common/button'
 import ExportToEmailButton from '#/components/exportToEmailButton/exportToEmailButton.component'
 import type { FailResponse } from '#/dataInterface'
