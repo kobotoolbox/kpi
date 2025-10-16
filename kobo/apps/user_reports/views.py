5ad4774ee68f7269c983a8b15142cd645f338f2a
Ruth Turk <81720958+RuthTurk@users.noreply.github.com>
2025-10-16 09:20:02 -0400

    feat(userReports): add `/api/v2/user-reports/` endpoint for superusers DEV-232 DEV-899 (#6243)
    ### 📣 Summary
    Add a new superuser-only endpoint, `/api/v2/user-reports/`, to access
    and filter all user usage data.
    
    ### 💭 Notes
    - Created a new Django app `user_reports` 
    - Used a SQL materialized view along with some Django tables to gather
    and calculate all the data for the endpoint
    - Our celery task `refresh_user_report_snapshots` runs every 30 minutes
    and works to update the data in batches
    - The endpoint is only accessible if Stripe is enabled 
    - Renamed api tag "Audit logs (superusers)" to "Server logs
    (superusers)" ([Zulip
    thread](https://chat.kobotoolbox.org/#narrow/stream/4-Kobo-Dev/topic/API.20Documentation/near/713167))
    
    
    ### 📖 Description
    
    This PR introduces a new endpoint `/api/v2/user-reports` that provides
    detailed information about users, including their personal details,
    metadata, organization info, subscriptions, usage statistics, and
    computed balances. It supports flexible nested filtering, allowing
    queries like:
    ```
    /api/v2/user-reports/?q=username__icontains:demo
    /api/v2/user-reports/?q=service_usage__total_storage_bytes__gte:1
    /api/v2/user-reports/?q=service_usage__balances__submission__balance_value__gte:5000
    ```
    To handle a large user base, the endpoint leverages a PostgreSQL
    materialized view that aggregates user-related data for fast retrieval.
    Since some data (like usage and submissions) lives in another database,
    a periodic Celery task `refresh_user_report_snapshots` was added to
    pre-compute and store this data in a new `BillingAndUsageSnapshot`
    table. The materialized view references this snapshot for further
    computations. A `BillingAndUsageSnapshotRun` table was also added to
    track each run and safely resume interrupted jobs.
    
    
    ### 👀 Preview steps
    
    1. ℹ️ have a superuser account and multiple regular users in multiple
    organizations
    2. create projects, make submissions, and buy stripe subscriptions
    3. checkout this branch and run migrations for `user_reports`
    4. login as a regular user 
    5. 🟢  checkout `/api/v2/user-reports/` and get a 403 Forbidden error
    6. login as a superuser
    7. run the celery task to refresh the data:
    `refresh-user-report-snapshot`
    8. 🟢checkout `/api/v2/user-reports/` and get 200 with all the users and
    data
    9. verify that all data is correct
    10. make some submissions 
    11. 🟢 verify that the data has been correctly refreshed
    
    ---------
    
    Co-authored-by: Raj Patel <rajpatel24998@gmail.com>
    Co-authored-by: Olivier Léger <olivierleger@gmail.com>

---
+++ kobo/apps/user_reports/views.py
@@ -0,0 +1,81 @@
+from django.db import ProgrammingError
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework import mixins, status, viewsets
+from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
+
+from kobo.apps.audit_log.permissions import SuperUserPermission
+from kobo.apps.user_reports.models import UserReports
+from kobo.apps.user_reports.seralizers import UserReportsSerializer
+from kpi.filters import SearchFilter
+from kpi.paginators import LimitOffsetPagination
+from kpi.permissions import IsAuthenticated
+from kpi.schema_extensions.v2.user_reports.serializers import UserReportsListResponse
+from kpi.utils.schema_extensions.markdown import read_md
+from kpi.utils.schema_extensions.response import open_api_200_ok_response
+
+
+@extend_schema(
+    tags=['Server logs (superusers)'],
+)
+@extend_schema_view(
+    list=extend_schema(
+        description=read_md('kpi', 'user_reports/list.md'),
+        responses=open_api_200_ok_response(
+            UserReportsListResponse,
+            require_auth=False,
+            raise_not_found=False,
+        ),
+    ),
+)
+class UserReportsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """
+    Available actions:
+    - list          → GET     /api/v2/user-reports/
+
+    Documentation:
+    - docs/api/v2/users_reports/list.md
+    """
+
+    queryset = UserReports.objects.all()
+    serializer_class = UserReportsSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (IsAuthenticated, SuperUserPermission)
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+
+    search_default_field_lookups = [
+        'username__icontains'
+    ]
+
+    ordering_fields = [
+        'username',
+        'email',
+        'date_joined',
+        'last_login',
+        'total_storage_bytes',
+        'total_submission_count_current_period',
+        'total_submission_count_all_time',
+        'total_nlp_usage_asr_seconds_all_time',
+        'total_nlp_usage_mt_characters_all_time',
+        'asset_count',
+        'deployed_asset_count',
+    ]
+    ordering = ['username']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    skip_distinct = True
+
+    def list(self, request, *args, **kwargs):
+        try:
+            return super().list(request, *args, **kwargs)
+        except ProgrammingError as e:
+            if 'relation "user_reports_userreportsmv" does not exist' in str(e):  # noqa
+                return Response(
+                    {
+                        'details': 'The data source for user reports is missing. '
+                        'Please run 0002_create_user_reports_mv to create the '
+                        'materialized view: user_reports_userreportsmv.',
+                    },
+                    status=status.HTTP_503_SERVICE_UNAVAILABLE,
+                )
+            raise e
