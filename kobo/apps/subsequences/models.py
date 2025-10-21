0a92a24b339eb791f605e453a941a054333d56b0
Olivier Léger <olivierleger@gmail.com>
2025-10-21 16:29:33 -0400

    Reactivate limits

--- kobo/apps/subsequences/models.py
+++ kobo/apps/subsequences/models.py
@@ -83,8 +83,7 @@ class SubmissionSupplement(SubmissionExtras):
                     raise InvalidAction from e
 
                 action = action_class(question_xpath, action_params, asset)
-                # TODO REMOVE The comment below
-                # action.check_limits(asset.owner)
+                action.check_limits(asset.owner)
 
                 question_supplemental_data = supplemental_data.setdefault(
                     question_xpath, {}
