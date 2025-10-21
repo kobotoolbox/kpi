cf8d07b2df89926b93ce1d5d3401d6df6c99c7dd
Guillermo <lgar89@gmail.com>
2025-10-20 18:48:54 -0600

    Prepare mfa adapter for endpoints replacement

--- kobo/apps/accounts/mfa/adapter.py
+++ kobo/apps/accounts/mfa/adapter.py
@@ -1,9 +1,22 @@
-from allauth.mfa.adapter import DefaultMfaAdapter
+from allauth.mfa.adapter import DefaultMFAAdapter
+from constance import config
 from .permissions import mfa_allowed_for_user
 
 
-class MfaAdapter(DefaultMfaAdapter):
+class MfaAdapter(DefaultMFAAdapter):
 
     def is_mfa_enabled(self, user, types=None) -> bool:
         super_enabled = super().is_mfa_enabled(user, types)
         return super_enabled and mfa_allowed_for_user(user)
+
+    def get_totp_label(self, user) -> str:
+        """Returns the label used for representing the given user in a TOTP QR
+        code.
+        """
+        return f'{config.MFA_ISSUER_NAME}:{user.username}'
+
+    def get_totp_issuer(self) -> str:
+        """Returns the TOTP issuer name that will be contained in the TOTP QR
+        code.
+        """
+        return config.MFA_ISSUER_NAME
