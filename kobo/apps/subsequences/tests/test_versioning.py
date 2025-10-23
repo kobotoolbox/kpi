8e82e61a8c42b6656ebffc9d9787c51086e41e92
rgraber <becca.graber@kobotoolbox.org>
2025-10-23 12:39:34 -0400

    fixup!: stuff

--- kobo/apps/subsequences/tests/test_versioning.py
+++ kobo/apps/subsequences/tests/test_versioning.py
@@ -1,6 +1,138 @@
-from django.test import TestCase
+from datetime import timedelta
 
+import pytest
+from mock import patch
+from ddt import data, ddt
+from django.test import TestCase
+from django.utils import timezone
+from freezegun import freeze_time
+
+from kobo.apps.subsequences.utils.versioning import (
+    new_transcript_revision_from_old,
+    separate_transcriptions, migrate_advanced_features, migrate_submission_supplementals,
+)
+
+
+@ddt
 class TestVersioning(TestCase):
+    def test_new_transcript_revision_from_old(self):
+        now = timezone.now()
+        old = {
+            'dateCreated': None,
+            'dateModified': '2025-10-22 17:09:38',
+            'languageCode': 'en',
+            'value': 'Transcribed new',
+        }
+        with freeze_time(now):
+            result = new_transcript_revision_from_old(old)
+        assert result['value'] == old['value']
+        assert result['language'] == old['languageCode']
+        assert result['_dateCreated'] == old['dateModified']
+        assert result['_uuid'] is not None
+        assert result['_dateAccepted'] is None
+
+    def test_new_transcript_revision_from_old_returns_none_for_bad_data(self):
+        old = {'badly': 'formatted'}
+        assert new_transcript_revision_from_old(old) is None
+
+    @data(True, False)
+    def test_separate_automated_and_manual_transcriptions(self, latest_is_automated):
+        now = timezone.now()
+        yesterday = timezone.now() - timedelta(days=1)
+        transcript_dict = {
+            'dateCreated': None,
+            'dateModified': now,
+            'languageCode': 'en',
+            'revisions': [
+                {
+                    'dateModified': yesterday,
+                    'languageCode': 'en',
+                    'value': 'Old transcript',
+                }
+            ],
+            'value': 'Latest transcript',
+        }
+        automated_transcription_value = (
+            'Latest transcript' if latest_is_automated else 'Old transcript'
+        )
+        manual, automated = separate_transcriptions(
+            transcript_dict, 'en', automated_transcription_value
+        )
+        new_automated_transcript = automated[0]
+        new_manual_transcript = manual[0]
+        expected_most_recent_transcript = (
+            new_automated_transcript if latest_is_automated else new_manual_transcript
+        )
+        expected_old_transcript = (
+            new_manual_transcript if latest_is_automated else new_automated_transcript
+        )
+
+        assert expected_most_recent_transcript['_dateCreated'] == now
+        assert expected_most_recent_transcript['value'] == 'Latest transcript'
+        assert expected_old_transcript['_dateCreated'] == yesterday
+        assert expected_old_transcript['value'] == 'Old transcript'
+
+    def test_migrate_transcriptions(self):
+        now = timezone.now()
+        one_year_ago = now - timedelta(days=365)
+        old_version = {'Audio_question': {'googlets': {'languageCode': 'en',
+                                 'regionCode': None,
+                                 'status': 'complete',
+                                 'value': 'This is audio that I am trying to '
+                                          'transcribe.'},
+                    'transcript': {'dateCreated': one_year_ago,
+                                   'dateModified': now,
+                                   'languageCode': 'en',
+                                   'revisions': [{'dateModified': one_year_ago,
+                                                  'languageCode': 'en',
+                                                  'value': 'This is audio that '
+                                                           'I am trying to '
+                                                           'transcribe.'},
+                                                 {}],
+                                   'value': 'This is audio that I am trying to '
+                                            'transcribe but i edited it.'},
+                                          }
+                       }
+        with patch('kobo.apps.subsequences.utils.versioning.generate_uuid_for_form', side_effect=['uuid1', 'uuid2']):
+            with freeze_time(now):
+                migrated = migrate_submission_supplementals(old_version)
+        expected = {
+            '_version': '20250820',
+            'Audio_question': {
+                                  'automatic_transcription': {
+                                      '_dateCreated': one_year_ago,
+                                      '_dateModified': one_year_ago,
+                                      '_versions': [
+                                          {
+                                              '_dateCreated': one_year_ago,
+                                              '_dateAccepted': now,
+                                              '_uuid':'uuid2',
+                                              'language': 'en',
+                                              'value': 'This is audio that I am trying to '
+                                                       'transcribe.',
+                                              'status': 'complete',
+                                          }
+                                      ]
+                                  },
+                                  'manual_transcription': {
+                                      '_dateCreated': now,
+                                      '_dateModified': now,
+                                      '_versions': [
+                                          {
+                                              '_dateCreated': now,
+                                              '_dateAccepted': None,
+                                              '_uuid':'uuid1',
+                                              'language': 'en',
+                                              'value': 'This is audio that I am trying to '
+                                                       'transcribe but i edited it.',
+                                          }
+                                      ]
+                                  },
+            }
+        }
+        assert migrated == expected
+
+    @pytest.mark.skip()
     def test_migrate_submission_extra_to_supplemental(self):
         old_version = {'Audio_question': {'googlets': {'languageCode': 'en',
                                  'regionCode': None,
--- kobo/apps/subsequences/utils/versioning.py
+++ kobo/apps/subsequences/utils/versioning.py
@@ -1,5 +1,13 @@
-from ..constants import SCHEMA_VERSIONS
+from django.utils import timezone
 
+from ..actions import ManualTranscriptionAction
+from ...openrosa.libs.utils.model_tools import generate_uuid_for_form
+from ..constants import SCHEMA_VERSIONS
+from ...subsequences__old.actions.automatic_transcription import AutomaticTranscriptionAction
+
+
+class InvalidSupplementalFormat(Exception):
+    pass
 
 def migrate_advanced_features(advanced_features: dict) -> dict | None:
 
@@ -13,7 +21,6 @@ def migrate_advanced_features(advanced_features: dict) -> dict | None:
 
     actionConfigs = migrated_advanced_features['_actionConfigs']
     for key, value in advanced_features.items():
-        print(f'{key=}, {value=}')
         if (
             key == 'transcript'
             and value
@@ -35,7 +42,6 @@ def migrate_advanced_features(advanced_features: dict) -> dict | None:
             ]
 
         if key == 'qual':
-            survey_qs = value['qual_survey']
             raise NotImplementedError
 
     return migrated_advanced_features
@@ -51,19 +57,121 @@ def migrate_submission_supplementals(supplemental_data:dict) -> dict:
     supplemental = {
         '_version': SCHEMA_VERSIONS[0],
     }
-    for question_xpath, action_results in supplemental_data:
+    for question_xpath, action_results in supplemental_data.items():
         question_results_by_action = {}
-        for action, results in action_results:
-            if action == 'googlets':
-                pass
-            if action == 'googletx':
-                pass
-            if action == 'qual':
-                pass
-            if action == 'transcript':
-                pass
-            if action == 'translation':
-                pass
+        automatic_transcript_language, automatic_transcript_result = (
+            get_automatic_transcription(action_results)
+        )
+        manual_transcripts, automatic_transcripts = separate_transcriptions(
+            action_results.get('transcript', None),
+            automatic_transcript_language,
+            automatic_transcript_result,
+        )
+        # should already be sorted by date created descending, but just in case
+        manual_transcripts.sort(reverse=True, key=lambda d: d['_dateCreated'])
+        automatic_transcripts.sort(reverse=True, key=lambda d: d['_dateCreated'])
 
-def get_automated_transcriptions_by_language(action_results:dict) -> dict:
-    pass
+        if len(manual_transcripts) > 0:
+            question_results_by_action['manual_transcription'] = {
+                '_dateCreated': manual_transcripts[-1]['_dateCreated'],
+                '_dateModified': manual_transcripts[0]['_dateCreated'],
+                '_versions': manual_transcripts,
+            }
+        if len(automatic_transcripts) > 0:
+            question_results_by_action['automatic_transcription'] = {
+                '_dateCreated': automatic_transcripts[-1]['_dateCreated'],
+                '_dateModified': automatic_transcripts[0]['_dateCreated'],
+                '_versions': automatic_transcripts,
+            }
+        supplemental[question_xpath] = question_results_by_action
+
+        # translation
+        # get source
+        tagged_manual_transcripts = [{**transcript, '_actionId': ManualTranscriptionAction.ID} for transcript in manual_transcripts]
+        tagged_automatic_transcripts = [{**transcript, '_actionId': AutomaticTranscriptionAction.ID} for transcript in automatic_transcripts]
+
+        all_tagged_transcripts = [*tagged_manual_transcripts, *tagged_automatic_transcripts]
+        all_tagged_transcripts.sort(reverse=True, key=lambda d: d['_dateCreated'])
+
+        most_recent_transcript_uuids_by_language = {}
+        for transcript in all_tagged_transcripts:
+            if most_recent_transcript_uuids_by_language.get(transcript['language']) is None:
+                most_recent_transcript_uuids_by_language[transcript['language']] = {'_uuid': transcript['_uuid'], '_actionId': transcript['_actionId']}
+
+        translations_dict = action_results.get('translation', {})
+        for language_code, translations in translations_dict.items():
+            pass
+
+
+
+
+    return supplemental
+
+
+def get_automatic_transcription(
+    action_results: dict,
+) -> tuple[str | None, str | None] | None:
+    googlets = action_results.get('googlets', {})
+    return googlets.get('languageCode', None), googlets.get('value', None)
+
+def get_automatic_translation(action_results:dict):
+    googletx = action_results.get('googletx', {})
+    return googletx.get('source', None), googletx.get('languageCode', None), googletx.get('value', None)
+
+
+
+def new_transcript_revision_from_old(old_transcript_revision_dict: dict) -> dict | None:
+    # ignore bad data
+    if (
+        'languageCode' not in old_transcript_revision_dict
+        or 'value' not in old_transcript_revision_dict
+    ):
+        return None
+    return {
+        '_dateCreated': old_transcript_revision_dict.get('dateModified', None),
+        'language': old_transcript_revision_dict['languageCode'],
+        'value': old_transcript_revision_dict['value'],
+        '_uuid': generate_uuid_for_form(),
+        '_dateAccepted': None,
+    }
+
+
+def separate_transcriptions(
+    transcription_dict: dict,
+    automatic_transcript_language: str = None,
+    automatic_transcript_value: str = None,
+) -> tuple[list, list]:
+    if not transcription_dict:
+        return [], []
+    automatic_transcriptions = []
+    manual_transcriptions = []
+    latest_revision = new_transcript_revision_from_old(transcription_dict)
+    if latest_revision:
+        if (
+            latest_revision['value'] == automatic_transcript_value
+            and latest_revision['language'] == automatic_transcript_language
+        ):
+            latest_revision['status'] = 'complete'
+            latest_revision['_dateAccepted'] = timezone.now()
+            automatic_transcriptions.append(latest_revision)
+        else:
+            manual_transcriptions.append(latest_revision)
+
+    for revision in transcription_dict.get('revisions', []):
+        revision_formatted = new_transcript_revision_from_old(revision)
+        if revision_formatted is None:
+            continue
+        if (
+            revision_formatted['language'] == automatic_transcript_language
+            and revision['value'] == automatic_transcript_value
+        ):
+            revision_formatted['status'] = 'complete'
+            revision_formatted['_dateAccepted'] = timezone.now()
+            automatic_transcriptions.append(revision_formatted)
+        else:
+            manual_transcriptions.append(revision_formatted)
+    return manual_transcriptions, automatic_transcriptions
+
+def separate_translations(translation_dict):
+    if not translation_dict:
+        return [],[]
