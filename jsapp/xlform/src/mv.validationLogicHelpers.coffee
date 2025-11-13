kobotoolbox/kpi

commit a0e9ce1c055992fe21754a5b570e03b8d9b99e76

refactor(coffeeScript): fix DS0002 in mv.skipLogicHelpers.coffee DEV-790 (#5982)
### 💭 Notes

For the decaffeination project adjusted code in
`mv.skipLogicHelpers.coffee` to no longer produce DS0002 warning. Also
did some cleanups while there.

### 👀 Preview steps

1. ℹ️ have an account and a project
2. go to Project page and choose "Edit form"
3. inside Form Builder use different functionalities
4. 🟢 notice that everything still works as previously

jsapp/xlform/src/mv.validationLogicHelpers.coffee
@@ -54,19 +54,10 @@ module.exports = do ->
         @state.button = @view_factory.create_empty()
       @render @destination
       return
-    constructor: (model_factory, view_factory, helper_factory, serialized_criteria) ->
-      @model_factory = model_factory
-      @view_factory = view_factory
-      @helper_factory = helper_factory
-
-      @state = serialize: () -> return serialized_criteria
-      if @questionTypeHasNoValidationOperators()
-        @use_hand_code_helper()
-      else
-        super(model_factory, view_factory, helper_factory, serialized_criteria)
 
     questionTypeHasNoValidationOperators: () ->
       typeId = @helper_factory.current_question.get('type').get('typeId')
+      # Note: Leszek: seems like a dead code, can't figure out how to setup a test to trigger it.
       if !typeId
         return console.error('no type id found for question', @helper_factory.current_question)
       operators = $skipLogicHelpers.question_types[typeId]?.operators

jsapp/xlform/src/mv.skipLogicHelpers.coffee
@@ -260,33 +260,27 @@ module.exports = do ->
     use_criterion_builder_helper: () ->
       @builder ?= @helper_factory.create_builder()
       presenters = @builder.build_criterion_builder(@state.serialize())
-
       if presenters == false
-        @state = null
+        # Note: pre-decaffination logic returned false, now it renders use_hand_code_helper instead. Seems to have no effect.
+        @use_hand_code_helper()
       else
         @state = new skipLogicHelpers.SkipLogicCriterionBuilderHelper(presenters[0], presenters[1], @builder, @view_factory, @)
         @render @destination
-      return
     use_hand_code_helper: () ->
       @state = new skipLogicHelpers.SkipLogicHandCodeHelper(@state.serialize(), @builder, @view_factory, @)
       @render @destination
       return
-    use_mode_selector_helper : () ->
+    use_mode_selector_helper: () ->
       @helper_factory.survey.off null, null, @state
       @state = new skipLogicHelpers.SkipLogicModeSelectorHelper(@view_factory, @)
       @render @destination
       return
     constructor: (@model_factory, @view_factory, @helper_factory, serialized_criteria) ->
-      @state = serialize: () -> return serialized_criteria
+      @state = serialize: () -> return serialized_criteria # Initial seeding, will be re-assigned a proper helper in next lines.
       if !serialized_criteria? || serialized_criteria == ''
-        serialized_criteria = ''
         @use_mode_selector_helper()
       else
         @use_criterion_builder_helper()
-
-      if !@state?
-        @state = serialize: () -> return serialized_criteria
-        @use_hand_code_helper()
 
   class skipLogicHelpers.SkipLogicCriterionBuilderHelper
     determine_criterion_delimiter_visibility: () ->

commit c1a69ba419d1e011acb193c0c396fc55c29847f8

fix(formbuilder): skip "add a condition" for 9 unhandled question types DEV-1283 DEV-1284 (#6468)
### 📣 Summary

In formbuilder, hide the not working "add a condition" button for 9
affect question types - Select One, Select Many, Point, Photo, Audio,
Video, Line, Area, Acknowledge.

### 💭 Notes

Regression since #5982 - redefining that constructor was important. But
because redefining constructors should be impossible in the first place,
fixed the problem in another way.

The button should work and not be hidden for Select One and Select Many
question types but fixing that is out of scope of this PR.

### 👀 Preview steps

1. create a new form or edit existing one
2. go to "settings" for affect question type
   1. :arrow_forward: go to "validation criteria" tab
   2. 🔴 [on main] arrive in the choice step between "add a condition" and
"Manually enter your validation logic in XLSForm code"
   3. :arrow_forward: [on main] click "Manually enter your validation logic
in XLSForm code"
   4. 🔴 [on main] arrive in manual step, and see a red trash icon on right
   5. 🟢 [on PR] arrive directly in the manual step
   6. 🟢 [on PR] don't see the red trash icon on right there
3. go to "settings" for unaffected question type
   1. :arrow_forward: go to "validation criteria" tab
   2. 🟢 arrive in the choice step
   3. :arrow_forward: click "Manually enter your validation logic in
XLSForm code"
   4. 🟢 see the trash icon

jsapp/xlform/src/mv.validationLogicHelpers.coffee
@@ -46,8 +46,12 @@ module.exports = do ->
 
   class validationLogicHelpers.ValidationLogicHelperContext extends $skipLogicHelpers.SkipLogicHelperContext
     use_mode_selector_helper: () ->
-      @state = new validationLogicHelpers.ValidationLogicModeSelectorHelper @view_factory, @
-      @render @destination
+      if @questionTypeHasNoValidationOperators()
+        @use_hand_code_helper()
+      else
+        @state = new validationLogicHelpers.ValidationLogicModeSelectorHelper @view_factory, @
+        @render @destination
+      return
     use_hand_code_helper: () ->
       @state = new validationLogicHelpers.ValidationLogicHandCodeHelper(@state.serialize(), @builder, @view_factory, @)
       if @questionTypeHasNoValidationOperators()