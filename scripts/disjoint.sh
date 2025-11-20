3284d1bf35b7d6c4c82a58dd43ae536869721203
Phil Edwards <phil@edwards.io>
2025-11-20 16:59:19 -0500

    Add script for mix-and-match backend + frontend
    Add helper script for running backend and frontend on 2 different branches

---
+++ scripts/disjoint.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+#
+# Disjoint / Split mode
+#
+#   Run KPI in 2 separate parts, one for frontend and one for backend,
+#   so the frontend and backend can be on different git branches.
+#
+# Run this script with two parameters:
+#
+#   disjoint.sh  -b <backend_dir>  -f <frontend_dir>
+#
+# This will:
+#
+#   - Copy webpack-stats.json output from the frontend to the backend
+#   - Re-copy continually (watch for changes to the frontend) until ^C.
+#
+# Before running, you'll need to do these steps manually:
+#
+#   1. Clone kpi in two different locations.
+#   2. From <backend_dir>, start your backend server
+#   3. From <frontend_dir>, start the frontend in watch mode (npm run watch)
+#
+
+backend=""
+frontend=""
+
+##
+## Argument parsing
+##
+
+usage="Usage: $0 -b <backend_dir> -f <frontend_dir>"
+help_deps="file watcher requires uvx or npx"
+
+# Parse -b and -f flags
+OPTIND=1
+while getopts "h?b:f:" opt; do
+    case "$opt" in
+    b)  backend=$(realpath "$OPTARG");;
+    f)  frontend=$(realpath "$OPTARG");;
+    h|\?)
+        echo "$usage"
+        exit 0;;
+    esac
+done
+
+# Exit with usage if -b or -f is missing
+if [[ -z $frontend || -z $backend ]]; then
+    echo "$usage"; exit 1; fi
+if [[ "$frontend" == "$backend" ]]; then
+    echo "frontend and backend directory should not be the same"; exit 1; fi
+
+echo "  backend:  $backend"
+echo "  frontend: $frontend"
+
+
+##
+## Copy once
+##
+
+copy_cmd="cp ${frontend}/webpack-stats.json ${backend}/webpack-stats.json"
+if [[ ! -f "${frontend}/webpack-stats.json" ]]
+then echo "! Make sure to start the frontend ($frontend) with \`npm run watch\`"
+else $copy_cmd; fi
+
+##
+## Watch & copy until ^C
+##
+
+pattern='webpack-stats.json'
+cd "$frontend" || exit 1
+
+# Watcher: if uvx is available, use 'watchdog'
+if command -v uvx >/dev/null 2>&1; then
+    echo 'Watching "webpack-stats.json" ..'; set -x
+    uvx --from watchdog@6.0.0 \
+        watchmedo \
+            shell-command -w \
+            --patterns="$pattern" \
+            --command="$copy_cmd"
+    exit 0
+fi
+
+# Watcher: if npx is available, use 'chokidar-cli'
+npx() {
+    if   command -v bunx >/dev/null 2>&1; then echo "bunx"
+    elif command -v npx  >/dev/null 2>&1; then echo "npx"
+    else echo "$help_deps"; exit 1; fi # last strategy; exit if unavailable
+}
+set -x
+$(npx) --package=chokidar-cli@3.0.0 \
+    chokidar \
+        "$pattern" \
+        --command="$copy_cmd"
+exit 0
