c169e79f1e72cb3d8a544bd033c626fbcf3b34b4
Guillermo <lgar89@gmail.com>
2025-12-03 22:48:57 -0600

    Refactor job to improve performance and simplicity

--- kobo/apps/long_running_migrations/jobs/0013_set_null_for_removed_users.py
+++ kobo/apps/long_running_migrations/jobs/0013_set_null_for_removed_users.py
@@ -1,17 +1,11 @@
-from django.db.models.query import QuerySet
-
 from kobo.apps.audit_log.models import AuditLog
 from kobo.apps.kobo_auth.shortcuts import User
 
 CHUNK_SIZE = 1000
 
 
-def process_user(user_id):
-    AuditLog.objects.filter(user_id=user_id).update(user_id=None)
-
-
-def get_queryset(from_user_pk: int) -> QuerySet:
-    users = (
+def process_chunk(from_user_pk):
+    user_ids = list(
         User.objects.order_by('pk')
         .filter(
             pk__gt=from_user_pk,
@@ -19,10 +13,15 @@ def get_queryset(from_user_pk: int) -> QuerySet:
             is_active=False,
             extra_details__date_removed__isnull=False,
         )
-        .values('id')[:CHUNK_SIZE]
+        .values_list('id', flat=True)[:CHUNK_SIZE]
     )
+    users_count = len(user_ids)
+    if users_count > 0:
+        print(f'Processing chunk of {users_count} users ...')
+        AuditLog.objects.filter(user_id__in=user_ids).update(user_id=None)
+        return user_ids[users_count - 1]
 
-    return users
+    return None
 
 
 def run():
@@ -30,15 +29,8 @@ def run():
     Set to null user_id for removed users in audit logs
     """
 
-    migrated_users = 0
     last_pk = 0
     while True:
-        users = get_queryset(last_pk)
-        if not users:
+        last_pk = process_chunk(last_pk)
+        if last_pk is None:
             break
-
-        users_count = len(users)
-        last_pk = users[len(users) - 1]['id']
-        print(f'Processing chunk of {users_count} users ...')
-        for user in users:
-            process_user(user['id'])
