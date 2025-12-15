9fe4d98cf273911a83920847663b196673953455
Guillermo <lgar89@gmail.com>
2025-12-15 12:45:09 -0600

    fix details about required params for choice select questions

--- kpi/schema_extensions/v2/subsequences/extensions.py
+++ kpi/schema_extensions/v2/subsequences/extensions.py
@@ -2,35 +2,29 @@ from drf_spectacular.extensions import OpenApiSerializerFieldExtension
 from drf_spectacular.plumbing import ResolvedComponent, build_array_type
 
 from kpi.schema_extensions.v2.generic.schema import GENERIC_STRING_SCHEMA
+from kpi.utils.schema_extensions.mixins import ComponentRegistrationMixin
 
 
-class SubsequenceParamsFieldExtension(OpenApiSerializerFieldExtension):
+class SubsequenceParamsFieldExtension(
+    ComponentRegistrationMixin, OpenApiSerializerFieldExtension
+):
     target_class = 'kpi.schema_extensions.v2.subsequences.fields.AdvancedFeatureParamsField'  # noqa
 
     def map_serializer_field(self, auto_schema, direction):
-        action_refs = self._get_action_refs(auto_schema.registry)
+        action_refs = self._get_action_refs(auto_schema)
         return build_array_type(schema={'anyOf': action_refs})
 
-    @classmethod
-    def _get_action_refs(cls, registry):
-        nlp_component = ResolvedComponent(
-            name='NLPActionParams',
-            schema=cls._get_nlp_params_schema(),
-            type=ResolvedComponent.SCHEMA,
-            object=dict,
+    def _get_action_refs(self, auto_schema):
+        nlp_component_ref = self._register_schema_component(
+            auto_schema, 'NLPActionParams', self._get_nlp_params_schema()
         )
-        registry.register_on_missing(nlp_component)
-        qual_component = ResolvedComponent(
-            name='QualActionParams',
-            schema=cls._get_qual_params_schema(),
-            type=ResolvedComponent.SCHEMA,
-            object=dict,
+        qual_component_ref = self._register_schema_component(
+            auto_schema, 'QualActionParams', self._get_qual_params_schema(auto_schema)
         )
-        registry.register_on_missing(qual_component)
 
         return [
-            nlp_component.ref,
-            qual_component.ref,
+            nlp_component_ref,
+            qual_component_ref,
         ]
 
     @classmethod
@@ -47,16 +41,19 @@ class SubsequenceParamsFieldExtension(OpenApiSerializerFieldExtension):
             },
         }
 
-    @classmethod
-    def _get_qual_params_schema(cls):
-        defs = cls._get_qual_defs()
+    def _get_qual_params_schema(self, auto_schema):
+        defs = self._get_qual_defs()
         return {
             'type': 'array',
-            'items': defs['qualQuestion'],
+            'items': {
+                'anyOf': [
+                    self._register_schema_component(auto_schema, 'QualSimpleQuestionParams', defs['qualSimpleQuestion']),
+                    self._register_schema_component(auto_schema, 'QualSelectQuestionParams', defs['qualSelectQuestion']),
+                ]
+            },
         }
 
-    @classmethod
-    def _get_qual_defs(cls):
+    def _get_qual_defs(self):
         definitions = {
             'qualLabels': {
                 'type': 'object',
@@ -71,6 +68,15 @@ class SubsequenceParamsFieldExtension(OpenApiSerializerFieldExtension):
                     'qualTags',
                     'qualText',
                     'qualNote',  # Takes no response data
+                ],
+            },
+            'qualSimpleQuestionType': {
+                'type': 'string',
+                'enum': [
+                    'qualInteger',
+                    'qualTags',
+                    'qualText',
+                    'qualNote',
                 ],
             },
             'qualSelectQuestionType': {
@@ -92,12 +98,23 @@ class SubsequenceParamsFieldExtension(OpenApiSerializerFieldExtension):
             },
             'required': ['labels', 'uuid'],
         }
-        definitions['qualQuestion'] = {
+        definitions['qualSimpleQuestion'] = {
             'type': 'object',
             'additionalProperties': False,
             'properties': {
                 'uuid': definitions['qualUuid'],
-                'type': definitions['qualQuestionType'],
+                'type': definitions['qualSimpleQuestionType'],
+                'labels': definitions['qualLabels'],
+                'options': {'type': 'object'},
+            },
+            'required': ['uuid', 'type', 'labels'],
+        }
+        definitions['qualSelectQuestion'] = {
+            'type': 'object',
+            'additionalProperties': False,
+            'properties': {
+                'uuid': definitions['qualUuid'],
+                'type': definitions['qualSelectQuestionType'],
                 'labels': definitions['qualLabels'],
                 'choices': {
                     'type': 'array',
@@ -105,7 +122,7 @@ class SubsequenceParamsFieldExtension(OpenApiSerializerFieldExtension):
                 },
                 'options': {'type': 'object'},
             },
-            'required': ['uuid', 'type', 'labels'],
+            'required': ['uuid', 'type', 'labels', 'choices'],
         }
 
         return definitions
