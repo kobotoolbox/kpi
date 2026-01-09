48ed14c1d21ee85d27c627efde17273e52fb12f0
Raj Patel <51355159+rajpatel24@users.noreply.github.com>
2026-01-09 11:58:10 +0530

    chore(forms): remove `/api/v1/forms` endpoint DEV-1257 (#6576)
    ### 📖 Description
    Removed `/api/v1/forms` endpoint:
    
    Deleted the viewset associated with `/api/v1/forms`.
    Removed the related serializer and permissions.
    Cleaned up all related test cases.

--- kobo/apps/openrosa/apps/api/urls.py
+++ kobo/apps/openrosa/apps/api/urls.py
@@ -11,7 +11,6 @@ from kobo.apps.openrosa.apps.api.viewsets.briefcase_api import BriefcaseApi
 from kobo.apps.openrosa.apps.api.viewsets.data_viewset import DataViewSet
 from kobo.apps.openrosa.apps.api.viewsets.metadata_viewset import MetaDataViewSet
 from kobo.apps.openrosa.apps.api.viewsets.xform_submission_api import XFormSubmissionApi
-from kobo.apps.openrosa.apps.api.viewsets.xform_viewset import XFormViewSet
 from kpi.models.asset import Asset
 from .utils.rest_framework.views import OpenRosaAPIView
 
@@ -392,7 +391,6 @@ class MultiLookupRouterWithPatchList(MultiLookupRouter):
 
 router = MultiLookupRouter(trailing_slash=False)
 
-router.register(r'forms', XFormViewSet)
 router.register(r'metadata', MetaDataViewSet, basename='metadata')
 router.register(r'media', AttachmentViewSet, basename='attachment')
 router.register(r'submissions', XFormSubmissionApi, basename='submissions')
