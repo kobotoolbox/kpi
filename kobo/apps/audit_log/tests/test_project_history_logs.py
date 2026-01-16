6ca4a6bd63a7802b245fdb83ea304456192ad3bd
Rebecca Graber <becca.graber@kobotoolbox.org>
2026-01-16 12:47:59 -0500

    feat(qual): incoporate backup llm DEV-721 (#6609)
    ### 📣 Summary
    Try a backup model if the primary LLM fails to provide a comprehensible
    response.
    
    
    ### 📖 Description
    Using Open AI's OSS120 as the primary model and Claude Sonnet as the
    backup, if the request to the primary model fails to provide an answer
    we can use, try the backup.
    
    
    ### 💭 Notes
    This PR does not include any way for users or server admins to change
    the primary/backup models. That is purposely left for future work.
    
    ### 👀 Preview steps
    Difficult to preview since it's hard to force OSS to fail, but can be
    regression tested with the same steps as
    https://github.com/kobotoolbox/kpi/pull/6580

--- kobo/apps/audit_log/tests/test_project_history_logs.py
+++ kobo/apps/audit_log/tests/test_project_history_logs.py
@@ -1,5 +1,6 @@
 import base64
 import copy
+import io
 import json
 import uuid
 from unittest.mock import patch
@@ -28,9 +29,10 @@ from kobo.apps.openrosa.apps.logger.xform_instance_parser import (
     remove_uuid_prefix,
 )
 from kobo.apps.openrosa.libs.utils.logger_tools import dict2xform
+from kobo.apps.subsequences.actions.automatic_bedrock_qual import OSS120, ClaudeSonnet
 from kobo.apps.subsequences.constants import Action
 from kobo.apps.subsequences.models import QuestionAdvancedFeature, SubmissionSupplement
-from kobo.apps.subsequences.tests.utils import MockLLMClient
+from kobo.apps.subsequences.tests.utils import MockLLMClient, get_mock_claude_response
 from kpi.constants import (
     ASSET_TYPE_TEMPLATE,
     CLONE_ARG_NAME,
@@ -2117,7 +2119,7 @@ class TestProjectHistoryLogs(BaseAuditLogTestCase):
 
         with patch(
             'kobo.apps.subsequences.actions.automatic_bedrock_qual.boto3.client',
-            return_value=MockLLMClient('answer'),
+            return_value=MockLLMClient(5),
         ):
             log_metadata = self._base_project_history_log_test(
                 url=reverse(
@@ -2140,7 +2142,7 @@ class TestProjectHistoryLogs(BaseAuditLogTestCase):
                 log_metadata, 'adminuser', instance.root_uuid
             )
             llm_info = log_metadata['llm']
-            self.assertEqual(llm_info['model'], 'llm_model')
+            self.assertEqual(llm_info['model'], OSS120.model_id)
             self.assertEqual(llm_info['input_tokens'], 10)
             self.assertEqual(llm_info['output_tokens'], 20)
 
@@ -2197,3 +2199,68 @@ class TestProjectHistoryLogs(BaseAuditLogTestCase):
                 log_metadata['llm']['error'],
                 'Unable to extract answer from LLM response object',
             )
+
+    def test_request_automatic_qa_data_includes_backup_model_if_used(self):
+        class MockErrorClient:
+            def invoke_model(self, modelId, *args, **kwargs):
+                if modelId == OSS120.model_id:
+                    return {'something': 'bad'}
+                else:
+                    return {
+                        'body': io.StringIO(
+                            json.dumps(
+                                get_mock_claude_response(
+                                    text='5', input_tokens=10, output_tokens=20
+                                )
+                            )
+                        )
+                    }
+
+        instance, submission = self._add_submission('adminuser')
+        submission = list(
+            self.asset.deployment.get_submissions(
+                user=User.objects.get(username='adminuser'),
+                query={'meta/rootUuid': add_uuid_prefix(instance.root_uuid)},
+            )
+        )[0]
+
+        # add a transcript
+        SubmissionSupplement.revise_data(
+            self.asset,
+            submission,
+            incoming_data={
+                '_version': '20250820',
+                'q1': {
+                    'manual_transcription': {'language': 'en', 'value': 'transcript'}
+                },
+            },
+        )
+
+        with patch(
+            'kobo.apps.subsequences.actions.automatic_bedrock_qual.boto3.client',
+            return_value=MockErrorClient(),
+        ):
+            log_metadata = self._base_project_history_log_test(
+                url=reverse(
+                    self._get_endpoint('submission-supplement'),
+                    args=[self.asset.uid, submission['_uuid']],
+                ),
+                method=self.client.patch,
+                request_data={
+                    '_version': '20250820',
+                    'q1': {
+                        Action.AUTOMATIC_BEDROCK_QUAL: {
+                            'uuid': 'uuid-qual-integer',
+                        }
+                    },
+                },
+                expected_action=AuditAction.MODIFY_AUTOMATIC_QA_DATA,
+                expected_subtype=PROJECT_HISTORY_LOG_PROJECT_SUBTYPE,
+            )
+            self._check_submission_log_metadata(
+                log_metadata, 'adminuser', instance.root_uuid
+            )
+            llm_info = log_metadata['llm']
+            self.assertEqual(llm_info['model'], ClaudeSonnet.model_id)
+            self.assertEqual(llm_info['input_tokens'], 10)
+            self.assertEqual(llm_info['output_tokens'], 20)
