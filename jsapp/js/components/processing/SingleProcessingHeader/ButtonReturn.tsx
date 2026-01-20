fd8a49513ecbae1af8d2d8e6e6697f56a8b9ece9
James Kiger <james.kiger@gmail.com>
2026-01-20 08:01:47 -0500

    Cleanup return button in header

--- jsapp/js/components/processing/SingleProcessingHeader/ButtonReturn.tsx
+++ jsapp/js/components/processing/SingleProcessingHeader/ButtonReturn.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Button from '#/components/common/button'
 import protectorHelpers from '#/protector/protectorHelpers'
@@ -17,40 +17,13 @@ interface Props {
  */
 export default function ButtonReturn({ assetUid, hasUnsavedWork }: Props) {
   const navigate = useNavigate()
-  const [isDoneButtonPending, setIsDoneButtonPending] = useState(false)
 
   /** Goes back to Data Table route for given project. */
   const onDone = () => {
     protectorHelpers.safeExecute(hasUnsavedWork, () => {
-      // HACK: If there are any changes to the data, we need to ensure that
-      // the latest asset is available in the Data Table, when it will rebuild
-      // itself, so that all the columns are rendered. This is needed for the case
-      // when user added/deleted transcript or translation (editing the text
-      // value for it is already handled properly by Data Table code).
-
-      // TODO: Add a way to check for changes in asset if needed. For now we're always forcing a data reload.
-
-      // if (singleProcessingStore.data.isPristine) {
+      // TODO: Consider finding a nice way to update data table when necessary,
+      // rather than always forcing a reload when closing processing view.
       navigateToDataTable()
-      // } else {
-      //   // Mark button as pending to let user know we wait for stuff.
-      //   setIsDoneButtonPending(true)
-
-      //   // We don't need to add these listeners prior to this moment, and we don't
-      //   // need to cancel them, as regardless of outcome, we will navigate out of
-      //   // current view.
-      //   unlisteners.push(actions.resources.loadAsset.completed.listen(navigateToDataTable))
-
-      //   // For failed load we still navigate to Data Table, as this is not
-      //   // something that would cause a massive disruption or data loss
-      //   unlisteners.push(actions.resources.loadAsset.failed.listen(navigateToDataTable))
-
-      //   // We force load asset to overwrite the cache, so that when
-      //   // `FormSubScreens` (a parent of Data Table) starts loading in a moment,
-      //   // it would fetch latest asset and make Data Table use it. To avoid
-      //   // race conditions we wait until it loads to leave.
-      //   actions.resources.loadAsset({ id: assetUid }, true)
-      // }
     })
   }
 
@@ -61,7 +34,7 @@ export default function ButtonReturn({ assetUid, hasUnsavedWork }: Props) {
 
   return (
     <section className={styles.column}>
-      <Button type='primary' size='l' label={t('DONE')} isPending={isDoneButtonPending} onClick={onDone} />
+      <Button type='primary' size='l' label={t('DONE')} onClick={onDone} />
     </section>
   )
 }
