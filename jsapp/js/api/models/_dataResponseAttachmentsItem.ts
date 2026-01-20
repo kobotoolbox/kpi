8f0cc4d53ea40bdc908823337b6e9dd8fc83e2b2
Rebecca Graber <becca.graber@kobotoolbox.org>
2026-01-20 07:41:31 -0500

    fix(subsequences): fix data attachment schema DEV-1579 (#6623)
    ### 💭 Notes
    Fix the schema for the `_attachments` field in the data endpoints to be
    an array of objects with:
    - required: download_url, mimetype, filename, uid, media_file_basename,
    question_xpath
    - optional: download_large_url, download_medium_url, download_small_url,
    is_deleted

--- jsapp/js/api/models/_dataResponseAttachmentsItem.ts
+++ jsapp/js/api/models/_dataResponseAttachmentsItem.ts
@@ -10,13 +10,15 @@ The endpoints are grouped by area of intended use. Each category contains relate
  * OpenAPI spec version: 2.0.0 (api_v2)
  */
 
-export type _DataResponseAttachments = {
-  download_url?: string
+export type _DataResponseAttachmentsItem = {
+  download_url: string
   download_large_url?: string
   download_medium_url?: string
   download_small_url?: string
-  mimetype?: string
-  filename?: string
-  uid?: string
-  question_xpath?: string
+  mimetype: string
+  media_file_basename: string
+  filename: string
+  uid: string
+  question_xpath: string
+  is_deleted?: boolean
 }
