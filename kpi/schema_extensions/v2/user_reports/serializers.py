9dfd41b658a94810ea013eaeb7d58c7dbcdea265
Raj Patel <rajpatel24998@gmail.com>
2026-01-21 21:14:28 +0530

    Update schema and example

--- kpi/schema_extensions/v2/user_reports/serializers.py
+++ kpi/schema_extensions/v2/user_reports/serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
 
 from kpi.schema_extensions.v2.me.fields import SocialAccountField
-from kpi.schema_extensions.v2.service_usage.serializers import ServiceUsageResponse
+from kpi.schema_extensions.v2.service_usage.fields import BalancesField, NlpUsageField
 from kpi.schema_extensions.v2.user_reports.fields import (
     ExtraDetailsField,
     OrganizationsField,
+    SubmissionCountField,
     SubscriptionsField,
 )
 from kpi.utils.schema_extensions.serializers import inline_serializer_class
+
+ServiceUsageResponse = inline_serializer_class(
+    name='UserReportsServiceUsageResponse',
+    fields={
+        'total_nlp_usage': NlpUsageField(),
+        'total_storage_bytes': serializers.IntegerField(),
+        'total_submission_count': SubmissionCountField(),
+        'balances': BalancesField(),
+        'current_period_start': serializers.DateTimeField(),
+        'current_period_end': serializers.DateTimeField(),
+        'last_updated': serializers.DateTimeField(),
+    },
+)
 
 UserReportsListResponse = inline_serializer_class(
     name='UserReportsListResponse',
@@ -30,7 +44,7 @@ UserReportsListResponse = inline_serializer_class(
         'organizations': OrganizationsField(),
         'extra_details': ExtraDetailsField(),
         'subscriptions': SubscriptionsField(),
-        'current_service_usage': ServiceUsageResponse(),
+        'service_usage': ServiceUsageResponse(),
         'account_restricted': serializers.BooleanField(),
         'asset_count': serializers.IntegerField(),
         'active_project_count': serializers.IntegerField(),
