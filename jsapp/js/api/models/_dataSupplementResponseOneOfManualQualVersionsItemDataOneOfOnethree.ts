2c4245ce599cbd8855e28e1f7a1acd836509ad33
Ruth Turk <81720958+RuthTurk@users.noreply.github.com>
2026-01-21 13:41:17 -0500

    fix(openapi): update `DataSupplementResponse` to use manual_qual DEV-1620 (#6634)
    ### 🗒️ Checklist
    
    1. [X] run linter locally
    2. [X] update developer docs (API, README, inline, etc.), if any
    3. [X] for user-facing doc changes create a Zulip thread at `#Support
    Docs Updates`, if any
    4. [X] draft PR with a title `<type>(<scope>)<!>: <title> DEV-1234`
    5. [X] assign yourself, tag PR: at least `Front end` and/or `Back end`
    or `workflow`
    6. [X] fill in the template below and delete template comments
    7. [X] review thyself: read the diff and repro the preview as written
    8. [X] open PR & confirm that CI passes & request reviewers, if needed
    9. [ ] delete this section before merging
    
    ### 👀 Preview steps
    Verify that the documentation for DataSupplementResponseOneOf uses
    manual_qual instead of qual
    <img width="907" height="542" alt="Screenshot 2026-01-21 at 12 43 09 PM"
    src="https://github.com/user-attachments/assets/f1c0a4e5-ff0a-4528-9d1a-08be80283aab"
    />

--- jsapp/js/api/models/_dataSupplementResponseOneOfManualQualVersionsItemDataOneOfOnethree.ts
+++ jsapp/js/api/models/_dataSupplementResponseOneOfManualQualVersionsItemDataOneOfOnethree.ts
@@ -9,7 +9,9 @@ The endpoints are grouped by area of intended use. Each category contains relate
 **General note**: All projects (whether deployed or draft), as well as all library content (questions, blocks, templates, and collections) in the user-facing application are represented in the API as "assets".
  * OpenAPI spec version: 2.0.0 (api_v2)
  */
+import type { _DataSupplementResponseOneOfManualQualVersionsItemDataOneOfOnethreeAllOf } from './_dataSupplementResponseOneOfManualQualVersionsItemDataOneOfOnethreeAllOf'
+import type { _DataSupplementResponseOneOfManualQualVersionsItemDataOneOfOnethreeAllOfTwo } from './_dataSupplementResponseOneOfManualQualVersionsItemDataOneOfOnethreeAllOfTwo'
 
-export type _DataSupplementResponseOneOfQualVersionsItemDataOneOfOnezeroAllOfTwo = {
-  value?: string[]
-}
+export type _DataSupplementResponseOneOfManualQualVersionsItemDataOneOfOnethree =
+  _DataSupplementResponseOneOfManualQualVersionsItemDataOneOfOnethreeAllOf &
+    _DataSupplementResponseOneOfManualQualVersionsItemDataOneOfOnethreeAllOfTwo
