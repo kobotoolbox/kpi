79d099b5fd04a055411dc53cc063e6d9c3146c90
Leszek <smutnyleszek@gmail.com>
2026-01-22 18:25:58 +0100

    fix bug in AsyncLanguageDisplayLabel when switching props (without remounting) didn't fetch new language name

--- jsapp/js/components/processing/SingleProcessingContent/TabTranscript/TranscriptEdit/headerLanguageAndDate.tsx
+++ jsapp/js/components/processing/SingleProcessingContent/TabTranscript/TranscriptEdit/headerLanguageAndDate.tsx
@@ -19,10 +19,6 @@ export default function HeaderLanguageAndDate({ transcriptVersion }: Props) {
   return (
     <React.Fragment>
       <label className={bodyStyles.transxHeaderLanguage}>
-        {/*
-        TODO: BUG sometimes when switching between submissions (through the SelectSubmission component), we end up with
-        wrong label being displayed (e.g. "Bangla (en)" or "English (bn)" - mixing 2 letter code with wrong name)
-        */}
         <AsyncLanguageDisplayLabel code={valueLanguageCode} />
       </label>
       {/* Note: there is no `_dateModified` here, because modifying through API is just creating a new version */}
