481b76f54856c56b94b1584ae63dc7a96953932e
Olivier Léger <olivierleger@gmail.com>
2026-01-28 17:27:23 -0500

    style: lint

--- kobo/apps/long_running_migrations/jobs/0016_noop.py
+++ kobo/apps/long_running_migrations/jobs/0016_noop.py
@@ -1,13 +1,3 @@
-from collections import defaultdict
-
-from django.db import transaction
-from django.db.models import Count
-
-from kobo.apps.organizations.models import OrganizationOwner, OrganizationUser
-from kobo.apps.organizations.utils import revoke_org_asset_perms
-from kobo.apps.organizations.tasks import transfer_member_data_ownership_to_org
-from kpi.utils.log import logging
-
 def run():
     """
     Does nothing, just to keep jobs numbers in sync with migrations
