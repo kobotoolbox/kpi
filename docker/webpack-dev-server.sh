f1da38f790eb672e2588c2604a5c4723ccefd912
Phil Edwards <phil@edwards.io>
2026-02-02 16:51:33 -0500

    Simplify in-container watch helper script

--- docker/webpack-dev-server.sh
+++ docker/webpack-dev-server.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
-set -eox
-source /etc/profile
-
-# If you've been doing this:
 # 
-#       ./run.py -cf run --rm --publish 3000:3000 kpi \
-#                                    npm run watch && \
-#                            ./run.py -cf restart kpi
+#   Convenience script. Start the webpack dev server
+#   in a temporary container from the host:
+# 
+#     ./kobo-install/run.py -cf run --rm --publish 3000:3000 \
+#         kpi bash ./docker/webpack-dev-server.sh
 #
-# Or this:
+#   This should launch successfully even if `node_modules` 
+#   is removed from the built image.
 #
-#       ./run.py -cf run --rm --publish 3000:3000 kpi npm run watch ; \
-#                            docker restart kobofe-kpi-1"
-#
-#
-# Both of these will probably fail now that node_modules isn't in the
-# container anymore.
-#
-# If you want a workflow like it now, do this?
-#
-#       ./run.py -cf run --rm --publish 3000:3000 kpi \
-#                    ./docker/webpack-dev-server.sh ; \
-#                            ./run.py -cf restart kpi
-
-echo "Syncing \`npm\` packages…"
-if ( ! check-dependencies ); then
-    npm install
-else
-    npm run postinstall
-fi
-
-echo "Starting webpack-dev-server…"
-npm run watch
-
+set -e
+(check-dependencies || npm install) && npm run watch
