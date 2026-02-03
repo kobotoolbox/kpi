a24521d9905a45186427135ec23ddd2ea757ca4a
Rebecca Graber <becca.graber@kobotoolbox.org>
2026-02-03 10:33:48 -0500

    refactor(dataCollectors): cleanup DEV-1560 (#6612)
    ### 💭 Notes
    1. Cosmetic - renaming/moving methods and variables for consistency,
    some additional clarifying documentation
    2. Consistency - reraising errors in all methods instead of sometimes
    swallowing them and sometimes not, treating member variables the same in
    signals
    3. Optimization - Replacing `keys` method, deemed unsafe in production
    
    ### 👀 Preview steps
    Regression testing only.
    Verify:
    1. Creating a new data collector and adding them to a group - they
    should now be able to see and submit to all projects belonging to the
    group owner
    4. Rotating a DC's token - all links should still work
    5. Removing a DC from the group - links should no longer work
    6. Deleting a DC group - DC links should no longer work
    7. DC group's owner loses manage project permissions - DC links should
    no longer work
    8. Delete DC - DC links should not longer work

--- kobo/apps/data_collectors/authentication.py
+++ kobo/apps/data_collectors/authentication.py
@@ -29,7 +29,7 @@ class DataCollectorUser(AnonymousUser):
         if perm != PERM_ADD_SUBMISSIONS:
             return False
         if isinstance(obj, XForm):
-            return obj.kpi_asset_uid in self.assets
+            return obj.kpi_asset_uid is not None and obj.kpi_asset_uid in self.assets
         if isinstance(obj, Asset):
             return obj.uid in self.assets
         return False
@@ -49,12 +49,14 @@ class DataCollectorTokenAuthentication(BaseAuthentication):
             collector = DataCollector.objects.get(token=key)
             server_user = DataCollectorUser()
             group = collector.group
+            server_user.name = collector.name
+            server_user.uid = collector.uid
             if group:
                 server_user.assets = list(group.assets.values_list('uid', flat=True))
-                server_user.name = collector.name
-                server_user.uid = collector.uid
                 server_user.group_uid = group.uid
                 server_user.group_name = group.name
+            else:
+                server_user.assets = []
             return server_user, key
         except DataCollector.DoesNotExist:
             raise AuthenticationFailed('Invalid token.')
