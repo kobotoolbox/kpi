7f5609fa854b5987718c228751c2041020b0ff1f
Paulo Amorim <pauloamorimbr@gmail.com>
2026-02-03 08:00:30 -0300

    feat(processing): toast on saving transcripts and translations DEV-1700 (#6681)
    ### 💭 Notes
    
    Add toast notification when saving a transcript or a translation to
    match production.
    
    ### 👀 Preview steps
    
    1. ℹ️ have an account and a project and audio answers
    2. Add or edit a transcript to the audio
    4. 🔴 [on base branch] no toast feedback is seen
    5. 🟢 [on PR] there's a toast feedback
    2. Add or edit a translation
    4. 🔴 [on base branch] no toast feedback is seen
    5. 🟢 [on PR] there's a toast feedback

--- jsapp/js/components/processing/SingleProcessingContent/TabTranslations/TranslationEdit/Editor.tsx
+++ jsapp/js/components/processing/SingleProcessingContent/TabTranslations/TranslationEdit/Editor.tsx
@@ -15,7 +15,7 @@ import type { LanguageCode } from '#/components/languages/languagesStore'
 import { userCan } from '#/components/permissions/utils'
 import { isSupplementVersionAutomatic } from '#/components/processing/common/utils'
 import type { AssetResponse } from '#/dataInterface'
-import { removeDefaultUuidPrefix } from '#/utils'
+import { notify, removeDefaultUuidPrefix } from '#/utils'
 import { SUBSEQUENCES_SCHEMA_VERSION } from '../../../common/constants'
 import bodyStyles from '../../../common/processingBody.module.scss'
 import HeaderLanguageAndDate from './HeaderLanguageAndDate'
@@ -128,6 +128,7 @@ export default function Editor({
 
     // Clear unsaved work status after successful save
     onUnsavedWorkChange(false)
+    notify(t('successfully updated'), 'success')
     onSave()
   }
 
