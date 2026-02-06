4a9396be3447a15744bdbaf5f07716793aabeb37
Leszek <smutnyleszek@gmail.com>
2026-02-06 13:29:50 +0100

    handle list invalidation happening through legacy code

--- jsapp/js/components/SidebarFormsList.tsx
+++ jsapp/js/components/SidebarFormsList.tsx
@@ -1,13 +1,16 @@
 import { Group, Stack, Text, UnstyledButton } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { Link } from 'react-router-dom'
+import { actions } from '#/actions'
 import type { Asset } from '#/api/models/asset'
-import { useAssetsList } from '#/api/react-query/manage-projects-and-library-content'
+import { invalidateList } from '#/api/mutation-defaults/common'
+import { getAssetsListQueryKey, useAssetsList } from '#/api/react-query/manage-projects-and-library-content'
 import { COMMON_QUERIES } from '#/constants'
 import type { AssetResponse } from '#/dataInterface'
 import type { IconName } from '#/k-icons'
 import { getRouteAssetUid } from '#/router/routerUtils'
+import { notify } from '#/utils'
 import styles from './SidebarFormsList.module.scss'
 import AssetName from './common/assetName'
 import Badge from './common/badge'
@@ -15,32 +18,54 @@ import Icon from './common/icon'
 import LoadingSpinner from './common/loadingSpinner'
 import { userCan } from './permissions/utils'
 
+const SidebarFormsListQueryKey = getAssetsListQueryKey({ q: COMMON_QUERIES.s, limit: 200, ordering: 'name' })
+
 export default function SidebarFormsList() {
   const [isCategoryDeployedOpened, categoryDeployedHandlers] = useDisclosure(false)
   const [isCategoryDraftOpened, categoryDraftHandlers] = useDisclosure(false)
   const [isCategoryArchivedOpened, categoryArchivedHandlers] = useDisclosure(false)
 
-  const {
-    data: assetsResponse,
-    isLoading,
-    isError,
-  } = useAssetsList({
-    q: COMMON_QUERIES.s,
-    limit: 200,
-    ordering: 'name',
-  })
+  const unlisteners: Function[] = []
+  useEffect(() => {
+    unlisteners.push(
+      // A list of different Reflux actions that upon completion would cause changes to the list of assets in SidebarFormsList
+      actions.resources.deleteAsset.completed.listen(() => invalidateList(SidebarFormsListQueryKey)),
+      actions.resources.cloneAsset.completed.listen(() => invalidateList(SidebarFormsListQueryKey)),
+      actions.resources.deployAsset.completed.listen(() => invalidateList(SidebarFormsListQueryKey)),
+      actions.resources.setDeploymentActive.completed.listen(() => invalidateList(SidebarFormsListQueryKey)),
+      actions.resources.updateAsset.completed.listen(() => invalidateList(SidebarFormsListQueryKey)),
+    )
+    // When unmounting, let's remember to unlisten all those Reflux actions
+    return () => {
+      unlisteners.forEach((clb) => clb())
+    }
+  }, [])
+
+  const assetsQuery = useAssetsList(
+    {
+      q: COMMON_QUERIES.s,
+      limit: 200,
+      ordering: 'name',
+    },
+    {
+      query: {
+        queryKey: SidebarFormsListQueryKey,
+        throwOnError: () => {
+          notify.error(t('There was an error getting the list.')) // TODO: update message in backend (DEV-????).
+          return false
+        },
+      },
+    },
+  )
   const deployedProjects = useMemo(() => {
-    if (!assetsResponse?.data?.results) return []
-    return assetsResponse.data.results.filter((asset) => asset.deployment_status === 'deployed')
-  }, [assetsResponse])
+    return assetsQuery.data?.data?.results?.filter((asset) => asset.deployment_status === 'deployed') || []
+  }, [assetsQuery.data])
   const draftProjects = useMemo(() => {
-    if (!assetsResponse?.data?.results) return []
-    return assetsResponse.data.results.filter((asset) => asset.deployment_status === 'draft')
-  }, [assetsResponse])
+    return assetsQuery.data?.data?.results?.filter((asset) => asset.deployment_status === 'draft') || []
+  }, [assetsQuery.data])
   const archivedProjects = useMemo(() => {
-    if (!assetsResponse?.data?.results) return []
-    return assetsResponse.data.results.filter((asset) => asset.deployment_status === 'archived')
-  }, [assetsResponse])
+    return assetsQuery.data?.data?.results?.filter((asset) => asset.deployment_status === 'archived') || []
+  }, [assetsQuery.data])
 
   function renderCategoryButton(options: {
     toggleFunction: () => void
@@ -66,7 +91,7 @@ export default function SidebarFormsList() {
 
   function renderProject(assetOriginal: Asset) {
     // TODO: because of a bug in OpenAPI schema, we can't use `userCan` easily. We cast it here until it is fixed.
-    // See: https://linear.app/kobotoolbox/issue/DEV-1727/openapi-schema-for-asset-wrongly-types-permissions-as-string
+    // See: https://linear.app/kobotoolbox/issue/DEV-1727/
     const asset = assetOriginal as unknown as AssetResponse
 
     let href = `/forms/${asset.uid}`
@@ -92,7 +117,11 @@ export default function SidebarFormsList() {
     )
   }
 
-  if (isLoading) {
+  if (assetsQuery.isError) {
+    return <Text>{t('Could not load assets')}</Text>
+  }
+
+  if (assetsQuery.isLoading) {
     return <LoadingSpinner />
   }
 
