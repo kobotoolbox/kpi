2ab41a5d17e38ad572224ea68b8a1ab4251e64c0
Leszek <smutnyleszek@gmail.com>
2026-02-12 16:21:16 +0100

    split SidebarFormsListCategory to separate component

--- jsapp/js/sidebar/SidebarFormsList.tsx
+++ jsapp/js/sidebar/SidebarFormsList.tsx
@@ -1,23 +1,13 @@
-import { Box, Group, Stack, Text, UnstyledButton } from '@mantine/core'
+import { Stack, Text } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
 import { actions } from '#/actions'
-import type { Asset } from '#/api/models/asset'
 import { invalidatePaginatedList } from '#/api/mutation-defaults/common'
 import { getAssetsListQueryKey, useAssetsList } from '#/api/react-query/manage-projects-and-library-content'
 import { COMMON_QUERIES } from '#/constants'
-import type { AssetResponse } from '#/dataInterface'
-import type { IconName } from '#/k-icons'
-import { ROUTES } from '#/router/routerConstants'
-import { getRouteAssetUid } from '#/router/routerUtils'
 import { notify } from '#/utils'
-import AssetName from '../components/common/assetName'
-import Badge from '../components/common/badge'
-import Icon from '../components/common/icon'
 import LoadingSpinner from '../components/common/loadingSpinner'
-import { userCan } from '../components/permissions/utils'
-import styles from './SidebarFormsList.module.scss'
+import SidebarFormsListCategory from './SidebarFormsListCategory'
 
 export const SidebarFormsListQueryKey = getAssetsListQueryKey({ q: COMMON_QUERIES.s, limit: 200, ordering: 'name' })
 
@@ -76,65 +66,6 @@ export default function SidebarFormsList() {
   const archivedProjects =
     assetsQuery.data?.data?.results?.filter((asset) => asset.deployment_status === 'archived') || []
 
-  function renderCategory(options: {
-    toggleFunction: () => void
-    isOpen: boolean
-    iconName: IconName
-    label: string
-    categoryAssets: Asset[]
-  }) {
-    return (
-      <>
-        <UnstyledButton
-          size='md'
-          variant='transparent'
-          onClick={options.toggleFunction}
-          className={styles.categoryButton}
-        >
-          <Group gap='xs'>
-            <Icon name={options.iconName} size='l' className={styles.categoryButtonIcon} />
-            <Box flex={1}>{options.label}</Box>
-            <Badge label={options.categoryAssets.length} color='light-storm' size='xs' />
-          </Group>
-        </UnstyledButton>
-
-        {options.isOpen && (
-          <Stack className={styles.categoryList} gap='0'>
-            {options.categoryAssets.map(renderProject)}
-          </Stack>
-        )}
-      </>
-    )
-  }
-
-  function renderProject(assetOriginal: Asset) {
-    // TODO: because of a bug in OpenAPI schema, we can't use `userCan` easily. We cast it here until it is fixed.
-    // See: https://linear.app/kobotoolbox/issue/DEV-1727/
-    const asset = assetOriginal as unknown as AssetResponse
-
-    let href = ROUTES.FORM.replace(':uid', asset.uid)
-    if (userCan('view_submissions', asset) && asset.has_deployment && asset.deployment__submission_count) {
-      href = ROUTES.FORM_SUMMARY.replace(':uid', asset.uid)
-    } else {
-      href = ROUTES.FORM_LANDING.replace(':uid', asset.uid)
-    }
-
-    const isActiveProject = asset.uid === getRouteAssetUid()
-
-    return (
-      <Link
-        to={href}
-        key={asset.uid}
-        style={{ background: isActiveProject ? 'var(--mantine-color-gray-7)' : 'transparent' }}
-        className={styles.projectLink}
-      >
-        <Text fz='12' p='3 6'>
-          <AssetName asset={asset} />
-        </Text>
-      </Link>
-    )
-  }
-
   if (assetsQuery.isError) {
     return <Text>{t('Could not load assets')}</Text>
   }
@@ -146,29 +77,29 @@ export default function SidebarFormsList() {
   return (
     // minHeight needed for flex to work properly with scrollable containers
     <Stack style={{ minHeight: 0 }}>
-      {renderCategory({
-        toggleFunction: categoryDeployedHandlers.toggle,
-        isOpen: isCategoryDeployedOpened,
-        iconName: 'deploy',
-        label: t('Deployed'),
-        categoryAssets: deployedProjects,
-      })}
+      <SidebarFormsListCategory
+        onToggleClick={categoryDeployedHandlers.toggle}
+        isOpen={isCategoryDeployedOpened}
+        iconName={'deploy'}
+        label={t('Deployed')}
+        projects={deployedProjects}
+      />
 
-      {renderCategory({
-        toggleFunction: categoryDraftHandlers.toggle,
-        isOpen: isCategoryDraftOpened,
-        iconName: 'drafts',
-        label: t('Draft'),
-        categoryAssets: draftProjects,
-      })}
+      <SidebarFormsListCategory
+        onToggleClick={categoryDraftHandlers.toggle}
+        isOpen={isCategoryDraftOpened}
+        iconName={'drafts'}
+        label={t('Draft')}
+        projects={draftProjects}
+      />
 
-      {renderCategory({
-        toggleFunction: categoryArchivedHandlers.toggle,
-        isOpen: isCategoryArchivedOpened,
-        iconName: 'archived',
-        label: t('Archived'),
-        categoryAssets: archivedProjects,
-      })}
+      <SidebarFormsListCategory
+        onToggleClick={categoryArchivedHandlers.toggle}
+        isOpen={isCategoryArchivedOpened}
+        iconName={'archived'}
+        label={t('Archived')}
+        projects={archivedProjects}
+      />
     </Stack>
   )
 }
