56fa617006bea7976245fc4c5b442a84913ce2db
Raj Patel <rajpatel24998@gmail.com>
2026-02-12 22:12:27 +0530

    Block user signup for blacklisted email addresses

--- kpi/tests/test_registration.py
+++ kpi/tests/test_registration.py
@@ -71,3 +71,91 @@ class RegistrationTestCase(TestCase):
             t('This email domain is not allowed to create an account').encode(),
             response.content,
         )
+
+    @override_config(
+        ENABLE_PASSWORD_MINIMUM_LENGTH_VALIDATION=False,
+        ENABLE_PASSWORD_USER_ATTRIBUTE_SIMILARITY_VALIDATION=False,
+        ENABLE_MOST_RECENT_PASSWORD_VALIDATION=False,
+        ENABLE_COMMON_PASSWORD_VALIDATION=False,
+        ENABLE_PASSWORD_CUSTOM_CHARACTER_RULES_VALIDATION=False,
+        REGISTRATION_BLACKLIST_EMAIL_DOMAINS='bad-domain.com\nmalicious.net',
+        REGISTRATION_BLACKLIST_ERROR_MESSAGE='Go away!'
+    )
+    def test_blacklisted_domain_cannot_register(self):
+        data = self.valid_data.copy()
+        data['email'] = 'hacker@bad-domain.com'
+
+        response = self.client.post(
+            reverse('account_signup'), data=data
+        )
+        self.assertIn(b'Go away!', response.content)
+
+    @override_config(
+        ENABLE_PASSWORD_MINIMUM_LENGTH_VALIDATION=False,
+        ENABLE_PASSWORD_USER_ATTRIBUTE_SIMILARITY_VALIDATION=False,
+        ENABLE_MOST_RECENT_PASSWORD_VALIDATION=False,
+        ENABLE_COMMON_PASSWORD_VALIDATION=False,
+        ENABLE_PASSWORD_CUSTOM_CHARACTER_RULES_VALIDATION=False,
+        REGISTRATION_BLACKLIST_EMAIL_DOMAINS='bad-domain.com'
+    )
+    def test_blacklisted_domain_with_no_error_message(self):
+        """
+        Test that if a blacklisted domain is used and no error message is set,
+        registration should not be blocked
+        """
+        data = self.valid_data.copy()
+        data['email'] = 'user@bad-domain.com'
+
+        response = self.client.post(
+            reverse('account_signup'), data=data
+        )
+        self.assertIn(b'', response.content)
+
+    @override_config(
+        ENABLE_PASSWORD_MINIMUM_LENGTH_VALIDATION=False,
+        ENABLE_PASSWORD_USER_ATTRIBUTE_SIMILARITY_VALIDATION=False,
+        ENABLE_MOST_RECENT_PASSWORD_VALIDATION=False,
+        ENABLE_COMMON_PASSWORD_VALIDATION=False,
+        ENABLE_PASSWORD_CUSTOM_CHARACTER_RULES_VALIDATION=False,
+        REGISTRATION_BLACKLIST_EMAIL_DOMAINS='bad-domain.com'
+    )
+    def test_non_blacklisted_domain_can_register(self):
+        response = self.client.post(
+            reverse('account_signup'), data=self.valid_data
+        )
+        self.assertRedirects(response, '/accounts/confirm-email/')
+
+    @override_config(
+        ENABLE_PASSWORD_MINIMUM_LENGTH_VALIDATION=False,
+        ENABLE_PASSWORD_USER_ATTRIBUTE_SIMILARITY_VALIDATION=False,
+        ENABLE_MOST_RECENT_PASSWORD_VALIDATION=False,
+        ENABLE_COMMON_PASSWORD_VALIDATION=False,
+        ENABLE_PASSWORD_CUSTOM_CHARACTER_RULES_VALIDATION=False,
+    )
+    def test_empty_blacklist_allows_registration(self):
+        response = self.client.post(
+            reverse('account_signup'), data=self.valid_data
+        )
+        self.assertRedirects(response, '/accounts/confirm-email/')
+
+    @override_config(
+        ENABLE_PASSWORD_MINIMUM_LENGTH_VALIDATION=False,
+        ENABLE_PASSWORD_USER_ATTRIBUTE_SIMILARITY_VALIDATION=False,
+        ENABLE_MOST_RECENT_PASSWORD_VALIDATION=False,
+        ENABLE_COMMON_PASSWORD_VALIDATION=False,
+        ENABLE_PASSWORD_CUSTOM_CHARACTER_RULES_VALIDATION=False,
+        REGISTRATION_BLACKLIST_EMAIL_DOMAINS='bad-domain.com'
+    )
+    def test_default_blacklist_error_message(self):
+        data = self.valid_data.copy()
+        data['email'] = 'user@bad-domain.com'
+
+        response = self.client.post(
+            reverse('account_signup'), data=data
+        )
+        self.assertIn(
+            b'Account creation restricted for this server. '
+            b'Your organization uses a separate private KoboToolbox server. '
+            b'Please contact your organization support team for assistance.',
+            response.content
+        )
