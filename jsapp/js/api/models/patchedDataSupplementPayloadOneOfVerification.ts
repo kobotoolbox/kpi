2eab71b894e764b29d6349dc66a074662b9a8e55
rgraber <becca.graber@kobotoolbox.org>
2026-02-13 11:14:10 -0500

    feat(qual): add verification to schemas DEV-1748

--- jsapp/js/api/models/patchedDataSupplementPayloadOneOfVerification.ts
+++ jsapp/js/api/models/patchedDataSupplementPayloadOneOfVerification.ts
@@ -9,13 +9,8 @@ The endpoints are grouped by area of intended use. Each category contains relate
 **General note**: All projects (whether deployed or draft), as well as all library content (questions, blocks, templates, and collections) in the user-facing application are represented in the API as "assets".
  * OpenAPI spec version: 2.0.0 (api_v2)
  */
-import type { DataSupplementAutomaticQualDataIntegerResponse } from './dataSupplementAutomaticQualDataIntegerResponse'
-import type { DataSupplementAutomaticQualDataSelectMultipleResponse } from './dataSupplementAutomaticQualDataSelectMultipleResponse'
-import type { DataSupplementAutomaticQualDataSelectOneResponse } from './dataSupplementAutomaticQualDataSelectOneResponse'
-import type { DataSupplementAutomaticQualDataTextResponse } from './dataSupplementAutomaticQualDataTextResponse'
 
-export type _DataSupplementResponseOneOfAutomaticBedrockQualVersionsItemData =
-  | DataSupplementAutomaticQualDataIntegerResponse
-  | DataSupplementAutomaticQualDataTextResponse
-  | DataSupplementAutomaticQualDataSelectOneResponse
-  | DataSupplementAutomaticQualDataSelectMultipleResponse
+export interface PatchedDataSupplementPayloadOneOfVerification {
+  uuid: string
+  verified: boolean
+}
