541e06a8c7ae6e93bd62ebdd9d4341bcef4f4d26
Guillermo <lgar89@gmail.com>
2026-02-17 15:12:27 -0600

    address PR review comments

--- kobo/apps/long_running_migrations/jobs/0011_backfill_exceeded_limit_counters.py
+++ kobo/apps/long_running_migrations/jobs/0011_backfill_exceeded_limit_counters.py
@@ -8,26 +8,24 @@ from kobo.apps.stripe.utils.limit_enforcement import check_exceeded_limit
 if settings.STRIPE_ENABLED:
     from kobo.apps.stripe.models import ExceededLimitCounter
 
-CHUNK_SIZE = 1000
-
 
 def process_user(user_id, username):
-    try:
-        user = User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        print(f'Race condition catched: user_id={user_id} no longer exists')
+    if not User.objects.filter(pk=user_id).exists():
+        print(f'Race condition catched: User(pk={user_id}) no longer exists')
         return 0
-    
+
     print(f'Checking exceeded limits for {username}...')
     counter = ExceededLimitCounter.objects.filter(
         user_id=user_id,
         limit_type=UsageType.STORAGE_BYTES,
     ).first()
+
+    user = User.objects.get(pk=user_id)
     if counter is None:
         counter = check_exceeded_limit(user, UsageType.STORAGE_BYTES)
-
     user.extra_details.data['done_storage_limits_check'] = True
     user.extra_details.save()
+
     return 1 if counter is None else 0
 
 
@@ -41,7 +39,7 @@ def get_queryset(from_user_pk: int) -> QuerySet:
             # Filter out users that already went through the exceeded limits check
             extra_details__data__done_storage_limits_check__isnull=True,
         )
-        .values('id', 'username')[:CHUNK_SIZE]
+        .values('id', 'username')[:settings.LONG_RUNNING_MIGRATION_BATCH_SIZE]
     )
 
     return users
@@ -64,8 +62,8 @@ def run():
             break
         print(f'Processing {users_count} users from {last_pk}')
         last_pk = users[users_count - 1]['id']
-        
-        for user in users
+
+        for user in users:
             result = process_user(user['id'], user['username'])
             if type(result) is int:
                 created_counters += result
