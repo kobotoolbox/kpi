6af5b813a1e7b1e90b6fd367efbc796969915f00
Guillermo <lgar89@gmail.com>
2026-02-24 11:18:23 -0600

    feat(sso): IdP model and admin screen DEV-1664 (#6738)
    ### 📣 Summary
    This PR adds the Identity Provider model and admin screen to manage the
    SCIM providers.
    
    ### 👀 Preview steps
    1. Go to the django admin 
    2. Search for the Identity Providers in the menu and create an Identity
    Provider register to test it works ok

---
+++ kobo/apps/kobo_scim/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import IdentityProvider
+
+
+@admin.register(IdentityProvider)
+class IdentityProviderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'is_active', 'date_created')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'slug')
+    readonly_fields = ('id', 'date_created', 'date_modified')
+    prepopulated_fields = {'slug': ('name',)}
+    fields = (
+        'id',
+        'name',
+        'slug',
+        'scim_api_key',
+        'is_active',
+        'date_created',
+        'date_modified',
+    )
