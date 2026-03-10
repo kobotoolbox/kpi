cbe9fa02c70b21e249b798d9140551fb3f10ab98
Leszek <smutnyleszek@gmail.com>
2026-03-10 13:52:17 +0100

    make InfiniteScrollTrigger be a default export

--- jsapp/js/components/common/InfiniteScrollTrigger.tsx
+++ jsapp/js/components/common/InfiniteScrollTrigger.tsx
@@ -3,7 +3,7 @@ import { useIntersection } from '@mantine/hooks'
 import React, { useEffect } from 'react'
 import ButtonNew from './ButtonNew'
 
-export interface InfiniteScrollTriggerProps {
+interface InfiniteScrollTriggerProps {
   hasNextPage: boolean
   isFetchingNextPage: boolean
   isError: boolean
@@ -20,14 +20,10 @@ export interface InfiniteScrollTriggerProps {
  * To make things DRY, this component already displays a spinner, a retry button, and an end of list message - based on
  * provided props.
  */
-export const InfiniteScrollTrigger: React.FC<InfiniteScrollTriggerProps> = ({
-  hasNextPage,
-  isFetchingNextPage,
-  isError,
-  onRetry,
-  onRequestFetchNextPage,
-  showEndMessage = true,
-}) => {
+export default function InfiniteScrollTrigger(props: InfiniteScrollTriggerProps) {
+  // Defaults to true
+  const showEndMessage = props.showEndMessage === undefined ? true : props.showEndMessage
+
   const { ref, entry } = useIntersection({
     // Fetch slightly before it actually appears on screen
     rootMargin: '100px',
@@ -36,14 +32,14 @@ export const InfiniteScrollTrigger: React.FC<InfiniteScrollTriggerProps> = ({
 
   useEffect(() => {
     // entry?.isIntersecting lets us know the trigger element is visible
-    if (entry?.isIntersecting && hasNextPage && !isFetchingNextPage && !isError) {
-      onRequestFetchNextPage()
+    if (entry?.isIntersecting && props.hasNextPage && !props.isFetchingNextPage && !props.isError) {
+      props.onRequestFetchNextPage()
     }
-  }, [entry?.isIntersecting, hasNextPage, isFetchingNextPage, isError, onRequestFetchNextPage])
+  }, [entry?.isIntersecting, props.hasNextPage, props.isFetchingNextPage, props.isError, props.onRequestFetchNextPage])
 
   // Show the control row if we are loading the next page, have an error, have more pages to load, or are showing
   // the end-of-list message
-  const shouldShowControlRow = isFetchingNextPage || isError || hasNextPage || showEndMessage
+  const shouldShowControlRow = props.isFetchingNextPage || props.isError || props.hasNextPage || showEndMessage
 
   if (!shouldShowControlRow) {
     return null
@@ -52,13 +48,13 @@ export const InfiniteScrollTrigger: React.FC<InfiniteScrollTriggerProps> = ({
   return (
     // We use `minHeight` to ensure the control row is never `0px` height which could cause strange behaviour.
     <Center ref={ref} py='md' style={{ minHeight: '54px' }}>
-      {isFetchingNextPage && <Loader size='sm' />}
-      {isError && !isFetchingNextPage && (
-        <ButtonNew leftIcon='reload' variant='danger-secondary' size='xs' onClick={onRetry}>
+      {props.isFetchingNextPage && <Loader size='sm' />}
+      {props.isError && !props.isFetchingNextPage && (
+        <ButtonNew leftIcon='reload' variant='danger-secondary' size='xs' onClick={props.onRetry}>
           {t('Retry')}
         </ButtonNew>
       )}
-      {!hasNextPage && !isFetchingNextPage && !isError && showEndMessage && (
+      {!props.hasNextPage && !props.isFetchingNextPage && !props.isError && showEndMessage && (
         <Text size='sm' c='var(--mantine-color-gray-2)'>
           {t("You've reached the end of the list")}
         </Text>
