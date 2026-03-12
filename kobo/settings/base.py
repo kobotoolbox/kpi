4ae7cf68069a4805037a40898026f83d1e376066
Olivier Léger <olivierleger@gmail.com>
2026-03-12 15:13:50 -0400

    chore: remove django-reversion package and drop its tables

--- kobo/settings/base.py
+++ kobo/settings/base.py
@@ -95,7 +95,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_prometheus',
-    'reversion',
+
     'private_storage',
     'kobo.apps.KpiConfig',
     'kobo.apps.accounts',
