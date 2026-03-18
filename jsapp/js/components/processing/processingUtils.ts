74ba48444c261df1cc63696e3d86d6a6346a654e
Leszek <smutnyleszek@gmail.com>
2026-03-18 23:43:23 +0100

    don't modify path array in getSupplementalPathParts

--- jsapp/js/components/processing/processingUtils.ts
+++ jsapp/js/components/processing/processingUtils.ts
@@ -36,10 +36,10 @@ interface SupplementalPathParts {
  */
 export function getSupplementalPathParts(path: string): SupplementalPathParts {
   const pathArr = path.split('/')
-  const lastEl = pathArr.pop()
-  const lastElArr = lastEl?.split('_') || []
+  // last element can be either single word or transcript/translation word plus underscore and two letter language code
+  const lastEl = pathArr[pathArr.length - 1]
 
-  // Handles path that is not a supplemental path
+  // Handles path that is not a supplemental path (i.e. doesn't start on `SUPPLEMENTAL_DETAILS_PROP`)
   if (pathArr[0] !== SUPPLEMENTAL_DETAILS_PROP) {
     return {
       sourceRowName: path,
@@ -49,11 +49,11 @@ export function getSupplementalPathParts(path: string): SupplementalPathParts {
   }
 
   let pathType: SupplementalPathPartsType
-  if (lastElArr[0] === 'transcript') {
+  if (lastEl.startsWith('transcript')) {
     pathType = 'transcript'
-  } else if (lastElArr[0] === 'translation') {
+  } else if (lastEl.startsWith('translation')) {
     pathType = 'translation'
-  } else if (lastElArr[0] === 'verified') {
+  } else if (lastEl === 'verified') {
     pathType = 'qualVerification'
   } else {
     // For now the only other type of data here is the qualitative analysis
@@ -62,21 +62,23 @@ export function getSupplementalPathParts(path: string): SupplementalPathParts {
   }
 
   const output: SupplementalPathParts = {
-    sourceRowName: pathArr[pathArr.length - 1],
+    // This is the name of the source row, without any groups.
+    sourceRowName: pathArr[pathArr.length - 2],
     // We start from second element, because first one is `SUPPLEMENTAL_DETAILS_PROP`
-    sourceRowPath: pathArr.slice(1, pathArr.length).join('/'),
+    sourceRowPath: pathArr.slice(1, pathArr.length - 1).join('/'),
     type: pathType,
   }
 
   // For verification we need to override things, because path is built differently (has a suffix)
   if (pathType === 'qualVerification') {
-    output.sourceRowName = pathArr[pathArr.length - 2]
-    output.sourceRowPath = pathArr.slice(1, pathArr.length - 1).join('/')
-    output.analysisQuestionUuid = pathArr[pathArr.length - 1]
+    output.sourceRowName = pathArr[pathArr.length - 3]
+    output.sourceRowPath = pathArr.slice(1, pathArr.length - 2).join('/')
+    output.analysisQuestionUuid = pathArr[pathArr.length - 2]
   }
 
   // For transx we add the language code
   if (pathType === 'transcript' || pathType === 'translation') {
+    const lastElArr = lastEl?.split('_') || []
     output.languageCode = lastElArr[1]
   }
 
