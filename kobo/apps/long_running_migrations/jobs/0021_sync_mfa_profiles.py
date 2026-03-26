e2e4020c2f42846f63e5a17088026570c6ae211b
Raj Patel <rajpatel24998@gmail.com>
2026-03-26 19:06:02 +0530

    fix(userReport): Minor updates

--- kobo/apps/long_running_migrations/jobs/0021_sync_mfa_profiles.py
+++ kobo/apps/long_running_migrations/jobs/0021_sync_mfa_profiles.py
@@ -1,8 +1,7 @@
-from django.db import transaction
-
 from kobo.apps.accounts.mfa.models import MfaMethodsWrapper
 from kobo.apps.openrosa.apps.main.models import UserProfile
 
+from kpi.deployment_backends.kc_access.utils import kc_transaction_atomic
 from kpi.utils.log import logging
 
 
@@ -16,7 +15,7 @@ def run():
         ).values_list('user_id', flat=True)
     )
 
-    with transaction.atomic():
+    with kc_transaction_atomic():
         updated_true = UserProfile.objects.filter(
             is_mfa_active=False,
             user_id__in=active_mfa_user_ids
