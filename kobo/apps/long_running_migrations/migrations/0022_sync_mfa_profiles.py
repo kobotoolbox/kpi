241529fa076ea3a81cc2783221d97f0a739a18be
Raj Patel <rajpatel24998@gmail.com>
2026-03-26 20:02:21 +0530

    Update long running migration 0022

--- kobo/apps/long_running_migrations/migrations/0022_sync_mfa_profiles.py
+++ kobo/apps/long_running_migrations/migrations/0022_sync_mfa_profiles.py
@@ -1,11 +1,22 @@
 from django.db import migrations
 
+from kobo.apps.user_reports.utils.migrations import DROP_MV_SQL
+
 
 def add_long_running_migration(apps, schema_editor):
     LongRunningMigration = apps.get_model(
         'long_running_migrations', 'LongRunningMigration'
     )
     LongRunningMigration.objects.create(name='0022_sync_mfa_profiles')
+
+
+def reset_user_reports_mv_migration(apps, schema_editor):
+    LongRunningMigration = apps.get_model(
+        'long_running_migrations', 'LongRunningMigration'
+    )
+    LongRunningMigration.objects.filter(
+        name='0019_recreate_user_reports_mv'
+    ).update(status='created')
 
 
 def noop(*args, **kwargs):
@@ -20,4 +31,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(add_long_running_migration, noop),
+        migrations.RunSQL(sql=DROP_MV_SQL, reverse_sql=migrations.RunSQL.noop),
+        migrations.RunPython(reset_user_reports_mv_migration, noop),
     ]
