c105f117434c1558f18ff2d87cc6987d0126ff46
Phil Edwards <phil@edwards.io>
2026-03-27 21:13:33 -0400

    port model.skipLogicParser -> .civet -> .ts

---
+++ jsapp/xlform/src/model.skipLogicParser.ts
@@ -0,0 +1,34 @@
+// 📘 generated from ./model.skipLogicParser.civet 
+import $factory from './model.validationLogicParserFactory'
+
+/* debug method for regexes
+err_if_unmatch = (re1, re2)->
+  if re1 and re2 and re1.toString() isnt re2.toString()
+    console.error ['inputs did not match', re1.toString(), re2.toString()].join '\n'
+log_equiv = (i1, i2, i3)->
+  console.log([i1.toString()])
+  err_if_unmatch(i1, i2) or err_if_unmatch(i2, i3)
+  i1
+*/
+
+
+const equalityCriterionPattern = /^\${(\w+)}\s*(!=|<=|>=|=|<|>)\s*((?:date\(\'\d{4}-\d{2}-\d{2}\'\))|(?:-?(?:\d+\.\d+|\.\d+|\d+.?))|(?:\'[^']+\'))/
+
+// /\${(\w+)}\s*((?:=|!=)\s*(?:NULL|''))/i
+const existenceCriterionPattern = /\${(\w+)}\s*((?:=|!=)\s*(?:NULL|''))/i
+
+// / and | or /gi
+const criteriaJoinPattern
+  = RegExp(`${' and '}|${' or '}`, 'gi')
+
+// /selected\(\$\{(\w+)\},\s*\'(\w+)\'\)/
+const selectMultiplePattern = /selected\(\$\{(\w+)\},\s*\'(\w+)\'\)/
+
+const skipLogicParser = $factory(
+  equalityCriterionPattern,
+  existenceCriterionPattern,
+  criteriaJoinPattern,
+  selectMultiplePattern,
+)
+
+export default skipLogicParser
