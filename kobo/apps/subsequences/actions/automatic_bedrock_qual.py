0506175d01dbd031a38ec7726e346a576724f2cf
Rebecca Graber <becca.graber@kobotoolbox.org>
2026-03-30 12:34:31 -0400

    fix(qual): remove explicit credentials from call to LLM DEV-1922 (#6886)
    ### 💭 Notes
    Internal only. Removes explicit credentials from the LLM request, boto3
    will get them from the environment.
    Doesn't remove them from `base.py` because we use the presence of a key
    in the environment to determine if we should load a bunch of other
    credentials removing them might have other consequences.
    
    
    ### 👀 Preview steps
    Regression test only. Verify requesting an LLM answer to a question
    still works as long as you are logged in to aws locally.

--- kobo/apps/subsequences/actions/automatic_bedrock_qual.py
+++ kobo/apps/subsequences/actions/automatic_bedrock_qual.py
@@ -143,8 +143,6 @@ class AutomaticBedrockQual(RequiresTranscriptionMixin, BaseQualAction):
         return boto3.client(
             service_name='bedrock-runtime',
             region_name=settings.AWS_BEDROCK_REGION_NAME,
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
             config=Config(
                 read_timeout=settings.AWS_BEDROCK_READ_TIMEOUT,
                 connect_timeout=settings.AWS_BEDROCK_CONNECT_TIMEOUT
