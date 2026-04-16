2c8ed8ba18641bbf60fdfb6093272ea662296518
Anji Tong <ollejna@gmail.com>
2026-04-16 17:09:04 -0400

    remove comment

--- jsapp/js/theme/kobo/Notification.ts
+++ jsapp/js/theme/kobo/Notification.ts
@@ -1,12 +1,6 @@
 import { Notification } from '@mantine/core'
 import classes from './Notification.module.css'
 
-//declare module '@mantine/core' {
-//  export interface MenuItemProps {
-//    variant?: 'danger'
-//  }
-//}
-
 export const NotificationThemeKobo = Notification.extend({
   classNames: classes,
 })
