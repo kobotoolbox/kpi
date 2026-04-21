5b9168b79566c7ddbfdaa4b5ff18ef7d050eafc9
Leszek <smutnyleszek@gmail.com>
2026-04-21 12:34:22 +0200

    menu wip

---
+++ jsapp/js/components/common/Menu.tsx
@@ -0,0 +1,6 @@
+import { Menu as MenuMantine } from '@mantine/core'
+import type { MenuProps as MenuPropsMantine } from '@mantine/core/lib/components'
+
+export type MenuProps = MenuPropsMantine
+
+export default MenuMantine
