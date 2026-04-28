830039518681678b594bdd7059ce2641abffe23e
Anji Tong <ollejna@gmail.com>
2026-04-28 15:22:48 +0000

    feat(autocomplete): add custom Autocomplete and styles DEV-1428 (#6957)
    ### 📣 Summary
    
    Add theming to a new Autocomplete component
    
    ### 💭 Notes
    Adds a custom Autocomplete with our theme applied to it as `InputBase`
    styles unfortunately don't apply. Note: there is probably a lot of
    changes to the custom component props that would make it similar to the
    wrapper component to `Select`, as well as changes made to it to take
    over the old `languagesSelector`. These are left out as this PR focuses
    only on the theming
    
    ### 👀 Preview steps
    1. ℹ️ have a storybook setup working
    2. navigate to design system > Autocomplete
    5. 🟢 [on PR] notice that the autocomplete looks like the mockups
    
    ---------
    
    Co-authored-by: Leszek Pietrzak <leszek@magicznyleszek.xyz>
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

---
+++ jsapp/js/theme/kobo/Autocomplete.ts
@@ -0,0 +1,6 @@
+import { Autocomplete } from '@mantine/core'
+import classes from './Autocomplete.module.css'
+
+export const AutocompleteThemeKobo = Autocomplete.extend({
+  classNames: classes,
+})
