9e65adc705638ff5b9d90e6091e5c743d7cd9c89
Anji Tong <ollejna@gmail.com>
2026-04-29 13:48:23 -0400

    WIP pagination

--- jsapp/js/components/common/languageSelectorNew.tsx
+++ jsapp/js/components/common/languageSelectorNew.tsx
@@ -1,43 +1,62 @@
 import { useMemo, useState } from 'react'
-import { useLanguagesList } from '#/api/react-query/other'
+import { getLanguagesListQueryKey, languagesList, useLanguagesList } from '#/api/react-query/other'
 import Autocomplete from './Autocomplete'
+import {useInfiniteQuery} from '@tanstack/react-query'
 
 const LanguageSelectorNew = () => {
-  const [recentSelections, setRecentSelections] = useState<string[]>([])
+  // TODO: how do we want to implement recently selected?
+  //const [recentSelections, setRecentSelections] = useState<string[]>([])
+  //const groupedData = useMemo(() => {
+  //  const recent = recentSelections.filter((item) => languageOptions.includes(item))
+  //  const others = languageOptions.filter((item) => !recent.includes(item))
+
+  //  const groups = []
+  //  if (recent.length > 0) {
+  //    groups.push({ group: 'Recently selected', items: recent })
+  //  }
+  //  groups.push({ group: 'All options', items: others })
+
+  //  return groups
+  //}, [recentSelections, languageOptions])
+
+  //const handleChange = (newValue: string) => {
+  //  setValue(newValue)
+
+  //  // Add to recent selections (max 5, most recent first)
+  //  if (newValue) {
+  //    setRecentSelections((prev) => {
+  //      const filtered = prev.filter((item) => item !== newValue)
+  //      return [newValue, ...filtered].slice(0, 5)
+  //    })
+  //  }
+  //}
+
   const [value, setValue] = useState('')
 
   // In your component:
   const { data, isLoading } = useLanguagesList()
+  const ITEMS_PER_PAGE = 100
 
   const languageOptions = data?.status === 200 ? data.data.results.map((lang) => `${lang.name} (${lang.code})`) : []
 
-  // Group data: recent first, then all others
-  const groupedData = useMemo(() => {
-    const recent = recentSelections.filter((item) => languageOptions.includes(item))
-    const others = languageOptions.filter((item) => !recent.includes(item))
 
-    const groups = []
-    if (recent.length > 0) {
-      groups.push({ group: 'Recently selected', items: recent })
-    }
-    groups.push({ group: 'All options', items: others })
+  const query = useInfiniteQuery({
+    queryKey: [...getLanguagesListQueryKey(), ITEMS_PER_PAGE, 'infinite'],
+    queryFn: ({ pageParam, signal }) =>
+      languagesList({ limit: ITEMS_PER_PAGE, start: pageParam }, { signal }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.status === 200 && lastPage.data.next) {
+        return allPages.length * ITEMS_PER_PAGE
+      }
+      return undefined
+    },
+  })
 
-    return groups
-  }, [recentSelections, languageOptions])
+  console.log('---------------', query.data)
 
-  const handleChange = (newValue: string) => {
-    setValue(newValue)
-
-    // Add to recent selections (max 5, most recent first)
-    if (newValue) {
-      setRecentSelections((prev) => {
-        const filtered = prev.filter((item) => item !== newValue)
-        return [newValue, ...filtered].slice(0, 5)
-      })
-    }
-  }
-
-  return <Autocomplete data={groupedData} value={value} onChange={handleChange} label='Select language' />
+  //TODO: Scrolling?
+  return <Autocomplete data={languageOptions} value={value} onChange={setValue} label='Select language' />
 }
 
 export default LanguageSelectorNew
