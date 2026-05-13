c4a4870e4933ca94475dafc99e6dcc46cc750c7c
Guillermo <lgar89@gmail.com>
2026-05-12 22:11:29 -0600

    remove references to the removed properties in the jsapp files

--- jsapp/js/dataInterface.ts
+++ jsapp/js/dataInterface.ts
@@ -263,11 +263,9 @@ export interface SubmissionResponse extends SubmissionResponseValueObject {
   _attachments: SubmissionAttachment[]
   // TODO: when does this happen to be array of nulls?
   _geolocation: number[] | null[]
-  _notes: string[]
   _status: string
   _submission_time: string
   _submitted_by: string | null
-  _tags: string[]
   // If submission was validated, this would be a proper response, otherwise it's empty object
   _validation_status: ValidationStatusResponse | {}
   _version_?: string
