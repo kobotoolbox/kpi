1cf52e11bfba6a92ff4c2fb56b3dcab014131a77
Leszek <smutnyleszek@gmail.com>
2026-05-13 12:18:29 +0200

    TextInput case rename pt 2

--- jsapp/js/components/common/DebouncedTextInput.tsx
+++ jsapp/js/components/common/DebouncedTextInput.tsx
@@ -1,7 +1,7 @@
 import { useDebouncedCallback } from '@mantine/hooks'
 import React, { useCallback, useEffect, useRef, useState } from 'react'
-import TextInput from './TextInputxxx'
-import type { TextInputProps } from './TextInputxxx'
+import TextInput from './TextInput'
+import type { TextInputProps } from './TextInput'
 
 export interface DebouncedTextInputProps extends Omit<TextInputProps, 'value' | 'onChange'> {
   value?: string
