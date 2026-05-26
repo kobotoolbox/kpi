acee2557c4db58fd0893897798ebc08f9dd254d9
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2026-05-26 17:43:59 +0200

    feat(dataTable): add processing columns DEV-1408 (#7065)
    ### 📣 Summary
    
    Display transcription/translation columns even if there is no data, but
    bulk processes are in progress. This change is part of incomplete
    feature, thus it is hidden behind a feature flag.
    
    ### 💭 Notes
    
    Build atop #7055
    
    Changes here:
    
    - Make it possible for Storybook to import `.coffee` files
    - Globally enable all feature flags in Storybook context
    - Reorder CSS imports in Storybook to match the main app
    - Fix vertical alignment of "Processing" of `DataTableCell` in the Data
    Table
    - In `ColumnsHideForm` we now pass `bulkActions` to ensure the virtual
    columns will be taken into account
    - Added stories file for `DataTableWrapper`
    - One default story that displays Data Table with two small submissions
    - One story that displays Data Table with a transcript column having one
    submission "in progress" (via bulk action)
    - Removed `getAllColumns` from `tableStore.ts` and made sure all the
    utilities correctly require `bulkActions` to be passed
    - `tableUtils.ts` `getAllDataColumns` function now uses `bulkActions`
    property requiring data from
    `/api/v2/assets/:uid/advanced-features/bulk-actions/` endpoint
    - Added `getVirtualSupplementalFieldsForBulkActions` utility function in
    `assetUtils.ts` that produces a list of "virtual" supplemental columns
    based on active bulk actions
    - Extended `injectSupplementalRowsIntoListOfRows` in `assetUtils.ts` to
    receive optional additional "virtual" fields (we pass
    `getVirtualSupplementalFieldsForBulkActions` output into it, but we keep
    it flexible to allow adding different fields later on)
      - So the flow is:
        1. `DataTableWrapper` uses `react-query` to fetch bulk actions
        2. `bulkActions` are passed on to `DataTable`
    3. `DataTable` is building columns with `tableUtils.getAllDataColumns`
    (`bulkActions` passed)
    4. `tableUtils.getAllDataColumns` is building a list of columns, by the
    end of the function it uses `getVirtualSupplementalFieldsForBulkActions`
    (`bulkActions`) and passes the output to
    `injectSupplementalRowsIntoListOfRows`
    - Added MSW mocking for asset endpoint
      - `assetMock` in `asset.mocks.ts` to be used in stories as a handler
      - `assetFactory` in `asset.factory.ts` to produce mock asset responses
    - Added MSW mocking for asset data endpoint
      - `assetDataMock` in `assetData.mocks.ts` - for stories handler
    - `assetDataFactort` in `assetData.factory.ts` to produce mock asset
    data responses
    - Improved MSW mocking for asset bulk actions
      - Cleanup and match other mocks in how it's written
    - `bulkActionFactory` in `bulkAction.factory.ts` to produce mock asset
    bulk action responses
    - Added MSW mocking for `…/service_usage/` endpoint in
    `organizationServiceUsage.mocks.ts` file
    
    ### 👀 Preview steps
    
    This is not testable in the UI, because API is not ready. But it is
    testable in Storybook thanks to mocks.
    
    1. ℹ️ Have a project with audio question and submissions
    2. Go to Project → Data → Table
    3. 🟢 Notice that the table still works :) 
    4. Run Storybook
    5. Go to Components → DataTableWrapper → Processing Column
    6. See that "Processing" appears in "transcript (fr)…" column for second
    submission, and doesn't appear in first and third submission
    
    ---------
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- jsapp/js/components/submissions/table.tsx
+++ jsapp/js/components/submissions/table.tsx
@@ -45,6 +45,7 @@ import tableStore from '#/components/submissions/tableStore'
 import type { TableStoreData } from '#/components/submissions/tableStore'
 import {
   buildFilterQuery,
+  getAllDataColumns,
   getBackgroundAudioQuestionName,
   getColumnHXLTags,
   getColumnLabel,
@@ -214,6 +215,9 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
     const prevAdditionalFields = prevProps.asset?.analysis_form_json?.additional_fields
     const newAdditionalFields = this.props.asset?.analysis_form_json?.additional_fields
 
+    const prevActiveBulkActions = prevProps.activeBulkActions
+    const newActiveBulkActions = this.props.activeBulkActions
+
     // If sort setting changed, we definitely need to get new submissions (which
     // will rebuild columns)
     if (
@@ -229,6 +233,10 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
     } else if (!isEqual(prevAdditionalFields, newAdditionalFields)) {
       // If additional fields have changed, it means that user has added
       // transcript or translations, thus we need to display more columns.
+      this._prepColumns(this.state.submissions)
+    } else if (!isEqual(prevActiveBulkActions, newActiveBulkActions)) {
+      // If bulk actions have changed, it means they might've just gotten loaded
+      // from API, and we might need to display more columns.
       this._prepColumns(this.state.submissions)
     }
   }
@@ -383,7 +391,7 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
   }
 
   onHideField(fieldId: string) {
-    tableStore.hideField(this.state.submissions, fieldId)
+    tableStore.hideField(this.props.asset, this.state.submissions, this.props.activeBulkActions || [], fieldId)
   }
 
   onFieldFrozenChange(fieldId: string, isFrozen: boolean) {
@@ -633,7 +641,7 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
    * Builds and gathers all necessary react-table data and stores in state.
    */
   _prepColumns(data: SubmissionResponse[]) {
-    const allColumns = tableStore.getAllColumns(data)
+    const allColumns = getAllDataColumns(this.props.asset, data, this.props.activeBulkActions)
 
     let showLabels = this.state.showLabels
     let showGroupName = this.state.showGroupName
@@ -1329,6 +1337,7 @@ export class DataTable extends React.Component<DataTableProps, DataTableState> {
             <ColumnsHideDropdown
               asset={this.props.asset}
               submissions={this.state.submissions}
+              bulkActions={this.props.activeBulkActions || []}
               showGroupName={this.state.showGroupName}
               translationIndex={this.state.translationIndex}
             />
