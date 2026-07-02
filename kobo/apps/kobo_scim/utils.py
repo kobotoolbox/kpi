6099aa2641bbbc99e5b639240d4376741ef78a31
Guillermo <lgar89@gmail.com>
2026-07-02 11:19:12 -0600

    avoid silent overwrite of formatted field

--- kobo/apps/kobo_scim/utils.py
+++ kobo/apps/kobo_scim/utils.py
@@ -72,6 +72,15 @@ def apply_scim_user_metadata(user, scim_data, enforce_strict_validation=False):
     """
     matched_any = False
 
+    extra_details_updated = False
+    profile_updates = {}
+    profile_field_to_metadata_key = {}
+
+    extra_user_detail = None
+    metadata = {}
+    original_metadata = {}
+    profile = None
+
     first_name = get_scim_value(scim_data, 'name.givenName')
     last_name = get_scim_value(scim_data, 'name.familyName')
     formatted = get_scim_value(scim_data, 'name.formatted')
@@ -91,9 +100,9 @@ def apply_scim_user_metadata(user, scim_data, enforce_strict_validation=False):
         if formatted is not None:
             extra_user_detail, _ = ExtraUserDetail.objects.get_or_create(user=user)
             metadata = extra_user_detail.data or {}
+            original_metadata = dict(metadata)
             metadata['name'] = formatted
-            extra_user_detail.data = metadata
-            extra_user_detail.save(update_fields=['data'])
+            extra_details_updated = True
 
     metadata_fields = getattr(config, 'USER_METADATA_FIELDS', None)
 
@@ -110,14 +119,6 @@ def apply_scim_user_metadata(user, scim_data, enforce_strict_validation=False):
         'address',
     }
 
-    extra_details_updated = False
-    profile_updates = {}
-    profile_field_to_metadata_key = {}
-
-    extra_user_detail = None
-    metadata = {}
-    original_metadata = {}
-    profile = None
 
     for field_def in metadata_fields:
         field_name = field_def.get('name')
@@ -129,6 +130,11 @@ def apply_scim_user_metadata(user, scim_data, enforce_strict_validation=False):
         value = get_scim_value(scim_data, scim_mapping)
 
         if value is None:
+            continue
+
+        if field_name == 'name' and formatted is not None:
+            # Skip if name.formatted was natively provided, because standard core 
+            # schemas take precedence over custom extension configurations
             continue
 
         matched_any = True
