1cd950d3d70e71a4cb2650656151f58e021f247d
Anji Tong <ollejna@gmail.com>
2026-07-07 13:29:46 -0400

    remove leftover from testing

--- jsapp/js/components/common/TagsInput.tsx
+++ jsapp/js/components/common/TagsInput.tsx
@@ -5,7 +5,7 @@ import { TagsInput as MantineTagsInput } from '@mantine/core'
 const DEFAULT_PLACEHOLDER = t('Type and confirm with ENTER')
 const TAGS_SEPARATOR = ','
 
-export interface KoboTagsInputProps extends Omit<TagsInputProps, 'onChange' | 'value'> {
+export interface KoboTagsInputProps extends Omit<TagsInputProps, 'onChange'> {
   value: string[]
   onChange: (tags: string[]) => void
 }
