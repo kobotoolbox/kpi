da5c123a1f23a9fb938d9dab68d023e10507d7e8
platreth <hugo.platret@gmail.com>
2026-07-10 12:16:18 +0200

    style: apply darker formatting to long-filename test DEV-1856

--- kobo/apps/openrosa/apps/api/tests/viewsets/test_xform_submission_api.py
+++ kobo/apps/openrosa/apps/api/tests/viewsets/test_xform_submission_api.py
@@ -443,21 +443,43 @@ class TestXFormSubmissionApi(TestAbstractViewSet):
         # corrupting it and raising during decode -> HTTP 500 (DEV-1856).
         s = self.surveys[0]
         media_file = '1335783522563.jpg'
-        media_path = os.path.join(self.main_directory, 'fixtures',
-                                  'transportation', 'instances', s, media_file)
+        media_path = os.path.join(
+            self.main_directory,
+            'fixtures',
+            'transportation',
+            'instances',
+            s,
+            media_file,
+        )
         long_filename = 'a' * 220 + '.xml'
         with open(media_path, 'rb') as mf:
-            mf = InMemoryUploadedFile(mf, 'media_file', media_file, 'image/jpg',
-                                      os.path.getsize(media_path), None)
+            mf = InMemoryUploadedFile(
+                mf,
+                'media_file',
+                media_file,
+                'image/jpg',
+                os.path.getsize(media_path),
+                None,
+            )
 
             submission_path = os.path.join(
-                self.main_directory, 'fixtures',
-                'transportation', 'instances', s, s + '.xml')
+                self.main_directory,
+                'fixtures',
+                'transportation',
+                'instances',
+                s,
+                s + '.xml',
+            )
 
             with open(submission_path, 'rb') as sf:
                 xml_file = InMemoryUploadedFile(
-                    sf, 'xml_submission_file', long_filename, 'text/xml',
-                    os.path.getsize(submission_path), None)
+                    sf,
+                    'xml_submission_file',
+                    long_filename,
+                    'text/xml',
+                    os.path.getsize(submission_path),
+                    None,
+                )
                 data = {'xml_submission_file': xml_file, 'media_file': mf}
                 request = self.factory.post('/submission', data)
                 response = self.view(request)
@@ -469,8 +491,7 @@ class TestXFormSubmissionApi(TestAbstractViewSet):
                 auth = DigestAuth('bob', 'bobbob')
                 request.META.update(auth(request.META, response))
                 response = self.view(request, username=self.user.username)
-                self.assertContains(response, 'Successful submission',
-                                    status_code=201)
+                self.assertContains(response, 'Successful submission', status_code=201)
 
     def test_post_submission_uuid_other_user_username_not_provided(self):
         alice_data = {
