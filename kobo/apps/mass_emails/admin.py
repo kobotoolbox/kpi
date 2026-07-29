f2ce75faf7508711d31959418483a2aa8e05ecf1
rgraber <becca.graber@kobotoolbox.org>
2026-07-29 13:46:07 -0400

    feat(massEmails): get rid of add to send option DEV-900

--- kobo/apps/mass_emails/admin.py
+++ kobo/apps/mass_emails/admin.py
@@ -30,40 +30,6 @@ class MassEmailConfigAdmin(admin.ModelAdmin):
                 return ('live',)
         return ()
 
-    @admin.action(description='Add to daily send queue')
-    def enqueue_mass_emails(self, request, queryset):
-        for config in queryset:
-            if config.type == EmailType.ONE_TIME:
-                if config.live:
-                    self.message_user(
-                        request,
-                        f'Emails for {config.name} have already been scheduled',
-                        level=messages.ERROR,
-                    )
-                else:
-                    config.live = True
-                    config.save()
-                    self.message_user(
-                        request,
-                        f'Emails for {config.name} have been scheduled for tomorrow',
-                        level=messages.SUCCESS,
-                    )
-            else:
-                if config.live:
-                    self.message_user(
-                        request,
-                        f'Emails for {config.name} are already part of the daily send',
-                        level=messages.ERROR,
-                    )
-                else:
-                    config.live = True
-                    config.save()
-                    self.message_user(
-                        request,
-                        f'Emails for {config.name} have been added to the daily send',
-                        level=messages.SUCCESS,
-                    )
-
     # wrap the original method so we can give it a clearer name
     @admin.action(description='Export recipient list')
     def export_recipient_lists(self, request, queryset):
