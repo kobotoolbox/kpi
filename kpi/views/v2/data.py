854b495ccd1b490f97cf7c732321d9ee3d762edb
rgraber <becca.graber@kobotoolbox.org>
2026-07-29 13:00:02 -0400

    fixup!: rm unused variable

--- kpi/views/v2/data.py
+++ kpi/views/v2/data.py
@@ -270,7 +270,6 @@ class DataViewSet(
     permission_classes = (SubmissionPermission,)
     log_type = AuditType.PROJECT_HISTORY
     logged_fields = []
-    log_get_requests = True
 
     @extend_schema(
         methods=['PATCH'],
