157850be89ae7adead7ec40e5e63ed591ca9a8bc
Leszek <smutnyleszek@gmail.com>
2026-08-06 15:34:57 +0200

    improve alert handling in translations tab

--- jsapp/js/components/processing/SingleProcessingContent/TabTranslations/TranslationAdd/StepBegin.tsx
+++ jsapp/js/components/processing/SingleProcessingContent/TabTranslations/TranslationAdd/StepBegin.tsx
@@ -3,19 +3,27 @@ import React from 'react'
 import cx from 'classnames'
 import Button from '#/components/common/button'
 import { userCan } from '#/components/permissions/utils'
+import ConflictingOngoingJobAlert from '#/components/processing/common/ConflictingOngoingJobAlert'
 import type { AssetResponse } from '#/dataInterface'
 import bodyStyles from '../../../common/processingBody.module.scss'
 
 interface Props {
   asset: AssetResponse
+  /** Whether any translation job is running, since no language is picked yet here. */
+  hasConflictingOngoingJob: boolean
   onNext: () => void
 }
 
-export default function Foo({ asset, onNext }: Props) {
+export default function StepBegin({ asset, hasConflictingOngoingJob, onNext }: Props) {
   return (
     <div className={cx(bodyStyles.root, bodyStyles.stepBegin)}>
       <header className={bodyStyles.header}>{t('This transcript does not have any translations yet')}</header>
 
+      {/*
+        We only warn here, without blocking: a job translating to one language
+        doesn't stop the user from adding another one. The language step checks
+        the picked language and blocks that case.
+      */}
       <Button
         type='primary'
         size='l'
@@ -23,6 +31,8 @@ export default function Foo({ asset, onNext }: Props) {
         onClick={onNext}
         isDisabled={!userCan('change_submissions', asset)}
       />
+
+      {hasConflictingOngoingJob && <ConflictingOngoingJobAlert mt='md' />}
     </div>
   )
 }
