198a3254b5c5169abd87e1b13440546bc70f2884
Leszek <smutnyleszek@gmail.com>
2026-08-10 19:29:00 +0200

    fix bulk total copy

--- jsapp/js/components/submissions/BulkProcessingModals/BulkTranscriptionModal/BulkTranscriptionModal.tsx
+++ jsapp/js/components/submissions/BulkProcessingModals/BulkTranscriptionModal/BulkTranscriptionModal.tsx
@@ -269,7 +269,7 @@ export function BulkTranscriptionModal(props: BulkTranscriptionModalProps) {
 
           <Text size='sm'>
             {t(
-              'Your ##total_files## audio files is a total of ##total_length##. This may take longer to complete than the total duration of your files.',
+              'Your ##total_files## audio files are a total of ##total_length##. This may take longer to complete than the total duration of your files.',
             )
               .replace('##total_files##', String(eligibleSubmissions.length))
               .replace(
