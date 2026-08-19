9fb6a9881c95938c25d1f06cffb9ee003230a4bc
Olivier Léger <olivierleger@gmail.com>
2026-08-19 17:05:49 -0400

    fix(massEmails): migration conflicts

--- kobo/apps/project_ownership/models/invite.py
+++ kobo/apps/project_ownership/models/invite.py
@@ -11,6 +11,7 @@ from kpi.fields import KpiUidField
 from kpi.models.abstract_models import AbstractTimeStampedModel
 from kpi.utils.log import logging
 from kpi.utils.mailer import EmailMessage, Mailer
+
 from .choices import InviteStatusChoices
 
 
