c12201c1c19ce7e897708df6af1f7e1cf46afae5
Guillermo <lgar89@gmail.com>
2026-08-27 10:40:09 -0600

    remove redundant try/except and atomic transaction

--- kobo/apps/accounts/signals.py
+++ kobo/apps/accounts/signals.py
@@ -63,21 +63,17 @@ def update_managed_sso_email_domains():
     Syncs the list of email domains across all managed SocialApps to the
     REGISTRATION_SSO_MANAGED_EMAIL_DOMAINS Constance config setting.
     """
-    try:
-        with transaction.atomic():
-            domains = (
-                SocialAppManagedDomain.objects.filter(social_app__managed=True)
-                .values_list('domain', flat=True)
-                .order_by('domain')
-            )
-            domain_string = '\n'.join(domains)
-            setattr(
-                constance.config,
-                'REGISTRATION_SSO_MANAGED_EMAIL_DOMAINS',
-                domain_string,
-            )
-    except Exception:
-        pass
+    domains = (
+        SocialAppManagedDomain.objects.filter(social_app__managed=True)
+        .values_list('domain', flat=True)
+        .order_by('domain')
+    )
+    domain_string = '\n'.join(domains)
+    setattr(
+        constance.config,
+        'REGISTRATION_SSO_MANAGED_EMAIL_DOMAINS',
+        domain_string,
+    )
 
 
 @receiver(post_save, sender=SocialAppManagedDomain)
