368b36399514d963c11ff444d104b9959d9a0254
Rebecca Graber <becca.graber@kobotoolbox.org>
2026-09-02 09:33:48 -0400

    feat(sso): actions on adding managed domain DEV-2758 (#7517)
    ### 📣 Summary
    Update users who have managed SSO accounts to limit their login
    capabilities to that SSO, and notify users with the corresponding domain
    who do not yet have SSO set up that they need to do so.
    
    
    ### 💭 Notes
    Right now, only the sweep task runs. There is a separate task for
    kicking off the celery task immediately on changing a
    `SocialAppCustomData` object.
    
    The task begins by finding users with the given email domain who are not
    sso-exempt and meet *any* of the following criteria:
    1. Have a usable password
    2. Have social accounts other than the managed SSO
    3. Do not have an account with the managed SSO and have not yet been
    notified of the change
    
    Users who already have a managed SSO account have all other social
    accounts deleted and their passwords unset, so they must login via SSO.
    Users who do not are notified via InAppMessage that they will need to
    link their account.
    
    The notification is set to expire after a year as an arbitrary deadline.
    In reality we will be removing it once users link their SSO.
    
    Notably not in scope:
    
    * Dismissing the notification once a user links their account and
    removing their other login methods
    * Determining the correct `requesting_user` for any InAppMessages
    created by the sweep task rather than the ones that will be run on
    activation. This information is only shown in Django admin and arguably
    shouldn't really be set anyway if the messages are created by an
    automatic process. It is not necessary to resolve this for the initial
    release
    
    ### 👀 Preview steps
    Make sure you have SSO enabled
    
    1. ℹ️ have an admin account
    2. In Django admin, update your SSO (via the SocialAppCustomData object)
    to `managed=False` and add the domain `kobotoolbox.org` (or whatever
    domain your SSO accepts)
    3. Create a new user (user1) with a username and password and email
    ending with `@kobotoolbox.org`
    4. Add an SSO login for the new user
    5. Create another new user (user2) with a username and password and the
    correct email domain. Do not add an SSO login.
    6. In Django admin, set the SSO to `managed=True`
    7. In a Django shell, import and run `managed_sso_sweep`
    8. 🟢 [on PR] In the django shell, check that user1.has_usable_password()
    is False
    9. Log in as user2
    10. 🟢 [on PR] Notice there is a notification prompting you to add an SSO
    account
    
    ---------
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    Co-authored-by: platreth <hugo.platret@gmail.com>

--- kobo/apps/accounts/utils.py
+++ kobo/apps/accounts/utils.py
@@ -1,9 +1,22 @@
-from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.models import SocialAccount, SocialApp
 from django.conf import settings
+from django.contrib.auth.hashers import UNUSABLE_PASSWORD_PREFIX
+from django.db.models import CharField, Count, F, Func, Q, Value
+from django.db.models.functions import Lower
 
 from kobo.apps.accounts.models import SocialAppManagedDomain
+from kobo.apps.help.models import InAppMessageUsers, MessageType
 from kobo.apps.kobo_auth.shortcuts import User
 from kobo.apps.stripe.constants import ACTIVE_STRIPE_STATUSES
+
+# Key used in InAppMessage.generic_related_objects, same convention as
+# the transfer identifier in kobo.apps.help.serializers
+SOCIAL_APP_IDENTIFIER = f'{SocialApp._meta.app_label}.{SocialApp._meta.model_name}'
+
+
+class SplitPart(Func):
+    function = 'SPLIT_PART'
+    output_field = CharField()
 
 
 def user_has_inactive_paid_subscription(username):
@@ -56,3 +69,37 @@ def user_is_managed_by_sso(user):
         ).exists()
         return user_has_social_account
     return False
+
+
+def users_needing_update(social_app: 'socialaccount.SocialApp', domain: str):
+    users_already_received_message = InAppMessageUsers.objects.filter(
+        in_app_message__message_type=MessageType.MANAGED_SSO_REMINDER,
+        in_app_message__generic_related_objects__contains={
+            SOCIAL_APP_IDENTIFIER: social_app.pk,
+        },
+    ).values_list('user_id', flat=True)
+    users = (
+        User.objects.exclude(extra_details__sso_exempt=True)
+        .exclude(id__in=users_already_received_message)
+        .annotate(
+            domain=SplitPart(Lower(F('email')), Value('@'), Value(2)),
+            managed_account=Count(
+                'socialaccount',
+                filter=Q(socialaccount__provider=social_app.provider_id),
+            ),
+            # `.exclude(socialaccount__provider=...)` would drop every user who
+            # has *any* account on the managed provider; a filtered Count is
+            # the only way to count the other accounts.
+            other_accounts=Count(
+                'socialaccount',
+                filter=~Q(socialaccount__provider=social_app.provider_id),
+            ),
+        )
+        .filter(domain=domain)
+        .exclude(
+            password__startswith=UNUSABLE_PASSWORD_PREFIX,
+            other_accounts=0,
+            managed_account__gte=1,
+        )
+    )
+    return users
