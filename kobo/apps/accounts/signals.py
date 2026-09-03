3276479486eb5da81b382cc57604dfa0674c6fe0
rgraber <becca.graber@kobotoolbox.org>
2026-09-03 17:08:52 -0400

    fixup!: fix

--- kobo/apps/accounts/signals.py
+++ kobo/apps/accounts/signals.py
@@ -108,5 +108,5 @@ def enforce_managed_sso(sender=None, **kwargs):
             inappmessageusers__isnull=True,
         ).update(valid_until=now)
         user.set_unusable_password()
-        user.save()
+        user.save(update_fields=['password'])
         update_session_auth_hash(request, user)
