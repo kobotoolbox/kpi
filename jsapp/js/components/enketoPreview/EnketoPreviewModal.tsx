cd8783769488de7071131434e95fd42a3a050b0a
Anji <ollejna@gmail.com>
2026-09-21 14:45:56 -0400

    Add hooks and get url

--- jsapp/js/components/enketoPreview/EnketoPreviewModal.tsx
+++ jsapp/js/components/enketoPreview/EnketoPreviewModal.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mantine/core'
 import { useEffect } from 'react'
 import { useAssetSnapshotsCreate } from '#/api/react-query/form-content'
+import LoadingSpinner from '../common/loadingSpinner'
 
 export interface EnketoPreviewModalProps {
   assetUrl: string
@@ -12,7 +13,21 @@ export function EnketoPreviewModal(props: EnketoPreviewModalProps) {
 
   useEffect(() => {
     snapshotMutation.mutate({ data: { asset: props.assetUrl, details: {} } })
-  })
+  }, [props.assetUrl])
 
-  return <Box>Stuff {props.assetUrl}</Box>
+  const previewUrl = snapshotMutation.data?.status === 201 ? snapshotMutation.data.data.enketopreviewlink : undefined
+
+  if (snapshotMutation.isPending) {
+    return <LoadingSpinner />
+  }
+
+  if (snapshotMutation.isError || !previewUrl) {
+    return t('Unable to load form preview')
+  }
+
+  return (
+    <Box>
+      <iframe src={previewUrl} title={t('Form Preview')} />
+    </Box>
+  )
 }
